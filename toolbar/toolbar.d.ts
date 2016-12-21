4b0330a8fa4a9996d3904f3419bda5d2404aed87
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-12-21 18:19:50 +0000

    b75f018 chore(toolbar): add doc for color property (#2318)

--- toolbar/toolbar.d.ts
+++ toolbar/toolbar.d.ts
@@ -6,6 +6,7 @@ export declare class MdToolbar {
     private renderer;
     private _color;
     constructor(elementRef: ElementRef, renderer: Renderer);
+    /** The color of the toolbar. Can be primary, accent, or warn. */
     color: string;
     private _updateColor(newColor);
     private _setElementColor(color, isAdd);
