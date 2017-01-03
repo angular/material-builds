ae8aa84f560f95708b21d11da9203433e65144c4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-01-03 17:34:05 +0000

    7d74287 chore: remove temporary sleep in menu e2e test (#2480)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -1,6 +1,6 @@
 /**
   * @license Angular Material v2.0.0-beta.1
-  * Copyright (c) 2016 Google, Inc. https://material.angular.io/
+  * Copyright (c) 2017 Google, Inc. https://material.angular.io/
   * License: MIT
   */
 (function (global, factory) {
