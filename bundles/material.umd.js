f4514cb899e4296688483fb059d5e3e195710545
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-03-07 00:59:34 +0000

    819fa0b fix(select): initial value not being displayed with FormControl and OnPush (#3434)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -6722,7 +6722,6 @@ var MdSelect = (function () {
     MdSelect.prototype.writeValue = function (value) {
         if (this.options) {
             this._setSelectionByValue(value);
-            this._changeDetectorRef.markForCheck();
         }
     };
     /**
@@ -6832,15 +6831,9 @@ var MdSelect = (function () {
      * found with the designated value, the select trigger is cleared.
      */
     MdSelect.prototype._setSelectionByValue = function (value) {
-        var options = this.options.toArray();
-        for (var i = 0; i < this.options.length; i++) {
-            if (options[i].value === value) {
-                options[i].select();
-                return;
-            }
-        }
-        // Clear selection if no item was selected.
-        this._clearSelection();
+        var correspondingOption = this.options.find(function (option) { return option.value === value; });
+        correspondingOption ? correspondingOption.select() : this._clearSelection();
+        this._changeDetectorRef.markForCheck();
     };
     /** Clears the select trigger and deselects every option in the list. */
     MdSelect.prototype._clearSelection = function () {
