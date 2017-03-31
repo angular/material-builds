06ce5b413dffed1eacd5f3574fc7b9ec7ba36bde
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-03-31 23:25:14 +0000

    f40296e fix: aot runtime issues (#3807)

--- typings/core/core.d.ts
+++ typings/core/core.d.ts
@@ -6,8 +6,6 @@ export { Portal, PortalHost, BasePortalHost, ComponentPortal, TemplatePortal } f
 export { PortalHostDirective, TemplatePortalDirective, PortalModule } from './portal/portal-directives';
 export { DomPortalHost } from './portal/dom-portal-host';
 export * from './platform/index';
-/** @deprecated */
-export { Platform as MdPlatform } from './platform/platform';
 export { Overlay, OVERLAY_PROVIDERS } from './overlay/overlay';
 export { OverlayContainer } from './overlay/overlay-container';
 export { FullscreenOverlayContainer } from './overlay/fullscreen-overlay-container';
@@ -23,15 +21,11 @@ export { HammerInput, HammerManager } from './gestures/gesture-annotations';
 export * from './ripple/index';
 export { AriaLivePoliteness, LiveAnnouncer, LIVE_ANNOUNCER_ELEMENT_TOKEN, LIVE_ANNOUNCER_PROVIDER } from './a11y/live-announcer';
 export * from './selection/selection';
-/** @deprecated */
-export { LiveAnnouncer as MdLiveAnnouncer } from './a11y/live-announcer';
 export * from './a11y/focus-trap';
 export { InteractivityChecker } from './a11y/interactivity-checker';
 export { isFakeMousedownFromScreenReader } from './a11y/fake-mousedown';
 export { A11yModule } from './a11y/index';
 export { UniqueSelectionDispatcher, UniqueSelectionDispatcherListener, UNIQUE_SELECTION_DISPATCHER_PROVIDER } from './coordination/unique-selection-dispatcher';
-/** @deprecated */
-export { UniqueSelectionDispatcher as MdUniqueSelectionDispatcher } from './coordination/unique-selection-dispatcher';
 export { MdLineModule, MdLine, MdLineSetter } from './line/line';
 export * from './style/index';
 export { MdError } from './errors/error';
