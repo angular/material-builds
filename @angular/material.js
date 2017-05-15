8d535b34d6f0ea8a857c07118564298594a369b7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-05-15 21:34:39 +0000

    84ea8b2 fix(datepicker): calendar toggle submitting parent form (#4531)

--- @angular/material.js
+++ @angular/material.js
@@ -20354,6 +20354,10 @@ class MdDatepickerToggle {
      */
     constructor(_intl) {
         this._intl = _intl;
+        /**
+         * Type of the button.
+         */
+        this.type = 'button';
     }
     /**
      * @return {?}
@@ -20380,6 +20384,7 @@ MdDatepickerToggle.decorators = [
                 template: '',
                 styles: [".mat-datepicker-toggle{display:inline-block;background:url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTE5IDNoLTFWMWgtMnYySDhWMUg2djJINWMtMS4xMSAwLTEuOTkuOS0xLjk5IDJMMyAxOWMwIDEuMS44OSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMlY1YzAtMS4xLS45LTItMi0yem0wIDE2SDVWOGgxNHYxMXpNNyAxMGg1djVIN3oiLz48L3N2Zz4=) no-repeat;background-size:contain;height:24px;width:24px;border:none;outline:0;vertical-align:middle;cursor:pointer} /*# sourceMappingURL=datepicker-toggle.css.map */ "],
                 host: {
+                    '[attr.type]': 'type',
                     '[class.mat-datepicker-toggle]': 'true',
                     '[attr.aria-label]': '_intl.openCalendarLabel',
                     '(click)': '_open($event)',
@@ -20396,6 +20401,7 @@ MdDatepickerToggle.ctorParameters = () => [
 ];
 MdDatepickerToggle.propDecorators = {
     'datepicker': [{ type: Input, args: ['mdDatepickerToggle',] },],
+    'type': [{ type: Input },],
     '_datepicker': [{ type: Input, args: ['matDatepickerToggle',] },],
 };
 
--- typings/datepicker/datepicker-toggle.d.ts
+++ typings/datepicker/datepicker-toggle.d.ts
@@ -2,7 +2,10 @@ import { MdDatepicker } from './datepicker';
 import { MdDatepickerIntl } from './datepicker-intl';
 export declare class MdDatepickerToggle<D> {
     _intl: MdDatepickerIntl;
+    /** Datepicker instance that the button will toggle. */
     datepicker: MdDatepicker<D>;
+    /** Type of the button. */
+    type: string;
     _datepicker: MdDatepicker<D>;
     constructor(_intl: MdDatepickerIntl);
     _open(event: Event): void;
