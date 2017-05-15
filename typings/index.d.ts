141110d14d04f382f2dfd8a9f41ff980cde8a1aa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-05-15 21:32:19 +0000

    58c252f fix(datepicker): export MD_DATE_FORMATS and MD_NATIVE_DATE_FORMATS (#4541)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,10 +3,9 @@
  */
 export * from './public_api';
 export { LIVE_ANNOUNCER_PROVIDER_FACTORY as ɵg } from './core/a11y/live-announcer';
-export { mixinDisabled as ɵu } from './core/common-behaviors/disabled';
+export { mixinDisabled as ɵs } from './core/common-behaviors/disabled';
 export { UNIQUE_SELECTION_DISPATCHER_PROVIDER_FACTORY as ɵh } from './core/coordination/unique-selection-dispatcher';
-export { MD_DATE_FORMATS as ɵs, MdDateFormats as ɵv } from './core/datetime/date-formats';
-export { MD_NATIVE_DATE_FORMATS as ɵt } from './core/datetime/native-date-formats';
+export { MdDateFormats as ɵt } from './core/datetime/date-formats';
 export { MdMutationObserverFactory as ɵa } from './core/observe-content/observe-content';
 export { OVERLAY_CONTAINER_PROVIDER as ɵc, OVERLAY_CONTAINER_PROVIDER_FACTORY as ɵb } from './core/overlay/overlay-container';
 export { OverlayPositionBuilder as ɵr } from './core/overlay/position/overlay-position-builder';
