2a5f81db36b6c5badbb76ad877a7dc087cd42521
Jeremy Elbourn <jelbourn@gmail.com>
2017-06-07 22:15:35 +0000

    4fcab57 docs(card): add guidance for a11y (#4781)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -12802,9 +12802,7 @@ var MdCardContent = /*@__PURE__*/(function () {
 MdCardContent.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-content, mat-card-content',
-                host: {
-                    '[class.mat-card-content]': 'true'
-                }
+                host: { 'class': 'mat-card-content' }
             },] },
 ];
 /**
@@ -12823,9 +12821,7 @@ var MdCardTitle = /*@__PURE__*/(function () {
 MdCardTitle.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-title, mat-card-title, [md-card-title], [mat-card-title]',
-                host: {
-                    '[class.mat-card-title]': 'true'
-                }
+                host: { 'class': 'mat-card-title' }
             },] },
 ];
 /**
@@ -12844,9 +12840,7 @@ var MdCardSubtitle = /*@__PURE__*/(function () {
 MdCardSubtitle.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-subtitle, mat-card-subtitle, [md-card-subtitle], [mat-card-subtitle]',
-                host: {
-                    '[class.mat-card-subtitle]': 'true'
-                }
+                host: { 'class': 'mat-card-subtitle' }
             },] },
 ];
 /**
@@ -12865,9 +12859,7 @@ var MdCardActions = /*@__PURE__*/(function () {
 MdCardActions.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-actions, mat-card-actions',
-                host: {
-                    '[class.mat-card-actions]': 'true'
-                }
+                host: { 'class': 'mat-card-actions' }
             },] },
 ];
 /**
@@ -12886,9 +12878,7 @@ var MdCardFooter = /*@__PURE__*/(function () {
 MdCardFooter.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-footer, mat-card-footer',
-                host: {
-                    '[class.mat-card-footer]': 'true'
-                }
+                host: { 'class': 'mat-card-footer' }
             },] },
 ];
 /**
@@ -12907,9 +12897,7 @@ var MdCardSmImage = /*@__PURE__*/(function () {
 MdCardSmImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-sm-image], [mat-card-sm-image]',
-                host: {
-                    '[class.mat-card-sm-image]': 'true'
-                }
+                host: { 'class': 'mat-card-sm-image' }
             },] },
 ];
 /**
@@ -12928,9 +12916,7 @@ var MdCardMdImage = /*@__PURE__*/(function () {
 MdCardMdImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-md-image], [mat-card-md-image]',
-                host: {
-                    '[class.mat-card-md-image]': 'true'
-                }
+                host: { 'class': 'mat-card-md-image' }
             },] },
 ];
 /**
@@ -12949,9 +12935,7 @@ var MdCardLgImage = /*@__PURE__*/(function () {
 MdCardLgImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-lg-image], [mat-card-lg-image]',
-                host: {
-                    'class.mat-card-lg-image': 'true'
-                }
+                host: { 'class': 'mat-card-lg-image' }
             },] },
 ];
 /**
@@ -12970,9 +12954,7 @@ var MdCardImage = /*@__PURE__*/(function () {
 MdCardImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-image], [mat-card-image]',
-                host: {
-                    '[class.mat-card-image]': 'true'
-                }
+                host: { 'class': 'mat-card-image' }
             },] },
 ];
 /**
@@ -12991,9 +12973,7 @@ var MdCardXlImage = /*@__PURE__*/(function () {
 MdCardXlImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-xl-image], [mat-card-xl-image]',
-                host: {
-                    '[class.mat-card-xl-image]': 'true'
-                }
+                host: { 'class': 'mat-card-xl-image' }
             },] },
 ];
 /**
@@ -13012,9 +12992,7 @@ var MdCardAvatar = /*@__PURE__*/(function () {
 MdCardAvatar.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-avatar], [mat-card-avatar]',
-                host: {
-                    '[class.mat-card-avatar]': 'true'
-                }
+                host: { 'class': 'mat-card-avatar' }
             },] },
 ];
 /**
@@ -13043,9 +13021,7 @@ MdCard.decorators = [
                 styles: [".mat-card{transition:box-shadow 280ms cubic-bezier(.4,0,.2,1);will-change:box-shadow;display:block;position:relative;padding:24px;border-radius:2px}.mat-card:not([class*=mat-elevation-z]){box-shadow:0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12)}@media screen and (-ms-high-contrast:active){.mat-card{outline:solid 1px}}.mat-card-flat{box-shadow:none}.mat-card-actions,.mat-card-content,.mat-card-subtitle,.mat-card-title{display:block;margin-bottom:16px}.mat-card-actions{margin-left:-16px;margin-right:-16px;padding:8px 0}.mat-card-actions[align=end]{display:flex;justify-content:flex-end}.mat-card-image{width:calc(100% + 48px);margin:0 -24px 16px -24px}.mat-card-xl-image{width:240px;height:240px;margin:-8px}.mat-card-footer{position:absolute;width:100%;min-height:5px;bottom:0;left:0}.mat-card-actions .mat-button,.mat-card-actions .mat-raised-button{margin:0 4px}.mat-card-header{display:flex;flex-direction:row}.mat-card-header-text{margin:0 8px}.mat-card-avatar{height:40px;width:40px;border-radius:50%;flex-shrink:0}.mat-card-lg-image,.mat-card-md-image,.mat-card-sm-image{margin:-8px 0}.mat-card-title-group{display:flex;justify-content:space-between;margin:0 -8px}.mat-card-sm-image{width:80px;height:80px}.mat-card-md-image{width:112px;height:112px}.mat-card-lg-image{width:152px;height:152px}@media (max-width:600px){.mat-card{padding:24px 16px}.mat-card-actions{margin-left:-8px;margin-right:-8px}.mat-card-image{width:calc(100% + 32px);margin:16px -16px}.mat-card-title-group{margin:0}.mat-card-xl-image{margin-left:0;margin-right:0}.mat-card-header{margin:-8px 0 0 0}}.mat-card-content>:first-child,.mat-card>:first-child{margin-top:0}.mat-card-content>:last-child,.mat-card>:last-child{margin-bottom:0}.mat-card-image:first-child{margin-top:-24px}.mat-card>.mat-card-actions:last-child{margin-bottom:-16px;padding-bottom:0}.mat-card-actions .mat-button:first-child,.mat-card-actions .mat-raised-button:first-child{margin-left:0;margin-right:0}.mat-card-subtitle:not(:first-child),.mat-card-title:not(:first-child){margin-top:-4px}.mat-card-header .mat-card-subtitle:not(:first-child){margin-top:-8px}.mat-card>.mat-card-xl-image:first-child{margin-top:-8px}.mat-card>.mat-card-xl-image:last-child{margin-bottom:-8px}"],
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card]': 'true'
-                }
+                host: { 'class': 'mat-card' }
             },] },
 ];
 /**
@@ -13067,9 +13043,7 @@ MdCardHeader.decorators = [
                 template: "<ng-content select=\"[md-card-avatar], [mat-card-avatar]\"></ng-content><div class=\"mat-card-header-text\"><ng-content select=\"md-card-title, mat-card-title, md-card-subtitle, mat-card-subtitle, [md-card-title], [mat-card-title], [md-card-subtitle], [mat-card-subtitle]\"></ng-content></div><ng-content></ng-content>",
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card-header]': 'true'
-                }
+                host: { 'class': 'mat-card-header' }
             },] },
 ];
 /**
@@ -13091,9 +13065,7 @@ MdCardTitleGroup.decorators = [
                 template: "<div><ng-content select=\"md-card-title, mat-card-title, md-card-subtitle, mat-card-subtitle, [md-card-title], [mat-card-title], [md-card-subtitle], [mat-card-subtitle]\"></ng-content></div><ng-content select=\"img\"></ng-content><ng-content></ng-content>",
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card-title-group]': 'true'
-                }
+                host: { 'class': 'mat-card-title-group' }
             },] },
 ];
 /**
--- @angular/material.js
+++ @angular/material.js
@@ -12084,9 +12084,7 @@ class MdCardContent {
 MdCardContent.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-content, mat-card-content',
-                host: {
-                    '[class.mat-card-content]': 'true'
-                }
+                host: { 'class': 'mat-card-content' }
             },] },
 ];
 /**
@@ -12102,9 +12100,7 @@ class MdCardTitle {
 MdCardTitle.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-title, mat-card-title, [md-card-title], [mat-card-title]',
-                host: {
-                    '[class.mat-card-title]': 'true'
-                }
+                host: { 'class': 'mat-card-title' }
             },] },
 ];
 /**
@@ -12120,9 +12116,7 @@ class MdCardSubtitle {
 MdCardSubtitle.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-subtitle, mat-card-subtitle, [md-card-subtitle], [mat-card-subtitle]',
-                host: {
-                    '[class.mat-card-subtitle]': 'true'
-                }
+                host: { 'class': 'mat-card-subtitle' }
             },] },
 ];
 /**
@@ -12138,9 +12132,7 @@ class MdCardActions {
 MdCardActions.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-actions, mat-card-actions',
-                host: {
-                    '[class.mat-card-actions]': 'true'
-                }
+                host: { 'class': 'mat-card-actions' }
             },] },
 ];
 /**
@@ -12156,9 +12148,7 @@ class MdCardFooter {
 MdCardFooter.decorators = [
     { type: Directive, args: [{
                 selector: 'md-card-footer, mat-card-footer',
-                host: {
-                    '[class.mat-card-footer]': 'true'
-                }
+                host: { 'class': 'mat-card-footer' }
             },] },
 ];
 /**
@@ -12174,9 +12164,7 @@ class MdCardSmImage {
 MdCardSmImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-sm-image], [mat-card-sm-image]',
-                host: {
-                    '[class.mat-card-sm-image]': 'true'
-                }
+                host: { 'class': 'mat-card-sm-image' }
             },] },
 ];
 /**
@@ -12192,9 +12180,7 @@ class MdCardMdImage {
 MdCardMdImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-md-image], [mat-card-md-image]',
-                host: {
-                    '[class.mat-card-md-image]': 'true'
-                }
+                host: { 'class': 'mat-card-md-image' }
             },] },
 ];
 /**
@@ -12210,9 +12196,7 @@ class MdCardLgImage {
 MdCardLgImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-lg-image], [mat-card-lg-image]',
-                host: {
-                    'class.mat-card-lg-image': 'true'
-                }
+                host: { 'class': 'mat-card-lg-image' }
             },] },
 ];
 /**
@@ -12228,9 +12212,7 @@ class MdCardImage {
 MdCardImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-image], [mat-card-image]',
-                host: {
-                    '[class.mat-card-image]': 'true'
-                }
+                host: { 'class': 'mat-card-image' }
             },] },
 ];
 /**
@@ -12246,9 +12228,7 @@ class MdCardXlImage {
 MdCardXlImage.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-xl-image], [mat-card-xl-image]',
-                host: {
-                    '[class.mat-card-xl-image]': 'true'
-                }
+                host: { 'class': 'mat-card-xl-image' }
             },] },
 ];
 /**
@@ -12264,9 +12244,7 @@ class MdCardAvatar {
 MdCardAvatar.decorators = [
     { type: Directive, args: [{
                 selector: '[md-card-avatar], [mat-card-avatar]',
-                host: {
-                    '[class.mat-card-avatar]': 'true'
-                }
+                host: { 'class': 'mat-card-avatar' }
             },] },
 ];
 /**
@@ -12292,9 +12270,7 @@ MdCard.decorators = [
                 styles: [".mat-card{transition:box-shadow 280ms cubic-bezier(.4,0,.2,1);will-change:box-shadow;display:block;position:relative;padding:24px;border-radius:2px}.mat-card:not([class*=mat-elevation-z]){box-shadow:0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12)}@media screen and (-ms-high-contrast:active){.mat-card{outline:solid 1px}}.mat-card-flat{box-shadow:none}.mat-card-actions,.mat-card-content,.mat-card-subtitle,.mat-card-title{display:block;margin-bottom:16px}.mat-card-actions{margin-left:-16px;margin-right:-16px;padding:8px 0}.mat-card-actions[align=end]{display:flex;justify-content:flex-end}.mat-card-image{width:calc(100% + 48px);margin:0 -24px 16px -24px}.mat-card-xl-image{width:240px;height:240px;margin:-8px}.mat-card-footer{position:absolute;width:100%;min-height:5px;bottom:0;left:0}.mat-card-actions .mat-button,.mat-card-actions .mat-raised-button{margin:0 4px}.mat-card-header{display:flex;flex-direction:row}.mat-card-header-text{margin:0 8px}.mat-card-avatar{height:40px;width:40px;border-radius:50%;flex-shrink:0}.mat-card-lg-image,.mat-card-md-image,.mat-card-sm-image{margin:-8px 0}.mat-card-title-group{display:flex;justify-content:space-between;margin:0 -8px}.mat-card-sm-image{width:80px;height:80px}.mat-card-md-image{width:112px;height:112px}.mat-card-lg-image{width:152px;height:152px}@media (max-width:600px){.mat-card{padding:24px 16px}.mat-card-actions{margin-left:-8px;margin-right:-8px}.mat-card-image{width:calc(100% + 32px);margin:16px -16px}.mat-card-title-group{margin:0}.mat-card-xl-image{margin-left:0;margin-right:0}.mat-card-header{margin:-8px 0 0 0}}.mat-card-content>:first-child,.mat-card>:first-child{margin-top:0}.mat-card-content>:last-child,.mat-card>:last-child{margin-bottom:0}.mat-card-image:first-child{margin-top:-24px}.mat-card>.mat-card-actions:last-child{margin-bottom:-16px;padding-bottom:0}.mat-card-actions .mat-button:first-child,.mat-card-actions .mat-raised-button:first-child{margin-left:0;margin-right:0}.mat-card-subtitle:not(:first-child),.mat-card-title:not(:first-child){margin-top:-4px}.mat-card-header .mat-card-subtitle:not(:first-child){margin-top:-8px}.mat-card>.mat-card-xl-image:first-child{margin-top:-8px}.mat-card>.mat-card-xl-image:last-child{margin-bottom:-8px}"],
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card]': 'true'
-                }
+                host: { 'class': 'mat-card' }
             },] },
 ];
 /**
@@ -12313,9 +12289,7 @@ MdCardHeader.decorators = [
                 template: "<ng-content select=\"[md-card-avatar], [mat-card-avatar]\"></ng-content><div class=\"mat-card-header-text\"><ng-content select=\"md-card-title, mat-card-title, md-card-subtitle, mat-card-subtitle, [md-card-title], [mat-card-title], [md-card-subtitle], [mat-card-subtitle]\"></ng-content></div><ng-content></ng-content>",
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card-header]': 'true'
-                }
+                host: { 'class': 'mat-card-header' }
             },] },
 ];
 /**
@@ -12334,9 +12308,7 @@ MdCardTitleGroup.decorators = [
                 template: "<div><ng-content select=\"md-card-title, mat-card-title, md-card-subtitle, mat-card-subtitle, [md-card-title], [mat-card-title], [md-card-subtitle], [mat-card-subtitle]\"></ng-content></div><ng-content select=\"img\"></ng-content><ng-content></ng-content>",
                 encapsulation: ViewEncapsulation.None,
                 changeDetection: ChangeDetectionStrategy.OnPush,
-                host: {
-                    '[class.mat-card-title-group]': 'true'
-                }
+                host: { 'class': 'mat-card-title-group' }
             },] },
 ];
 /**
