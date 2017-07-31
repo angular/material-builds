ca790c493fd22b947f00c4ec509994e94ab3f36b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-07-31 19:28:25 +0000

    f78726d chore: add expansion panel to prerender check (#6166)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-edcbb24');
+const VERSION = new Version('2.0.0-beta.8-f78726d');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
