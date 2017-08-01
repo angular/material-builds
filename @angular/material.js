b568fc762aecb782e8aaaaeaee15214c78293809
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-01 20:31:01 +0000

    3c19234 chore: fix invalid paths for codeowner folders (#6191)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-51eca3b');
+const VERSION = new Version('2.0.0-beta.8-3c19234');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
