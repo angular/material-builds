abe1b608c50dd2e2ca78624879aef4cddb4a7630
tinayuangao <tinagao@google.com>
2017-08-01 22:01:43 +0000

    a503e19 Add more demos in button accessibility demo page (#6099)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-7a7ec7a');
+const VERSION = new Version('2.0.0-beta.8-a503e19');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-7a7ec7a');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-a503e19');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
