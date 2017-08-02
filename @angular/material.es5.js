94caf1d69af07f4dd15eef188463e437c5299636
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-02 21:52:46 +0000

    4168fb3 build: stop hardcoding rollup globals for CDK entry points (#6150)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.8-fa3cf12');
+var VERSION = new Version('2.0.0-beta.8-4168fb3');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
