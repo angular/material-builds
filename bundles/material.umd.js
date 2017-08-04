12a8bee415798ec8778bfb8c9a11473030058f29
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-04 21:21:10 +0000

    ea6e829 build: retry attemps for tunnel connections (#6246)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-1af2ce6');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-ea6e829');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
