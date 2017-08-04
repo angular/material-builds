8678d0fdea4591a0925f7d1132a93c4c6b964edc
Cyrille Tuzi <web@cyrilletuzi.com>
2017-08-04 21:14:50 +0000

    1af2ce6 docs(getting-started): mention importing hammer in same file as bootstrap (#6225)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-5967f6e');
+const VERSION = new Version('2.0.0-beta.8-1af2ce6');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
