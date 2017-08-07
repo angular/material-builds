326017471bf9d8a4ebdb5749e5b89be8629c66f9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-07 17:40:43 +0000

    509e3e9 build: fix error when running devapp on windows (#6288)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-5ecc509');
+const VERSION = new Version('2.0.0-beta.8-509e3e9');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-5ecc509');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-509e3e9');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
