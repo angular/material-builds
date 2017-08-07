0094620fa53d5ad29f0185a05e08edab9c3ce001
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-07 17:38:13 +0000

    5ecc509 chore: fix missing types error for screenshot-tool functions (#6286)

--- @angular/material.js
+++ @angular/material.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-0850981');
+const VERSION = new Version('2.0.0-beta.8-5ecc509');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
