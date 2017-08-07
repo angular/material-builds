b72abfd6a772911e3bbf5fa59c721c86ce4f1295
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-07 18:09:58 +0000

    0680774 ci: shorter wait timeout for saucelabs and browserstack (#6317)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -32,7 +32,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.8-fbc0b95');
+var VERSION = new Version('2.0.0-beta.8-0680774');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
