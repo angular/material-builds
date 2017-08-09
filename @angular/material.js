0542f7dce36a829b3e3103fc2cb45fe73d20d3ab
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-09 22:23:07 +0000

    2286ad5 test(): screenshot diff for button-toggle (#6374)

--- @angular/material.js
+++ @angular/material.js
@@ -33,7 +33,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-d48b957');
+const VERSION = new Version('2.0.0-beta.8-2286ad5');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
