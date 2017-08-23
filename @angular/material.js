a08560d4202354dc124e701f89b1893d4d9ed0da
Austin <amcdaniel2@gmail.com>
2017-08-23 01:11:44 +0000

    595cffd a11y(toolbar): add toolbar accessibility demo page (#6145)

--- @angular/material.js
+++ @angular/material.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-0f6a2ec');
+const VERSION = new Version('2.0.0-beta.8-595cffd');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
