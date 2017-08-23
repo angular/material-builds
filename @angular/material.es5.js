27dc28103bb8ac37541e08fd3e4afd00993792cd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-23 18:51:24 +0000

    844a81c build: shorter wait delay for tunnels (#6607)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.8-4cff5ce');
+var VERSION = new Version('2.0.0-beta.8-844a81c');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-4cff5ce');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-844a81c');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
