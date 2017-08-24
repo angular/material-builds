5c4b134b521df1b3e0c134fc1e24e857282bc7e4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-24 17:33:15 +0000

    dde6e19 build: better caching of node modules in travis (#6619)

--- @angular/material.js
+++ @angular/material.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-eef28c0');
+const VERSION = new Version('2.0.0-beta.8-dde6e19');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
