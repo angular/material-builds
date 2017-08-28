af1dd1f32bdf779f77b37c725e82240c660b1007
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-28 23:59:44 +0000

    9644840 build: write custom tslint rules in typescript (#6650)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.8-5d1ce9f');
+var VERSION = new Version('2.0.0-beta.8-9644840');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
--- @angular/material.js
+++ @angular/material.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-5d1ce9f');
+const VERSION = new Version('2.0.0-beta.8-9644840');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
