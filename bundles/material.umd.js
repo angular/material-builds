b8c6307c5a3fa40db187b68d2134afab17c6edc1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-28 23:52:03 +0000

    6df8144 test(): screenshot diff for expansion (#6666)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.8-412c9fc');
+var VERSION = new _angular_core.Version('2.0.0-beta.8-6df8144');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
