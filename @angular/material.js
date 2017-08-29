7062ed2cb66f0b82165160e42c0a6d4e425ebf23
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-08-29 00:02:25 +0000

    deca032 build: secondary entry-points resolving for windows (#6652)

--- @angular/material.js
+++ @angular/material.js
@@ -35,7 +35,7 @@ import { CDK_ROW_TEMPLATE, CDK_TABLE_TEMPLATE, CdkCell, CdkCellDef, CdkColumnDef
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.8-9644840');
+const VERSION = new Version('2.0.0-beta.8-deca032');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
