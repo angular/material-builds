29264be4c1b2a4af5bd1ca6c87db9523d69cbda8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-06 17:36:53 +0000

    001001c build: custom tslint rules not executing in IDEs (#6817)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -37,7 +37,7 @@ import { CdkStep, CdkStepLabel, CdkStepper, CdkStepperModule, CdkStepperNext, Cd
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.10-3989153');
+var VERSION = new Version('2.0.0-beta.10-001001c');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
