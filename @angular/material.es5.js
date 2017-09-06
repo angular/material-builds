a32fe250f4bc26562814a91b455d7eb9b3c3c4d6
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-09-06 17:31:15 +0000

    3989153 docs(table): fix overflow issue on ie11 site (#6792)

--- @angular/material.es5.js
+++ @angular/material.es5.js
@@ -37,7 +37,7 @@ import { CdkStep, CdkStepLabel, CdkStepper, CdkStepperModule, CdkStepperNext, Cd
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.10-0786518');
+var VERSION = new Version('2.0.0-beta.10-3989153');
 var MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
