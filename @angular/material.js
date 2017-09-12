489b27f0c07b595aaa9e838eadfd7c44a0f144eb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-12 00:43:56 +0000

    85993d3 fix(datepicker): toggle not reacting to disabled state changes in datepicker or input (#6964)

--- @angular/material.js
+++ @angular/material.js
@@ -37,7 +37,7 @@ import { CdkStep, CdkStepLabel, CdkStepper, CdkStepperModule, CdkStepperNext, Cd
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.11-2cefe67');
+const VERSION = new Version('2.0.0-beta.11-85993d3');
 
 const MATERIAL_COMPATIBILITY_MODE = new InjectionToken('md-compatibility-mode');
 /**
--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -40,7 +40,7 @@ function __extends(d, b) {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('2.0.0-beta.11-2cefe67');
+var VERSION = new _angular_core.Version('2.0.0-beta.11-85993d3');
 var MATERIAL_COMPATIBILITY_MODE = new _angular_core.InjectionToken('md-compatibility-mode');
 /**
  * Returns an exception to be thrown if the consumer has used
