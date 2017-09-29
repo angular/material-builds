1dacd80576f4ac509fe29a358dc7b222ea7e5343
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2017-09-29 20:24:06 +0000

    b1ac7b0 refactor: rename all OverlayState references to match the new naming (#7367)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { MatTabBase as ɵe23, _MatTabMixinBase as ɵf23 } from './tab';
-export { MatTabHeaderBase as ɵa23, _MatTabHeaderMixinBase as ɵb23 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc23, _MatTabLabelWrapperMixinBase as ɵd23 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi23, MatTabNavBase as ɵg23, _MatTabLinkMixinBase as ɵj23, _MatTabNavMixinBase as ɵh23 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe21, _MatTabMixinBase as ɵf21 } from './tab';
+export { MatTabHeaderBase as ɵa21, _MatTabHeaderMixinBase as ɵb21 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc21, _MatTabLabelWrapperMixinBase as ɵd21 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi21, MatTabNavBase as ɵg21, _MatTabLinkMixinBase as ɵj21, _MatTabNavMixinBase as ɵh21 } from './tab-nav-bar/tab-nav-bar';
