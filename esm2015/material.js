a8f3472678f2571e4424a665bb85cf3de21b8abe
Austin <amcdaniel2@gmail.com>
2017-10-27 22:53:31 +0000

    4c811d4 chore(docs): adds toc to api docs (#7827)

--- esm2015/material.js
+++ esm2015/material.js
@@ -41,7 +41,7 @@ import { MAT_TOOLTIP_SCROLL_STRATEGY, MAT_TOOLTIP_SCROLL_STRATEGY_PROVIDER, MAT_
 /**
  * Current version of Angular Material.
  */
-const VERSION = new Version('2.0.0-beta.12-b3a493c');
+const VERSION = new Version('2.0.0-beta.12-4c811d4');
 
 /**
  * Generated bundle index. Do not edit.
