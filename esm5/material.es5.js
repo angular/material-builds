5c278afdb73228546210ea0fe4000095eb2ec1bb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-02 03:29:40 +0000

    c4e12b5 refactor(list): easier overriding of list-item height (#8120)

--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -46,7 +46,7 @@ import { MAT_TOOLTIP_SCROLL_STRATEGY, MAT_TOOLTIP_SCROLL_STRATEGY_PROVIDER, MAT_
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('2.0.0-beta.12-9e4c636');
+var VERSION = new Version('2.0.0-beta.12-c4e12b5');
 
 /**
  * @fileoverview added by tsickle
