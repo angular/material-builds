131ae377115971fcc70efd5b0bb7bc5addf3efbd
Andrew Seguin <andrewseguin@users.noreply.github.com>
2017-11-06 22:24:22 +0000

    020ed61 docs(table): add selection example (#8161)

--- bundles/material-menu.umd.js
+++ bundles/material-menu.umd.js
@@ -1232,10 +1232,10 @@ exports.MatMenu = MatMenu;
 exports.MAT_MENU_DEFAULT_OPTIONS = MAT_MENU_DEFAULT_OPTIONS;
 exports.MatMenuItem = MatMenuItem;
 exports.MatMenuTrigger = MatMenuTrigger;
-exports.ɵa21 = MatMenuItemBase;
-exports.ɵb21 = _MatMenuItemMixinBase;
-exports.ɵd21 = MAT_MENU_SCROLL_STRATEGY_PROVIDER;
-exports.ɵc21 = MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY;
+exports.ɵa8 = MatMenuItemBase;
+exports.ɵb8 = _MatMenuItemMixinBase;
+exports.ɵd8 = MAT_MENU_SCROLL_STRATEGY_PROVIDER;
+exports.ɵc8 = MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
