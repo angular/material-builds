ba9c4d0694f4e6b2b9234476205a5843f35b80ec
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-06 22:24:28 +0000

    0c25770 build: fix screenshot-tool app deployment (#8174)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -27326,7 +27326,7 @@ var MatToolbarModule = (function () {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-020ed61');
+var VERSION = new _angular_core.Version('5.0.0-rc0-0c25770');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
@@ -27543,10 +27543,10 @@ exports._MatListOptionMixinBase = _MatListOptionMixinBase;
 exports.MatListOptionChange = MatListOptionChange;
 exports.MatListOption = MatListOption;
 exports.MatSelectionList = MatSelectionList;
-exports.ɵa8 = MatMenuItemBase;
-exports.ɵb8 = _MatMenuItemMixinBase;
-exports.ɵd8 = MAT_MENU_SCROLL_STRATEGY_PROVIDER;
-exports.ɵc8 = MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY;
+exports.ɵa21 = MatMenuItemBase;
+exports.ɵb21 = _MatMenuItemMixinBase;
+exports.ɵd21 = MAT_MENU_SCROLL_STRATEGY_PROVIDER;
+exports.ɵc21 = MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY;
 exports.MAT_MENU_SCROLL_STRATEGY = MAT_MENU_SCROLL_STRATEGY;
 exports.fadeInItems = fadeInItems;
 exports.transformMenu = transformMenu;
