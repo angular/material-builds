3e582495bcc0b60d72a5f8c991a3e277671ab411
Jeremy Elbourn <jelbourn@gmail.com>
2017-11-15 21:58:48 +0000

    7f34a35 build(ci): add config for circleci (#8460)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -27494,7 +27494,7 @@ var MatToolbarModule = (function () {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc0-cf39a41');
+var VERSION = new _angular_core.Version('5.0.0-rc0-7f34a35');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -46,7 +46,7 @@ import { MAT_TOOLTIP_SCROLL_STRATEGY, MAT_TOOLTIP_SCROLL_STRATEGY_PROVIDER, MAT_
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('5.0.0-rc0-cf39a41');
+var VERSION = new Version('5.0.0-rc0-7f34a35');
 
 /**
  * @fileoverview added by tsickle
