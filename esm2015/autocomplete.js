2142d5b48d52d1d81f00018026e5d0409c30d6c7
Jeremy Elbourn <jelbourn@gmail.com>
2017-11-28 18:49:57 +0000

    2dbc766 fix(overlay): make config immutable for existing refs (#7376)

--- esm2015/autocomplete.js
+++ esm2015/autocomplete.js
@@ -578,8 +578,7 @@ class MatAutocompleteTrigger {
         }
         else {
             /** Update the panel width, in case the host width has changed */
-            this._overlayRef.getConfig().width = this._getHostWidth();
-            this._overlayRef.updateSize();
+            this._overlayRef.updateSize({ width: this._getHostWidth() });
         }
         if (this._overlayRef && !this._overlayRef.hasAttached()) {
             this._overlayRef.attach(this._portal);
