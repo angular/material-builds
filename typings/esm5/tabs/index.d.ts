7e59eb829f914f208cdd2ccf9f485c8446f4f348
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-29 21:54:59 +0000

    71305ce build: add material experimental package (#8591)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatTabBase as ɵe15, _MatTabMixinBase as ɵf15 } from './tab';
-export { MatTabHeaderBase as ɵa15, _MatTabHeaderMixinBase as ɵb15 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc15, _MatTabLabelWrapperMixinBase as ɵd15 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi15, MatTabNavBase as ɵg15, _MatTabLinkMixinBase as ɵj15, _MatTabNavMixinBase as ɵh15 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe22, _MatTabMixinBase as ɵf22 } from './tab';
+export { MatTabHeaderBase as ɵa22, _MatTabHeaderMixinBase as ɵb22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc22, _MatTabLabelWrapperMixinBase as ɵd22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi22, MatTabNavBase as ɵg22, _MatTabLinkMixinBase as ɵj22, _MatTabNavMixinBase as ɵh22 } from './tab-nav-bar/tab-nav-bar';
