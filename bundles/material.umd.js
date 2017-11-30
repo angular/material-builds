6cd70b1149aeea62f000896f782d5877e052a783
Joey Perrott <josephperrott.github@gmail.com>
2017-11-30 03:07:58 +0000

    27dfd15 chore: add webhook notifications for travisCI and CircleCI for testing (#8718)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -27646,7 +27646,7 @@ var MatToolbarModule = (function () {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-71305ce');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-27dfd15');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
