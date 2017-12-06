f2e035a82559065e4f29f3ff11fa444ac9b9f2e3
mmalerba <mmalerba@google.com>
2017-12-06 00:51:16 +0000

    d7d995d docs: update examples (#8831)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatTabBase as ɵe22, _MatTabMixinBase as ɵf22 } from './tab';
-export { MatTabHeaderBase as ɵa22, _MatTabHeaderMixinBase as ɵb22 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc22, _MatTabLabelWrapperMixinBase as ɵd22 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi22, MatTabNavBase as ɵg22, _MatTabLinkMixinBase as ɵj22, _MatTabNavMixinBase as ɵh22 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe10, _MatTabMixinBase as ɵf10 } from './tab';
+export { MatTabHeaderBase as ɵa10, _MatTabHeaderMixinBase as ɵb10 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc10, _MatTabLabelWrapperMixinBase as ɵd10 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi10, MatTabNavBase as ɵg10, _MatTabLinkMixinBase as ɵj10, _MatTabNavMixinBase as ɵh10 } from './tab-nav-bar/tab-nav-bar';
