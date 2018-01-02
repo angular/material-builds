a7fe83c48ec8be031c13a8ee0ffe9dfc04b17e4b
Joey Perrott <josephperrott.github@gmail.com>
2018-01-02 18:59:49 +0000

    474121b bump to 5.0.3 and changelog (#9195)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -28276,7 +28276,7 @@ var MatToolbarModule = /** @class */ (function () {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('5.0.2-c3d7cd9');
+var VERSION = new _angular_core.Version('5.0.3-474121b');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -47,7 +47,7 @@ import { MAT_TOOLTIP_DEFAULT_OPTIONS, MAT_TOOLTIP_SCROLL_STRATEGY, MAT_TOOLTIP_S
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('5.0.2-c3d7cd9');
+var VERSION = new Version('5.0.3-474121b');
 
 /**
  * @fileoverview added by tsickle
