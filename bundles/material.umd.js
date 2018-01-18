259f3ac1638a477f004d74a904b2b146cbcb78d7
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-01-18 00:44:13 +0000

    92896a0 chore: 5.1.0 release (#9450)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -28865,7 +28865,7 @@ var MatToolbarModule = /** @class */ (function () {
 /**
  * Current version of Angular Material.
  */
-var VERSION = new _angular_core.Version('5.0.4-4ffcdee');
+var VERSION = new _angular_core.Version('5.1.0-92896a0');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
