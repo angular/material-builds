de8b94bf534dde165e078db0dcde9f8f9266be2b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-01-22 20:59:30 +0000

    7024dc4 chore: update dashboard functions (#9504)

--- esm2015/menu.js
+++ esm2015/menu.js
@@ -1022,5 +1022,5 @@ MatMenuModule.ctorParameters = () => [];
  * Generated bundle index. Do not edit.
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuItemBase as ɵa21, _MatMenuItemMixinBase as ɵb21, MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd21, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc21 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuItemBase as ɵa7, _MatMenuItemMixinBase as ɵb7, MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd7, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc7 };
 //# sourceMappingURL=menu.js.map
