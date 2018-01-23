ed92c9a536be66b813a482ef0f4268a0f6fc3f93
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-01-23 15:39:09 +0000

    0b8955b fix(datepicker): calendar keyboard controls not working if the user clicks on blank area (#9494)

--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -47,7 +47,7 @@ import { MAT_TOOLTIP_DEFAULT_OPTIONS, MAT_TOOLTIP_SCROLL_STRATEGY, MAT_TOOLTIP_S
 /**
  * Current version of Angular Material.
  */
-var VERSION = new Version('5.1.0-4e1a6a8');
+var VERSION = new Version('5.1.0-0b8955b');
 
 /**
  * @fileoverview added by tsickle
