68b7a4b9c05bb03db4279935437211afb83e65ce
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-01-23 21:19:41 +0000

    e5c8880 chore: additional files for examples are missing (#9548)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatTabBase as ɵe21, _MatTabMixinBase as ɵf21 } from './tab';
-export { MatTabHeaderBase as ɵa21, _MatTabHeaderMixinBase as ɵb21 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc21, _MatTabLabelWrapperMixinBase as ɵd21 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi21, MatTabNavBase as ɵg21, _MatTabLinkMixinBase as ɵj21, _MatTabNavMixinBase as ɵh21 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe22, _MatTabMixinBase as ɵf22 } from './tab';
+export { MatTabHeaderBase as ɵa22, _MatTabHeaderMixinBase as ɵb22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc22, _MatTabLabelWrapperMixinBase as ɵd22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi22, MatTabNavBase as ɵg22, _MatTabLinkMixinBase as ɵj22, _MatTabNavMixinBase as ɵh22 } from './tab-nav-bar/tab-nav-bar';
