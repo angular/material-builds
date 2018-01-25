e4ee41c17171cb0f2fd6d55ff02023d974aa78dc
Luiz Machado <machado@odahcam.com>
2018-01-25 20:29:28 +0000

    0f5583d docs(checkbox): fix typo in section header (#9602)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatTabBase as ɵe20, _MatTabMixinBase as ɵf20 } from './tab';
-export { MatTabHeaderBase as ɵa20, _MatTabHeaderMixinBase as ɵb20 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc20, _MatTabLabelWrapperMixinBase as ɵd20 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi20, MatTabNavBase as ɵg20, _MatTabLinkMixinBase as ɵj20, _MatTabNavMixinBase as ɵh20 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe22, _MatTabMixinBase as ɵf22 } from './tab';
+export { MatTabHeaderBase as ɵa22, _MatTabHeaderMixinBase as ɵb22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc22, _MatTabLabelWrapperMixinBase as ɵd22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi22, MatTabNavBase as ɵg22, _MatTabLinkMixinBase as ɵj22, _MatTabNavMixinBase as ɵh22 } from './tab-nav-bar/tab-nav-bar';
