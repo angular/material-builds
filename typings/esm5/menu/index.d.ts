ee1f2d49a4ae678f727d0e4501ce0dbf64f41551
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-06 17:41:00 +0000

    bbf62cd feat: add bottom sheet component (#9764)

--- typings/esm5/menu/index.d.ts
+++ typings/esm5/menu/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatMenuItemBase as ɵa23, _MatMenuItemMixinBase as ɵb23 } from './menu-item';
-export { MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd23, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc23 } from './menu-trigger';
+export { MatMenuItemBase as ɵa21, _MatMenuItemMixinBase as ɵb21 } from './menu-item';
+export { MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd21, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc21 } from './menu-trigger';
