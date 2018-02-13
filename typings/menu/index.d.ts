4d86121b212f7c080e5da8870cbb48b156796e6b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-13 16:09:29 +0000

    2f891e6 build: enforce consistent version names (#9917)

--- typings/menu/index.d.ts
+++ typings/menu/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatMenuItemBase as ɵa23, _MatMenuItemMixinBase as ɵb23 } from './menu-item';
-export { MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd23, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc23 } from './menu-trigger';
+export { MatMenuItemBase as ɵa22, _MatMenuItemMixinBase as ɵb22 } from './menu-item';
+export { MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd22, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc22 } from './menu-trigger';
