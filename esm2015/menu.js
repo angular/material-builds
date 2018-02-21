09218dee5a46b41cad82bdcbf2ecaa4429c2667a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-21 17:57:18 +0000

    master - 514fa91 build: publish docs-content based on branch (#10020)

--- esm2015/menu.js
+++ esm2015/menu.js
@@ -1145,5 +1145,5 @@ MatMenuModule.ctorParameters = () => [];
  * Generated bundle index. Do not edit.
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MatMenuItemBase as ɵa24, _MatMenuItemMixinBase as ɵb24, MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd24, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc24 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MatMenuItemBase as ɵa23, _MatMenuItemMixinBase as ɵb23, MAT_MENU_SCROLL_STRATEGY_PROVIDER as ɵd23, MAT_MENU_SCROLL_STRATEGY_PROVIDER_FACTORY as ɵc23 };
 //# sourceMappingURL=menu.js.map
