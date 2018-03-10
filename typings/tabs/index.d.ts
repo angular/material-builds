e0eed03f374ef8e79be67ee87b75385e40837e4c
Austin <amcdaniel2@gmail.com>
2018-03-10 00:34:20 +0000

    master - 6273d6a feat(schematics): dashboard schematic (#10011)

--- typings/tabs/index.d.ts
+++ typings/tabs/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatTabBase as ɵe21, _MatTabMixinBase as ɵf21 } from './tab';
-export { MatTabHeaderBase as ɵa21, _MatTabHeaderMixinBase as ɵb21 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵc21, _MatTabLabelWrapperMixinBase as ɵd21 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵi21, MatTabNavBase as ɵg21, _MatTabLinkMixinBase as ɵj21, _MatTabNavMixinBase as ɵh21 } from './tab-nav-bar/tab-nav-bar';
+export { MatTabBase as ɵe25, _MatTabMixinBase as ɵf25 } from './tab';
+export { MatTabHeaderBase as ɵa25, _MatTabHeaderMixinBase as ɵb25 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵc25, _MatTabLabelWrapperMixinBase as ɵd25 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵi25, MatTabNavBase as ɵg25, _MatTabLinkMixinBase as ɵj25, _MatTabNavMixinBase as ɵh25 } from './tab-nav-bar/tab-nav-bar';
