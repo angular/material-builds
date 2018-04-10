26fda7372e670b752529cdbadf8ffcc2a40dfd40
Jeremy Elbourn <jelbourn@gmail.com>
2018-04-10 00:27:55 +0000

    master - 4211b39 build(bazel): create script to run on top of ng_package output (#10776)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -31883,7 +31883,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-09a60bb');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.1-4211b39');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-09a60bb');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-4211b39');
 
 /**
  * @fileoverview added by tsickle
