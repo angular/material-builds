d21bff2262efb4fc8708f63c78c76a6471873e97
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-11 17:46:44 +0000

    master - 5b5f2ee fix(stepper): header icon collapsing with very long labels (#10341)

--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-718cee0');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.1-5b5f2ee');
 
 /**
  * @fileoverview added by tsickle
