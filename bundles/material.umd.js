d855ce1107fba57dac727c7a4ce4d8d21d6ea0c5
Jeremy Elbourn <jelbourn@google.com>
2018-04-13 23:52:22 +0000

    master - fcd81a4 chore: bump version to 6.0.0-rc.5 with changelog

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -31880,7 +31880,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.4-0ca87f0');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.5-fcd81a4');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
