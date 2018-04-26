652a0c53c9cd4782945a359278a2d685546cea6f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-04-26 18:31:11 +0000

    master - fd44381 chore: bump to rc.13 (#11035)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -31989,7 +31989,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-2ad13bf');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-fd44381');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2ad13bf');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.13-fd44381');
 
 /**
  * @fileoverview added by tsickle
