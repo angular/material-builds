2472dc9a91d35d7536057db7e42ed8b7f9df239f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-26 23:23:20 +0000

    master - e7b5bd4 build: fix e2e setup error (#11026)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -31989,7 +31989,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-b384c37');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.13-e7b5bd4');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-b384c37');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.13-e7b5bd4');
 
 /**
  * @fileoverview added by tsickle
