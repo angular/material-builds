73297c0f68e8ddc5d31076140f85dd6568f787fe
Joey Perrott <josephperrott.github@gmail.com>
2018-04-26 05:02:56 +0000

    master - 85252b3 feat(accordion): allow intermediate elements between accordion and expansion panel (#11016)

--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-d52bc1d');
+const /** @type {?} */ VERSION = new Version('6.0.0-rc.12-85252b3');
 
 /**
  * @fileoverview added by tsickle
--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-d52bc1d');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-85252b3');
 
 /**
  * @fileoverview added by tsickle
