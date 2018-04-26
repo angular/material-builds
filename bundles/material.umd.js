9d179ca9dab5cfd52a7ca9e57a78cfe112140dd9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-04-26 05:23:13 +0000

    master - 2e54f13 build: bump to rxjs 6.0 final (#11002)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -31989,7 +31989,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-05207e1');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.12-2e54f13');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-05207e1');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.12-2e54f13');
 
 /**
  * @fileoverview added by tsickle
