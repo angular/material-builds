17fdee5f183cd9c7e2cd78fca21215313e9b6b86
Jeremy Elbourn <jelbourn@gmail.com>
2018-04-30 20:13:11 +0000

    master - 7bbc040 fix(schematics): allow more tsconfig locations for ng update (#11058)

--- menu/typings/index.d.ts
+++ menu/typings/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatMenuItemBase as ɵa24, _MatMenuItemMixinBase as ɵb24 } from './menu-item';
+export { MatMenuItemBase as ɵa23, _MatMenuItemMixinBase as ɵb23 } from './menu-item';
