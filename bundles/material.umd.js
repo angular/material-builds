6d300e35a8e5169f3c7cf60443f053097812c3bc
Igor Minar <iminar@google.com>
2018-05-02 15:57:40 +0000

    master - 86ef891 docs: update information about schematics (#11091)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -32041,7 +32041,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-29bf024');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-86ef891');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
