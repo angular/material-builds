26cfd6ce5cdf0a9a210c8f64c423a12aa83070d8
Joey Perrott <josephperrott.github@gmail.com>
2018-05-02 16:31:51 +0000

    master - 645edc5 Revert "feat(table): enable multiple data rows (#10964)" (#11102)

--- typings/esm5/menu/index.d.ts
+++ typings/esm5/menu/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MatMenuItemBase as ɵa24, _MatMenuItemMixinBase as ɵb24 } from './menu-item';
-export { MAT_MENU_PANEL as ɵc24 } from './menu-panel';
+export { MatMenuItemBase as ɵa18, _MatMenuItemMixinBase as ɵb18 } from './menu-item';
+export { MAT_MENU_PANEL as ɵc18 } from './menu-panel';
