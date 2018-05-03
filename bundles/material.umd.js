2c308cb55eb581479b25a11ce012f4d2b1baaf88
Joey Perrott <josephperrott.github@gmail.com>
2018-05-03 18:28:43 +0000

    master - fb2a3aa fix(accordion): uncomment cdk accordion example, add cdkAccordionItem selector (#11131)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -32214,7 +32214,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-9540988');
+var /** @type {?} */ VERSION = new core.Version('6.0.0-rc.14-fb2a3aa');
 
 exports.VERSION = VERSION;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
--- esm5/material.es5.js
+++ esm5/material.es5.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new Version('6.0.0-rc.14-9540988');
+var /** @type {?} */ VERSION = new Version('6.0.0-rc.14-fb2a3aa');
 
 /**
  * @fileoverview added by tsickle
