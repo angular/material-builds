985bbaa9644a198f70b8d016716358056e1eafca
mmalerba <mmalerba@google.com>
2018-05-04 18:26:26 +0000

    master - 37193d8 fix(focus-monitor): reenter ngzone before emitting (#10549)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.0.0-01ec602';
-exports.cdkVersion = '^6.0.0-01ec602';
-exports.angularVersion = '^6.0.0-01ec602';
+exports.materialVersion = '^6.0.0-37193d8';
+exports.cdkVersion = '^6.0.0-37193d8';
+exports.angularVersion = '^6.0.0-37193d8';
 //# sourceMappingURL=lib-versions.js.map
