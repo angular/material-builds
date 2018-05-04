61fe164fa00dd3393aef684e4e94a4e7f3ac73f4
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-05-04 17:51:50 +0000

    master - c15e307 feat(table): enable multiple data rows (#11116)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.0.0-fbf06bb';
-exports.cdkVersion = '^6.0.0-fbf06bb';
-exports.angularVersion = '^6.0.0-fbf06bb';
+exports.materialVersion = '^6.0.0-c15e307';
+exports.cdkVersion = '^6.0.0-c15e307';
+exports.angularVersion = '^6.0.0-c15e307';
 //# sourceMappingURL=lib-versions.js.map
