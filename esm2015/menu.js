816e7c0922dda4e960a9eec11fe1e9c1cac2437c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-08 17:06:48 +0000

    master - 9e9daf8 fix(autocomplete): panel blending in with background in high contrast mode (#11190)

--- esm2015/menu.js
+++ esm2015/menu.js
@@ -1249,5 +1249,5 @@ MatMenuModule.decorators = [
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24, MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24, MAT_MENU_PANEL as ɵe24, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa7, MatMenuItemBase as ɵb7, _MatMenuItemMixinBase as ɵc7, MAT_MENU_PANEL as ɵe7, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd7 };
 //# sourceMappingURL=menu.js.map
--- menu/typings/index.d.ts
+++ menu/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24 } from './menu-directive';
-export { MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24 } from './menu-item';
-export { MAT_MENU_PANEL as ɵe24 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa7 } from './menu-directive';
+export { MatMenuItemBase as ɵb7, _MatMenuItemMixinBase as ɵc7 } from './menu-item';
+export { MAT_MENU_PANEL as ɵe7 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd7 } from './menu-trigger';
