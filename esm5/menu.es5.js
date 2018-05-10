85ac97e8ab5e1c67376c2c6f8d4aefa47b3ec3cf
Ben Elliott <4996462+benelliott@users.noreply.github.com>
2018-05-10 16:17:46 +0000

    master - fbf5648 feat(elevation): add $opacity argument to elevation mixins (#10877)

--- esm5/menu.es5.js
+++ esm5/menu.es5.js
@@ -1546,5 +1546,5 @@ var MatMenuModule = /** @class */ (function () {
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa23, MatMenuItemBase as ɵb23, _MatMenuItemMixinBase as ɵc23, MAT_MENU_PANEL as ɵe23, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd23 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24, MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24, MAT_MENU_PANEL as ɵe24, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24 };
 //# sourceMappingURL=menu.es5.js.map
