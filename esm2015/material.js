c6d3dde2e2eb7ec29d3a61b3afe8bc16a2d1fe32
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-10 16:00:24 +0000

    master - 61dd937 fix(tabs): handle long tab labels in mat-tab-nav-bar (#10903)

--- esm2015/material.js
+++ esm2015/material.js
@@ -49,7 +49,7 @@ export { _MatTreeNodeMixinBase, _MatNestedTreeNodeMixinBase, MatTreeNode, MatTre
 /**
  * Current version of Angular Material.
  */
-const /** @type {?} */ VERSION = new Version('6.0.1-03e749a');
+const /** @type {?} */ VERSION = new Version('6.0.1-61dd937');
 
 /**
  * @fileoverview added by tsickle
