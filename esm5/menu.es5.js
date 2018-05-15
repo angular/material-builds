9871984f16902fc3ee9d180a4a900bb5b0c28370
Ray Gervais <raygervais@hotmail.ca>
2018-05-15 18:07:09 +0000

    master - 8ae6ad6 update(stepper): Adds Vertical Stepper example (#10942)

--- esm5/menu.es5.js
+++ esm5/menu.es5.js
@@ -1511,5 +1511,5 @@ var MatMenuModule = /** @class */ (function () {
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa10, MatMenuItemBase as ɵb10, _MatMenuItemMixinBase as ɵc10, MAT_MENU_PANEL as ɵe10, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd10 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24, MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24, MAT_MENU_PANEL as ɵe24, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24 };
 //# sourceMappingURL=menu.es5.js.map
