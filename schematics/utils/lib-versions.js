df6642a9df3a5b94265766b4358bf84536da77ed
tinayuangao <tinagao@google.com>
2018-05-21 22:16:12 +0000

    master - 72f8d9d chore: bump the version to 6.1.0 with changelog (#11440)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.0.0';
-exports.cdkVersion = '^6.0.0';
+exports.materialVersion = '^6.1.0-72f8d9d';
+exports.cdkVersion = '^6.1.0-72f8d9d';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
