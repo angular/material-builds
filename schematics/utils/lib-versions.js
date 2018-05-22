1c18eb0cc0c909edc93e5b1029b65d19195f8da3
mmalerba <mmalerba@google.com>
2018-05-22 23:58:52 +0000

    master - 64a70ad feat(a11y): add cdkAriaLive directive (#11352)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-f1f34bc';
-exports.cdkVersion = '^6.1.0-f1f34bc';
+exports.materialVersion = '^6.1.0-64a70ad';
+exports.cdkVersion = '^6.1.0-64a70ad';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
