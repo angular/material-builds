2e5ce5c00089fe807a804592e49e42ee81216841
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-22 16:40:28 +0000

    master - 3fdd2cc fix(cdk-dialog): afterClosed event emitting twice on close (#11407)

--- menu/typings/index.d.ts
+++ menu/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24 } from './menu-directive';
-export { MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24 } from './menu-item';
-export { MAT_MENU_PANEL as ɵf24 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe24 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa19 } from './menu-directive';
+export { MatMenuItemBase as ɵb19, _MatMenuItemMixinBase as ɵc19 } from './menu-item';
+export { MAT_MENU_PANEL as ɵf19 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd19, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe19 } from './menu-trigger';
--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-984f8dc';
-exports.cdkVersion = '^6.1.0-984f8dc';
+exports.materialVersion = '^6.1.0-3fdd2cc';
+exports.cdkVersion = '^6.1.0-3fdd2cc';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
