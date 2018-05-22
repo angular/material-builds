65858bff030b31ee663ab6edbd5349d92aa1841d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-22 16:30:30 +0000

    master - 31fd6a2 chore: fix typo in form field docs (#11420)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-f59658d';
-exports.cdkVersion = '^6.1.0-f59658d';
+exports.materialVersion = '^6.1.0-31fd6a2';
+exports.cdkVersion = '^6.1.0-31fd6a2';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
