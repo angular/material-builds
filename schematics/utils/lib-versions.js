143da4ab9edf02eec14a5f1b628e3642836c2def
mmalerba <mmalerba@google.com>
2018-05-22 16:29:40 +0000

    master - f59658d virtual-scroll: merge into master under cdk-experimental (#11354)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-72f8d9d';
-exports.cdkVersion = '^6.1.0-72f8d9d';
+exports.materialVersion = '^6.1.0-f59658d';
+exports.cdkVersion = '^6.1.0-f59658d';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
