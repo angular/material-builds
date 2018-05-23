44bb3ba4123e0cce0382bb002818a3a9e0e13946
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-23 00:03:12 +0000

    master - 4b2cc1a fix(checkbox,radio): remove webkit tap highlights (#11349)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-64a70ad';
-exports.cdkVersion = '^6.1.0-64a70ad';
+exports.materialVersion = '^6.1.0-4b2cc1a';
+exports.cdkVersion = '^6.1.0-4b2cc1a';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
