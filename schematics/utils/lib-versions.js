89a4c4de7c5728dfb3cc2ff859a48c489beb80b5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-23 04:09:55 +0000

    master - c68ccc9 perf(overlay): only compute and emit position changes if there are subscribers (#11431)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-3dc3fcd';
-exports.cdkVersion = '^6.1.0-3dc3fcd';
+exports.materialVersion = '^6.1.0-c68ccc9';
+exports.cdkVersion = '^6.1.0-c68ccc9';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
