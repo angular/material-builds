85e882c2ee20eb5bcb2b5650c2a25b67c6db46f9
mmalerba <mmalerba@google.com>
2018-05-29 17:09:04 +0000

    master - 315c9f8 chore: add misisng branches to run on CI (#11562)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.1.0-9062640';
-exports.cdkVersion = '^6.1.0-9062640';
+exports.materialVersion = '^6.1.0-315c9f8';
+exports.cdkVersion = '^6.1.0-315c9f8';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
