90a6b7fdc1eba9c6efcbd8ada3fd19c0319867bc
mmalerba <mmalerba@google.com>
2018-05-29 21:46:28 +0000

    master - 7ceedc7 chore: add 6.2.x branch to CI (#11567)

--- bundles/material-autocomplete.umd.js
+++ bundles/material-autocomplete.umd.js
@@ -1055,7 +1055,7 @@ exports.MAT_AUTOCOMPLETE_SCROLL_STRATEGY_FACTORY_PROVIDER = MAT_AUTOCOMPLETE_SCR
 exports.MAT_AUTOCOMPLETE_VALUE_ACCESSOR = MAT_AUTOCOMPLETE_VALUE_ACCESSOR;
 exports.getMatAutocompleteMissingPanelError = getMatAutocompleteMissingPanelError;
 exports.MatAutocompleteTrigger = MatAutocompleteTrigger;
-exports.ɵa27 = MatAutocompleteOrigin;
+exports.ɵa28 = MatAutocompleteOrigin;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.2.0-23aa90b';
-exports.cdkVersion = '^6.2.0-23aa90b';
+exports.materialVersion = '^6.2.0-7ceedc7';
+exports.cdkVersion = '^6.2.0-7ceedc7';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
