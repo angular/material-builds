f518eabb34a529fe9617375614451ebf9f23137b
Adrien Crivelli <adrien.crivelli@gmail.com>
2018-05-29 20:26:23 +0000

    master - 23aa90b docs(tabs): remove duplicated shortcut (#11541)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.2.0-b30ddc6';
-exports.cdkVersion = '^6.2.0-b30ddc6';
+exports.materialVersion = '^6.2.0-23aa90b';
+exports.cdkVersion = '^6.2.0-23aa90b';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
