6b42180cd08d69f84410b080c85d766de727d7af
Andrew Seguin <andrewseguin@users.noreply.github.com>
2018-05-29 20:20:17 +0000

    master - b30ddc6 chore(table): add expandable rows demo (#11506)

--- esm2015/menu.js
+++ esm2015/menu.js
@@ -1244,5 +1244,5 @@ MatMenuModule.decorators = [
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24, MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24, MAT_MENU_PANEL as ɵf24, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe24 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa17, MatMenuItemBase as ɵb17, _MatMenuItemMixinBase as ɵc17, MAT_MENU_PANEL as ɵf17, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd17, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe17 };
 //# sourceMappingURL=menu.js.map
--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.2.0-dcf9c3d';
-exports.cdkVersion = '^6.2.0-dcf9c3d';
+exports.materialVersion = '^6.2.0-b30ddc6';
+exports.cdkVersion = '^6.2.0-b30ddc6';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
