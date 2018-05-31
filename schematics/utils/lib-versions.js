af5924b45a4c506fc9575b3d05bb4a50443412bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-05-31 16:37:31 +0000

    master - 2786b34 fix(tooltip): long continuous strings overflowing tooltip container (#11363)

--- schematics/utils/lib-versions.js
+++ schematics/utils/lib-versions.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.materialVersion = '^6.2.0-bb297c9';
-exports.cdkVersion = '^6.2.0-bb297c9';
+exports.materialVersion = '^6.2.0-2786b34';
+exports.cdkVersion = '^6.2.0-2786b34';
 exports.angularVersion = '^6.0.0';
 //# sourceMappingURL=lib-versions.js.map
