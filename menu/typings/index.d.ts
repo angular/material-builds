0be06a0cd30f3327f3e4d70fdb3dd16513caa9f1
Jeremy Elbourn <jelbourn@gmail.com>
2018-07-11 15:37:10 +0000

    master - dc0732b docs: update README for Q3 2018 (#12143)

--- menu/typings/index.d.ts
+++ menu/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa19 } from './menu-directive';
-export { MatMenuItemBase as ɵb19, _MatMenuItemMixinBase as ɵc19 } from './menu-item';
-export { MAT_MENU_PANEL as ɵf19 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd19, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe19 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa21 } from './menu-directive';
+export { MatMenuItemBase as ɵb21, _MatMenuItemMixinBase as ɵc21 } from './menu-item';
+export { MAT_MENU_PANEL as ɵf21 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd21, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe21 } from './menu-trigger';
