e80629211c557905555800c04d04d67f1d24070e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-07-18 17:04:26 +0000

    master - 7aa64df docs(datepicker): custom datepicker header demo styling not working (#12253)

--- esm5/menu.es5.js
+++ esm5/menu.es5.js
@@ -1581,5 +1581,5 @@ var MatMenuModule = /** @class */ (function () {
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa18, MatMenuItemBase as ɵb18, _MatMenuItemMixinBase as ɵc18, MAT_MENU_PANEL as ɵf18, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd18, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe18 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa12, MatMenuItemBase as ɵb12, _MatMenuItemMixinBase as ɵc12, MAT_MENU_PANEL as ɵf12, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd12, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe12 };
 //# sourceMappingURL=menu.es5.js.map
--- typings/menu/index.d.ts
+++ typings/menu/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa18 } from './menu-directive';
-export { MatMenuItemBase as ɵb18, _MatMenuItemMixinBase as ɵc18 } from './menu-item';
-export { MAT_MENU_PANEL as ɵf18 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd18, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe18 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa12 } from './menu-directive';
+export { MatMenuItemBase as ɵb12, _MatMenuItemMixinBase as ɵc12 } from './menu-item';
+export { MAT_MENU_PANEL as ɵf12 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd12, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe12 } from './menu-trigger';
