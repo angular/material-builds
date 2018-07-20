1ee05a7b9f1548e75b74173ad61648ce74a6c85b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-20 14:43:48 +0000

    master - 944caf9 fix(ripple): don't hide directive host in high contrast (#12168)

--- menu/typings/index.d.ts
+++ menu/typings/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa17 } from './menu-directive';
-export { MatMenuItemBase as ɵb17, _MatMenuItemMixinBase as ɵc17 } from './menu-item';
-export { MAT_MENU_PANEL as ɵf17 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd17, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe17 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa16 } from './menu-directive';
+export { MatMenuItemBase as ɵb16, _MatMenuItemMixinBase as ɵc16 } from './menu-item';
+export { MAT_MENU_PANEL as ɵf16 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd16, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe16 } from './menu-trigger';
