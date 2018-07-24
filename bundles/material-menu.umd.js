9c94b4f1c6f7237fb0f2561636f966b2078a2eb2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-07-24 17:29:10 +0000

    master - e23709f build: remove unused dependencies (#12346)

--- bundles/material-menu.umd.js
+++ bundles/material-menu.umd.js
@@ -1601,12 +1601,12 @@ exports.matMenuAnimations = matMenuAnimations;
 exports.fadeInItems = fadeInItems;
 exports.transformMenu = transformMenu;
 exports.MatMenuContent = MatMenuContent;
-exports.ɵa23 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
-exports.ɵb23 = MatMenuItemBase;
-exports.ɵc23 = _MatMenuItemMixinBase;
-exports.ɵf23 = MAT_MENU_PANEL;
-exports.ɵd23 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
-exports.ɵe23 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
+exports.ɵa9 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
+exports.ɵb9 = MatMenuItemBase;
+exports.ɵc9 = _MatMenuItemMixinBase;
+exports.ɵf9 = MAT_MENU_PANEL;
+exports.ɵd9 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
+exports.ɵe9 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
