8875123fc71bd13a0117e3ce9a7e77ce63dd8584
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-08-01 14:12:31 +0000

    master - db8f83d fix(drag-drop): account for transition-delay when waiting for the animation to finish (#12466)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -32848,10 +32848,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /**
  * Current version of Angular Material.
  */
-var /** @type {?} */ VERSION = new core.Version('6.4.2-6a02b94');
+var /** @type {?} */ VERSION = new core.Version('6.4.2-db8f83d');
 
 exports.VERSION = VERSION;
-exports.ɵa29 = MatAutocompleteOrigin;
+exports.ɵa28 = MatAutocompleteOrigin;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
 exports._MatAutocompleteMixinBase = _MatAutocompleteMixinBase;
