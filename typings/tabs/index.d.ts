608a5407cd59e821f1b50655580ee42832876a80
Joey Perrott <josephperrott.github@gmail.com>
2018-08-08 21:49:11 +0000

    master - 5a560b2 feat(breakpoint-observer): Emit matching state of each query provided (#12506)

--- typings/tabs/index.d.ts
+++ typings/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
-export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
-export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa2 } from './ink-bar';
+export { MatTabBase as ɵf2, _MatTabMixinBase as ɵg2 } from './tab';
+export { MatTabHeaderBase as ɵb2, _MatTabHeaderMixinBase as ɵc2 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd2, _MatTabLabelWrapperMixinBase as ɵe2 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj2, MatTabNavBase as ɵh2, _MatTabLinkMixinBase as ɵk2, _MatTabNavMixinBase as ɵi2 } from './tab-nav-bar/tab-nav-bar';
