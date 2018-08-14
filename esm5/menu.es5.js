912d6e62cc82ed52477e236f4b1d94923ba23ed7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-14 17:14:46 +0000

    master - 3a84259 build: update project dependencies (#12675)

--- esm5/menu.es5.js
+++ esm5/menu.es5.js
@@ -1593,5 +1593,5 @@ var MatMenuModule = /** @class */ (function () {
  * @suppress {checkTypes} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24, MatMenuItemBase as ɵb24, _MatMenuItemMixinBase as ɵc24, MAT_MENU_PANEL as ɵf24, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd24, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe24 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa20, MatMenuItemBase as ɵb20, _MatMenuItemMixinBase as ɵc20, MAT_MENU_PANEL as ɵf20, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd20, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe20 };
 //# sourceMappingURL=menu.es5.js.map
