7f60dc56b7737afde164ebe7d37a65cb8be10130
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-08-24 03:48:53 +0000

    master - 0ef9e38 refactor(schematics): remove type checker for inheritance rule (#12802)

--- schematics/update/rules/checkInheritanceRule.js
+++ schematics/update/rules/checkInheritanceRule.js
@@ -9,32 +9,52 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const chalk_1 = require("chalk");
 const tslint_1 = require("tslint");
+const ts = require("typescript");
 const property_names_1 = require("../material/data/property-names");
 /**
- * Rule that walks through every property access expression and updates properties that have
- * been changed in favor of the new name.
+ * Map of classes that have been updated. Each class name maps to the according property change
+ * data.
+ */
+const changedClassesMap = new Map();
+property_names_1.propertyNames.filter(data => data.whitelist && data.whitelist.classes).forEach(data => {
+    data.whitelist.classes.forEach(name => changedClassesMap.set(name, data));
+});
+/**
+ * Rule that identifies class declarations that extend CDK or Material classes and had
+ * a public property change.
  */
 class Rule extends tslint_1.Rules.TypedRule {
     applyWithProgram(sourceFile, program) {
-        return this.applyWithWalker(new CheckInheritanceWalker(sourceFile, this.getOptions(), program));
+        return this.applyWithWalker(new Walker(sourceFile, this.getOptions(), program));
     }
 }
 exports.Rule = Rule;
-class CheckInheritanceWalker extends tslint_1.ProgramAwareRuleWalker {
-    visitClassDeclaration(declaration) {
-        // Check if user is extending an Angular Material class whose properties have changed.
-        const type = this.getTypeChecker().getTypeAtLocation(declaration.name);
-        const baseTypes = this.getTypeChecker().getBaseTypes(type);
-        baseTypes.forEach(t => {
-            const propertyData = property_names_1.propertyNames.find(data => data.whitelist && new Set(data.whitelist.classes).has(t.symbol.name));
-            if (propertyData) {
-                this.addFailureAtNode(declaration, `Found class "${chalk_1.bold(declaration.name.text)}" which extends class` +
-                    ` "${chalk_1.bold(t.symbol.name)}". Please note that the base class property` +
-                    ` "${chalk_1.red(propertyData.replace)}" has changed to "${chalk_1.green(propertyData.replaceWith)}".` +
-                    ` You may need to update your class as well`);
+class Walker extends tslint_1.ProgramAwareRuleWalker {
+    visitClassDeclaration(node) {
+        const baseTypes = this._determineBaseTypes(node);
+        if (!baseTypes) {
+            return;
+        }
+        baseTypes.forEach(typeName => {
+            const data = changedClassesMap.get(typeName);
+            if (data) {
+                this.addFailureAtNode(node, `Found class "${chalk_1.bold(node.name.text)}" which extends class ` +
+                    `"${chalk_1.bold(typeName)}". Please note that the base class property ` +
+                    `"${chalk_1.red(data.replace)}" has changed to "${chalk_1.green(data.replaceWith)}". ` +
+                    `You may need to update your class as well`);
             }
         });
     }
+    _determineBaseTypes(node) {
+        if (!node.heritageClauses) {
+            return null;
+        }
+        return node.heritageClauses
+            .reduce((types, clause) => types.concat(clause.types), [])
+            .map(typeExpression => typeExpression.expression)
+            .filter(expression => expression && ts.isIdentifier(expression))
+            .map(identifier => identifier.text);
+    }
 }
-exports.CheckInheritanceWalker = CheckInheritanceWalker;
+exports.Walker = Walker;
 //# sourceMappingURL=checkInheritanceRule.js.map
