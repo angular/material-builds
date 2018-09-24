8f03a4a69d0cc248e05483822bfc5d427615e628
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-09-24 20:47:50 +0000

    master - 5f1f072 build: livereload not working for core/theming files (#13275)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -34138,10 +34138,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /** *
  * Current version of Angular Material.
   @type {?} */
-var VERSION = new core.Version('7.0.0-beta.2-0f818fd');
+var VERSION = new core.Version('7.0.0-beta.2-5f1f072');
 
 exports.VERSION = VERSION;
-exports.ɵa30 = MatAutocompleteOrigin;
+exports.ɵa29 = MatAutocompleteOrigin;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
@@ -34391,12 +34391,12 @@ exports.MAT_SELECTION_LIST_VALUE_ACCESSOR = MAT_SELECTION_LIST_VALUE_ACCESSOR;
 exports.MatSelectionListChange = MatSelectionListChange;
 exports.MatListOption = MatListOption;
 exports.MatSelectionList = MatSelectionList;
-exports.ɵa21 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
-exports.ɵb21 = MatMenuItemBase;
-exports.ɵc21 = _MatMenuItemMixinBase;
-exports.ɵf21 = MAT_MENU_PANEL;
-exports.ɵd21 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
-exports.ɵe21 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
+exports.ɵa16 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
+exports.ɵb16 = MatMenuItemBase;
+exports.ɵc16 = _MatMenuItemMixinBase;
+exports.ɵf16 = MAT_MENU_PANEL;
+exports.ɵd16 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
+exports.ɵe16 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
 exports.MAT_MENU_SCROLL_STRATEGY = MAT_MENU_SCROLL_STRATEGY;
 exports.MatMenuModule = MatMenuModule;
 exports.MatMenu = MatMenu;
--- typings/esm5/menu/index.d.ts
+++ typings/esm5/menu/index.d.ts
@@ -2,7 +2,7 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa21 } from './menu-directive';
-export { MatMenuItemBase as ɵb21, _MatMenuItemMixinBase as ɵc21 } from './menu-item';
-export { MAT_MENU_PANEL as ɵf21 } from './menu-panel';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd21, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe21 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa16 } from './menu-directive';
+export { MatMenuItemBase as ɵb16, _MatMenuItemMixinBase as ɵc16 } from './menu-item';
+export { MAT_MENU_PANEL as ɵf16 } from './menu-panel';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd16, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe16 } from './menu-trigger';
