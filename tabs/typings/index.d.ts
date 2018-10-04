6aee13ef90d52a4728c23ca23de9e8922f687a6c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-04 19:09:26 +0000

    master - 3254992 build: update bazel typescript rules (#13408)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
-export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
-export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa17 } from './ink-bar';
+export { MatTabBase as ɵf17, _MatTabMixinBase as ɵg17 } from './tab';
+export { MatTabHeaderBase as ɵb17, _MatTabHeaderMixinBase as ɵc17 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd17, _MatTabLabelWrapperMixinBase as ɵe17 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj17, MatTabNavBase as ɵh17, _MatTabLinkMixinBase as ɵk17, _MatTabNavMixinBase as ɵi17 } from './tab-nav-bar/tab-nav-bar';
