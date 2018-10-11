6b8d8463b2d8cb5027e1bbee42a7c942b458f54d
Kevin Lanni <therealklanni@gmail.com>
2018-10-11 17:11:52 +0000

    master - a2c23a4 docs: fix typo in typography example (#13564)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -34335,10 +34335,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /** *
  * Current version of Angular Material.
   @type {?} */
-var VERSION = new core.Version('7.0.0-rc.1-2dff301');
+var VERSION = new core.Version('7.0.0-rc.1-a2c23a4');
 
 exports.VERSION = VERSION;
-exports.ɵa30 = MatAutocompleteOrigin;
+exports.ɵa28 = MatAutocompleteOrigin;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
