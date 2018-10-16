4e7459da9a519c96b3dfd77153e4d83fa73e9e85
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-16 21:07:07 +0000

    master - b6fbe65 chore: update required statuses for github robot (#13639)

--- esm2015/tabs.js
+++ esm2015/tabs.js
@@ -1692,5 +1692,5 @@ MatTabsModule.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 
-export { MatInkBar, _MAT_INK_BAR_POSITIONER, MatTabBody, MatTabBodyPortal, MatTabHeader, MatTabLabelWrapper, MatTab, MatTabLabel, MatTabNav, MatTabLink, MatTabContent, MatTabsModule, MatTabChangeEvent, MatTabGroupBase, _MatTabGroupMixinBase, MatTabGroup, matTabsAnimations, _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24, MatTabBase as ɵf24, _MatTabMixinBase as ɵg24, MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24, MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24, MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 };
+export { MatInkBar, _MAT_INK_BAR_POSITIONER, MatTabBody, MatTabBodyPortal, MatTabHeader, MatTabLabelWrapper, MatTab, MatTabLabel, MatTabNav, MatTabLink, MatTabContent, MatTabsModule, MatTabChangeEvent, MatTabGroupBase, _MatTabGroupMixinBase, MatTabGroup, matTabsAnimations, _MAT_INK_BAR_POSITIONER_FACTORY as ɵa18, MatTabBase as ɵf18, _MatTabMixinBase as ɵg18, MatTabHeaderBase as ɵb18, _MatTabHeaderMixinBase as ɵc18, MatTabLabelWrapperBase as ɵd18, _MatTabLabelWrapperMixinBase as ɵe18, MatTabLinkBase as ɵj18, MatTabNavBase as ɵh18, _MatTabLinkMixinBase as ɵk18, _MatTabNavMixinBase as ɵi18 };
 //# sourceMappingURL=tabs.js.map
