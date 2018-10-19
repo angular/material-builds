ff7de6ee54e933e7b100e03b845908f52e9912a6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-10-19 21:13:54 +0000

    master - c38ebb6 fix(drag-drop): not picking up handle that isn't a direct descendant (#13360)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa19 } from './ink-bar';
-export { MatTabBase as ɵf19, _MatTabMixinBase as ɵg19 } from './tab';
-export { MatTabHeaderBase as ɵb19, _MatTabHeaderMixinBase as ɵc19 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd19, _MatTabLabelWrapperMixinBase as ɵe19 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj19, MatTabNavBase as ɵh19, _MatTabLinkMixinBase as ɵk19, _MatTabNavMixinBase as ɵi19 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa23 } from './ink-bar';
+export { MatTabBase as ɵf23, _MatTabMixinBase as ɵg23 } from './tab';
+export { MatTabHeaderBase as ɵb23, _MatTabHeaderMixinBase as ɵc23 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd23, _MatTabLabelWrapperMixinBase as ɵe23 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj23, MatTabNavBase as ɵh23, _MatTabLinkMixinBase as ɵk23, _MatTabNavMixinBase as ɵi23 } from './tab-nav-bar/tab-nav-bar';
