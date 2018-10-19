893c6963078825d26debf29161ebb55b54b07a88
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-19 21:51:31 +0000

    master - 2f15784 refactor(schematics): better ng-update success message (#13687)

--- bundles/material-tabs.umd.js
+++ bundles/material-tabs.umd.js
@@ -2154,17 +2154,17 @@ exports.MatTabGroupBase = MatTabGroupBase;
 exports._MatTabGroupMixinBase = _MatTabGroupMixinBase;
 exports.MatTabGroup = MatTabGroup;
 exports.matTabsAnimations = matTabsAnimations;
-exports.ɵa24 = _MAT_INK_BAR_POSITIONER_FACTORY;
-exports.ɵf24 = MatTabBase;
-exports.ɵg24 = _MatTabMixinBase;
-exports.ɵb24 = MatTabHeaderBase;
-exports.ɵc24 = _MatTabHeaderMixinBase;
-exports.ɵd24 = MatTabLabelWrapperBase;
-exports.ɵe24 = _MatTabLabelWrapperMixinBase;
-exports.ɵj24 = MatTabLinkBase;
-exports.ɵh24 = MatTabNavBase;
-exports.ɵk24 = _MatTabLinkMixinBase;
-exports.ɵi24 = _MatTabNavMixinBase;
+exports.ɵa14 = _MAT_INK_BAR_POSITIONER_FACTORY;
+exports.ɵf14 = MatTabBase;
+exports.ɵg14 = _MatTabMixinBase;
+exports.ɵb14 = MatTabHeaderBase;
+exports.ɵc14 = _MatTabHeaderMixinBase;
+exports.ɵd14 = MatTabLabelWrapperBase;
+exports.ɵe14 = _MatTabLabelWrapperMixinBase;
+exports.ɵj14 = MatTabLinkBase;
+exports.ɵh14 = MatTabNavBase;
+exports.ɵk14 = _MatTabLinkMixinBase;
+exports.ɵi14 = _MatTabNavMixinBase;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
-export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
-export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa14 } from './ink-bar';
+export { MatTabBase as ɵf14, _MatTabMixinBase as ɵg14 } from './tab';
+export { MatTabHeaderBase as ɵb14, _MatTabHeaderMixinBase as ɵc14 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd14, _MatTabLabelWrapperMixinBase as ɵe14 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj14, MatTabNavBase as ɵh14, _MatTabLinkMixinBase as ɵk14, _MatTabNavMixinBase as ɵi14 } from './tab-nav-bar/tab-nav-bar';
