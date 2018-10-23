eec718dbf7304a4db0eb8ee0deaf50e75d685961
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-23 17:35:17 +0000

    master - 4cdf5ba fix(stroked-button): ripples and focus overlay have incorrect radius (#13745)

--- bundles/material-grid-list.umd.js
+++ bundles/material-grid-list.umd.js
@@ -1192,7 +1192,7 @@ exports.MatGridTileText = MatGridTileText;
 exports.MatGridAvatarCssMatStyler = MatGridAvatarCssMatStyler;
 exports.MatGridTileHeaderCssMatStyler = MatGridTileHeaderCssMatStyler;
 exports.MatGridTileFooterCssMatStyler = MatGridTileFooterCssMatStyler;
-exports.ɵa10 = MAT_GRID_LIST;
+exports.ɵa8 = MAT_GRID_LIST;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
