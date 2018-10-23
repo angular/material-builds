ba354264426a40d861b8b191ffdf1b3fda456ffb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-23 19:19:24 +0000

    master - 3ca3e39 build: update dgeni-packages version (#13772)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa14 } from './ink-bar';
-export { MatTabBase as ɵf14, _MatTabMixinBase as ɵg14 } from './tab';
-export { MatTabHeaderBase as ɵb14, _MatTabHeaderMixinBase as ɵc14 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd14, _MatTabLabelWrapperMixinBase as ɵe14 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj14, MatTabNavBase as ɵh14, _MatTabLinkMixinBase as ɵk14, _MatTabNavMixinBase as ɵi14 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa22 } from './ink-bar';
+export { MatTabBase as ɵf22, _MatTabMixinBase as ɵg22 } from './tab';
+export { MatTabHeaderBase as ɵb22, _MatTabHeaderMixinBase as ɵc22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd22, _MatTabLabelWrapperMixinBase as ɵe22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj22, MatTabNavBase as ɵh22, _MatTabLinkMixinBase as ɵk22, _MatTabNavMixinBase as ɵi22 } from './tab-nav-bar/tab-nav-bar';
