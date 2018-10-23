c50d1fcdf0fd5604f6e81e61ab49a846fade5830
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-10-23 18:05:12 +0000

    7.0.x - 7877404 fix(stroked-button): ripples and focus overlay have incorrect radius (#13745)

--- typings/esm5/grid-list/index.d.ts
+++ typings/esm5/grid-list/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_GRID_LIST as ɵa5, MatGridListBase as ɵb5 } from './grid-list-base';
+export { MAT_GRID_LIST as ɵa11, MatGridListBase as ɵb11 } from './grid-list-base';
