0f6461def35b673f1d6d1fa6bfe2184bfedf410b
Nathan <natjkern@gmail.com>
2018-10-25 15:32:41 +0000

    master - d2d8a1f feat(drag-utils): add utility function for cloning array items from one array to another (#13743)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -34484,10 +34484,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /** *
  * Current version of Angular Material.
   @type {?} */
-var VERSION = new core.Version('7.0.1-4fbbb9d');
+var VERSION = new core.Version('7.0.1-d2d8a1f');
 
 exports.VERSION = VERSION;
-exports.ɵa29 = MatAutocompleteOrigin;
+exports.ɵa30 = MatAutocompleteOrigin;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
