6981dde4a1493b007855dedae85b82d271eca2e4
Joey Perrott <josephperrott.github@gmail.com>
2018-11-03 00:28:56 +0000

    master - 077f68e fix(breakpoints): set max-widths for breakpoints to non-integers to handle subpixel queries (#13828)

--- bundles/material-grid-list.umd.js
+++ bundles/material-grid-list.umd.js
@@ -1192,7 +1192,7 @@ exports.MatGridTileText = MatGridTileText;
 exports.MatGridAvatarCssMatStyler = MatGridAvatarCssMatStyler;
 exports.MatGridTileHeaderCssMatStyler = MatGridTileHeaderCssMatStyler;
 exports.MatGridTileFooterCssMatStyler = MatGridTileFooterCssMatStyler;
-exports.ɵa6 = MAT_GRID_LIST;
+exports.ɵa2 = MAT_GRID_LIST;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
