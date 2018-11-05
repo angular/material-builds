25af7bbe8da2c678dce6617776020ebe98f0992c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-05 18:24:03 +0000

    master - 057909d build: update bazel sass_rules (#13963)

--- typings/esm5/tabs/index.d.ts
+++ typings/esm5/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
-export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
-export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa22 } from './ink-bar';
+export { MatTabBase as ɵf22, _MatTabMixinBase as ɵg22 } from './tab';
+export { MatTabHeaderBase as ɵb22, _MatTabHeaderMixinBase as ɵc22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd22, _MatTabLabelWrapperMixinBase as ɵe22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj22, MatTabNavBase as ɵh22, _MatTabLinkMixinBase as ɵk22, _MatTabNavMixinBase as ɵi22 } from './tab-nav-bar/tab-nav-bar';
