3b99054e4b464c43b9fe2eb75c0e5854bb4656f5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-09 19:20:15 +0000

    master - 41bb296 build: remove @angular/upgrade from dev dependencies (#14068)

--- esm5/grid-list.es5.js
+++ esm5/grid-list.es5.js
@@ -1166,5 +1166,5 @@ var MatGridListModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 
-export { MatGridListModule, MatGridList, MatGridTile, MatGridTileText, MatGridAvatarCssMatStyler, MatGridTileHeaderCssMatStyler, MatGridTileFooterCssMatStyler, MAT_GRID_LIST as ɵa2 };
+export { MatGridListModule, MatGridList, MatGridTile, MatGridTileText, MatGridAvatarCssMatStyler, MatGridTileHeaderCssMatStyler, MatGridTileFooterCssMatStyler, MAT_GRID_LIST as ɵa4 };
 //# sourceMappingURL=grid-list.es5.js.map
