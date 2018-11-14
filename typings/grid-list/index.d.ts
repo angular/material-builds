81140dc2cd16920fb61753554a931329e6a1e45d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-14 22:44:05 +0000

    master - 09f7578 build: validate CDK public API using ts-api-guardian (#14131)

--- typings/grid-list/index.d.ts
+++ typings/grid-list/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_GRID_LIST as ɵa5, MatGridListBase as ɵb5 } from './grid-list-base';
+export { MAT_GRID_LIST as ɵa2, MatGridListBase as ɵb2 } from './grid-list-base';
