773d9dcc4b609e1bbd5dc3073e0aee48442d02a2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-14 22:44:21 +0000

    master - dfcba2b build: add tslib to ts_library rules by default (#14128)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -34596,10 +34596,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
 /** *
  * Current version of Angular Material.
   @type {?} */
-var VERSION = new core.Version('7.0.3-09f7578');
+var VERSION = new core.Version('7.0.3-dfcba2b');
 
 exports.VERSION = VERSION;
-exports.ɵa29 = MatAutocompleteOrigin;
+exports.ɵa30 = MatAutocompleteOrigin;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
