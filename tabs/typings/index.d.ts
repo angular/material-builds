2909aa76fe229307eba2170b4e89f36a1b26f85b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-11-27 00:55:57 +0000

    master - fccbfb5 build: remove windows runfile workaround (#14173)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
-export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
-export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa21 } from './ink-bar';
+export { MatTabBase as ɵf21, _MatTabMixinBase as ɵg21 } from './tab';
+export { MatTabHeaderBase as ɵb21, _MatTabHeaderMixinBase as ɵc21 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd21, _MatTabLabelWrapperMixinBase as ɵe21 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj21, MatTabNavBase as ɵh21, _MatTabLinkMixinBase as ɵk21, _MatTabNavMixinBase as ɵi21 } from './tab-nav-bar/tab-nav-bar';
