0b2c3e8231833736d739315054bdb29f434ccdd9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-11-27 15:47:58 +0000

    master - db7491c fix(drag-drop): only add top-level drop lists to drop group (#14130)

--- typings/tabs/index.d.ts
+++ typings/tabs/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa23 } from './ink-bar';
-export { MatTabBase as ɵf23, _MatTabMixinBase as ɵg23 } from './tab';
-export { MatTabHeaderBase as ɵb23, _MatTabHeaderMixinBase as ɵc23 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd23, _MatTabLabelWrapperMixinBase as ɵe23 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj23, MatTabNavBase as ɵh23, _MatTabLinkMixinBase as ɵk23, _MatTabNavMixinBase as ɵi23 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa22 } from './ink-bar';
+export { MatTabBase as ɵf22, _MatTabMixinBase as ɵg22 } from './tab';
+export { MatTabHeaderBase as ɵb22, _MatTabHeaderMixinBase as ɵc22 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd22, _MatTabLabelWrapperMixinBase as ɵe22 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj22, MatTabNavBase as ɵh22, _MatTabLinkMixinBase as ɵk22, _MatTabNavMixinBase as ɵi22 } from './tab-nav-bar/tab-nav-bar';
