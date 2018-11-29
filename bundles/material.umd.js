0c05d55223955cdf0cf6ff2cf0ce3fafdc238374
Gabriel Caruana <gabb1995@gmail.com>
2018-11-29 15:48:07 +0000

    master - 6347ace docs(table): add toggling to expandable rows example (#13897)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36236,7 +36236,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-4a7cb2b');
+var VERSION = new core.Version('7.1.0-6347ace');
 
 exports.VERSION = VERSION;
 exports.ɵa29 = MatAutocompleteOrigin;
@@ -36448,7 +36448,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa2 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
