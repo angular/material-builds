029ff2270ac88eb138511666822020c38462a7bf
Jeremy Elbourn <jelbourn@gmail.com>
2018-12-03 21:46:16 +0000

    master - db155e4 chore: tweak wording for changelog message in stage-release script (#14374)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36236,7 +36236,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.0-903a884');
+var VERSION = new core.Version('7.1.0-db155e4');
 
 exports.VERSION = VERSION;
 exports.ɵa30 = MatAutocompleteOrigin;
@@ -36448,7 +36448,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
