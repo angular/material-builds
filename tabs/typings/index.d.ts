7f4620ab99b0e1a1a62f47af47ccaf420c539b49
Jeremy Elbourn <jelbourn@gmail.com>
2018-12-03 21:51:13 +0000

    master - c28a91e chore: bump version w/ changelog for master (#14372)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa23 } from './ink-bar';
-export { MatTabBase as ɵf23, _MatTabMixinBase as ɵg23 } from './tab';
-export { MatTabHeaderBase as ɵb23, _MatTabHeaderMixinBase as ɵc23 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd23, _MatTabLabelWrapperMixinBase as ɵe23 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj23, MatTabNavBase as ɵh23, _MatTabLinkMixinBase as ɵk23, _MatTabNavMixinBase as ɵi23 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
+export { MatTabBase as ɵf24, _MatTabMixinBase as ɵg24 } from './tab';
+export { MatTabHeaderBase as ɵb24, _MatTabHeaderMixinBase as ɵc24 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd24, _MatTabLabelWrapperMixinBase as ɵe24 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj24, MatTabNavBase as ɵh24, _MatTabLinkMixinBase as ɵk24, _MatTabNavMixinBase as ɵi24 } from './tab-nav-bar/tab-nav-bar';
