f2216e05450e8e1dae65db35bd86883290b08fa0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-12-04 21:42:40 +0000

    master - 0cacbd6 build: cronjob cannot install angular snapshot builds (#14332)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36238,10 +36238,10 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.1.1-e9a890a');
+var VERSION = new core.Version('7.1.1-0cacbd6');
 
 exports.VERSION = VERSION;
-exports.ɵa29 = MatAutocompleteOrigin;
+exports.ɵa28 = MatAutocompleteOrigin;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
 exports.MatAutocompleteSelectedEvent = MatAutocompleteSelectedEvent;
 exports.MatAutocompleteBase = MatAutocompleteBase;
