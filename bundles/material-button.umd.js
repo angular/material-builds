f5f45434a12151e30d804229ff4b1425d2fbb540
vivian-hu <41269630+vivian-hu@users.noreply.github.com>
2018-12-12 23:55:24 +0000

    revert-13298-snack-bar-multi-announcement - 039dd7d Revert "fix(snack-bar): announcing same message twice to screen readers (#13298)"

--- bundles/material-button.umd.js
+++ bundles/material-button.umd.js
@@ -85,13 +85,9 @@ var _MatButtonMixinBase = core$1.mixinColor(core$1.mixinDisabled(core$1.mixinDis
  */
 var MatButton = /** @class */ (function (_super) {
     __extends(MatButton, _super);
-    function MatButton(elementRef, 
-    /**
-     * @deprecated Platform checks for SSR are no longer needed
-     * @breaking-change 8.0.0
-     */
-    _platform, _focusMonitor, _animationMode) {
+    function MatButton(elementRef, _platform, _focusMonitor, _animationMode) {
         var _this = _super.call(this, elementRef) || this;
+        _this._platform = _platform;
         _this._focusMonitor = _focusMonitor;
         _this._animationMode = _animationMode;
         /**
