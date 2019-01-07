3c3039df1cacb7fbe1b5dcecc062a6362d469979
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-07 04:30:57 +0000

    master - 0fbe6dd build: do not load tslib and moment twice in tests (#14715)

--- bundles/material-tabs.umd.js
+++ bundles/material-tabs.umd.js
@@ -2257,17 +2257,17 @@ exports.MatTabGroupBase = MatTabGroupBase;
 exports._MatTabGroupMixinBase = _MatTabGroupMixinBase;
 exports.MatTabGroup = MatTabGroup;
 exports.matTabsAnimations = matTabsAnimations;
-exports.ɵa22 = _MAT_INK_BAR_POSITIONER_FACTORY;
-exports.ɵf22 = MatTabBase;
-exports.ɵg22 = _MatTabMixinBase;
-exports.ɵb22 = MatTabHeaderBase;
-exports.ɵc22 = _MatTabHeaderMixinBase;
-exports.ɵd22 = MatTabLabelWrapperBase;
-exports.ɵe22 = _MatTabLabelWrapperMixinBase;
-exports.ɵj22 = MatTabLinkBase;
-exports.ɵh22 = MatTabNavBase;
-exports.ɵk22 = _MatTabLinkMixinBase;
-exports.ɵi22 = _MatTabNavMixinBase;
+exports.ɵa20 = _MAT_INK_BAR_POSITIONER_FACTORY;
+exports.ɵf20 = MatTabBase;
+exports.ɵg20 = _MatTabMixinBase;
+exports.ɵb20 = MatTabHeaderBase;
+exports.ɵc20 = _MatTabHeaderMixinBase;
+exports.ɵd20 = MatTabLabelWrapperBase;
+exports.ɵe20 = _MatTabLabelWrapperMixinBase;
+exports.ɵj20 = MatTabLinkBase;
+exports.ɵh20 = MatTabNavBase;
+exports.ɵk20 = _MatTabLinkMixinBase;
+exports.ɵi20 = _MatTabNavMixinBase;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa22 } from './ink-bar';
-export { MatTabBase as ɵf22, _MatTabMixinBase as ɵg22 } from './tab';
-export { MatTabHeaderBase as ɵb22, _MatTabHeaderMixinBase as ɵc22 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd22, _MatTabLabelWrapperMixinBase as ɵe22 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj22, MatTabNavBase as ɵh22, _MatTabLinkMixinBase as ɵk22, _MatTabNavMixinBase as ɵi22 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa20 } from './ink-bar';
+export { MatTabBase as ɵf20, _MatTabMixinBase as ɵg20 } from './tab';
+export { MatTabHeaderBase as ɵb20, _MatTabHeaderMixinBase as ɵc20 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd20, _MatTabLabelWrapperMixinBase as ɵe20 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj20, MatTabNavBase as ɵh20, _MatTabLinkMixinBase as ɵk20, _MatTabNavMixinBase as ɵi20 } from './tab-nav-bar/tab-nav-bar';
