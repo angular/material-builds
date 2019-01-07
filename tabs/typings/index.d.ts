9d7772a8829dbdb121bced967db3dbb301f18c8a
mmalerba <mmalerba@google.com>
2019-01-07 23:13:59 +0000

    master - 22b0ad6 chore: bump version to 7.2.1 w/ changelog (#14748)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,8 +2,8 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa20 } from './ink-bar';
-export { MatTabBase as ɵf20, _MatTabMixinBase as ɵg20 } from './tab';
-export { MatTabHeaderBase as ɵb20, _MatTabHeaderMixinBase as ɵc20 } from './tab-header';
-export { MatTabLabelWrapperBase as ɵd20, _MatTabLabelWrapperMixinBase as ɵe20 } from './tab-label-wrapper';
-export { MatTabLinkBase as ɵj20, MatTabNavBase as ɵh20, _MatTabLinkMixinBase as ɵk20, _MatTabNavMixinBase as ɵi20 } from './tab-nav-bar/tab-nav-bar';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa23 } from './ink-bar';
+export { MatTabBase as ɵf23, _MatTabMixinBase as ɵg23 } from './tab';
+export { MatTabHeaderBase as ɵb23, _MatTabHeaderMixinBase as ɵc23 } from './tab-header';
+export { MatTabLabelWrapperBase as ɵd23, _MatTabLabelWrapperMixinBase as ɵe23 } from './tab-label-wrapper';
+export { MatTabLinkBase as ɵj23, MatTabNavBase as ɵh23, _MatTabLinkMixinBase as ɵk23, _MatTabNavMixinBase as ɵi23 } from './tab-nav-bar/tab-nav-bar';
