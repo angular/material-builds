d3506516a094596265f888d2fcc1c600d29691a9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-22 17:56:00 +0000

    7.2.x - 524f83e build: allow recovering from failed publish script (#14906)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36529,7 +36529,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.2.2-730b0a3');
+var VERSION = new core.Version('7.2.2-524f83e');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -36742,7 +36742,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa7 = MAT_GRID_LIST;
+exports.ɵa2 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
