337426456f58c003c0804a338b8234b0e4ba4979
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-23 19:25:03 +0000

    master - f0708b4 build: add missing bazel action names (#14910)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36538,7 +36538,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.2.1-e1b9c2d');
+var VERSION = new core.Version('7.2.1-f0708b4');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -36751,7 +36751,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa6 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
