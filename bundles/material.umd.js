c4eef83852b3f44415d7f25c1411f3fbf12fde32
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-12 15:35:20 +0000

    master - 0d054db fix(layout): breakpoint observer completing unrelated subscribers when preceding subscriber unsubscribes (#14988)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36721,7 +36721,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.2-312cd9f');
+var VERSION = new core.Version('7.3.2-0d054db');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -36934,7 +36934,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
