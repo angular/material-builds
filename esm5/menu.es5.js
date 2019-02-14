4b25ae8a258eee347e3f5614a13f47c4ae277794
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-02-14 18:04:24 +0000

    master - e2c9873 feat(a11y): add injection token configure default politeness and duration (#15126)

--- esm5/menu.es5.js
+++ esm5/menu.es5.js
@@ -1808,5 +1808,5 @@ var MatMenuModule = /** @class */ (function () {
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa14, MatMenuItemBase as ɵb14, _MatMenuItemMixinBase as ɵc14, MAT_MENU_PANEL as ɵf14, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd14, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe14 };
+export { MAT_MENU_SCROLL_STRATEGY, MatMenuModule, MatMenu, MAT_MENU_DEFAULT_OPTIONS, MatMenuItem, MatMenuTrigger, matMenuAnimations, fadeInItems, transformMenu, MatMenuContent, MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa23, MatMenuItemBase as ɵb23, _MatMenuItemMixinBase as ɵc23, MAT_MENU_PANEL as ɵf23, MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵd23, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵe23 };
 //# sourceMappingURL=menu.es5.js.map
