80d5faacc2d0512999bbc532008161afd97f0325
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-02-21 22:25:42 +0000

    master - 1b1a569 test(table): remove redundant test (#15273)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36767,7 +36767,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-140c4e0');
+var VERSION = new core.Version('7.3.3-1b1a569');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -36980,7 +36980,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa3 = MAT_GRID_LIST;
+exports.ɵa4 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
