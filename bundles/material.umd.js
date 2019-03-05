dc5907cab9e46cc699efb5f62f54589c06b479be
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-05 15:11:39 +0000

    master - 1c503ff fix(datepicker): toggle throwing an error if datepicker is not defined on init (#15256)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -15774,7 +15774,10 @@ var MatDatepickerToggle = /** @class */ (function () {
          * @return {?}
          */
         function () {
-            return this._disabled === undefined ? this.datepicker.disabled : !!this._disabled;
+            if (this._disabled === undefined && this.datepicker) {
+                return this.datepicker.disabled;
+            }
+            return !!this._disabled;
         },
         set: /**
          * @param {?} value
@@ -36849,7 +36852,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-3607677');
+var VERSION = new core.Version('7.3.3-1c503ff');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -37109,12 +37112,12 @@ exports.MAT_SELECTION_LIST_VALUE_ACCESSOR = MAT_SELECTION_LIST_VALUE_ACCESSOR;
 exports.MatSelectionListChange = MatSelectionListChange;
 exports.MatListOption = MatListOption;
 exports.MatSelectionList = MatSelectionList;
-exports.ɵa22 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
-exports.ɵb22 = MatMenuItemBase;
-exports.ɵc22 = _MatMenuItemMixinBase;
-exports.ɵf22 = MAT_MENU_PANEL;
-exports.ɵd22 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
-exports.ɵe22 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
+exports.ɵa23 = MAT_MENU_DEFAULT_OPTIONS_FACTORY;
+exports.ɵb23 = MatMenuItemBase;
+exports.ɵc23 = _MatMenuItemMixinBase;
+exports.ɵf23 = MAT_MENU_PANEL;
+exports.ɵd23 = MAT_MENU_SCROLL_STRATEGY_FACTORY;
+exports.ɵe23 = MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER;
 exports.MAT_MENU_SCROLL_STRATEGY = MAT_MENU_SCROLL_STRATEGY;
 exports.MatMenuModule = MatMenuModule;
 exports.MatMenu = MatMenu;
