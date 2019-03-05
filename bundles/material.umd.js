1ef7f591e13ed47a15c830911df98c696c580b24
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-05 19:51:49 +0000

    7.3.x - 173a682 refactor(stepper): remove dependency on @angular/forms (#15134)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36786,7 +36786,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.3-824aad2');
+var VERSION = new core.Version('7.3.3-173a682');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -36999,7 +36999,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa3 = MAT_GRID_LIST;
+exports.ɵa6 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
