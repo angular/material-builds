fa63e5bc112f4ad387ad41b23c703e2247f89321
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-11 17:48:17 +0000

    master - c2f3d48 build: throw error if example is missing a title (#15430)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36918,7 +36918,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.4-7a2565d');
+var VERSION = new core.Version('7.3.4-c2f3d48');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -37131,7 +37131,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa4 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
