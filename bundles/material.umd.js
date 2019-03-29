54e11f04b07a9af8badcace80a41ce6d004b1cb0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-03-29 22:13:57 +0000

    master - b72c1b7 chore(drag-drop): fix incorrect property description (#15656)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36964,7 +36964,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.6-03fb711');
+var VERSION = new core.Version('7.3.6-b72c1b7');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -37177,7 +37177,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa5 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
