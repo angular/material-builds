461093ee224f9ffcd335a190591861e21c5a1859
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-02 16:08:52 +0000

    master - 11839a5 build: add validation for prebuilt cdk css (#15588)

--- esm2015/grid-list.js
+++ esm2015/grid-list.js
@@ -865,5 +865,5 @@ MatGridListModule.decorators = [
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { MatGridListModule, MatGridList, MatGridTile, MatGridTileText, MatGridAvatarCssMatStyler, MatGridTileHeaderCssMatStyler, MatGridTileFooterCssMatStyler, MAT_GRID_LIST as ɵa4 };
+export { MatGridListModule, MatGridList, MatGridTile, MatGridTileText, MatGridAvatarCssMatStyler, MatGridTileHeaderCssMatStyler, MatGridTileFooterCssMatStyler, MAT_GRID_LIST as ɵa3 };
 //# sourceMappingURL=grid-list.js.map
