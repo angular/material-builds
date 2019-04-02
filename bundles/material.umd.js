c7aba6d3031d6799239f78b7032b7bccb79aabf8
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-04-02 16:09:56 +0000

    master - 472cccd docs(layout): fix breakpoint url to material.io (#15688)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -36987,7 +36987,7 @@ MatTreeNestedDataSource = /** @class */ (function (_super) {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('7.3.6-11839a5');
+var VERSION = new core.Version('7.3.6-472cccd');
 
 exports.VERSION = VERSION;
 exports.MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY = MAT_AUTOCOMPLETE_DEFAULT_OPTIONS_FACTORY;
@@ -37200,7 +37200,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa3 = MAT_GRID_LIST;
+exports.ɵa5 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
