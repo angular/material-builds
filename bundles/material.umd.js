7cfac995a7bbf271335dcdbd27fb82015040e4c3
mmalerba <mmalerba@google.com>
2019-04-25 23:34:44 +0000

    master - 35fece8 ci: fix snapshots (#15908)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-beta.2-7469c09');
+var VERSION = new core.Version('8.0.0-beta.2-35fece8');
 
 /**
  * @fileoverview added by tsickle
@@ -37311,7 +37311,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa5 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
