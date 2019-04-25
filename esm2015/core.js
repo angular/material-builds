c65624926192df7bf8370a26fa8ffc16f43f2c32
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-04-25 22:37:08 +0000

    master - 7469c09 chore: minor prototype fixes (#15907)

--- esm2015/core.js
+++ esm2015/core.js
@@ -25,7 +25,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2-b730ba8');
+const VERSION = new Version('8.0.0-beta.2-7469c09');
 
 /**
  * @fileoverview added by tsickle
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-beta.2-b730ba8');
+var VERSION = new Version('8.0.0-beta.2-7469c09');
 
 /**
  * @fileoverview added by tsickle
