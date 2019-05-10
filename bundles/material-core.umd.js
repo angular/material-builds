db3bbb3f734930e9a2cc640d6046154cf3958820
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-10 20:55:42 +0000

    master - 7774ae1 fix(popover-edit): direction not being passed to overlay (#15951)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-cbad345');
+var VERSION = new core.Version('8.0.0-rc.0-7774ae1');
 
 /**
  * @fileoverview added by tsickle
