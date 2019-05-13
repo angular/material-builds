ff21e517e4d032e34bc2cd011573f08434725368
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-05-13 04:52:12 +0000

    master - e630bd6 fix(drag-drop): allow for element in DropListRef to be changed (#15091)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.0-rc.0-f7c53d6');
+var VERSION = new core.Version('8.0.0-rc.0-e630bd6');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/core.js
+++ esm2015/core.js
@@ -25,7 +25,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0-f7c53d6');
+const VERSION = new Version('8.0.0-rc.0-e630bd6');
 
 /**
  * @fileoverview added by tsickle
