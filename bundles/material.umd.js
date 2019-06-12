91b8b3b4fd937cf8ed05217f0bfb95757608430d
Joey Perrott <josephperrott.github@gmail.com>
2019-06-12 21:52:56 +0000

    master - 69b1b2e fix(popover-edit): remove fallthroughs in switch statement (#16281)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.0.1-c3ff175');
+var VERSION$1 = new core.Version('8.0.1-69b1b2e');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.0.1-c3ff175');
+var VERSION$2 = new core.Version('8.0.1-69b1b2e');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39199,7 +39199,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
