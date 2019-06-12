067021659f489914e0ac5269b2bd87b4a859f375
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-06-12 17:44:51 +0000

    master - 57b507b build: update yarn to v1.16.0 (#16260)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.0.1-e050b64');
+var VERSION$1 = new core.Version('8.0.1-57b507b');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.0.1-e050b64');
+var VERSION$2 = new core.Version('8.0.1-57b507b');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39199,7 +39199,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
