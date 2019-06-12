55275a42c1374940df3e7e0ee99f4b068dd6936f
Joey Perrott <josephperrott.github@gmail.com>
2019-06-12 22:20:15 +0000

    master - 933d44c chore: enable noFallthroughCasesInSwitch in the project's tsconfigs (#16282)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.0.1-69b1b2e');
+var VERSION$1 = new core.Version('8.0.1-933d44c');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.0.1-69b1b2e');
+var VERSION$2 = new core.Version('8.0.1-933d44c');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
