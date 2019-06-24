c1d62a4fd603fb64ebb90f8ef80781996b8c3ab4
Joey Perrott <josephperrott.github@gmail.com>
2019-06-24 21:10:53 +0000

    master - d511fbea3 build: setup RBE caching and remote build event service on CI (#16370)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.0.1-43a01e99c');
+var VERSION$1 = new Version('8.0.1-d511fbea3');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.0.1-43a01e99c');
+var VERSION$2 = new Version('8.0.1-d511fbea3');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
