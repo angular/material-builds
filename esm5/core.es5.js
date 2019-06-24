75e2d0292abfeea500e5de92e761747fa09bc45b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-06-24 15:58:09 +0000

    master - a1372e5ae chore: bump linter versions (#16365)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.0.1-10370ef65');
+var VERSION$1 = new Version('8.0.1-a1372e5ae');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.0.1-10370ef65');
+var VERSION$2 = new Version('8.0.1-a1372e5ae');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
