3c6bebbad1a15bad95cd3c4ce81eae63b0feccd1
Joey Perrott <josephperrott.github@gmail.com>
2019-06-26 16:02:25 +0000

    master - 2bf06125a build: run CI unit tests using bazel (#16379)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.0.1-3537e978c');
+const VERSION$1 = new Version('8.0.1-2bf06125a');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.0.1-3537e978c');
+const VERSION$2 = new Version('8.0.1-2bf06125a');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
