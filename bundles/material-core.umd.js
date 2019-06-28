d89f291b54246ecc78671dcf15325bf024cf1d03
Vanessa Schmitt <vschmitt@google.com>
2019-06-28 23:28:49 +0000

    master - d29df3813 feat(material-experimental/chips): add grid keyboard shortcuts (#16384)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.0.1-aa2236820');
+var VERSION$1 = new core.Version('8.0.1-d29df3813');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.0.1-aa2236820');
+var VERSION$2 = new core.Version('8.0.1-d29df3813');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.0.1-aa2236820');
+const VERSION$1 = new Version('8.0.1-d29df3813');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.0.1-aa2236820');
+const VERSION$2 = new Version('8.0.1-d29df3813');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
