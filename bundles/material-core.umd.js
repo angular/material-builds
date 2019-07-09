146dfca9122a1b5df3e6c5e69f35d528871a8423
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-07-09 15:16:59 +0000

    master - 8e975eeb6 fix(material-experimental/button): buttons not outlined in high contrast mode (#16410)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.0.2-0a4b5180d');
+var VERSION$1 = new core.Version('8.0.2-8e975eeb6');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.0.2-0a4b5180d');
+var VERSION$2 = new core.Version('8.0.2-8e975eeb6');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
