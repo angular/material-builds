c3048cb70d6d47c78f52a43c813f256821749bb8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-07-09 15:09:12 +0000

    master - 60d570bb9 build: api-docs can display incorrect module for entry-point (#16470)

--- tabs/typings/index.d.ts
+++ tabs/typings/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa23 } from './ink-bar';
-export { MatPaginatedTabHeader as ɵb23 } from './paginated-tab-header';
+export { _MAT_INK_BAR_POSITIONER_FACTORY as ɵa24 } from './ink-bar';
+export { MatPaginatedTabHeader as ɵb24 } from './paginated-tab-header';
