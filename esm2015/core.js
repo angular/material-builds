92eac0d932528ff4e1620947a0640f3716e9af74
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-07-11 18:59:12 +0000

    master - b60e070eb build: do not build e2e tests in unit tests job

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.0.2-fe266149a');
+const VERSION$1 = new Version('8.0.2-b60e070eb');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.0.2-fe266149a');
+const VERSION$2 = new Version('8.0.2-b60e070eb');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
