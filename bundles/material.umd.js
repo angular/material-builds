f470bbed450d2e0fb4e2a08bb8d4d118db706e16
Jeremy Elbourn <jelbourn@google.com>
2019-07-16 19:26:36 +0000

    master - 6aa418d4f chore: bump version to 8.1.1 w/ changelog (#16539) (#16541)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.0-ec8d4303b');
+var VERSION$1 = new core.Version('8.1.1-6aa418d4f');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.0-ec8d4303b');
+var VERSION$2 = new core.Version('8.1.1-6aa418d4f');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39428,7 +39428,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa5 = MAT_GRID_LIST;
+exports.ɵa2 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.0-ec8d4303b');
+var VERSION$1 = new Version('8.1.1-6aa418d4f');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.0-ec8d4303b');
+var VERSION$2 = new Version('8.1.1-6aa418d4f');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
