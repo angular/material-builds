dc2abb928952070173023282098a729491f8dd01
Vanessa Schmitt <vschmitt@google.com>
2019-07-16 17:42:50 +0000

    master - f9ebb262a fix(mdc chips) Don't let MDC set aria-checked attribute on option chips (#16530)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.0-40ad0fcf7');
+var VERSION$1 = new core.Version('8.1.0-f9ebb262a');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.0-40ad0fcf7');
+var VERSION$2 = new core.Version('8.1.0-f9ebb262a');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39428,7 +39428,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa3 = MAT_GRID_LIST;
+exports.ɵa4 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
