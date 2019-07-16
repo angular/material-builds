f3cd31db4ddf1323e764493e1d5b230802e8237a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-07-16 21:56:58 +0000

    master - 74794d4fa build: enable strict null checks for release scripts (#16542)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.1-6aa418d4f');
+var VERSION$1 = new core.Version('8.1.1-74794d4fa');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.1-6aa418d4f');
+var VERSION$2 = new core.Version('8.1.1-74794d4fa');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
