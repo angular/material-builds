063fb47a95797e689bb498bde8557853204eb551
Vanessa Schmitt <vschmitt@google.com>
2019-07-25 00:14:58 +0000

    master - 67532dbbb fix(mdc chips) Fix matChipRemove Expression has changed errors (#16574)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.1-f23a56a90');
+var VERSION$1 = new core.Version('8.1.1-67532dbbb');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.1-f23a56a90');
+var VERSION$2 = new core.Version('8.1.1-67532dbbb');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39577,7 +39577,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa6 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
