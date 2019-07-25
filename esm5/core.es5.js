b390c246d15e3f70bab61c6e3f28b995fe0022d5
Jeremy Elbourn <jelbourn@google.com>
2019-07-25 20:25:32 +0000

    master - 2a04ccfd4 feat(material-experimental): slide-toggle test harnesses (#16545)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.1-615070acb');
+var VERSION$1 = new Version('8.1.1-2a04ccfd4');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.1-615070acb');
+var VERSION$2 = new Version('8.1.1-2a04ccfd4');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
