1a4e71cf12307b49540a4c7507832b746b15d828
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-07-29 23:05:49 +0000

    master - 5c87286bd chore: bump version to 8.1.2 w/ changelog (#16639)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.1-88631b936');
+const VERSION$1 = new Version('8.1.2-5c87286bd');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.1-88631b936');
+const VERSION$2 = new Version('8.1.2-5c87286bd');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
