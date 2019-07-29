9703d15627b2af971394d1d775e8505b3ffceea0
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-07-29 23:03:07 +0000

    8.1.x - 8e6ff7ee0 chore: bump version to 8.1.2 w/ changelog (#16638)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.1-d90a3e3d3');
+var VERSION$1 = new core.Version('8.1.2-8e6ff7ee0');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.1-d90a3e3d3');
+var VERSION$2 = new core.Version('8.1.2-8e6ff7ee0');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
