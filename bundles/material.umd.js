9df79a2a1bc0912417f8d2787369822e2dfabc38
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-01 19:18:30 +0000

    master - 8e321aee7 test(sidenav): refactor flaky tests in Edge (#16625)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.2-b8c9da626');
+var VERSION$1 = new core.Version('8.1.2-8e321aee7');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.2-b8c9da626');
+var VERSION$2 = new core.Version('8.1.2-8e321aee7');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39577,7 +39577,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa4 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
