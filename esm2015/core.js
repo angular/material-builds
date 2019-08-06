bd0d93343712d255835c168a46b829db9553ecc7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-06 19:32:38 +0000

    master - 92d95ef24 fix(material-experimental): typo in harness focus methods (#16673)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.2-725a2b3c5');
+const VERSION$1 = new Version('8.1.2-92d95ef24');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.2-725a2b3c5');
+const VERSION$2 = new Version('8.1.2-92d95ef24');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
