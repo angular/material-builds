cd36b4cad915b538a32962d0b196e7a02e927c43
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-06 19:11:46 +0000

    master - 8b5afcc41 build: add API goldens for portal (#16655)

--- typings/esm5/grid-list/index.d.ts
+++ typings/esm5/grid-list/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_GRID_LIST as ɵa3, MatGridListBase as ɵb3 } from './grid-list-base';
+export { MAT_GRID_LIST as ɵa8, MatGridListBase as ɵb8 } from './grid-list-base';
