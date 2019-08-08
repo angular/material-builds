1c597ede16f0f7a16628bfdce6d027345111d6ad
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-08 19:28:09 +0000

    master - 8ced0e76e build: remove staticQueryRule (#16713)

--- typings/esm5/menu/index.d.ts
+++ typings/esm5/menu/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24 } from './menu';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵb24, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵc24 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa22 } from './menu';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵb22, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵc22 } from './menu-trigger';
