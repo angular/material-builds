6c5826edf627215868faf2d1cb5db27c51ef7a8b
mbehrlich <mattehrlich86@gmail.com>
2019-08-08 22:58:59 +0000

    master - 6416babbb feat(google-maps): add google-map component skeleton (#16623)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.2-917871b42');
+var VERSION$1 = new core.Version('8.1.2-6416babbb');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.2-917871b42');
+var VERSION$2 = new core.Version('8.1.2-6416babbb');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39636,7 +39636,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa5 = MAT_GRID_LIST;
+exports.ɵa8 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
