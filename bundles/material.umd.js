c11914dba9d83de46aa3e1cf0d7cea7aab41a19f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-08 17:52:18 +0000

    master - 93aba14c5 fix(material-experimental/mdc-chips): ripple not being clipped in Safari (#16712)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.2-e6afdbb9d');
+var VERSION$1 = new core.Version('8.1.2-93aba14c5');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.2-e6afdbb9d');
+var VERSION$2 = new core.Version('8.1.2-93aba14c5');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39622,7 +39622,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa5 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
