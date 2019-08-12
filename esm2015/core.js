91ecceb28110fdeee02b7139c4da84249989034a
mmalerba <mmalerba@google.com>
2019-08-12 23:21:21 +0000

    master - 8942602b0 Update MDC to latest alpha (#16724)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.2-097f4335a');
+const VERSION$1 = new Version('8.1.2-8942602b0');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.2-097f4335a');
+const VERSION$2 = new Version('8.1.2-8942602b0');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
