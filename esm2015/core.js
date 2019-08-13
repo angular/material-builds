7c8741b76c76dd5c8cf075ddf669bc5fff6d8635
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-13 19:01:14 +0000

    master - 2cbabf06e feat(material-experimental): add option to filter checkbox har… (#16662)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.2-417adf865');
+const VERSION$1 = new Version('8.1.2-2cbabf06e');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.2-417adf865');
+const VERSION$2 = new Version('8.1.2-2cbabf06e');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
