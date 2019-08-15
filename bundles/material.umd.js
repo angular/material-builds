857d189ceab6eb63601d83cb6fdf88dd086af2b2
mmalerba <mmalerba@google.com>
2019-08-15 00:13:22 +0000

    master - 6fe8c06e9 fix(cdk-experimental/clipboard): add missing tsconfig (#16785)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.3-6e1fe2cca');
+var VERSION$1 = new core.Version('8.1.3-6fe8c06e9');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.3-6e1fe2cca');
+var VERSION$2 = new core.Version('8.1.3-6fe8c06e9');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
@@ -39662,7 +39662,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa5 = MAT_GRID_LIST;
+exports.ɵa8 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.3-6e1fe2cca');
+var VERSION$1 = new Version('8.1.3-6fe8c06e9');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.3-6e1fe2cca');
+var VERSION$2 = new Version('8.1.3-6fe8c06e9');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
