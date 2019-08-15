0d206be43f2c2a09cc0b6e0be84100e6b52027f6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-15 21:13:01 +0000

    master - 7c472f0d0 build: add API goldens for portal (#16708)

--- typings/menu/index.d.ts
+++ typings/menu/index.d.ts
@@ -2,5 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa24 } from './menu';
-export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵb24, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵc24 } from './menu-trigger';
+export { MAT_MENU_DEFAULT_OPTIONS_FACTORY as ɵa23 } from './menu';
+export { MAT_MENU_SCROLL_STRATEGY_FACTORY as ɵb23, MAT_MENU_SCROLL_STRATEGY_FACTORY_PROVIDER as ɵc23 } from './menu-trigger';
