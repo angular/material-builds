32cddce9b647b7cf719ad736991ccfb0b9e7fdb9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-20 17:06:18 +0000

    master - c7919261f build: switch test targets to karma_web_test_suite (#16818)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.3-d5d7db4c8');
+const VERSION$1 = new Version('8.1.3-c7919261f');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.3-d5d7db4c8');
+const VERSION$2 = new Version('8.1.3-c7919261f');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
