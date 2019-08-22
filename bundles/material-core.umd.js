85fd84f6e7965347cd062b4d712a635f3379c801
mmalerba <mmalerba@google.com>
2019-08-22 23:11:51 +0000

    master - 342c15e02 feat(cdk-experimental/testing): Add a public stabilize method to TestElement (#16847)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.3-5194bea42');
+var VERSION$1 = new core.Version('8.1.3-342c15e02');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.3-5194bea42');
+var VERSION$2 = new core.Version('8.1.3-342c15e02');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
