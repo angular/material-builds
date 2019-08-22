ae383dc055d0c1e27955fad86758846a866bdefc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-22 16:16:10 +0000

    master - 2d3e205f8 build: remove unnecessary dependency in dev-app devserver (#16842)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.3-fbf293a16');
+var VERSION$1 = new Version('8.1.3-2d3e205f8');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.3-fbf293a16');
+var VERSION$2 = new Version('8.1.3-2d3e205f8');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
