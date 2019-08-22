0d165a4cbf213653157e763fa40b0f3c834df559
Joey Perrott <josephperrott.github@gmail.com>
2019-08-22 16:02:25 +0000

    master - fbf293a16 fix: correct DevInfra alias (#16846)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.3-2d976a924');
+var VERSION$1 = new Version('8.1.3-fbf293a16');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.3-2d976a924');
+var VERSION$2 = new Version('8.1.3-fbf293a16');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
