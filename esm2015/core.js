d915bc3d5b9bd917f18096bc94089785aa643bb5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-23 21:50:43 +0000

    master - 21228c7dd build: yarn dev-app script should run with ibazel by default (#16856)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.3-ab9a847da');
+const VERSION$1 = new Version('8.1.3-21228c7dd');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.3-ab9a847da');
+const VERSION$2 = new Version('8.1.3-21228c7dd');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
