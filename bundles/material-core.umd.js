eb1f3b5199edb01d026ff3a022747e54aaacc4f7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-08-23 21:49:34 +0000

    master - ab9a847da feat(material-experimental): add test harness for input (#16674)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.3-59cc66fdd');
+var VERSION$1 = new core.Version('8.1.3-ab9a847da');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.3-59cc66fdd');
+var VERSION$2 = new core.Version('8.1.3-ab9a847da');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
