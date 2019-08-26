45a80721bd9ccb4a381812df63af8431ad9dccb8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-08-26 23:22:45 +0000

    master - 62cd6f1f3 refactor(material-experimental): remove hardcoded ripple durations (#16883)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.4-421ce3a3d');
+var VERSION$1 = new Version('8.1.4-62cd6f1f3');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.4-421ce3a3d');
+var VERSION$2 = new Version('8.1.4-62cd6f1f3');
 /**
  * Injection token that configures whether the Material sanity checks are enabled.
  * @type {?}
