929831aad9860b96091215fdbb26155185af3f08
mmalerba <mmalerba@google.com>
2019-08-29 23:28:57 +0000

    master - 784756dad  feat(cdk-experimental/testing): add support for matching selector on TestElement (#16848)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-52c33c76f');
+var VERSION$1 = new core.Version('8.1.4-784756dad');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-52c33c76f');
+var VERSION$2 = new core.Version('8.1.4-784756dad');
 /**
  * \@docs-private
  * @return {?}
--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-52c33c76f');
+var VERSION$1 = new core.Version('8.1.4-784756dad');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-52c33c76f');
+var VERSION$2 = new core.Version('8.1.4-784756dad');
 /**
  * \@docs-private
  * @return {?}
@@ -39728,7 +39728,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa5 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
