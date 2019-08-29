eb05929215c586865d580084edb44e6cd4c2b019
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-08-29 21:19:19 +0000

    master - c860e6064 feat(material-experimental/mdc-button): focused state (#16819)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-55e3d1e20');
+var VERSION$1 = new core.Version('8.1.4-c860e6064');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-55e3d1e20');
+var VERSION$2 = new core.Version('8.1.4-c860e6064');
 /**
  * \@docs-private
  * @return {?}
@@ -39728,7 +39728,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
