544ede3bb30b07e0335c8169bd953a8ca94f5541
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-08-29 23:18:32 +0000

    master - 52c33c76f feat(material-experimental/mdc-chips): add harness skeleton (#16737)

--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.4-9027c6f37');
+var VERSION$1 = new Version('8.1.4-52c33c76f');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.4-9027c6f37');
+var VERSION$2 = new Version('8.1.4-52c33c76f');
 /**
  * \@docs-private
  * @return {?}
