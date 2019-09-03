997a8fa539c1319d5ad38580b9f573f8e50173e0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-03 21:46:21 +0000

    master - ce71a4544 build: convert prerender gulp task to bazel (#16943)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-ff6ee4eea');
+var VERSION$1 = new core.Version('8.1.4-ce71a4544');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-ff6ee4eea');
+var VERSION$2 = new core.Version('8.1.4-ce71a4544');
 /**
  * \@docs-private
  * @return {?}
