f320d13092f98ecf317126db91637aa206a0d055
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-05 20:15:33 +0000

    master - cd0611d71 build: add more linting around async/await (#16976)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-050a027a3');
+var VERSION$1 = new core.Version('8.1.4-cd0611d71');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-050a027a3');
+var VERSION$2 = new core.Version('8.1.4-cd0611d71');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.4-050a027a3');
+var VERSION$1 = new Version('8.1.4-cd0611d71');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.4-050a027a3');
+var VERSION$2 = new Version('8.1.4-cd0611d71');
 /**
  * \@docs-private
  * @return {?}
