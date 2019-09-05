41b2dad29bb859c10f6798421da361981960019a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-09-05 23:41:50 +0000

    master - a4fc77973 docs(material-experimental/button): update class JsDoc (#16988)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-680ed0085');
+var VERSION$1 = new core.Version('8.1.4-a4fc77973');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-680ed0085');
+var VERSION$2 = new core.Version('8.1.4-a4fc77973');
 /**
  * \@docs-private
  * @return {?}
