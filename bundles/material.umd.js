a5aa2333bdbfc55bea80a5ec0a7af51c6c6fbe38
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-09-05 21:54:26 +0000

    master - 7ad684cc6 feat(material-experimental/button): anchor disabled state (#16962)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-c1057547e');
+var VERSION$1 = new core.Version('8.1.4-7ad684cc6');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-c1057547e');
+var VERSION$2 = new core.Version('8.1.4-7ad684cc6');
 /**
  * \@docs-private
  * @return {?}
@@ -39752,7 +39752,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa4 = MAT_GRID_LIST;
+exports.ɵa2 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
