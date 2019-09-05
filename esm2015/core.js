2a82e3f2e378385262a36634058829fefee461a5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-05 20:12:48 +0000

    master - 050a027a3 test: dialog harness tests leak open dialogs (#16983)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.4-f59427156');
+const VERSION$1 = new Version('8.1.4-050a027a3');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.4-f59427156');
+const VERSION$2 = new Version('8.1.4-050a027a3');
 /**
  * \@docs-private
  * @return {?}
