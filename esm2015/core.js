37ecda8a8d1ac24e9c040593dc9914c147517652
Joey Perrott <josephperrott.github@gmail.com>
2019-09-05 21:12:02 +0000

    master - c1057547e ci: move to latest lock-closed commit for github action (#16984)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.4-cd0611d71');
+const VERSION$1 = new Version('8.1.4-c1057547e');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.4-cd0611d71');
+const VERSION$2 = new Version('8.1.4-c1057547e');
 /**
  * \@docs-private
  * @return {?}
--- grid-list/typings/index.d.ts
+++ grid-list/typings/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_GRID_LIST as ɵa5, MatGridListBase as ɵb5 } from './grid-list-base';
+export { MAT_GRID_LIST as ɵa4, MatGridListBase as ɵb4 } from './grid-list-base';
