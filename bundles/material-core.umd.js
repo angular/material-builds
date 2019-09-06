5038bedd14b3afca2b338452872f025fcc251d28
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-06 17:46:24 +0000

    master - 64619a922 build: universal-app theme should use mdc-theming and mdc-typography mixins (#16994)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-8f6171d0b');
+var VERSION$1 = new core.Version('8.1.4-64619a922');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-8f6171d0b');
+var VERSION$2 = new core.Version('8.1.4-64619a922');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.1.4-8f6171d0b');
+var VERSION$1 = new Version('8.1.4-64619a922');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.1.4-8f6171d0b');
+var VERSION$2 = new Version('8.1.4-64619a922');
 /**
  * \@docs-private
  * @return {?}
