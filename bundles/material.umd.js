55099ea31a6cef9367a35dec46c70daaf8c0a709
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-09-06 17:53:35 +0000

    master - a1e58631b feat(material-experimental/button): add to universal app (#16997)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-64619a922');
+var VERSION$1 = new core.Version('8.1.4-a1e58631b');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-64619a922');
+var VERSION$2 = new core.Version('8.1.4-a1e58631b');
 /**
  * \@docs-private
  * @return {?}
