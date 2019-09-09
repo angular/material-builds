2a4effb7656809f459ce9ed1266be08b703a7e3f
Wayou Liu <liuwayong@gmail.com>
2019-09-09 14:57:46 +0000

    master - 5fb0d577b fix(button): fix typo in comment (#17001)

--- typings/esm5/grid-list/index.d.ts
+++ typings/esm5/grid-list/index.d.ts
@@ -2,4 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-export { MAT_GRID_LIST as ɵa2, MatGridListBase as ɵb2 } from './grid-list-base';
+export { MAT_GRID_LIST as ɵa8, MatGridListBase as ɵb8 } from './grid-list-base';
