fc5644d870f1471d344daaed4cecf3b03f5a0574
Wayou Liu <liuwayong@gmail.com>
2019-09-09 14:58:20 +0000

    master - 357da7ba2 chore(ripple): fix typo in comment (#17000)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.1.4-5fb0d577b');
+const VERSION$1 = new Version('8.1.4-357da7ba2');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.1.4-5fb0d577b');
+const VERSION$2 = new Version('8.1.4-357da7ba2');
 /**
  * \@docs-private
  * @return {?}
