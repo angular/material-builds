93fd9541e7f15dd2223390aeb457630aaef8bbdf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-09 14:20:30 +0000

    master - dadb3e1c8 fix(progress-spinner): animation node inserted into wrong style root when using ngIf with ShadowDom encapsulation (#16982)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.1.4-c881474d0');
+var VERSION$1 = new core.Version('8.1.4-dadb3e1c8');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.1.4-c881474d0');
+var VERSION$2 = new core.Version('8.1.4-dadb3e1c8');
 /**
  * \@docs-private
  * @return {?}
@@ -39769,7 +39769,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa5 = MAT_GRID_LIST;
+exports.ɵa3 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
