006fa895517ead927a8df936a2deca6cdc39a18e
Michael Prentice <splaktar@gmail.com>
2019-09-10 21:33:46 +0000

    master - b24fdb6e5 docs(youtube-player): fix typo (#17036)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.2.0-f6564cd3c');
+const VERSION$1 = new Version('8.2.0-b24fdb6e5');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.2.0-f6564cd3c');
+const VERSION$2 = new Version('8.2.0-b24fdb6e5');
 /**
  * \@docs-private
  * @return {?}
