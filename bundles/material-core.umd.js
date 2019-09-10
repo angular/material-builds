dd7fcad5a00db2d753ba04641cba4e39ba1862b3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-09-10 21:56:04 +0000

    8.2.x - 9e8e4e679 build: group changelog entries by packages (#17017)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.2.0-f6564cd3c');
+var VERSION$1 = new core.Version('8.2.0-9e8e4e679');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.2.0-f6564cd3c');
+var VERSION$2 = new core.Version('8.2.0-9e8e4e679');
 /**
  * \@docs-private
  * @return {?}
