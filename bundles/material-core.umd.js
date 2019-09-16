d2a0c6884fc807639837585745bab3ef436230a3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-09-16 17:02:40 +0000

    master - 7cf3df762 build: move breaking changes script away from gulp (#17083)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.2.0-59b0ee4f2');
+var VERSION$1 = new core.Version('8.2.0-7cf3df762');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.2.0-59b0ee4f2');
+var VERSION$2 = new core.Version('8.2.0-7cf3df762');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core.es5.js
+++ esm5/core.es5.js
@@ -27,7 +27,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new Version('8.2.0-59b0ee4f2');
+var VERSION$1 = new Version('8.2.0-7cf3df762');
 
 /**
  * @fileoverview added by tsickle
@@ -67,7 +67,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new Version('8.2.0-59b0ee4f2');
+var VERSION$2 = new Version('8.2.0-7cf3df762');
 /**
  * \@docs-private
  * @return {?}
