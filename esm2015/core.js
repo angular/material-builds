85c76be230b423fba449ae6f7c36f25c3f80dc0b
mmalerba <mmalerba@google.com>
2019-09-17 21:27:47 +0000

    master - ef439b409 feat(radio): move test harness out of experimental (#17117)

--- esm2015/core.js
+++ esm2015/core.js
@@ -26,7 +26,7 @@ import { CommonModule } from '@angular/common';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION$1 = new Version('8.2.0-26df0359c');
+const VERSION$1 = new Version('8.2.0-ef439b409');
 
 /**
  * @fileoverview added by tsickle
@@ -60,7 +60,7 @@ AnimationDurations.EXITING = '195ms';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$2 = new Version('8.2.0-26df0359c');
+const VERSION$2 = new Version('8.2.0-ef439b409');
 /**
  * \@docs-private
  * @return {?}
