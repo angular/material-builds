fcaaaea97292ec6ac536690a7d901d8a0f9a7481
mmalerba <mmalerba@google.com>
2019-09-18 20:27:10 +0000

    master - 7b73ea561 cleanup: restructure select harness to follow the same file st… (#17120)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('8.2.0-6a732d324-6a732d3');
+var VERSION = new Version('8.2.0-7b73ea561-7b73ea5');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
