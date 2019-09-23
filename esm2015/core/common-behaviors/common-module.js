c935fe628ac5c78e977b76c5c3c3da1b11da3b98
Nathan Tate <nathantate@google.com>
2019-09-23 22:55:10 +0000

    master - 2112fac64 feat(material-experimental/radio): use MDC DOM structure and styles (#17179)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.0-30de283ad-30de283');
+const VERSION = new Version('8.2.0-2112fac64-2112fac');
 /**
  * \@docs-private
  * @return {?}
--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('8.2.0-30de283ad-30de283');
+var VERSION = new Version('8.2.0-2112fac64-2112fac');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('8.2.0-30de283ad-30de283');
+var VERSION$1 = new Version('8.2.0-2112fac64-2112fac');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
