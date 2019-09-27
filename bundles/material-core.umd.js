eedde01607b42d5ece7d711083ef0f99da5fbd11
Nathan Tate <nathantate@google.com>
2019-09-27 17:50:33 +0000

    master - 8e141efee fix(material-experimental/radio): rename mdc-radio APIs to match existing radio APIs (#17178)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('8.2.0-f084f13ba-f084f13');
+    var VERSION = new i0.Version('8.2.0-8e141efee-8e141ef');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('8.2.0-f084f13ba-f084f13');
+    var VERSION$1 = new i0.Version('8.2.0-8e141efee-8e141ef');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.0-f084f13ba-f084f13');
+const VERSION = new Version('8.2.0-8e141efee-8e141ef');
 /**
  * \@docs-private
  * @return {?}
