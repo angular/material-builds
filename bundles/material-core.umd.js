0d49dac5d6b5773aed357778ec1f5b96417a396b
J Z <906641+jzischkau@users.noreply.github.com>
2019-09-27 16:56:57 +0000

    master - 9bd73d38b docs(material/select) add missing backtick (#17151)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('8.2.0-da14f16af-da14f16');
+    var VERSION = new i0.Version('8.2.0-9bd73d38b-9bd73d3');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('8.2.0-da14f16af-da14f16');
+    var VERSION$1 = new i0.Version('8.2.0-9bd73d38b-9bd73d3');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
