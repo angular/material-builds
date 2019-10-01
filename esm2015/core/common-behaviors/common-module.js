5d9c436ec5e126fb2abf82eb6335444f221a8289
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-01 16:47:10 +0000

    master - f30d26f29 build: tests should not fail if stylesheets contain quotes (#17251)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.0-17f25462b-17f2546');
+const VERSION = new Version('8.2.0-f30d26f29-f30d26f');
 /**
  * \@docs-private
  * @return {?}
