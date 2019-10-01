de207c2c20fe5e177f62f5cc21e51d20c168dc38
Anders Kjær Damgaard <akd@akd.io>
2019-10-01 16:51:28 +0000

    master - 4c6e6ea87 docs(icon): fix unescaped `<img>` tags in overview (#17250)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.0-f30d26f29-f30d26f');
+const VERSION = new Version('8.2.0-4c6e6ea87-4c6e6ea');
 /**
  * \@docs-private
  * @return {?}
