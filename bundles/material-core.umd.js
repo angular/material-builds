1ff76a387fb084c6eb7db993f8276ebd7337de19
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-02 18:12:28 +0000

    master - 7f071c804 chore: bump version to 8.2.2 w/ changelog (#17256)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('8.2.0-sha-3b0f60469');
+    var VERSION = new i0.Version('8.2.2-sha-7f071c804');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('8.2.0-sha-3b0f60469');
+    var VERSION$1 = new i0.Version('8.2.2-sha-7f071c804');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.0-sha-3b0f60469');
+const VERSION = new Version('8.2.2-sha-7f071c804');
 /**
  * \@docs-private
  * @return {?}
