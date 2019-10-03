b67e952897e920143f106182a5d108c50116feaf
Jeremy Elbourn <jelbourn@google.com>
2019-10-03 22:31:06 +0000

    master - 569c149fc chore: bump version to 9.0.0-next.0 w/ changelog (#17288)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.2-sha-29be7686e');
+const VERSION = new Version('9.0.0-next.0-sha-569c149fc');
 /**
  * \@docs-private
  * @return {?}
