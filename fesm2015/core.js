eb69c0b777200e147c3782c91538ec4650ccdd34
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-03 18:24:43 +0000

    master - 4dbf9f636 fix(material-experimental/mdc-button): add ::after to reflect active styles (#17283)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('8.2.2-sha-c4f2174e2');
+const VERSION = new Version('8.2.2-sha-4dbf9f636');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('8.2.2-sha-c4f2174e2');
+const VERSION$1 = new Version('8.2.2-sha-4dbf9f636');
 /**
  * \@docs-private
  * @return {?}
