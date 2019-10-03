1964eb0661c509f1dce0445ad377a2946c7ee51b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-03 23:30:24 +0000

    master - 293388633 build: remove workaround for ivy test job (#16817)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-569c149fc');
+const VERSION = new Version('9.0.0-next.0-sha-293388633');
 /**
  * \@docs-private
  * @return {?}
