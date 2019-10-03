6fce4e5cc707a27b85e6f965bb0a658dfa9e4373
Martin Probst <martin@probst.io>
2019-10-03 05:28:33 +0000

    master - 361d1cf80 chore(drag-drop): Fix compilation in TS3.6. (#17217)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.2-sha-b7ca14b7b');
+const VERSION = new Version('8.2.2-sha-361d1cf80');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('8.2.2-sha-b7ca14b7b');
+const VERSION = new Version('8.2.2-sha-361d1cf80');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('8.2.2-sha-b7ca14b7b');
+const VERSION$1 = new Version('8.2.2-sha-361d1cf80');
 /**
  * \@docs-private
  * @return {?}
