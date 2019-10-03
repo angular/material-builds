78acf59bd3e70a7745744299503ba7482af0772f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-03 19:11:09 +0000

    master - 29be7686e build: re-add theming scss bundle to material package (#17278)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('8.2.2-sha-8669606b0');
+const VERSION = new Version('8.2.2-sha-29be7686e');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('8.2.2-sha-8669606b0');
+var VERSION = new Version('8.2.2-sha-29be7686e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
