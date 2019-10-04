a87a9ef36c9e93c5af570105803c6061619fd1fb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-04 16:22:04 +0000

    master - 711a33e77 build: publish snapshot scripts incorrectly updating unrelated code (#17290)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-31b30fce0');
+const VERSION = new Version('9.0.0-next.0-sha-711a33e77');
 /**
  * \@docs-private
  * @return {?}
