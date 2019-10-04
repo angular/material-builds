dfaedee7b121600c5d35c728a0dc886e6bd8d98f
mmalerba <mmalerba@google.com>
2019-10-04 01:20:05 +0000

    master - ab3b6d2e1 docs(cdk/testing): add guide for using harnesses (#17195)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-next.0-sha-a78edf86f');
+var VERSION = new Version('9.0.0-next.0-sha-ab3b6d2e1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
