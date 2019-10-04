fa2077677ef31563956e457df4331e11740931e6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-04 17:03:17 +0000

    master - 6b3f87e6f build: generate api docs for all tertiary entry-points (#17293)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-next.0-sha-711a33e77');
+var VERSION = new Version('9.0.0-next.0-sha-6b3f87e6f');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
