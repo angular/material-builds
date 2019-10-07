6d411f0474cbeffe6598484b051797b3378e8394
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-07 16:57:12 +0000

    master - 45a43008d build: changelog should not show changes to google-maps (#17313)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-b228f079e');
+const VERSION = new Version('9.0.0-next.0-sha-45a43008d');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-b228f079e');
+const VERSION$1 = new Version('9.0.0-next.0-sha-45a43008d');
 /**
  * \@docs-private
  * @return {?}
