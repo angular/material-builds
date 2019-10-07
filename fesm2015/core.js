8b1edc97eab1bc733c32dbefc56c963e9a212d7f
mbehrlich <mattehrlich86@gmail.com>
2019-10-07 23:52:00 +0000

    master - f50755031 refactor(google-maps): remove duplicate module (#17322)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-45a43008d');
+const VERSION = new Version('9.0.0-next.0-sha-f50755031');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-45a43008d');
+const VERSION$1 = new Version('9.0.0-next.0-sha-f50755031');
 /**
  * \@docs-private
  * @return {?}
