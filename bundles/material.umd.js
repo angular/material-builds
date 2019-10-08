ecba2fae95a82bb5ca6c64956dfbf2ce7bf6a9f9
Miles Malerba <mmalerba@google.com>
2019-10-08 18:38:38 +0000

    8.2.x - 79c6c6ae1 Add button harness test changes from https://github.com/angular/components/commit/f537fbafbdb711f9b237687c1cb2b78ebbe277ce

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -59,7 +59,7 @@ var __assign = function() {
  * Current version of Angular Material.
  * @type {?}
  */
-var VERSION$1 = new core.Version('8.2.2-sha-cf48568d9');
+var VERSION$1 = new core.Version('8.2.2-sha-79c6c6ae1');
 
 /**
  * @fileoverview added by tsickle
@@ -99,7 +99,7 @@ var AnimationDurations = /** @class */ (function () {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-var VERSION$2 = new core.Version('8.2.2-sha-cf48568d9');
+var VERSION$2 = new core.Version('8.2.2-sha-79c6c6ae1');
 /**
  * \@docs-private
  * @return {?}
@@ -40030,7 +40030,7 @@ exports.MatPrefix = MatPrefix;
 exports.MatSuffix = MatSuffix;
 exports.MatLabel = MatLabel;
 exports.matFormFieldAnimations = matFormFieldAnimations;
-exports.ɵa2 = MAT_GRID_LIST;
+exports.ɵa5 = MAT_GRID_LIST;
 exports.MatGridListModule = MatGridListModule;
 exports.MatGridList = MatGridList;
 exports.MatGridTile = MatGridTile;
