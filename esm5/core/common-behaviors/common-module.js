00cd42935f36fed26350fd3de90da8da9f7cd3b2
mmalerba <mmalerba@google.com>
2019-10-09 15:34:16 +0000

    master - a1f59b262 chore: copy 8.2.3 changelog to master (#17335)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-next.0-sha-1a589ce00');
+var VERSION = new Version('9.0.0-next.0-sha-a1f59b262');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
