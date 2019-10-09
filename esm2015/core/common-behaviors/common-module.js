a8071b418b34cba0412aa53dd93caf565b1bfa54
Joey Perrott <josephperrott.github@gmail.com>
2019-10-09 17:50:47 +0000

    master - d3fe81186 build: set up remote-http-caching (#17342)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-664a60647');
+const VERSION = new Version('9.0.0-next.0-sha-d3fe81186');
 /**
  * \@docs-private
  * @return {?}
