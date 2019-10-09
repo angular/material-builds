b85f8872d1dee0152df343386d0e656487f43312
Joey Perrott <josephperrott.github@gmail.com>
2019-10-09 15:58:30 +0000

    master - 664a60647 chore: remove unused branch-manager config (#17343)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-95e19b2e5');
+const VERSION = new Version('9.0.0-next.0-sha-664a60647');
 /**
  * \@docs-private
  * @return {?}
