f2a4ffbc1606d4afa4120e47b5476b3a96046970
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-10 18:55:19 +0000

    master - b836050fd build: reintroduce static query rule (#17360)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-next.0-sha-d3fe81186');
+    var VERSION = new i0.Version('9.0.0-next.0-sha-b836050fd');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-d3fe81186');
+    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-b836050fd');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-next.0-sha-d3fe81186');
+var VERSION = new Version('9.0.0-next.0-sha-b836050fd');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
