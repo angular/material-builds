b0225e6d62b0ba64ffa32fa6aaf2392bcea67bd4
Roy <donroyco@users.noreply.github.com>
2019-10-10 20:48:01 +0000

    master - 885d06a50 docs(dialog): align text with material design (#17344)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-1efd5d3ae');
+const VERSION = new Version('9.0.0-next.0-sha-885d06a50');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-1efd5d3ae');
+const VERSION = new Version('9.0.0-next.0-sha-885d06a50');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-1efd5d3ae');
+const VERSION$1 = new Version('9.0.0-next.0-sha-885d06a50');
 /**
  * \@docs-private
  * @return {?}
