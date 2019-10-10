d9beb843d4618a52bba947a8173cea4220c328fe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-10 19:30:02 +0000

    master - 49e72a9ac build: ensure to never output files next to sources (#17362)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-next.0-sha-b836050fd');
+var VERSION = new Version('9.0.0-next.0-sha-49e72a9ac');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
