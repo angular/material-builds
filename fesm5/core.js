27bd4355d2ab384a8547e73bbbb1dca79722b1c1
Alan Agius <alan.agius4@gmail.com>
2019-10-14 17:27:49 +0000

    master - 41166b7d2 feat: change tslib from direct dependency to peerDependency (#17393)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-next.0-sha-f06597715');
+var VERSION = new Version('9.0.0-next.0-sha-41166b7d2');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-next.0-sha-f06597715');
+var VERSION$1 = new Version('9.0.0-next.0-sha-41166b7d2');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
