358a1e5a382ff062a217509ba5336795af5d6064
Adam <admosity@users.noreply.github.com>
2019-10-16 17:37:48 +0000

    master - 5c752c24a docs(drag-drop): expose DragRef and DragListRef to docs (#17413)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-40665e96e');
+const VERSION = new Version('9.0.0-next.0-sha-5c752c24a');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-40665e96e');
+const VERSION$1 = new Version('9.0.0-next.0-sha-5c752c24a');
 /**
  * \@docs-private
  * @return {?}
