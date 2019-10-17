dd4c6cc3b24a7226a0372b8a37c3fdb02a66bd9e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-17 19:20:50 +0000

    master - e20253102 build: do not show inherited protected members in api docs (#17430)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-818a8c7d6');
+const VERSION = new Version('9.0.0-next.0-sha-e20253102');
 /**
  * \@docs-private
  * @return {?}
