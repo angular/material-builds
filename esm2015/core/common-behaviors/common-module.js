e7f72b30aba87101427ddbeda90c2be905629b17
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-18 17:01:41 +0000

    master - 34e848f36 feat(drag-drop): allow for custom class to be set on preview (#17304)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-f10b22268');
+const VERSION = new Version('9.0.0-next.0-sha-34e848f36');
 /**
  * \@docs-private
  * @return {?}
