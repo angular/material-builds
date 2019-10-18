1fc51663f3f5fbfe1d478dba506b2e195c30912b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-18 16:29:01 +0000

    master - 593a06cbb fix(drag-drop): unable to drop into connected list inside shad… (#17424)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-next.0-sha-939c18df0');
+var VERSION = new Version('9.0.0-next.0-sha-593a06cbb');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-next.0-sha-939c18df0');
+var VERSION$1 = new Version('9.0.0-next.0-sha-593a06cbb');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
