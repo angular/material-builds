d5e2fc762337ba2d2ac5123119e3087d2086bff0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-22 18:16:15 +0000

    master - 4b48b965f build: re-enable release output validation for source maps (#17462)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-af68f57b2');
+const VERSION = new Version('9.0.0-next.0-sha-4b48b965f');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-af68f57b2');
+const VERSION$1 = new Version('9.0.0-next.0-sha-4b48b965f');
 /**
  * \@docs-private
  * @return {?}
--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-next.0-sha-af68f57b2');
+var VERSION = new Version('9.0.0-next.0-sha-4b48b965f');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-next.0-sha-af68f57b2');
+var VERSION$1 = new Version('9.0.0-next.0-sha-4b48b965f');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
