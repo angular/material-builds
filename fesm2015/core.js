a3629463160bf272235ca0f93ffdca8656d860bb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-22 19:02:18 +0000

    master - 305230435 refactor: remove event faking apis from public api (#17382)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-271ab777a');
+const VERSION = new Version('9.0.0-next.0-sha-305230435');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-271ab777a');
+const VERSION$1 = new Version('9.0.0-next.0-sha-305230435');
 /**
  * \@docs-private
  * @return {?}
--- sidenav/public-api.d.ts
+++ sidenav/public-api.d.ts
@@ -6,6 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 export * from './sidenav-module';
-export { throwMatDuplicatedDrawerError, MatDrawerToggleResult, MAT_DRAWER_DEFAULT_AUTOSIZE, MAT_DRAWER_DEFAULT_AUTOSIZE_FACTORY, MatDrawerContent, MatDrawer, MatDrawerContainer, } from './drawer';
+export { throwMatDuplicatedDrawerError, MatDrawerToggleResult, MAT_DRAWER_DEFAULT_AUTOSIZE, MAT_DRAWER_DEFAULT_AUTOSIZE_FACTORY, MatDrawerContent, MatDrawer, MatDrawerContainer, MatDrawerMode, } from './drawer';
 export * from './sidenav';
 export * from './drawer-animations';
