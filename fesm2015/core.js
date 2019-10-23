214a966a23a3c03f081bef93dd15165b6f4a7ad6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-10-23 22:16:16 +0000

    master - cd0a47e20 chore: clean up leftover references to private/testing (#17486)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-d3235c98b');
+const VERSION = new Version('9.0.0-next.0-sha-cd0a47e20');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-d3235c98b');
+const VERSION$1 = new Version('9.0.0-next.0-sha-cd0a47e20');
 /**
  * \@docs-private
  * @return {?}
