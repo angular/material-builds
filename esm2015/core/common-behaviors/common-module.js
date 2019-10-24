fbc2d54e05345534d405b4cbb1106691423b6305
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-24 21:41:05 +0000

    master - 409e656bc fix(material-experimental/mdc-tabs): add `.mdc-tab__ripple` container (#17498)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-646d47fd5');
+const VERSION = new Version('9.0.0-next.0-sha-409e656bc');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-646d47fd5');
+const VERSION = new Version('9.0.0-next.0-sha-409e656bc');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-646d47fd5');
+const VERSION$1 = new Version('9.0.0-next.0-sha-409e656bc');
 /**
  * \@docs-private
  * @return {?}
