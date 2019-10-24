ebc7e248202781bceee78f3816d56555120fab95
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-10-24 18:04:48 +0000

    master - c44aee6c3 build: update mdc version placeholder range (#17496)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-next.0-sha-cd0a47e20');
+    var VERSION = new i0.Version('9.0.0-next.0-sha-c44aee6c3');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-cd0a47e20');
+    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-c44aee6c3');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0-sha-cd0a47e20');
+const VERSION = new Version('9.0.0-next.0-sha-c44aee6c3');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-next.0-sha-cd0a47e20');
+const VERSION$1 = new Version('9.0.0-next.0-sha-c44aee6c3');
 /**
  * \@docs-private
  * @return {?}
