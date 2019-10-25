6d208bd960eb666dbc9d81845d39dc30a0d59f2d
Jan Malchert <25508038+JanMalch@users.noreply.github.com>
2019-10-25 22:57:54 +0000

    master - 0d29f4d38 docs: describe dev-env setup for Windows machines (#17467)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-next.0-sha-e58b84316');
+    var VERSION = new i0.Version('9.0.0-next.0-sha-0d29f4d38');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-e58b84316');
+    var VERSION$1 = new i0.Version('9.0.0-next.0-sha-0d29f4d38');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-next.0-sha-e58b84316');
+const VERSION = new Version('9.0.0-next.0-sha-0d29f4d38');
 /**
  * \@docs-private
  * @return {?}
