c593752d5300a074b3a2ff9212cf7984a6d9b1d4
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-10-28 22:13:51 +0000

    master - d3fd8b323 docs: add package ID prefix to h3's ids (#17527)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-next.1-sha-3fdab10bd');
+    var VERSION = new i0.Version('9.0.0-next.1-sha-d3fd8b323');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-next.1-sha-3fdab10bd');
+    var VERSION$1 = new i0.Version('9.0.0-next.1-sha-d3fd8b323');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
