1d3862ed5723109a8905317bc2007b4a5328e479
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-01 18:02:25 +0000

    master - 031e15f68 docs(chips): chips autocomplete demo separator key codes not working correctly (#17558)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -12,7 +12,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.0-sha-438f7e4f2');
+var VERSION = new Version('9.0.0-rc.0-sha-031e15f68');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
