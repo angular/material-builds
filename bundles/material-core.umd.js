3d64ce69875c150eee0bb61e2fa2a54c4593f4fe
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-01 17:59:15 +0000

    master - 438f7e4f2 build: set up API golden for google-maps package (#17565)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.0-sha-09fe82fb2');
+    var VERSION = new i0.Version('9.0.0-rc.0-sha-438f7e4f2');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.0-sha-09fe82fb2');
+    var VERSION$1 = new i0.Version('9.0.0-rc.0-sha-438f7e4f2');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
