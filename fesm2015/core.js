d8a8a75e4709e78244a2b5f901be0ad8ba9467a3
Victor <victoraugusto16@gmail.com>
2019-11-05 22:55:51 +0000

    master - 4adb2fd40 docs(table): use correct directives for flex table (#17612)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0-sha-47fa62f9d');
+const VERSION = new Version('9.0.0-rc.0-sha-4adb2fd40');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.0-sha-47fa62f9d');
+const VERSION$1 = new Version('9.0.0-rc.0-sha-4adb2fd40');
 /**
  * \@docs-private
  * @return {?}
