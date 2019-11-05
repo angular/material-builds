74f4fb8586a97779a964548518bd750880531d3c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-05 22:55:36 +0000

    master - 47fa62f9d build: update goldens to ngtsc output (#17605)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.0-sha-790b53eba');
+const VERSION = new Version('9.0.0-rc.0-sha-47fa62f9d');
 /**
  * \@docs-private
  * @return {?}
