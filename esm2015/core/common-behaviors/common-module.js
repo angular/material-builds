4585568b14e272836e5c57519289a107a655d924
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-06 13:26:08 +0000

    master - de2342de9 build: set up API golden files for testing targets (#17627)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.0-sha-fcd9bf73c');
+const VERSION = new Version('9.0.0-rc.0-sha-de2342de9');
 /**
  * \@docs-private
  * @return {?}
