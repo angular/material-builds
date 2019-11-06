ffec637c5ef977ce7a0c2a9cc65e6cd9cdbd40c2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-06 20:35:33 +0000

    master - 1255139a3 test: menu test flaky due to missing animation flush (#17624)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.0-sha-ab0095e5b');
+const VERSION = new Version('9.0.0-rc.0-sha-1255139a3');
 /**
  * \@docs-private
  * @return {?}
