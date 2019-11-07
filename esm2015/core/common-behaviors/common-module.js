40b49b3e912cfd4c3fdfd7a1e0bb7268aba9ddff
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-07 19:26:05 +0000

    master - 2e03208b4 build: remove example package workaround for flat module bundle (#17642)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.0-sha-5c92c06b4');
+const VERSION = new Version('9.0.0-rc.0-sha-2e03208b4');
 /**
  * \@docs-private
  * @return {?}
