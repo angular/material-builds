1d5522bc21ad2a35c8aed51a61695b510d22e86d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-08 14:42:42 +0000

    master - b8b0f57c3 build: switch mdc snapshot test to run against master branch (#17652)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -17,7 +17,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.0-sha-d3d885965');
+const VERSION = new Version('9.0.0-rc.0-sha-b8b0f57c3');
 /**
  * \@docs-private
  * @return {?}
