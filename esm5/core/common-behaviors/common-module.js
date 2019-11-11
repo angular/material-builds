9d26cbbe0946aa49a2614dc992af3acf8760442d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-11 20:47:54 +0000

    master - 969d5c25a fix(overlay): maxWidth and maxHeight not applied when using ex… (#17586)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.1-sha-903777fae');
+var VERSION = new Version('9.0.0-rc.1-sha-969d5c25a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
