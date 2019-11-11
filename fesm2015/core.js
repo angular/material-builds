a8a6b4f23d7ddb8cceac02328574cc87a6b731c7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-11 19:35:57 +0000

    master - 1a2befa1e fix(clipboard): namespace copied output name (#17598)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1-sha-39c007413');
+const VERSION = new Version('9.0.0-rc.1-sha-1a2befa1e');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.1-sha-39c007413');
+const VERSION$1 = new Version('9.0.0-rc.1-sha-1a2befa1e');
 /**
  * \@docs-private
  * @return {?}
