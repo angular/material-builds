27f27ef133d0e52eedb1c7402e858749a50af075
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-11 19:23:27 +0000

    master - 1fcea5015 build: remove release packaging gulp code (#17668)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.1-sha-fa18ce172');
+var VERSION = new Version('9.0.0-rc.1-sha-1fcea5015');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
