a2a2388ed3ec49e78fca59c7f195c8c3c47d5a4d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-12 19:50:44 +0000

    master - 49f2ed1a2 test: disambiguate MDC component test names (#17674)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1-sha-320f38749');
+const VERSION = new Version('9.0.0-rc.1-sha-49f2ed1a2');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.1-sha-320f38749');
+const VERSION$1 = new Version('9.0.0-rc.1-sha-49f2ed1a2');
 /**
  * \@docs-private
  * @return {?}
