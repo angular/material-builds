f3f7d71afd04d5ca83ca106c216f8d8607613e01
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-14 20:38:48 +0000

    master - bfa88fdae fix(ng-update): migrations not running for release candidate versions (#17704)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.1-sha-5715a9c2f');
+const VERSION = new Version('9.0.0-rc.1-sha-bfa88fdae');
 /**
  * \@docs-private
  * @return {?}
