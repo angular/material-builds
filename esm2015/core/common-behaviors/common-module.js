853cf3e305dc5cf761b1f97ded300eee123fe969
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-18 18:00:31 +0000

    master - bc58e79f0 build: expand decorators validation rule to cover class members (#17673)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-sha-a5437cd39');
+const VERSION = new Version('9.0.0-rc.2-sha-bc58e79f0');
 /**
  * \@docs-private
  * @return {?}
