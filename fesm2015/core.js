cbf1a0f26b4dd20558e4e6722d2c6baead61a5de
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-18 16:11:24 +0000

    master - d2ea0b8e5 build: Object.assign linting not working (#17733)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.2-sha-37015ace1');
+const VERSION = new Version('9.0.0-rc.2-sha-d2ea0b8e5');
 
 /**
  * @fileoverview added by tsickle
@@ -77,7 +77,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.2-sha-37015ace1');
+const VERSION$1 = new Version('9.0.0-rc.2-sha-d2ea0b8e5');
 /**
  * \@docs-private
  * @return {?}
