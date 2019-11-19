541603618aa0f493250e7c58cb96c9d1a0302fe8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-19 14:57:32 +0000

    master - 56fe79159 build: fix errors in mdc-snackbar build (#17747)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-sha-bc58e79f0');
+const VERSION = new Version('9.0.0-rc.2-sha-56fe79159');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.2-sha-bc58e79f0');
+var VERSION = new Version('9.0.0-rc.2-sha-56fe79159');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
