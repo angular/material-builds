2520fb49cffeb6aae5c8deca9ed7204c47cd3ff0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-20 03:21:59 +0000

    master - aaa5783a1 build: remove unused variable in docs-content publishing script (#17703)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.2-sha-15a5171de');
+const VERSION = new Version('9.0.0-rc.2-sha-aaa5783a1');
 /**
  * \@docs-private
  * @return {?}
