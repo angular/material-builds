a9d1fc7f1eb1f91d06272849b2a330ea23f9d78f
mmalerba <mmalerba@google.com>
2019-11-22 23:37:21 +0000

    master - ddb29606b docs(cdk/testing): add documentation for harness environment authors (#17754)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.3-sha-376606521');
+    var VERSION = new i0.Version('9.0.0-rc.3-sha-ddb29606b');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.3-sha-376606521');
+    var VERSION$1 = new i0.Version('9.0.0-rc.3-sha-ddb29606b');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.3-sha-376606521');
+const VERSION = new Version('9.0.0-rc.3-sha-ddb29606b');
 /**
  * \@docs-private
  * @return {?}
