b0bcd5ac05c8211c1243c7686aea24e67861f029
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-22 00:26:34 +0000

    master - 3f6a1fda9 build: enable bazel node modules symlinking [resubmit w/ fixes] (#17776)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -18,7 +18,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.3-sha-ef85134c0');
+const VERSION = new Version('9.0.0-rc.3-sha-3f6a1fda9');
 /**
  * \@docs-private
  * @return {?}
