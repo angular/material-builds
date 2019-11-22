73b62f258304ccea3899c13f06d8924071bf33e9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-11-22 23:18:48 +0000

    master - a8cff1e27 build: add lint rule to avoid relative cross entry-point imports/exports (#17741)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.3-sha-e02bb8252');
+    var VERSION = new i0.Version('9.0.0-rc.3-sha-a8cff1e27');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.3-sha-e02bb8252');
+    var VERSION$1 = new i0.Version('9.0.0-rc.3-sha-a8cff1e27');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
