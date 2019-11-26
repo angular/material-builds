ac88386614eb1bacb312f6cb01af8727240de225
mmalerba <mmalerba@google.com>
2019-11-26 18:18:08 +0000

    master - d299a31a2 chore(ivy): fix host listener inheritence for mdc-chips and mdc-anchor (#17790)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-rc.4-sha-71165f103');
+var VERSION = new Version('9.0.0-rc.4-sha-d299a31a2');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-rc.4-sha-71165f103');
+var VERSION$1 = new Version('9.0.0-rc.4-sha-d299a31a2');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
