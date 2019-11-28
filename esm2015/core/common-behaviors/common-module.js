6bf5583af1f7fc96a72498cb93a0917f10724d71
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-11-28 15:53:40 +0000

    master - ab0f30df7 fix(youtube-player): clean up internal observables (#17835)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.4-sha-d4a2a0c10');
+const VERSION = new Version('9.0.0-rc.4-sha-ab0f30df7');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.4-sha-d4a2a0c10');
+const VERSION = new Version('9.0.0-rc.4-sha-ab0f30df7');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.4-sha-d4a2a0c10');
+const VERSION$1 = new Version('9.0.0-rc.4-sha-ab0f30df7');
 /**
  * \@docs-private
  * @return {?}
