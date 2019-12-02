fbad7d1b3f79e0d9de2a11c963aafc2791feab00
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-02 21:02:58 +0000

    master - 2d407a257 docs(tree): injectable decorator set on wrong class (#17843)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.4-sha-ab0f30df7');
+const VERSION = new Version('9.0.0-rc.4-sha-2d407a257');
 /**
  * \@docs-private
  * @return {?}
