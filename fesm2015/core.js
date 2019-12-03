b4cd8e3e377709c3f9723d482d25c5e7e7f75c9c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-03 16:10:11 +0000

    master - 017728a3b fix(menu): keyboard controls not working if all items are disabled inside lazy content (#17407)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.4-sha-9b19e6a31');
+const VERSION = new Version('9.0.0-rc.4-sha-017728a3b');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.4-sha-9b19e6a31');
+const VERSION$1 = new Version('9.0.0-rc.4-sha-017728a3b');
 /**
  * \@docs-private
  * @return {?}
