b9cf56e98aa86265b14989295c5766215ca103fa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-03 05:44:00 +0000

    master - 4ab803165 test: clean up usages of deprecated TestBed method (#17846)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.4-sha-7defaa692');
+var VERSION = new Version('9.0.0-rc.4-sha-4ab803165');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
