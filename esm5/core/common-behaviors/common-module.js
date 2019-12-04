123a06f8f51d413ee514089a579b6414fc7bb934
Joey Perrott <josephperrott.github@gmail.com>
2019-12-04 00:01:00 +0000

    master - 03a9c1da7 build: migrate to new --define=angular_ivy_enabled flag (#17792)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.4-sha-f4d0c1809');
+var VERSION = new Version('9.0.0-rc.4-sha-03a9c1da7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
