245dc28e35d4c2460b781e6d8d381a19aeb66385
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-12-10 21:11:14 +0000

    master - c8abc5f9e build: fix mdc snapshot test cronjob (#17909)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.5-sha-6c12ea889');
+const VERSION = new Version('9.0.0-rc.5-sha-c8abc5f9e');
 /**
  * \@docs-private
  * @return {?}
