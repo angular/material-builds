76875996e301d57d95f2f54c4939d32895121cfc
Joey Perrott <josephperrott.github@gmail.com>
2019-12-10 00:55:07 +0000

    master - 2a8d21db6 build: set incompatible_strict_action_env flag to allow caching (#17893)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.5-sha-5da2ae18c');
+const VERSION = new Version('9.0.0-rc.5-sha-2a8d21db6');
 /**
  * \@docs-private
  * @return {?}
