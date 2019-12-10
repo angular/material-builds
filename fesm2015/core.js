d4767b70daa91a922d6fc4be6f4b76cc679b0785
Joakim Zebic <joakim.zebic@gmail.com>
2019-12-10 00:55:45 +0000

    master - e7508adfb fix(material/chips): fix ripple element opacity when using CSS variable theming (#17576)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5-sha-a3e953ee7');
+const VERSION = new Version('9.0.0-rc.5-sha-e7508adfb');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.5-sha-a3e953ee7');
+const VERSION$1 = new Version('9.0.0-rc.5-sha-e7508adfb');
 /**
  * \@docs-private
  * @return {?}
