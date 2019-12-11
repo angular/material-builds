ff9abad310964e2f1407237827c42346babbbed8
mmalerba <mmalerba@google.com>
2019-12-11 17:16:49 +0000

    master - 498b8848e Revert "chore: update MDC to latest canary" (#17932)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.5-sha-e6b638403');
+const VERSION = new Version('9.0.0-rc.5-sha-498b8848e');
 /**
  * \@docs-private
  * @return {?}
