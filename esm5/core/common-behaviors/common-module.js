584735071a43c367bc6b6c7fa00b1eef2ba85d3a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-12-12 22:14:41 +0000

    master - 0753f9e76 fix(material-experimental/mdc-progress-bar): update to latest canary to include adapter changes (#17938)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.5-sha-1cab7df96');
+var VERSION = new Version('9.0.0-rc.5-sha-0753f9e76');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
