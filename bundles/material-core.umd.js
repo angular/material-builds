7a0b7d42c28f0af55135f5829e840080920bfec6
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-12-13 22:14:12 +0000

    master - 887d06305 chore: fix README link to npm (#17964)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.5-sha-af7aa860a');
+    var VERSION = new i0.Version('9.0.0-rc.5-sha-887d06305');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.5-sha-af7aa860a');
+    var VERSION$1 = new i0.Version('9.0.0-rc.5-sha-887d06305');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
