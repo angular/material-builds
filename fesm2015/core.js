ad911dff0ef033d830e3bac5d2b86726609b2e5c
Andrew Seguin <andrewseguin@users.noreply.github.com>
2019-12-19 22:33:00 +0000

    master - abe9e9c67 chore: bump version to 9.0.0-rc.6 w/ changelog (#18011)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5-sha-3299d8c6f');
+const VERSION = new Version('9.0.0-rc.6-sha-abe9e9c67');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.5-sha-3299d8c6f');
+const VERSION$1 = new Version('9.0.0-rc.6-sha-abe9e9c67');
 /**
  * \@docs-private
  * @return {?}
