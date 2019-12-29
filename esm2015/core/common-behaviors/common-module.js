62bfe9c7729e2e573eb246f81d03b2cd9a2e0f07
Michael Prentice <splaktar@gmail.com>
2019-12-29 22:48:03 +0000

    master - 8a42b9890 docs(material/autocomplete): fix exception on page load (#17956)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-c9856eeb5');
+const VERSION = new Version('9.0.0-rc.6-sha-8a42b9890');
 /**
  * \@docs-private
  * @return {?}
