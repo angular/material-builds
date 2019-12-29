a57d14408fcfd8e36d43a54f39047fc3dd1fc874
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:41:32 +0000

    master - c36466c2f fix(scrolling): update virtual scroll viewport size on resize (#18058)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-99af8e9c3');
+const VERSION = new Version('9.0.0-rc.6-sha-c36466c2f');
 /**
  * \@docs-private
  * @return {?}
