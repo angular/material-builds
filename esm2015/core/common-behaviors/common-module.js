6464885165dd705af3c229baca55cb93a5e5c328
Vanessa Schmitt <vschmitt@google.com>
2019-12-29 22:43:28 +0000

    master - 22d7f77e8 fix(mdc-chips): Set aria-required on input instead of grid (#18049)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-c36466c2f');
+const VERSION = new Version('9.0.0-rc.6-sha-22d7f77e8');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.6-sha-c36466c2f');
+var VERSION = new Version('9.0.0-rc.6-sha-22d7f77e8');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
