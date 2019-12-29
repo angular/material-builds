612c0a3ffdd689d47cb94e7b0d93ac9e444ce150
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:44:02 +0000

    master - 17a7bcb8d fix(material-experimental/mdc-chips): avoid potential server-s… (#18044)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-22d7f77e8');
+const VERSION = new Version('9.0.0-rc.6-sha-17a7bcb8d');
 /**
  * \@docs-private
  * @return {?}
