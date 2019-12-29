270ead7dff99349f2d08b003f36d7b0b088eaeeb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2019-12-29 22:47:54 +0000

    master - c9856eeb5 fix(sidenav): move focus into sidenav in side mode if autoFocu… (#17995)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.6-sha-e746895ea');
+var VERSION = new Version('9.0.0-rc.6-sha-c9856eeb5');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
