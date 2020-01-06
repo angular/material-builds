b974aef69f7a6e848eea24b95b69e24e6db08d01
Greg Magolan <gmagolan@gmail.com>
2020-01-06 23:10:23 +0000

    master - ad689cdbb build: update to rules_nodejs 1.0.0 (#18064)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-rc.6-sha-551154816');
+var VERSION = new Version('9.0.0-rc.6-sha-ad689cdbb');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-rc.6-sha-551154816');
+var VERSION$1 = new Version('9.0.0-rc.6-sha-ad689cdbb');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
