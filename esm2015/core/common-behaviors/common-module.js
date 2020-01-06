1087383ede158f7482a226c7f64cc399970f56cf
Michael Prentice <splaktar@gmail.com>
2020-01-06 23:06:26 +0000

    master - 551154816 docs: fix typos in CDK and Material tree examples (#18067)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-ef46a02ba');
+const VERSION = new Version('9.0.0-rc.6-sha-551154816');
 /**
  * \@docs-private
  * @return {?}
