d17ba0004a1db5adc5f1c2bf3c205c8b88a510f1
Raz Luvaton <rluvaton@gmail.com>
2020-01-06 23:11:42 +0000

    master - 0429dfc56 Fix part of #13195 (Size not enforced) (#18028)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.6-sha-f2cbc066a');
+const VERSION = new Version('9.0.0-rc.6-sha-0429dfc56');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.6-sha-f2cbc066a');
+const VERSION$1 = new Version('9.0.0-rc.6-sha-0429dfc56');
 /**
  * \@docs-private
  * @return {?}
