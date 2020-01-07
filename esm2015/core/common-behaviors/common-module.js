0b68693ea9459ab7f3726fad5b06240dd396fe67
Greg Magolan <gmagolan@gmail.com>
2020-01-07 00:01:09 +0000

    master - 9e7ba2512 build: try/catch the @angular/bazel rules_nodejs 1.0.0 patch (#18118)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-0429dfc56');
+const VERSION = new Version('9.0.0-rc.6-sha-9e7ba2512');
 /**
  * \@docs-private
  * @return {?}
