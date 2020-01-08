478323f8782433b599f1f055aa32d906acb73fb1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-08 22:32:42 +0000

    master - 7b7e633d5 fix(material-experimental/mdc-chips): prevent default space an… (#18084)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.6-sha-3b4e4963a');
+var VERSION = new Version('9.0.0-rc.6-sha-7b7e633d5');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
