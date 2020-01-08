9444bd5af9273269b49d4ae3cb93b525d1940196
Filipe Silva <filipematossilva@gmail.com>
2020-01-08 20:08:54 +0000

    master - 9fa5a6eef refactor: default to agnostic ts type to schematics one (#18133)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.6-sha-5e13b52c7');
+const VERSION = new Version('9.0.0-rc.6-sha-9fa5a6eef');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.6-sha-5e13b52c7');
+const VERSION$1 = new Version('9.0.0-rc.6-sha-9fa5a6eef');
 /**
  * \@docs-private
  * @return {?}
