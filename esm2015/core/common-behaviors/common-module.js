6ccc6350a12b11f54a2043cc85881e7bb6cd1c23
Jeremy Elbourn <jelbourn@google.com>
2020-01-08 18:43:14 +0000

    master - 5e13b52c7 docs: update readme for Q1 2020 (#18128)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-636ae5c87');
+const VERSION = new Version('9.0.0-rc.6-sha-5e13b52c7');
 /**
  * \@docs-private
  * @return {?}
