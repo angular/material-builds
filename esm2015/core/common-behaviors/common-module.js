ebf911d87c3d46a69120c25144590ba6f86ca8d8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-08 20:26:15 +0000

    master - d1de6fa26 refactor: remove abstract directives injection workaround (#18109)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-f6cf22fab');
+const VERSION = new Version('9.0.0-rc.6-sha-d1de6fa26');
 /**
  * \@docs-private
  * @return {?}
