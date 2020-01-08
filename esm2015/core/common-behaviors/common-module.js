b3044de061d02f9c163dd045de69398b320e7ab8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-08 20:26:01 +0000

    master - f6cf22fab build: focus trap demo not working on IE/Edge (#18106)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.6-sha-9fa5a6eef');
+const VERSION = new Version('9.0.0-rc.6-sha-f6cf22fab');
 /**
  * \@docs-private
  * @return {?}
