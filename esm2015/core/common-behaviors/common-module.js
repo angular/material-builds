91312c2150c4471656476be2071fc117b771d72c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-10 00:29:45 +0000

    master - a909f81ec build: do not merge interface members with class API members (#18088)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.7-sha-02c24ec85');
+const VERSION = new Version('9.0.0-rc.7-sha-a909f81ec');
 /**
  * \@docs-private
  * @return {?}
