adc751575b6fa2e33c569298f5460957ff8eb31f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-01-11 05:12:29 +0000

    master - 5df091688 docs(cdk): correct code sample for test harnesses (#18153)

--- bundles/material-select.umd.js
+++ bundles/material-select.umd.js
@@ -133,6 +133,8 @@
     function MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY(overlay) {
         return function () { return overlay.scrollStrategies.reposition(); };
     }
+    /** Injection token that can be used to provide the default options the select module. */
+    var MAT_SELECT_CONFIG = new core.InjectionToken('MAT_SELECT_CONFIG');
     /** @docs-private */
     var MAT_SELECT_SCROLL_STRATEGY_PROVIDER = {
         provide: MAT_SELECT_SCROLL_STRATEGY,
@@ -179,7 +181,7 @@
     }());
     var MatSelect = /** @class */ (function (_super) {
         tslib.__extends(MatSelect, _super);
-        function MatSelect(_viewportRuler, _changeDetectorRef, _ngZone, _defaultErrorStateMatcher, elementRef, _dir, _parentForm, _parentFormGroup, _parentFormField, ngControl, tabIndex, scrollStrategyFactory, _liveAnnouncer) {
+        function MatSelect(_viewportRuler, _changeDetectorRef, _ngZone, _defaultErrorStateMatcher, elementRef, _dir, _parentForm, _parentFormGroup, _parentFormField, ngControl, tabIndex, scrollStrategyFactory, _liveAnnouncer, defaults) {
             var _this = _super.call(this, elementRef, _defaultErrorStateMatcher, _parentForm, _parentFormGroup, ngControl) || this;
             _this._viewportRuler = _viewportRuler;
             _this._changeDetectorRef = _changeDetectorRef;
@@ -281,6 +283,14 @@
             _this.tabIndex = parseInt(tabIndex) || 0;
             // Force setter to be called in case id was not specified.
             _this.id = _this.id;
+            if (defaults) {
+                if (defaults.disableOptionCentering != null) {
+                    _this.disableOptionCentering = defaults.disableOptionCentering;
+                }
+                if (defaults.typeaheadDebounceInterval != null) {
+                    _this.typeaheadDebounceInterval = defaults.typeaheadDebounceInterval;
+                }
+            }
             return _this;
         }
         Object.defineProperty(MatSelect.prototype, "focused", {
@@ -1185,7 +1195,8 @@
             { type: forms.NgControl, decorators: [{ type: core.Self }, { type: core.Optional }] },
             { type: String, decorators: [{ type: core.Attribute, args: ['tabindex',] }] },
             { type: undefined, decorators: [{ type: core.Inject, args: [MAT_SELECT_SCROLL_STRATEGY,] }] },
-            { type: a11y.LiveAnnouncer }
+            { type: a11y.LiveAnnouncer },
+            { type: undefined, decorators: [{ type: core.Optional }, { type: core.Inject, args: [MAT_SELECT_CONFIG,] }] }
         ]; };
         MatSelect.propDecorators = {
             trigger: [{ type: core.ViewChild, args: ['trigger',] }],
@@ -1254,6 +1265,7 @@
      * Generated bundle index. Do not edit.
      */
 
+    exports.MAT_SELECT_CONFIG = MAT_SELECT_CONFIG;
     exports.MAT_SELECT_SCROLL_STRATEGY = MAT_SELECT_SCROLL_STRATEGY;
     exports.MAT_SELECT_SCROLL_STRATEGY_PROVIDER = MAT_SELECT_SCROLL_STRATEGY_PROVIDER;
     exports.MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY = MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY;
--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-bfdf3239a";
+export declare const materialVersion = "9.0.0-rc.7-sha-5df091688";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
