2a801de876f48e71d482a08191790136fa5c90a9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-14 20:57:22 +0000

    9.0.x - a7f7e9b82 fix(google-maps): sub-components throwing during server-side rendering (#18059)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.7-sha-b2da5375d');
+const VERSION = new Version('9.0.0-rc.7-sha-a7f7e9b82');
 /**
  * \@docs-private
  * @return {?}
