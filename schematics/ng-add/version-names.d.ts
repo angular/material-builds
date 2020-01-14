30aa525ff0b9327a3c42efbf7123c6d6014c0cc8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-14 20:55:58 +0000

    master - 149669e6e fix(google-maps): sub-components throwing during server-side rendering (#18059)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-71955d2e1";
+export declare const materialVersion = "9.0.0-rc.7-sha-149669e6e";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
