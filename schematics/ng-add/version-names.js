8e5aa4fa2e1f9d70e95c7ffa5297b7ff863cf699
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-01-15 20:05:21 +0000

    master - 500d235e2 fix(material-experimental/mdc-input): input should not inherit parent classnames (#18180)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.7-sha-149669e6e';
+    exports.materialVersion = '9.0.0-rc.7-sha-500d235e2';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
