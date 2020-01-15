95d38dd4409c9343a69d4a3d517cec55f6338b6c
mmalerba <mmalerba@google.com>
2020-01-15 20:16:26 +0000

    master - d550e4ef9 fix(material/form-field): undeprecated legacy and standard appearances (#18144)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-1d9033e97";
+export declare const materialVersion = "9.0.0-rc.7-sha-d550e4ef9";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.7-sha-1d9033e97';
+    exports.materialVersion = '9.0.0-rc.7-sha-d550e4ef9';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
