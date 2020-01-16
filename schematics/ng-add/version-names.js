b12a7a287a39cc3d8ca0bc75b720657dd52995f7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-16 17:29:44 +0000

    master - 9321d4ce6 build: print error if prerender test fails (#18190)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.7-sha-16159b3fb';
+    exports.materialVersion = '9.0.0-rc.7-sha-9321d4ce6';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
