22377f4293a30737afe6a8ba5043af91cdda3b93
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-01-16 22:42:32 +0000

    9.0.x - dfa5f40e6 chore: bump version to 9.0.0-rc.8 w/ changelog (#18199)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.7-sha-9ea569460');
+const VERSION = new Version('9.0.0-rc.8-sha-dfa5f40e6');
 /**
  * \@docs-private
  * @return {?}
