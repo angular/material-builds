6ef46fea9eefb7a301fcd1be3b61aa9fb4b509d6
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-01-16 22:20:04 +0000

    master - 8f62ffe73 fix(material/input): apply static class binding to host (#18196)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.7-sha-3e2e0234e');
+const VERSION = new Version('9.0.0-rc.7-sha-8f62ffe73');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-3e2e0234e";
+export declare const materialVersion = "9.0.0-rc.7-sha-8f62ffe73";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
