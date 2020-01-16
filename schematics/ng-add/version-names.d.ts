03272ab125eff191efe0c1b67376982ec7b6157a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-16 21:42:21 +0000

    master - 3e2e0234e fix(google-maps): internal events run inside NgZone (#18034)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-4405429d9";
+export declare const materialVersion = "9.0.0-rc.7-sha-3e2e0234e";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
