3ccaa23408bd97e5cbdc9d87362d0ae4b56c80f6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-16 22:31:49 +0000

    master - f46d7b33d build: enable strict attribute type checking (#17977)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.7-sha-8f62ffe73');
+const VERSION = new Version('9.0.0-rc.7-sha-f46d7b33d');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.7-sha-8f62ffe73";
+export declare const materialVersion = "9.0.0-rc.7-sha-f46d7b33d";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
