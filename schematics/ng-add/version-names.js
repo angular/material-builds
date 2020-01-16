522f0cb4563ab23781ec018adea3c64d2c822fe9
Michael Prentice <splaktar@gmail.com>
2020-01-16 18:32:48 +0000

    master - 4405429d9 docs(material/table): use a hover color that is consistent with Material Design spec (#18193)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.7-sha-6973145a5';
+    exports.materialVersion = '9.0.0-rc.7-sha-4405429d9';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
