144532e173e78ade40b381428538688d6ff535d9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-17 22:27:27 +0000

    master - 97a7e2bab docs(icon): add note about HttpClientModule requirement (#18070)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-2d866ae90";
+export declare const materialVersion = "9.0.0-rc.8-sha-97a7e2bab";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-2d866ae90';
+    exports.materialVersion = '9.0.0-rc.8-sha-97a7e2bab';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
