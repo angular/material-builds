3dc6c6189a88f38972e6d764acb945039179947e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 22:02:39 +0000

    master - a949db30c fix(drag-drop): prevent dragging selected text with the mouse (#18103)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-5c9f0d6f2";
+export declare const materialVersion = "9.0.0-rc.8-sha-a949db30c";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-5c9f0d6f2';
+    exports.materialVersion = '9.0.0-rc.8-sha-a949db30c';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
