5e2d7c25f953aaec35da12cfbbb3aebf218e0245
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 22:08:10 +0000

    master - c3194314f feat(drag-drop): support scrolling parent elements apart from list and viewport (#18082)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-3cbc94a08');
+const VERSION = new Version('9.0.0-rc.8-sha-c3194314f');
 /**
  * \@docs-private
  * @return {?}
