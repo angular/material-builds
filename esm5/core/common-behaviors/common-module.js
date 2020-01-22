1448375d1a7db4f45725003892a0fd0a77f046b5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 05:41:27 +0000

    9.0.x - 6b3a271c9 fix(youtube-player): memory leak if player is destroyed before it is done initializing (#18046)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.8-sha-8ea430f5e');
+var VERSION = new Version('9.0.0-rc.8-sha-6b3a271c9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
