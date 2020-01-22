49207d7caa0b96a47251cbc30d9c408f25b35c42
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 18:57:13 +0000

    9.0.x - 9086a4b4e fix(datepicker): able to focus disabled datepicker toggle while disabled via click (#18231)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.8-sha-6b3a271c9');
+    var VERSION = new i0.Version('9.0.0-rc.8-sha-9086a4b4e');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.8-sha-6b3a271c9');
+    var VERSION$1 = new i0.Version('9.0.0-rc.8-sha-9086a4b4e');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
