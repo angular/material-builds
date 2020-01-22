ec0e51ea4029fd54b005f5cebf3e55c53f61b415
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-22 02:23:36 +0000

    master - 433fa7d2d build: update rules_sass version to latest (#18242)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-97a7e2bab');
+const VERSION = new Version('9.0.0-rc.8-sha-433fa7d2d');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-97a7e2bab";
+export declare const materialVersion = "9.0.0-rc.8-sha-433fa7d2d";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
