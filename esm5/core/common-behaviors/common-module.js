25d0baec0ee9b73b8629aa4b45775a8c229f7aca
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 02:43:42 +0000

    master - 8f2007f78 fix(youtube-player): not picking up static startSeconds and endSeconds (#18214)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.8-sha-a15473b6a');
+var VERSION = new Version('9.0.0-rc.8-sha-8f2007f78');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
