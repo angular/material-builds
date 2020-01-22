889f21b88ffcec5a67984a87925f484bea4b69a1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-22 02:37:28 +0000

    master - 6df6d17ad build: fix local browser testing target for macos and linux (#18237)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8-sha-706200f08');
+const VERSION = new Version('9.0.0-rc.8-sha-6df6d17ad');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.0-rc.8-sha-706200f08');
+const VERSION$1 = new Version('9.0.0-rc.8-sha-6df6d17ad');
 /**
  * \@docs-private
  * @return {?}
