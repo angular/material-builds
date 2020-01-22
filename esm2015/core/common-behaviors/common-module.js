eb170eb92965be1bbab37701189b720acfbf9afc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-22 05:38:43 +0000

    master - 2c8dca897 fix(youtube-player): memory leak if player is destroyed before it is done initializing (#18046)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-5b8c581dc');
+const VERSION = new Version('9.0.0-rc.8-sha-2c8dca897');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-5b8c581dc';
+    exports.materialVersion = '9.0.0-rc.8-sha-2c8dca897';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
