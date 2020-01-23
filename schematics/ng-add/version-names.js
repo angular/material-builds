4c1633fec7977238bdf533a95d4f5d1b1485c4a9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-23 22:10:23 +0000

    master - 0cd759c2e build: bump to latest Angular RC (#18263)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-39dd21665';
+    exports.materialVersion = '9.0.0-rc.8-sha-0cd759c2e';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
