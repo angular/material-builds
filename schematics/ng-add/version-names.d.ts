b2d7bfb489711b508afc3e1883c6183406e40535
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-25 00:53:54 +0000

    master - 27498ea35 build: run saucelabs and browserstack tests with ivy (#18276)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-ce40ec2dc";
+export declare const materialVersion = "9.0.0-rc.8-sha-27498ea35";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
