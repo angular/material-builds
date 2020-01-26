472685b21b8eddbea4ed3eae5ccb93b075d6af13
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-26 21:53:26 +0000

    master - 27111eb04 feat(material-experimental/mdc-form-field): support theming through feature targeting (#18265)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-f43e3e896');
+const VERSION = new Version('9.0.0-rc.8-sha-27111eb04');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-f43e3e896';
+    exports.materialVersion = '9.0.0-rc.8-sha-27111eb04';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
