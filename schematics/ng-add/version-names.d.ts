e140e5f7f9ba64278fffe4a31db5ef19cff18ee3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-26 22:02:18 +0000

    master - aa2a5b33a build: create script that simplifies bazel testing workflow (#18016)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-0e30214cd";
+export declare const materialVersion = "9.0.0-rc.8-sha-aa2a5b33a";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
