11d00bd8a1c30e0b9dd1e57ff7fdf415c77ff9c9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-01-26 21:34:25 +0000

    master - e37b5e57b build: update to angular v9.0.0-rc.11 (#18283)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-27498ea35';
+    exports.materialVersion = '9.0.0-rc.8-sha-e37b5e57b';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
