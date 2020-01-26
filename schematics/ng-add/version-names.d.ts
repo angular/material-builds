121107baf2ceabfa9a6c210fd13086449e758679
Michael Prentice <splaktar@gmail.com>
2020-01-26 21:57:45 +0000

    master - 0e30214cd docs(examples): all examples should use mat-form-field's fill appearance by default (#18252)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-27111eb04";
+export declare const materialVersion = "9.0.0-rc.8-sha-0e30214cd";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
