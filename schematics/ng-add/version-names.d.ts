055e651d83ec1d7b538479e91aa872bec966ca1f
Karl Seamon <bannanafiend@gmail.com>
2020-01-27 22:26:52 +0000

    master - b163b644d feat(popover-edit): Ability to disable edit on specific cells (#18273)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-efa82637e";
+export declare const materialVersion = "9.0.0-rc.8-sha-b163b644d";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
