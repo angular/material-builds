69a3589836675f37ff87e88901bac153208190d5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-27 06:37:09 +0000

    master - 0760454ae fix(drag-drop): not picking up initial disabled state of handle (#16643)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-d9cf17534';
+    exports.materialVersion = '9.0.0-rc.8-sha-0760454ae';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
