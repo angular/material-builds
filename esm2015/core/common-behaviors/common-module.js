e074488659b19ab2aeb0c62d695ba836e8c7e955
Michael Prentice <splaktar@gmail.com>
2020-01-28 18:08:09 +0000

    master - 6ef58a25d docs(datepicker): convert date module tables to be mobile-friendly (#18210)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-b163b644d');
+const VERSION = new Version('9.0.0-rc.8-sha-6ef58a25d');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-b163b644d';
+    exports.materialVersion = '9.0.0-rc.8-sha-6ef58a25d';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
