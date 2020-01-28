ad693370290394f290e2b93f352528a96eedbbf8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-28 19:47:08 +0000

    master - 290d102a5 fix(schematics): global typography class always being added (#18315)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-a8fde4a96";
+export declare const materialVersion = "9.0.0-rc.8-sha-290d102a5";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
