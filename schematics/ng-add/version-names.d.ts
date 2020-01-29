ce78060bbb1bb751303512c28f5346c7919a0fdc
Jeremy Elbourn <jelbourn@gmail.com>
2020-01-29 06:57:22 +0000

    master - ea171358a fix(cdk/a11y): make cdk-high-contrast work w/ emulated view encapsulation (#18152)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-96f1625f7";
+export declare const materialVersion = "9.0.0-rc.8-sha-ea171358a";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
