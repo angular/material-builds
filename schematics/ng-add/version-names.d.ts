bceb5826f064d69d82959a68644334e2151e2f86
Olivier Suritz <osuritz@google.com>
2020-01-29 15:49:48 +0000

    master - 27ef8425f fix(material/icon/testing): Adds size specs to fake icon (#18306)

--- schematics/ng-add/version-names.d.ts
+++ schematics/ng-add/version-names.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/material/schematics/ng-add/version-names" />
 /** Name of the Material version that is shipped together with the schematics. */
-export declare const materialVersion = "9.0.0-rc.8-sha-bb08fa795";
+export declare const materialVersion = "9.0.0-rc.8-sha-27ef8425f";
 /**
  * Range of Angular versions that can be used together with the Angular Material version
  * that provides these schematics.
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.8-sha-bb08fa795';
+    exports.materialVersion = '9.0.0-rc.8-sha-27ef8425f';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
