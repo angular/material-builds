ae02ad228ca0715bbf8c23e792cf40769a402119
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-01-29 15:48:58 +0000

    master - bb08fa795 fix(material-experimental/mdc-chips): chip removal not working if animations are disabled (#18314)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.0-rc.8-sha-bbb92a7e2');
+    var VERSION = new i0.Version('9.0.0-rc.8-sha-bb08fa795');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.0-rc.8-sha-bbb92a7e2');
+    var VERSION$1 = new i0.Version('9.0.0-rc.8-sha-bb08fa795');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.8-sha-bbb92a7e2');
+const VERSION = new Version('9.0.0-rc.8-sha-bb08fa795');
 /**
  * \@docs-private
  * @return {?}
