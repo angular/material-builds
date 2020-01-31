de3af5642093849780b3ba632ce4b831755bd6ce
mmalerba <mmalerba@google.com>
2020-01-31 21:33:44 +0000

    master - 12edc0b61 chore: update to latest version of MDC (#18343)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.9-sha-4231d5b04');
+const VERSION = new Version('9.0.0-rc.9-sha-12edc0b61');
 /**
  * \@docs-private
  * @return {?}
