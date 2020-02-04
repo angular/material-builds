43e9f787eb11ce2853e03d3c3f2449d017394c30
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-04 18:57:23 +0000

    master - 22d7cf643 docs: update dev environment instructions (#18359)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.9-sha-058198a40');
+const VERSION = new Version('9.0.0-rc.9-sha-22d7cf643');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.9-sha-058198a40';
+    exports.materialVersion = '9.0.0-rc.9-sha-22d7cf643';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
