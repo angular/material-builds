5f631fa2d1298dd4be3316f48cd86adff787930a
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-04 19:41:10 +0000

    master - 3af0d8ac6 fix(material-experimental): consolidate mixins in e2e theme file (#18385)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.9-sha-6a97f025d');
+var VERSION = new Version('9.0.0-rc.9-sha-3af0d8ac6');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
