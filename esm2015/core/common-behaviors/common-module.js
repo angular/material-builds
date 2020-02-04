e32879a0a2bf33a805161da621c29e96ee9d02ac
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-04 18:59:17 +0000

    master - 447ca7138 refactor: rename `nav` schematic to `navigation` (#18364)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.9-sha-2fc488cfe');
+const VERSION = new Version('9.0.0-rc.9-sha-447ca7138');
 /**
  * \@docs-private
  * @return {?}
