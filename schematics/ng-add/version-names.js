5b1874b565646c220fd4e2809e2addd1176edc1c
mmalerba <mmalerba@google.com>
2020-02-04 18:47:49 +0000

    master - 058198a40 chore: delete import that was missed in #18274 (#18334)

--- schematics/ng-add/version-names.js
+++ schematics/ng-add/version-names.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /** Name of the Material version that is shipped together with the schematics. */
-    exports.materialVersion = '9.0.0-rc.9-sha-2ec7254f8';
+    exports.materialVersion = '9.0.0-rc.9-sha-058198a40';
     /**
      * Range of Angular versions that can be used together with the Angular Material version
      * that provides these schematics.
