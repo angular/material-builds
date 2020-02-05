f3cd0d1bee4f53678950e0a6d4b6a22e5f71545f
Miles Malerba <mmalerba@google.com>
2020-02-05 18:39:22 +0000

    9.0.x - 1001b70ec Revert "build: remove unused angular bazel postinstall patch (#18373)"

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.0.0-rc.9-sha-a35639a16`;
+    const fallbackMaterialVersionRange = `~9.0.0-rc.9-sha-1001b70ec`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
