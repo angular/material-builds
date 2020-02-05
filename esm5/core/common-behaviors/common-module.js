d9b814acf57547afbb91eb7c85abfce404f4bca7
mmalerba <mmalerba@google.com>
2020-02-05 23:38:22 +0000

    9.0.x - 351d0c4f3 fix(form-field): fix underline jank in fill variant (#18406)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.0.0-rc.9-sha-1001b70ec');
+var VERSION = new Version('9.0.0-rc.9-sha-351d0c4f3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
