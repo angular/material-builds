14f9a8beff9a3e7cfa41b3209cca2f7132caa279
Jeremy Elbourn <jelbourn@google.com>
2020-02-06 18:38:50 +0000

    9.0.x - 7c348bc68 build: bump deps for 9.0.0 (#18415)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.0-rc.10-sha-edb5f8a66');
+const VERSION = new Version('9.0.0-rc.10-sha-7c348bc68');
 /**
  * \@docs-private
  * @return {?}
--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.0.0-rc.10-sha-edb5f8a66');
+var VERSION = new Version('9.0.0-rc.10-sha-7c348bc68');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.0.0-rc.10-sha-edb5f8a66');
+var VERSION$1 = new Version('9.0.0-rc.10-sha-7c348bc68');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
