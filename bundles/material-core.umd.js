7692d117de54733e378176bda0a5857387d83a45
Jeremy Elbourn <jelbourn@google.com>
2020-02-20 19:12:23 +0000

    master - c246a1c3a Merge google-maps-next branch into master (#18485)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.0.1-sha-d1a6ea74d');
+    var VERSION = new i0.Version('9.0.1-sha-c246a1c3a');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.0.1-sha-d1a6ea74d');
+    var VERSION$1 = new i0.Version('9.0.1-sha-c246a1c3a');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
