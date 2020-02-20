f2934752b34973190f9edc90a1d0dc599f961141
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-02-20 23:14:12 +0000

    master - 3d351802c fix(material-experimental): rename theme files to match stable components (#18383)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.0.1-sha-01e4fbac8`;
+    const fallbackMaterialVersionRange = `~9.0.1-sha-3d351802c`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
