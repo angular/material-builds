359c789666c759d5718516efebd5bf52225af01e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-02-20 19:35:59 +0000

    master - 01e4fbac8 fix(ng-update): do not throw if project does not have dependencies (#18470)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.1-sha-aea79f08e');
+const VERSION = new Version('9.0.1-sha-01e4fbac8');
 /**
  * \@docs-private
  * @return {?}
