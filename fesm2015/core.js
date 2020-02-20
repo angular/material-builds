0ec10d7d2f68b0c350fa36a29aca55219c6eef2e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-20 19:33:16 +0000

    master - e9debff8d build: use local bazel installation in test script watch mode (#18512)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.1-sha-afb3f9a1c');
+const VERSION = new Version('9.0.1-sha-e9debff8d');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.1-sha-afb3f9a1c');
+const VERSION$1 = new Version('9.0.1-sha-e9debff8d');
 /**
  * \@docs-private
  * @return {?}
