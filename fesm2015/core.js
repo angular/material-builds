0e2dec7e92b59fac0b623272b11361b7990a2fce
Antonio Cardenas <antonioalx66@gmail.com>
2020-02-20 19:33:29 +0000

    master - 2e939d4ce docs(youtube-player component): fix incorrect syntax

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.0.1-sha-e9debff8d');
+const VERSION = new Version('9.0.1-sha-2e939d4ce');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.0.1-sha-e9debff8d');
+const VERSION$1 = new Version('9.0.1-sha-2e939d4ce');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.0.1-sha-e9debff8d`;
+    const fallbackMaterialVersionRange = `~9.0.1-sha-2e939d4ce`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
