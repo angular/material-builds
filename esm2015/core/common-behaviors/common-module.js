4046d1ff1ba9e75bc35732b0373d236e67806537
CaerusKaru <caerus.karu@gmail.com>
2020-02-20 19:18:48 +0000

    master - a761359c8 test(layout): remove unneeded LayoutModule (#18447)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.0.1-sha-4b329f089');
+const VERSION = new Version('9.0.1-sha-a761359c8');
 /**
  * \@docs-private
  * @return {?}
