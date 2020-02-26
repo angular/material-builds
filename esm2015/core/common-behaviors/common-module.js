a220e5670705b66b24227c7ffb3d7d46cb2d41fa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-02-26 15:21:31 +0000

    master - 76846b8b9 fix(material-experimental/mdc-radio): use consistent ripple timings (#18590)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-99d3e1289');
+const VERSION = new Version('9.1.0-sha-76846b8b9');
 /**
  * \@docs-private
  * @return {?}
