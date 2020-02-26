d9c6bab6b962ce1dedecee90f3f2d2152d4f1c68
Jean de Klerk <deklerk@google.com>
2020-02-26 15:37:12 +0000

    master - 7b3ea26af docs(material/table): add reorderable columns example (#18456)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-9badc54cc');
+const VERSION = new Version('9.1.0-sha-7b3ea26af');
 /**
  * \@docs-private
  * @return {?}
