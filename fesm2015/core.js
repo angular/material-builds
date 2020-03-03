fd7141427842a0b208cfe8d0964940279b6cd434
mmalerba <mmalerba@google.com>
2020-03-03 23:13:51 +0000

    9.1.x - b3d8315c8 demo(mdc-list): Add a demo page for the MDC-based list (#18604)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.1.0-sha-9343d0852');
+const VERSION = new Version('9.1.0-sha-b3d8315c8');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.1.0-sha-9343d0852');
+const VERSION$1 = new Version('9.1.0-sha-b3d8315c8');
 /**
  * \@docs-private
  * @return {?}
