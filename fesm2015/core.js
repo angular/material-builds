c67fe1f46dafa76aa60f914fdefe0bb9dd364744
mmalerba <mmalerba@google.com>
2020-03-04 22:02:23 +0000

    master - 400901b84 fix: event emitter type (#18664)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.1.0-sha-c2c7f441a');
+const VERSION = new Version('9.1.0-sha-400901b84');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.1.0-sha-c2c7f441a');
+const VERSION$1 = new Version('9.1.0-sha-400901b84');
 /**
  * \@docs-private
  * @return {?}
