9b5278b26d391ac3d7360303b7097c8a03062a62
mmalerba <mmalerba@google.com>
2020-03-04 22:01:51 +0000

    master - c2c7f441a docs(tooltip): scrollable example not working (#18661)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.1.0-sha-e5d886692');
+var VERSION = new Version('9.1.0-sha-c2c7f441a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.0-sha-e5d886692`;
+    const fallbackMaterialVersionRange = `~9.1.0-sha-c2c7f441a`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
