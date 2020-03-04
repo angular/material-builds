66056e720b43c74643aefce87443037b80addc98
mmalerba <mmalerba@google.com>
2020-03-04 22:23:10 +0000

    master - 60e21dac1 chore: add title format and "needs triage" label in issue temp… (#18717)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.1.0-sha-aca253350');
+var VERSION = new Version('9.1.0-sha-60e21dac1');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.1.0-sha-aca253350');
+var VERSION$1 = new Version('9.1.0-sha-60e21dac1');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
