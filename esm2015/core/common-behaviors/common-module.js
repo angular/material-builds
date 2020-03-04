12ef6515f5ce91f33792a4153df00c379c11a461
Jeremy Elbourn <jelbourn@google.com>
2020-03-04 21:54:43 +0000

    9.1.x - bf84ba945 chore: bump version to 9.1.1 w/ changelog (#18700)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-b3d8315c8');
+const VERSION = new Version('9.1.1-sha-bf84ba945');
 /**
  * \@docs-private
  * @return {?}
