0dff47253702a2e146d17468094e736e0f64d40f
mmalerba <mmalerba@google.com>
2020-03-04 22:04:37 +0000

    master - 27aac7240 build: include tslib helpers in umd bundles (#18672)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-77a0d5f3c');
+const VERSION = new Version('9.1.0-sha-27aac7240');
 /**
  * \@docs-private
  * @return {?}
