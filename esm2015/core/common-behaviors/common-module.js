7a9fe00fa9b40e95ff190eb65f0a1b8e5adf47aa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-05 22:53:01 +0000

    master - 18e1e48f0 build: downgrade ibazel version to v0.12.0

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-cf085b4c4');
+const VERSION = new Version('9.1.0-sha-18e1e48f0');
 /**
  * \@docs-private
  * @return {?}
