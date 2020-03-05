b8bd277e91fd8c885b484b8c33853587506b088d
Andrew Seguin <andrewjs@google.com>
2020-03-05 19:21:13 +0000

    master - f95af1a84 fix(typography): default to normal letter spacing

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-cf13ecbb5');
+const VERSION = new Version('9.1.0-sha-f95af1a84');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.1.0-sha-cf13ecbb5');
+const VERSION = new Version('9.1.0-sha-f95af1a84');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.1.0-sha-cf13ecbb5');
+const VERSION$1 = new Version('9.1.0-sha-f95af1a84');
 /**
  * \@docs-private
  * @return {?}
