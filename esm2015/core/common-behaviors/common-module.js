4666da72ae4d0aec7159a414a5fdc79426013dc1
mmalerba <mmalerba@google.com>
2020-03-05 18:52:25 +0000

    master - cf13ecbb5 build: upload release packages as job artifacts for PRs (#18710)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-8c51ddabe');
+const VERSION = new Version('9.1.0-sha-cf13ecbb5');
 /**
  * \@docs-private
  * @return {?}
