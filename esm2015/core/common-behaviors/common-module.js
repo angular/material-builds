65208767c7ae05c8a8948ccd295ac4ce50a22dc4
crisbeto <crisbeto@abv.bg>
2020-03-05 22:52:45 +0000

    master - cf085b4c4 fix(material-experimental/mdc-progress-bar): account for breaking changes in latest canary

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-f95af1a84');
+const VERSION = new Version('9.1.0-sha-cf085b4c4');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.0-sha-f95af1a84`;
+    const fallbackMaterialVersionRange = `~9.1.0-sha-cf085b4c4`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
