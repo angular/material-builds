c8d909449bf17261eace0b7b51b06ea2c7c100bb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-06 20:09:21 +0000

    master - 3447891cf refactor: ensure typography mixins expect consistent arguments (#18738)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.0-sha-d60c9af42');
+const VERSION = new Version('9.1.0-sha-3447891cf');
 /**
  * \@docs-private
  * @return {?}
