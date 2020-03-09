146e53f7b1a9b6affeec3b94d4ff8b548dad67b8
Andrew Flower <andrew.flower@gmail.com>
2020-03-09 22:02:35 +0000

    master - 6af149c81 docs(sort): fix grammar in the 'sort' documentation (#18751)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.1.1-sha-c538354f7');
+    var VERSION = new i0.Version('9.1.1-sha-6af149c81');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.1.1-sha-c538354f7');
+    var VERSION$1 = new i0.Version('9.1.1-sha-6af149c81');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
