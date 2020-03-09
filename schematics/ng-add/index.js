1a18eb3f9205b65008c74add5f473317ffe2ce39
mmalerba <mmalerba@google.com>
2020-03-09 22:50:19 +0000

    master - 40ae1b100 feat(cdk/testing): Allow custom `querySelectorAll` method (#18178)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.1-sha-47af01f52`;
+    const fallbackMaterialVersionRange = `~9.1.1-sha-40ae1b100`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
