eb1bd895fb63f9a80f5a0f9a19cfc39d2fbcb0d2
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-03-09 22:49:41 +0000

    master - e1bc61def build: Bump up year in license. (#18739)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.1-sha-5460fabe7');
+const VERSION = new Version('9.1.1-sha-e1bc61def');
 /**
  * \@docs-private
  * @return {?}
