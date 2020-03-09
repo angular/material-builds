d6f2dd159b98c111ed80137e35f2e970dcd5dd3f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-09 22:02:45 +0000

    master - 5460fabe7 fix(material-experimental/mdc-slider): resolve warnings about active event listeners (#18583)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -19,7 +19,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.1-sha-6af149c81');
+const VERSION = new Version('9.1.1-sha-5460fabe7');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.1-sha-6af149c81`;
+    const fallbackMaterialVersionRange = `~9.1.1-sha-5460fabe7`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
