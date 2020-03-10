202d7ec2aaf6fcf5b8404b8a2588ed9be62e7a88
mmalerba <mmalerba@google.com>
2020-03-10 19:34:35 +0000

    master - 807498dbb chore: bump version to 9.1.2 w/ changelog (#18782)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -13,7 +13,7 @@ import { VERSION as CDK_VERSION } from '@angular/cdk';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.1.1-sha-59ae34bf9');
+var VERSION = new Version('9.1.2-sha-807498dbb');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
