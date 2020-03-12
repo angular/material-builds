2726e2dbc32f8d1783ceb9a07e61928768840596
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:43:08 +0000

    master - b2e86918e fix(material-experimetal/mdc-form-field): account for breaking changes in MDC (#18795)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.2-sha-3854db02b');
+const VERSION = new Version('9.1.2-sha-b2e86918e');
 /**
  * \@docs-private
  * @return {?}
