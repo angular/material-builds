a5a95063d8eab147c693007f443a4ee61ce51c20
Michael Prentice <splaktar@gmail.com>
2020-03-12 21:49:07 +0000

    master - bcb36c26d docs(material/expansion-panel): throws No provider found for DateAdapter (#18772)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.1.2-sha-b4d28385e');
+var VERSION = new Version('9.1.2-sha-bcb36c26d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
