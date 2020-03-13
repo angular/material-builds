c05cb52c71729a684de065eee098e59c8c049f91
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-03-13 18:07:05 +0000

    master - b5b3c8a63 Revert "fix(radio): clicks not landing correctly in some cases on Chrome (#18357)" (#18820)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.1.2-sha-bcb36c26d');
+var VERSION = new Version('9.1.2-sha-b5b3c8a63');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.2-sha-bcb36c26d`;
+    const fallbackMaterialVersionRange = `~9.1.2-sha-b5b3c8a63`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
