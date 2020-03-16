eeb7b755604e724d646b984c51d4c5af66cabf9f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-03-16 17:12:11 +0000

    master - cc09ad8c9 chore: bump version to 9.1.3 w/ changelog (#18841) (#18842)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.1.2-sha-941b5a352');
+    var VERSION = new i0.Version('9.1.3-sha-cc09ad8c9');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.1.2-sha-941b5a352');
+    var VERSION$1 = new i0.Version('9.1.3-sha-cc09ad8c9');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.1.2-sha-941b5a352');
+var VERSION = new Version('9.1.3-sha-cc09ad8c9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
