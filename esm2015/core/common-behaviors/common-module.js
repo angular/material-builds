78b7e5a62ee37bd2d293ad79cb789bdf575b3d74
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-16 22:49:07 +0000

    master - f9fffab5a refactor(column-resize): simplify RTL handling (#18835)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.3-sha-cc09ad8c9');
+const VERSION = new Version('9.1.3-sha-f9fffab5a');
 /**
  * \@docs-private
  * @return {?}
