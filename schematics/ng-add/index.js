28dab7839109e39799dc793e8e8368b627ab73b1
Tobias Schweizer <tobiasschweizer@users.noreply.github.com>
2020-03-16 22:49:41 +0000

    master - 511f07639 docs(form-field): Use Form Validation in custom control example (#18844)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.1.3-sha-f9fffab5a`;
+    const fallbackMaterialVersionRange = `~9.1.3-sha-511f07639`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
