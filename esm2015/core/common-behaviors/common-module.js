7580932f61fd9f1caf8135818607d7b3630644de
Nick Walther <61717337+nickwalther@users.noreply.github.com>
2020-03-17 21:31:37 +0000

    master - b3a2c5614 feat(focus-monitor): Add eventual detection mode option to foc… (#18684)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.1.3-sha-511f07639');
+const VERSION = new Version('9.1.3-sha-b3a2c5614');
 /**
  * \@docs-private
  * @return {?}
--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.1.3-sha-511f07639');
+var VERSION = new Version('9.1.3-sha-b3a2c5614');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.1.3-sha-511f07639');
+var VERSION$1 = new Version('9.1.3-sha-b3a2c5614');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
