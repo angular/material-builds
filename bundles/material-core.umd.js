96a9bc9fbe90a2e7aa335229e2e443b81a6b403f
Hsuan Lee <HsuanGM@gmail.com>
2020-03-21 05:21:48 +0000

    master - 11536e47c test(dialog): add missing event dispatch (#18869)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.2.0-next.0-sha-4ea71574c');
+    var VERSION = new i0.Version('9.2.0-next.0-sha-11536e47c');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.2.0-next.0-sha-4ea71574c');
+    var VERSION$1 = new i0.Version('9.2.0-next.0-sha-11536e47c');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
