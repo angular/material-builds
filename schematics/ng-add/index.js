261ff75b8acdbfda3d69c65a30898df029816f32
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-03-25 22:30:22 +0000

    master - 29e74eb94 chore: bump version to 9.2.0 w/ changelog (#18921)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-next.0-sha-cc1e0fe59`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-29e74eb94`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
