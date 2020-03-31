681a4857cbecf7564fb51a8010c563a155f663be
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-03-31 17:30:59 +0000

    master - 2fce0c6c8 build: resolve module names in circular deps test (#18957)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.0-sha-f70f49af7');
+const VERSION = new Version('9.2.0-sha-2fce0c6c8');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-f70f49af7`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-2fce0c6c8`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
