a98c7490049b68a545763ba0743b9c8bed46b282
Livvie Lin <livvie.lin@gmail.com>
2020-03-31 16:10:17 +0000

    master - 93f6b3020 docs(button): add more comprehensive button examples (#18878)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.2.0-sha-5354fe602');
+var VERSION = new Version('9.2.0-sha-93f6b3020');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.2.0-sha-5354fe602');
+var VERSION$1 = new Version('9.2.0-sha-93f6b3020');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
