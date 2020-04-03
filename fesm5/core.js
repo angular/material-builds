d71e77b4bf5ea180c4e1256d87a280cb42439b69
Greg Magolan <gmagolan@gmail.com>
2020-04-03 17:32:08 +0000

    master - d51339c66 refactor: use $execpath instead of deprecated $location (#18961)

--- fesm5/core.js
+++ fesm5/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-var VERSION = new Version('9.2.0-sha-8a7128847');
+var VERSION = new Version('9.2.0-sha-d51339c66');
 
 /**
  * @license
@@ -60,7 +60,7 @@ var AnimationDurations = /** @class */ (function () {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION$1 = new Version('9.2.0-sha-8a7128847');
+var VERSION$1 = new Version('9.2.0-sha-d51339c66');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
