805a94cdb6eb291f487f30b7ee433bf19aa5e53e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-03 20:07:20 +0000

    master - e0b579784 build: sanitize postinstall patch markers (#18982)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-e4cfa2b19`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-e0b579784`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
