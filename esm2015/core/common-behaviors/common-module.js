138b9373bbf281174bbf12032b44673369e4d7b9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-06 17:14:11 +0000

    9.2.x - 8ab769daa refactor(tree): remove renderer usage (#18930)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.0-sha-f023328a8');
+const VERSION = new Version('9.2.0-sha-8ab769daa');
 /**
  * \@docs-private
  * @return {?}
