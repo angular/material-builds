423036b60b72f8ed77986c3e9cb27895ee71612e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-06 22:52:29 +0000

    master - 619f7f8de fix(autocomplete): provide horizontal fallback positions (#18906)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-92bbc7705`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-619f7f8de`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
