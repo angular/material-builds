f7a81f07e4f0b3b46f408224e00b24d72624e2bf
Vanessa Schmitt <vschmitt@google.com>
2020-04-07 16:52:26 +0000

    master - 96fd5856f fix(material-experimental/mdc-chips): support custom errorStat… (#18974)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.0-sha-029e36329');
+const VERSION = new Version('9.2.0-sha-96fd5856f');
 /**
  * \@docs-private
  * @return {?}
