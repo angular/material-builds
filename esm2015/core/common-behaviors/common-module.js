96d065b3416cd9a183601b8c2a838444e7bc9e66
Vanessa Schmitt <vschmitt@google.com>
2020-04-07 05:20:32 +0000

    master - 029e36329 fix(material-experimental/mdc-chips) Replace default values (#19006)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.0-sha-3ede17dc9');
+const VERSION = new Version('9.2.0-sha-029e36329');
 /**
  * \@docs-private
  * @return {?}
