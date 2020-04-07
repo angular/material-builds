84c3c6cd2bdf6a22e3cb6f9a71919c3093bde0c7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-07 17:45:31 +0000

    master - 638320e08 fix(a11y): avoid errors when trying to add high contrast class (#18323)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.0-sha-a5b29c582');
+const VERSION = new Version('9.2.0-sha-638320e08');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.0-sha-a5b29c582');
+const VERSION$1 = new Version('9.2.0-sha-638320e08');
 /**
  * \@docs-private
  * @return {?}
