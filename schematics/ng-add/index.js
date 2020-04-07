27d2aa4257295bc1bcfb5e2bbbe55177ca8aaa19
Michael Leibman <michael.leibman@gmail.com>
2020-04-07 17:53:20 +0000

    master - a85b63c19 perf(focus-monitor): optimize event registration (#18667)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-638320e08`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-a85b63c19`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
