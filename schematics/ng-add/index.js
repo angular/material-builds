99f3d64be089581c1e963cace4cb301a6d4b5726
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-08 04:54:45 +0000

    master - d6564cc79 build: add MDC components to SSR kitchen sink (#19013)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-3554e38ed`;
+    const fallbackMaterialVersionRange = `~9.2.0-sha-d6564cc79`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
