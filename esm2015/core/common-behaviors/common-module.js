d84210975332d64a021b5f84ca00dd1ff050b54e
mmalerba <mmalerba@google.com>
2020-04-14 21:24:00 +0000

    9.2.x - b0424a351 chore: bump version to 9.2.1 w/ changelog (#19073)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.0-sha-ac565a2cc');
+const VERSION = new Version('9.2.1-sha-b0424a351');
 /**
  * \@docs-private
  * @return {?}
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.0-sha-ac565a2cc`;
+    const fallbackMaterialVersionRange = `~9.2.1-sha-b0424a351`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
