d18c87b5a2dad797a7abfc2085846bbd51390dbc
Cyrille Tuzi <web@cyrilletuzi.com>
2020-04-17 16:28:04 +0000

    master - 7d47b4592 refactor(schematics): remove comments in collection.json (#19077)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.2.1-sha-712ce5c81');
+    var VERSION = new i0.Version('9.2.1-sha-7d47b4592');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.2.1-sha-712ce5c81');
+    var VERSION$1 = new i0.Version('9.2.1-sha-7d47b4592');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
