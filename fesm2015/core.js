a07d886816a2a0c5051f8a560e056e29e90d1a5b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:39:57 +0000

    master - 7e660371d fix(drag-drop): unable to start dragging in list if dragged item is destroyed (#19055)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.1-sha-cd7597925');
+const VERSION = new Version('9.2.1-sha-7e660371d');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.1-sha-cd7597925');
+const VERSION$1 = new Version('9.2.1-sha-7e660371d');
 /**
  * \@docs-private
  * @return {?}
