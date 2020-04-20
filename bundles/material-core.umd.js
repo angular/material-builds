adc0217c2cbb7952ebc1a4aba4ece38cdd27d4a3
mbehrlich <mehrlich@google.com>
2020-04-20 14:52:22 +0000

    master - 3e00f4c5f feat(google-maps): expose the underlying Google Maps objects. (#18613)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.2.1-sha-04d158816');
+    var VERSION = new i0.Version('9.2.1-sha-3e00f4c5f');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.2.1-sha-04d158816');
+    var VERSION$1 = new i0.Version('9.2.1-sha-3e00f4c5f');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
