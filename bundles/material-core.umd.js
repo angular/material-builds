48c1c1aea021b0705a12ff772ce89bd3b837cab1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:47:28 +0000

    master - 4e6083b6e fix(select): don't select active item when tabbing away while closed (#18797)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.2.1-sha-77f65e3e2');
+    var VERSION = new i0.Version('9.2.1-sha-4e6083b6e');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.2.1-sha-77f65e3e2');
+    var VERSION$1 = new i0.Version('9.2.1-sha-4e6083b6e');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
