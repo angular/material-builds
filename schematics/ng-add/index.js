1f8b4c8630cce1f93b628a83f95c38bb47d84167
Ariel Lee <ariellee616@gmail.com>
2020-04-20 14:51:11 +0000

    master - 04d158816 feat(cdk/tree): support optional trackBy in FlatTreeControl (#18708)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.1-sha-4e6083b6e`;
+    const fallbackMaterialVersionRange = `~9.2.1-sha-04d158816`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
