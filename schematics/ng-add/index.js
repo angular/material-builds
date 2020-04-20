3ef2d1e393d0667d4d17d4d98c28a3f5b87dad47
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:41:11 +0000

    master - a2d041803 fix(material-experimental/mdc-slide-toggle): strong focus indication not visible (#19048)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.1-sha-7e660371d`;
+    const fallbackMaterialVersionRange = `~9.2.1-sha-a2d041803`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
