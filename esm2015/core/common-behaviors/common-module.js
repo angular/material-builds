a879f65ec89ea9c7b2569150371d159dc90c0f8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-04-20 14:52:42 +0000

    master - d5a9eaa15 fix(material-experimental/mdc-chips): not disabling all animations when animations are disabled (#18440)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-3e00f4c5f');
+const VERSION = new Version('9.2.1-sha-d5a9eaa15');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.2.1-sha-3e00f4c5f');
+var VERSION = new Version('9.2.1-sha-d5a9eaa15');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
