b651d8c66051fe9b01a0eb74fe1102c7ea607fa6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-20 14:38:11 +0000

    master - 3c99efc98 fix(material-experimental/mdc-radio): strong focus indicator not working (#19091)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.1-sha-b5d43ed5c`;
+    const fallbackMaterialVersionRange = `~9.2.1-sha-3c99efc98`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
