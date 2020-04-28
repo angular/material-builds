49269dcc502a59814480f88d16712ce5e11c3fba
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-04-28 20:48:01 +0000

    master - 76f1341e3 build: dev-infra-private package not updated locally (#19196)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-668f79193');
+const VERSION = new Version('9.2.1-sha-76f1341e3');
 /**
  * \@docs-private
  * @return {?}
