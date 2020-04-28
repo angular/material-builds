9d3e2d136057b985e1a683010a4ccbfddb07c8d8
Walter S <SchnWalter@users.noreply.github.com>
2020-04-28 22:12:21 +0000

    master - 666417dc2 chore(cdk/a11y): fix a handful of typos  (#19181)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.1-sha-afbaad326');
+const VERSION = new Version('9.2.1-sha-666417dc2');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.1-sha-afbaad326');
+const VERSION$1 = new Version('9.2.1-sha-666417dc2');
 /**
  * \@docs-private
  * @return {?}
