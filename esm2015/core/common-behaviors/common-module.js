ca9de26383c72e8469268cfcea9b6f6a69bc3203
Alberto Aldegheri <albyrock87@gmail.com>
2020-04-29 15:03:55 +0000

    9.2.x - 12c705a07 fix(drag-drop): drop-list wrong enter position (#19116)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-328de599e');
+const VERSION = new Version('9.2.1-sha-12c705a07');
 /**
  * \@docs-private
  * @return {?}
