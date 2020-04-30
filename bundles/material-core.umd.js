5c62a5914cb66ab8d515acd96ea684f5cfad4d4b
Jeremy Elbourn <jelbourn@google.com>
2020-04-30 22:10:32 +0000

    9.2.x - 9e0ae2ea7 Revert "build: add server-side rendering check for ground overlay (#19188)"

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('9.2.1-sha-bacd18b7f');
+    var VERSION = new i0.Version('9.2.1-sha-9e0ae2ea7');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('9.2.1-sha-bacd18b7f');
+    var VERSION$1 = new i0.Version('9.2.1-sha-9e0ae2ea7');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
