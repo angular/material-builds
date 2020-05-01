37191747c3d4a24ff4ae5347b38bf6c29ea05667
Bram Peirs <peirsbram@me.com>
2020-05-01 13:57:53 +0000

    9.2.x - 8e4d8c9ee docs(material/datepicker): fix typo in overview (#19216)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.2-sha-8c241cb62');
+const VERSION = new Version('9.2.2-sha-8e4d8c9ee');
 /**
  * \@docs-private
  * @return {?}
