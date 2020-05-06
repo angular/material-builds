a1587d36ddcb67cda6e898bb3ea7008ec5ec20d6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-06 20:09:09 +0000

    9.2.x - 70ddc361a refactor: add missing abstract directive decorators (#19256)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.2-sha-9abc8ea3f');
+const VERSION = new Version('9.2.2-sha-70ddc361a');
 /**
  * \@docs-private
  * @return {?}
--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.2.2-sha-9abc8ea3f');
+var VERSION = new Version('9.2.2-sha-70ddc361a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
