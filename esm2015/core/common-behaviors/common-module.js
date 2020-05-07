f602b12eb08860d7a9e2626ed396b78e53779a04
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-07 14:08:53 +0000

    master - ec5f74c32 fix(datepicker): double role definition on calendar cell (#17297)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-ecf410539');
+const VERSION = new Version('9.2.1-sha-ec5f74c32');
 /**
  * \@docs-private
  * @return {?}
