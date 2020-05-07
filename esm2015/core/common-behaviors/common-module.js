84ab6154a1831547d629605b71a28aba6966ff35
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-07 20:51:01 +0000

    master - a4171e076 build: add dev app for MDC sidenav (#19282)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-38059c3e0');
+const VERSION = new Version('9.2.1-sha-a4171e076');
 /**
  * \@docs-private
  * @return {?}
