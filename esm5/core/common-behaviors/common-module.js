7323b2e9994b03fff143fee4e9ce009485ad2eca
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-07 05:36:45 +0000

    master - f02a726c7 build: fix "test all" script running incorrectly (#19261)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.2.1-sha-448523bff');
+var VERSION = new Version('9.2.1-sha-f02a726c7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
