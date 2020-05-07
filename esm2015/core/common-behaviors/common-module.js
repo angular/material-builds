f492b724225f1d5200b041735f2420e9976c7448
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-07 17:24:56 +0000

    master - 38059c3e0 test(datepicker): fix test failure against view engine (#19273)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-ec5f74c32');
+const VERSION = new Version('9.2.1-sha-38059c3e0');
 /**
  * \@docs-private
  * @return {?}
