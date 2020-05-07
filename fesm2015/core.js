a8b44f9aa7c304cd5982316f3ce5203c22e7e3fd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-07 05:38:38 +0000

    master - d4d12a95d build: do not build runfile trees unnecessarily

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.1-sha-f02a726c7');
+const VERSION = new Version('9.2.1-sha-d4d12a95d');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.1-sha-f02a726c7');
+const VERSION$1 = new Version('9.2.1-sha-d4d12a95d');
 /**
  * \@docs-private
  * @return {?}
