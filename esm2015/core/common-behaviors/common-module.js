6ea79ae87045824bf957b2c1a89372139e55c1c6
Jeremy Elbourn <jelbourn@google.com>
2020-05-08 21:57:51 +0000

    master - bd5841268 docs: update master changelog for 9.2.2 and 9.2.3 (#19295)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-7b42c7e42');
+const VERSION = new Version('9.2.1-sha-bd5841268');
 /**
  * \@docs-private
  * @return {?}
