4daf81d93e7c6381f1a92dfe15bbceb65ad26987
Liz Mitchell <lizmitchell@google.com>
2020-05-08 20:44:54 +0000

    master - 7b42c7e42 fix(material-experimental/form-field): add setLabelRequired adapter (#19284)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-ba521d23a');
+const VERSION = new Version('9.2.1-sha-7b42c7e42');
 /**
  * \@docs-private
  * @return {?}
