325ed6582ed7367cffd95f0d888b9192660b03a9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-05-08 07:00:49 +0000

    master - 5284a57b2 feat(material-experimental/mdc-form-field): expose `MAT_FORM_FIELD` injection token (#19277)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.1-sha-a4171e076');
+const VERSION = new Version('9.2.1-sha-5284a57b2');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.1-sha-a4171e076');
+const VERSION = new Version('9.2.1-sha-5284a57b2');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.1-sha-a4171e076');
+const VERSION$1 = new Version('9.2.1-sha-5284a57b2');
 /**
  * \@docs-private
  * @return {?}
