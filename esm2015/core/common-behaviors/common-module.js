fb0f2d3769d6f4477b767d41d4217e2b504086f0
mmalerba <mmalerba@google.com>
2020-05-12 05:47:12 +0000

    master - 8125651ec refactor(material-experimental/mdc-chips): implement trailing icon foundation (#19318)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('10.0.0-next.0-sha-eb218e55f');
+const VERSION = new Version('10.0.0-next.0-sha-8125651ec');
 /**
  * \@docs-private
  * @return {?}
