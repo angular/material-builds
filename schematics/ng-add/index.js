57b3f3abf5908950779262658c9621b711059249
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-13 02:15:40 +0000

    master - 4eee81a88 fix(datepicker): strong focus indication in calendar always shown (#19304)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~10.0.0-next.0-sha-de128e88b`;
+    const fallbackMaterialVersionRange = `~10.0.0-next.0-sha-4eee81a88`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
