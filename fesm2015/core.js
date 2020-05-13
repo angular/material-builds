cba5e779315d49c74b31f3cee06d19d83275bc40
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-13 02:16:03 +0000

    master - a586f02da fix(text-field): unable to undo/redo in autosized text field on firefox (#19238)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.0-sha-4eee81a88');
+const VERSION = new Version('10.0.0-next.0-sha-a586f02da');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('10.0.0-next.0-sha-4eee81a88');
+const VERSION$1 = new Version('10.0.0-next.0-sha-a586f02da');
 /**
  * \@docs-private
  * @return {?}
