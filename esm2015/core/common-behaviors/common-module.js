b5d1121544074e78f8a7957030d9ca9ee1ed0477
Miles Malerba <mmalerba@google.com>
2020-05-18 22:13:27 +0000

    master - 16f5f793d chore: bump version to 9.2.4 w/ changelog

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('10.0.0-next.0-sha-189d98e8b');
+const VERSION = new Version('10.0.0-next.0-sha-16f5f793d');
 /**
  * \@docs-private
  * @return {?}
