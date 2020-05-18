58f18a891791388622859d444a033aa602860ec8
Miles Malerba <mmalerba@google.com>
2020-05-18 21:59:06 +0000

    9.2.x - fec578dbc chore: bump version to 9.2.4 w/ changelog

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.3-sha-bad049cda');
+const VERSION = new Version('9.2.4-sha-fec578dbc');
 /**
  * \@docs-private
  * @return {?}
