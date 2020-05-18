2a357b8f1bd712b2d7555e9c5fed31eabb8d2c9c
mmalerba <mmalerba@google.com>
2020-05-18 23:41:40 +0000

    master - e72e9aea5 chore: bump version to 10.0.0-next.1 w/ changelog (#19388)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('10.0.0-next.0-sha-16f5f793d');
+const VERSION = new Version('10.0.0-next.1-sha-e72e9aea5');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.0-sha-16f5f793d');
+const VERSION = new Version('10.0.0-next.1-sha-e72e9aea5');
 
 /**
  * @fileoverview added by tsickle
@@ -92,7 +92,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('10.0.0-next.0-sha-16f5f793d');
+const VERSION$1 = new Version('10.0.0-next.1-sha-e72e9aea5');
 /**
  * \@docs-private
  * @return {?}
