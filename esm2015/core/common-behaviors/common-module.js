8af5a0ba5b94f621efc37f9ea1667e48272d685e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-19 21:02:06 +0000

    master - 2d64ad72b build: update to latest MDC canary and fix error (#19389)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('10.0.0-next.1-sha-e72e9aea5');
+const VERSION = new Version('10.0.0-next.1-sha-2d64ad72b');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1-sha-e72e9aea5');
+const VERSION = new Version('10.0.0-next.1-sha-2d64ad72b');
 
 /**
  * @fileoverview added by tsickle
@@ -92,7 +92,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('10.0.0-next.1-sha-e72e9aea5');
+const VERSION$1 = new Version('10.0.0-next.1-sha-2d64ad72b');
 /**
  * \@docs-private
  * @return {?}
