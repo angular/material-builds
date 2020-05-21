b5867a0c73b3056a1ed779395324041fb190749e
Marco Marche <marco.marche@gnucoop.com>
2020-05-21 04:45:17 +0000

    9.2.x - f72b95f3b fix(ng-add): ng add @angular/material fails in library projects (#19164)

--- esm5/core/common-behaviors/common-module.js
+++ esm5/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-var VERSION = new Version('9.2.4-sha-586b862bb');
+var VERSION = new Version('9.2.4-sha-f72b95f3b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.4-sha-586b862bb');
+const VERSION = new Version('9.2.4-sha-f72b95f3b');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.4-sha-586b862bb');
+const VERSION$1 = new Version('9.2.4-sha-f72b95f3b');
 /**
  * \@docs-private
  * @return {?}
