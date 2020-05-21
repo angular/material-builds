9364bb7cbe4d93329d6bc3906287208b6d20a80b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-21 04:34:27 +0000

    master - d309ee225 fix(drag-drop): error during device emulation on firefox (#19396)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('10.0.0-next.1-sha-bc280f33d');
+const VERSION = new Version('10.0.0-next.1-sha-d309ee225');
 /**
  * \@docs-private
  * @return {?}
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1-sha-bc280f33d');
+const VERSION = new Version('10.0.0-next.1-sha-d309ee225');
 
 /**
  * @fileoverview added by tsickle
@@ -92,7 +92,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('10.0.0-next.1-sha-bc280f33d');
+const VERSION$1 = new Version('10.0.0-next.1-sha-d309ee225');
 /**
  * \@docs-private
  * @return {?}
