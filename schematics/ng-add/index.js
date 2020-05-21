08cb2ac031fe2f0fc5dd5ad8d28d2023cd80ae28
Annie Wang <anniewang8697@gmail.com>
2020-05-21 02:38:16 +0000

    9.2.x - 8eff31af6 refactor(components-example): extract regions from component example source files (#19376)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -27,7 +27,7 @@
      * Note that the fallback version range does not use caret, but tilde because that is
      * the default for Angular framework dependencies in CLI projects.
      */
-    const fallbackMaterialVersionRange = `~9.2.4-sha-7bae4e193`;
+    const fallbackMaterialVersionRange = `~9.2.4-sha-8eff31af6`;
     /**
      * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
      * automatically executed if developers run `ng add @angular/material`.
