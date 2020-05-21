a2beb1ac757f2c8b9752da4f8a7401c82b029def
Karl Seamon <bannanafiend@gmail.com>
2020-05-21 01:35:36 +0000

    9.2.x - 7bae4e193 fix(column-resize): Unbreak Sass build by importing duplicate styles file (#19404)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -20,7 +20,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * Current version of Angular Material.
  * @type {?}
  */
-const VERSION = new Version('9.2.4-sha-fe396c2f8');
+const VERSION = new Version('9.2.4-sha-7bae4e193');
 
 /**
  * @fileoverview added by tsickle
@@ -80,7 +80,7 @@ if (false) {
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION$1 = new Version('9.2.4-sha-fe396c2f8');
+const VERSION$1 = new Version('9.2.4-sha-7bae4e193');
 /**
  * \@docs-private
  * @return {?}
