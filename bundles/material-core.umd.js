fc37c896034d2997870c47cc1f0a8fa18c9373c0
Andrew Scott <atscott01@gmail.com>
2020-05-22 03:07:30 +0000

    master - 5f125395e test: add characterization tests for DocsMarkedRenderer (#19413)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-next.1-sha-8dbee4e06');
+    var VERSION = new i0.Version('10.0.0-next.1-sha-5f125395e');
 
     /**
      * @license
@@ -280,7 +280,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-next.1-sha-8dbee4e06');
+    var VERSION$1 = new i0.Version('10.0.0-next.1-sha-5f125395e');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -15,7 +15,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-next.1-sha-8dbee4e06');
+const VERSION = new Version('10.0.0-next.1-sha-5f125395e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
