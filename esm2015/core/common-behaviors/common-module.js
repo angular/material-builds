5cf2e2d373f39fcd3b7b69a85280c2948ba86264
Andrew Scott <atscott01@gmail.com>
2020-05-22 03:08:09 +0000

    9.2.x - 8d67249d2 test: add characterization tests for DocsMarkedRenderer (#19413)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -20,7 +20,7 @@ import { DOCUMENT } from '@angular/common';
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
 /** @type {?} */
-const VERSION = new Version('9.2.4-sha-f72b95f3b');
+const VERSION = new Version('9.2.4-sha-8d67249d2');
 /**
  * \@docs-private
  * @return {?}
