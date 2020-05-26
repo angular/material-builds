1451bec13f8e2cd7b99a5f5fe065921546b134f5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-05-26 17:33:34 +0000

    master - 2defaf7dc refactor(platform): change deprecated APIs for version 10 (#19347)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -15,7 +15,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-next.1-sha-25d2e0566');
+const VERSION = new Version('10.0.0-next.1-sha-2defaf7dc');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.0.0-next.1-sha-25d2e0566`;
+const fallbackMaterialVersionRange = `~10.0.0-next.1-sha-2defaf7dc`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
