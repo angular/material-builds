db045e9a27b94a343c90a35a5b955fe80265d45c
Andy Chrzaszcz <andy9775@gmail.com>
2020-05-26 18:51:13 +0000

    master - 7e7406441 docs(cdk/focus-trap): fix FocusTrap class documentation typo (#19431)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -15,7 +15,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-next.1-sha-dd73e36b6');
+const VERSION = new Version('10.0.0-next.1-sha-7e7406441');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
