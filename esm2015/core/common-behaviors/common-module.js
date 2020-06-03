9cea50497039a9e8092c1be51178e627c6e4c7c2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-03 21:17:16 +0000

    master - b365ba74c build: set dev app density class on body (#19424)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.0-sha-f82caf486');
+const VERSION = new Version('10.0.0-rc.0-sha-b365ba74c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
