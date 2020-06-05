9be1f1f4d981c07d9436ba075bf46e84f8c87ab4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-05 02:47:57 +0000

    master - 94571c5aa fix(cdk/testing/testbed): set defined event properties as configurable

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.0-sha-f22626444');
+const VERSION = new Version('10.0.0-rc.0-sha-94571c5aa');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
