bd2bf13d4b5171c5413909d2cf776e49e61efafb
Annie Wang <anniewang8697@gmail.com>
2020-06-05 03:34:17 +0000

    master - a72883fae refactor: update snippets/embedded example comments in material docs md to reflect new API (chips, menu, tabs) (#19486)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.0-sha-9716a0164');
+    var VERSION = new i0.Version('10.0.0-rc.0-sha-a72883fae');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.0-sha-9716a0164');
+    var VERSION$1 = new i0.Version('10.0.0-rc.0-sha-a72883fae');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
