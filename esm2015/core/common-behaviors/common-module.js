2b0a9d668da8c45ad7d90171215acf8acf8add2e
RN Lee <rnlee0054@gmail.com>
2020-06-05 03:03:28 +0000

    master - 31afc80cf docs(material/expansion): Fix expansion-expand-collapse-all-example selector (#19499)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.0-sha-84bfa7cdf');
+const VERSION = new Version('10.0.0-rc.0-sha-31afc80cf');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.0.0-rc.0-sha-84bfa7cdf');
+const VERSION = new Version('10.0.0-rc.0-sha-31afc80cf');
 
 /**
  * @license
@@ -58,7 +58,7 @@ let AnimationDurations = /** @class */ (() => {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.0.0-rc.0-sha-84bfa7cdf');
+const VERSION$1 = new Version('10.0.0-rc.0-sha-31afc80cf');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
