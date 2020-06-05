99386cb355923246219bac6ba5c7bf53a577f84c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-05 03:23:18 +0000

    master - ee63f63c1 fix(material-experimental/mdc-chips): removing chip on any key press (#19425)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.0-sha-a95da9264');
+const VERSION = new Version('10.0.0-rc.0-sha-ee63f63c1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
