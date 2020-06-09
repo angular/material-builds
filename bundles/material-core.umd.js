0141e8c774619c1556dfef82508709e5c935b90c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-09 20:07:18 +0000

    master - b618a96be fix(material-experimental/mdc-menu): missing padding after latest canary release (#19548)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.1-sha-6c212b74c');
+    var VERSION = new i0.Version('10.0.0-rc.1-sha-b618a96be');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-6c212b74c');
+    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-b618a96be');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
