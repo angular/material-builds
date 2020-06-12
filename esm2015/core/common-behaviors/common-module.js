6c2bbd9424a2555cd5387f37ea164d6fb5bc557a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-12 17:15:55 +0000

    master - 277aeb041 docs(stepper): error state example not set up correctly (#19416)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.1-sha-b1e2a6d2f');
+const VERSION = new Version('10.0.0-rc.1-sha-277aeb041');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
