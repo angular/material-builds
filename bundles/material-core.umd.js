fe6138519ab7b225994e6871831e2e7e12a63212
Kyle Owsen <kowsen@google.com>
2020-06-12 05:53:01 +0000

    master - 69796c292 Stop arrow key navigation through matChipRemove directives from deleting mdc-chip-rows.

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.1-sha-f92e0df07');
+    var VERSION = new i0.Version('10.0.0-rc.1-sha-69796c292');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-f92e0df07');
+    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-69796c292');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
