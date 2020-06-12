3575fe6330af7e901946c33b996ced57381b7e8c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-12 21:42:37 +0000

    10.0.x - 34cd8eeb2 build: fix wrong directory for notification script (#19626)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.1-sha-dc38ab748');
+const VERSION = new Version('10.0.0-rc.1-sha-34cd8eeb2');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
