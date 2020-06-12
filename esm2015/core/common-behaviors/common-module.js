e4cef5e5f3dcbbc52a5727ade8a2f958ae1a4344
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-12 17:16:41 +0000

    master - bde24edd2 build: postinstall should not generate summaries for dev-infra pkg (#19566)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.1-sha-277aeb041');
+const VERSION = new Version('10.0.0-rc.1-sha-bde24edd2');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
