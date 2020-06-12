f78f69f0ebcef960ab6e5b7fbce58721496d0264
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-12 18:06:02 +0000

    10.0.x - 72af3cd10 build: fix size check failure (#19625)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.1-sha-e9dc45aba');
+    var VERSION = new i0.Version('10.0.0-rc.1-sha-72af3cd10');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-e9dc45aba');
+    var VERSION$1 = new i0.Version('10.0.0-rc.1-sha-72af3cd10');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
