6bc8e5ad79fb47e636fb746e0c148a9dbb337c40
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-13 00:05:24 +0000

    10.0.x - a57668a38 build: switch to yaml file for size test golden  (#19621)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.0.0-rc.1-sha-3fc101c91');
+const VERSION = new Version('10.0.0-rc.1-sha-a57668a38');
 
 /**
  * @license
@@ -58,7 +58,7 @@ let AnimationDurations = /** @class */ (() => {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.0.0-rc.1-sha-3fc101c91');
+const VERSION$1 = new Version('10.0.0-rc.1-sha-a57668a38');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
