8581114d730e3dab173af85b587aca06ca4ed0f3
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-06-17 22:40:51 +0000

    master - 042823109 feat(cdk-experimental/listbox): add file skeleton (#19612)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.0.0-rc.2-sha-d8c2d6380`;
+const fallbackMaterialVersionRange = `~10.0.0-rc.2-sha-042823109`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
