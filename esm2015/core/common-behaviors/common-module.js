437b422fb20dc3728753be537d0fd502c5c0b21b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-17 22:58:14 +0000

    master - 78fcea463 fix(cdk-experimental/column-resize): not working inside OnPush component (#19671)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-316262457');
+const VERSION = new Version('10.0.0-rc.2-sha-78fcea463');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
