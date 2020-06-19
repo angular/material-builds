84e6df22fb4a50e2a7f71a14ad72a362d2abf4be
mmalerba <mmalerba@google.com>
2020-06-19 20:54:54 +0000

    master - 166bb5248 feat(material-experimental/mdc-list): add MDC foundation for action/nav list (#19601)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-33aeea9a9');
+const VERSION = new Version('10.0.0-rc.2-sha-166bb5248');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
