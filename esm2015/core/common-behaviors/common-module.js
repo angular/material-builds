663c970afe7cfea7ac68f8fa7ce3be3fb2345b27
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-22 18:31:07 +0000

    10.0.x - 87a2d99bf build: fix syntax error in docs (#19699)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-10888f393');
+const VERSION = new Version('10.0.0-rc.2-sha-87a2d99bf');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
