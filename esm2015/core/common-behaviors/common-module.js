c59e218f1838b1bc5ce31eb03a0b5de3691b35b9
Jeremy Elbourn <jelbourn@google.com>
2020-06-23 21:58:26 +0000

    10.0.x - 6df4bc7c1 chore: change dev/test app codeowners (#19734)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.3-sha-02a642b95');
+const VERSION = new Version('10.0.0-rc.3-sha-6df4bc7c1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
