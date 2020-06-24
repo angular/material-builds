0233204ff705510327e4799097e5f0c558c379fa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-24 21:20:29 +0000

    master - f128a33b3 test: fix schematic test infrastructure failing with Angular CLI v10 (#19749)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-e086544b9');
+const VERSION = new Version('10.0.0-rc.2-sha-f128a33b3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
