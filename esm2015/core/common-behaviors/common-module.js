ea4729206519880c5f9ae821ca97bee40a1d3849
Jeremy Elbourn <jelbourn@google.com>
2020-06-25 05:16:40 +0000

    master - e211cab7e chore: copy 10.0.0 version and changelog to master (#19753)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-f04482320');
+const VERSION = new Version('10.0.0-rc.2-sha-e211cab7e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
