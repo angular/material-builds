455e1ee50978a0fad6b5f2ca5f7f4614a0f1794b
Andy Chrzaszcz <andy9775@gmail.com>
2020-06-25 20:45:34 +0000

    master - b3d6d92ea feat(cdk-experimental/menu): implement menu handling logic (#19701)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.0.0-rc.2-sha-e211cab7e`;
+const fallbackMaterialVersionRange = `~10.0.0-rc.2-sha-b3d6d92ea`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
