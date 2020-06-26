a29e131b484dbe9074fd231fcdd05dec4e3db89e
Wagner Maciel <wagnermaciel@google.com>
2020-06-26 04:05:25 +0000

    master - 219b4ead3 test(chips): add performance tests for mat-chips (#19550)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-55c39d83c');
+const VERSION = new Version('10.0.0-rc.2-sha-219b4ead3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
