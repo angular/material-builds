8ff636950a5ff3b5784606876241fe0a05173e87
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-26 03:23:40 +0000

    master - 56ec32bee build: switch to chromium provided in dev-infra package (#19725)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-5d50ec7b9');
+const VERSION = new Version('10.0.0-rc.2-sha-56ec32bee');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
