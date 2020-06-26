5e12ad64f9353bfe207fea675e77a3e88dbc6d06
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:38:29 +0000

    master - 8147cc956 docs(select): switch examples to fill appearance (#19708)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.2-sha-ab74abec7');
+    var VERSION = new i0.Version('10.0.0-rc.2-sha-8147cc956');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-ab74abec7');
+    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-8147cc956');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
