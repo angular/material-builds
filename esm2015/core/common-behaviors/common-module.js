8781bde02cdd9d1cbe1be0f1341876c9a8665bd3
Annie Wang <annieyw@google.com>
2020-06-26 03:30:49 +0000

    master - e3cf243ed build: build docs-content and examples-highlighter on PRs (#19719)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-dbb9df3d5');
+const VERSION = new Version('10.0.0-rc.2-sha-e3cf243ed');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
