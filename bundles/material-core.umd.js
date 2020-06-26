5c4001aef0063bf322398324747d01e0aac19f6a
Annie Wang <annieyw@google.com>
2020-06-26 03:03:44 +0000

    master - 970d15b88 docs: prevent 'undefined' from appearing in embedded example filenames (#19762)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.2-sha-23e0249bb');
+    var VERSION = new i0.Version('10.0.0-rc.2-sha-970d15b88');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-23e0249bb');
+    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-970d15b88');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
