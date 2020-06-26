32a464a74535866ea2d5a4621e62019e630c30b5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-06-26 03:12:23 +0000

    master - b68ae0de0 build: avoid conflicts when publishing snapshot builds (#19737)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-cc36af7d1');
+const VERSION = new Version('10.0.0-rc.2-sha-b68ae0de0');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
