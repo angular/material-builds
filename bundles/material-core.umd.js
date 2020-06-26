548fbf91ae4ab44b2dd43ae0f9a59de66c1128af
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:10:49 +0000

    master - cc36af7d1 feat(youtube-player): support passing in the playerVars parameter (#19746)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.0.0-rc.2-sha-970d15b88');
+    var VERSION = new i0.Version('10.0.0-rc.2-sha-cc36af7d1');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-970d15b88');
+    var VERSION$1 = new i0.Version('10.0.0-rc.2-sha-cc36af7d1');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
