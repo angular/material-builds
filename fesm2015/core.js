e034bd868953745e34466fbdb480947f2f1e6de7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 03:35:01 +0000

    master - ab74abec7 build: some scripts not being linted (#19711)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.0.0-rc.2-sha-e3cf243ed');
+const VERSION = new Version('10.0.0-rc.2-sha-ab74abec7');
 
 /**
  * @license
@@ -58,7 +58,7 @@ let AnimationDurations = /** @class */ (() => {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.0.0-rc.2-sha-e3cf243ed');
+const VERSION$1 = new Version('10.0.0-rc.2-sha-ab74abec7');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
