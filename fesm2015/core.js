281089177718d110372b0d96bd07250ec49052d8
Zack Elliott <4220717+zelliott@users.noreply.github.com>
2020-06-29 15:20:57 +0000

    master - 3d03bf377 fix(focus indicators): Improve styles that determine when focus indicators should render on selection controls. (#19546)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.0.0-rc.2-sha-2a9741824');
+const VERSION = new Version('10.0.0-rc.2-sha-3d03bf377');
 
 /**
  * @license
@@ -58,7 +58,7 @@ let AnimationDurations = /** @class */ (() => {
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.0.0-rc.2-sha-2a9741824');
+const VERSION$1 = new Version('10.0.0-rc.2-sha-3d03bf377');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
