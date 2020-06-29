e731cdbf5fd12bb383484174b898dd2dbf84b03f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-29 15:58:14 +0000

    master - e1bfb927d fix(form-field): error in older versions of edge (#19698)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-next.0-sha-9af441dc6');
+const VERSION = new Version('10.1.0-next.0-sha-e1bfb927d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
