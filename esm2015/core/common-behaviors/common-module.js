03d1101b0e5b0f754e58d06182ff6f31035d26eb
Alberto Aldegheri <albyrock87@gmail.com>
2020-06-29 16:02:25 +0000

    master - 1d6ae5873 fix(cdk/scrolling): fixed-size-virtual-scroll wrong range and position when items length changes and current scroll is greater than new data length (#19578)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-next.0-sha-3ffcc632d');
+const VERSION = new Version('10.1.0-next.0-sha-1d6ae5873');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
