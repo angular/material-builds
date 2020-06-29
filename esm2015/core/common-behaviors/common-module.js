a3d79d22c6f4a39eef37079e899e47993250180d
Jeremy Elbourn <jelbourn@google.com>
2020-06-29 15:48:19 +0000

    master - f4e18ac01 chore: bump master version to 10.1.0-next.0 (#19796)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.0-rc.2-sha-081d833bd');
+const VERSION = new Version('10.1.0-next.0-sha-f4e18ac01');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
