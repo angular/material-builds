971a22a8e5fd1fe247ecb422be9bc0aa633903bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-30 23:05:42 +0000

    master - 00c5a539f fix(cdk/schematics): errors when attempting to read some files (#19783)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.1.0-next.0-sha-f61a6d979');
+const VERSION = new Version('10.1.0-next.0-sha-00c5a539f');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.1.0-next.0-sha-f61a6d979');
+const VERSION$1 = new Version('10.1.0-next.0-sha-00c5a539f');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
