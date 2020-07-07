3957d73fd20f9d66e7b55e42f0cbdb7719717d60
Emanuel Hein <github@e-hein.de>
2020-07-07 19:46:20 +0000

    10.0.x - b0367266b refactor: share environment idependent specs (#19804)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.1-sha-81d053498');
+const VERSION = new Version('10.0.1-sha-b0367266b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
