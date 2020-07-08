6fcddba7467c52ddb059e1758c8db65a082522fc
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-07-08 16:56:31 +0000

    master - 7fa0e98e0 docs(material-experimental/mdc-card): add README (#19890)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.1.0-next.0-sha-90ac2668e');
+const VERSION = new Version('10.1.0-next.0-sha-7fa0e98e0');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.1.0-next.0-sha-90ac2668e');
+const VERSION$1 = new Version('10.1.0-next.0-sha-7fa0e98e0');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
