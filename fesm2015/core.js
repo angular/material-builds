76ff285717abc95d1d2a9229245845360a131f3f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-08 16:59:14 +0000

    master - 39d48c74b fix(drag-drop): not starting auto scroll when inside boundary (#19865)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.1.0-next.0-sha-523f71a1b');
+const VERSION = new Version('10.1.0-next.0-sha-39d48c74b');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.1.0-next.0-sha-523f71a1b');
+const VERSION$1 = new Version('10.1.0-next.0-sha-39d48c74b');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
