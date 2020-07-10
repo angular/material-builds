f08b38a6777a5e9de10092730fe250965338a5ac
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-10 00:18:45 +0000

    master - 912ad8dcf fix(material-experimental/mdc-snack-bar): error during server-side rendering (#19922)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.1.0-next.0-sha-a9e0ac564');
+    var VERSION = new i0.Version('10.1.0-next.0-sha-912ad8dcf');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-a9e0ac564');
+    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-912ad8dcf');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
