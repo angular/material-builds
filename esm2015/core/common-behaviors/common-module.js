18f0161d4727da88a5296669b0a64e2cdf264768
Andrew Seguin <andrewseguin@users.noreply.github.com>
2020-07-10 00:11:33 +0000

    master - dd8a68c3c docs(material-experimental/mdc-chips): add README (#19891)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-next.0-sha-54c2d1676');
+const VERSION = new Version('10.1.0-next.0-sha-dd8a68c3c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.1.0-next.0-sha-54c2d1676`;
+const fallbackMaterialVersionRange = `~10.1.0-next.0-sha-dd8a68c3c`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
