1895f04e5ea0c1304e8a85f1d7f1965257956099
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-10 00:23:09 +0000

    master - 36d4c5d59 refactor(popover-edit): remove circular reference (#19679)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-next.0-sha-e538e1409');
+const VERSION = new Version('10.1.0-next.0-sha-36d4c5d59');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
