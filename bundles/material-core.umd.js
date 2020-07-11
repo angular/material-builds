954b91b97567fdc532dd45c67a23102e1014d454
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-11 00:19:33 +0000

    master - 7427b2658 fix(overlay): outside click dispatcher not accounting for shadow DOM (#19904)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.1.0-next.0-sha-9ce2a9013');
+    var VERSION = new i0.Version('10.1.0-next.0-sha-7427b2658');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-9ce2a9013');
+    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-7427b2658');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
