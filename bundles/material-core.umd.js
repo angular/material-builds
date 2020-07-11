fc548a8817627d15740824c0790a653b0cb9604d
Vanessa Schmitt <vschmitt@google.com>
2020-07-11 00:25:06 +0000

    master - 544e33514 feat(a11y): Add optional home/end key support to ListKeyManager (#19834)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.1.0-next.0-sha-b441bdf6b');
+    var VERSION = new i0.Version('10.1.0-next.0-sha-544e33514');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-b441bdf6b');
+    var VERSION$1 = new i0.Version('10.1.0-next.0-sha-544e33514');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
