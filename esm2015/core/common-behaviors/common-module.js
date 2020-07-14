b8804857f10dae0553d61677e3f85ae40b800c75
mmalerba <mmalerba@google.com>
2020-07-14 18:05:40 +0000

    10.0.x - ba5c2feb0 chore: bump version to 10.0.2 w/ changelog (#19972)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.1-sha-3e9b77fbd');
+const VERSION = new Version('10.0.2-sha-ba5c2feb0');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.0.1-sha-3e9b77fbd`;
+const fallbackMaterialVersionRange = `~10.0.2-sha-ba5c2feb0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
