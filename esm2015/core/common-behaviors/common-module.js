2295a34e071d2c14d2d59a150cbf9ea28b2a85de
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-20 22:55:44 +0000

    master - 747a5d845 fix(material-experimental/mdc-snack-bar): blending in with background in high contrast mode (#19996)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-sha-0777c44c0');
+const VERSION = new Version('10.1.0-sha-747a5d845');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
