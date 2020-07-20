8c32e23553b413ae83b1cc83d9168c8c7a119312
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-20 22:55:26 +0000

    10.0.x - a04c660ee fix(material-experimental/mdc-snack-bar): blending in with background in high contrast mode (#19996)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.0.2-sha-8ae68190d');
+const VERSION = new Version('10.0.2-sha-a04c660ee');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.0.2-sha-8ae68190d');
+const VERSION = new Version('10.0.2-sha-a04c660ee');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.0.2-sha-8ae68190d');
+const VERSION$1 = new Version('10.0.2-sha-a04c660ee');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
