44e950f78ea7083fb7f43b2518519fc7561e9251
Wagner Maciel <wagnermaciel@google.com>
2020-07-21 04:09:13 +0000

    10.0.x - c9aeb3086 Revert "perf(int-bar): Convert MatInkBarFoundation adapter to class object (#19986)" (#20051)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.0.2-sha-a04c660ee`;
+const fallbackMaterialVersionRange = `~10.0.2-sha-c9aeb3086`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
