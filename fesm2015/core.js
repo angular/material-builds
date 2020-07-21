81395fbdd4d0445bd7f070124321992867e216eb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-21 19:30:06 +0000

    master - d53d5b62d fix(drag-drop): not transferring input value when cloning element for preview (#20009)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.1.0-sha-dc245d206');
+const VERSION = new Version('10.1.0-sha-d53d5b62d');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.1.0-sha-dc245d206');
+const VERSION$1 = new Version('10.1.0-sha-d53d5b62d');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
