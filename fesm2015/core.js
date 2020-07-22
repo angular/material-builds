7b9d879f79421d7b89d471b51dd16cdde6630075
Nikhil Walvekar <walvekar@gmail.com>
2020-07-22 22:52:37 +0000

    master - 5f76afa7a docs(stepper): Make CDK stepper example accessible in dark mode. (#20013)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.1.0-sha-e7fe772a7');
+const VERSION = new Version('10.1.0-sha-5f76afa7a');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.1.0-sha-e7fe772a7');
+const VERSION$1 = new Version('10.1.0-sha-5f76afa7a');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
