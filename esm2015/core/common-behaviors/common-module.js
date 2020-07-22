c0ce6440133742a96d49ffa0dd188eac73530d2f
Nikhil Walvekar <walvekar@gmail.com>
2020-07-22 22:51:23 +0000

    10.1.x - ae75fab6e docs(stepper): Make CDK stepper example accessible in dark mode. (#20013)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-sha-40d0b2294');
+const VERSION = new Version('10.1.0-sha-ae75fab6e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
