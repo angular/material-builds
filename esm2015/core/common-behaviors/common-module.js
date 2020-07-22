694cf67c66bae831b84a6502fb35f8544fc7ea55
Michael Prentice <splaktar@gmail.com>
2020-07-22 22:48:36 +0000

    10.1.x - 40d0b2294 docs(material/table): fix example overflowing docs site width (#20063)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.0-sha-310e0b0b0');
+const VERSION = new Version('10.1.0-sha-40d0b2294');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
