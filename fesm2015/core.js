cfaacb66c19d1ed65a5485fcc9c7fd817741fdce
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-28 14:28:45 +0000

    master - a5274a92d fix(cdk/testing): emit pointer events on hover and mouseAway (#20098)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.0-next.0-sha-04b75239b');
+const VERSION = new Version('10.2.0-next.0-sha-a5274a92d');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.0-next.0-sha-04b75239b');
+const VERSION$1 = new Version('10.2.0-next.0-sha-a5274a92d');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
