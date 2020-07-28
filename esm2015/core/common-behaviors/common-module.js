f24ee3c6e016afdf6abed95a5bdc881bc33da425
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-28 14:37:13 +0000

    10.1.x - 37b9664ea fix(overlay): only dispatch position change event if requested (#19785)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.1-sha-f58407dca');
+const VERSION = new Version('10.1.1-sha-37b9664ea');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
