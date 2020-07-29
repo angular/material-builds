52ca2c0bc3ebc3db50aa377a139edb14f72bac26
Joey Perrott <josephperrott.github@gmail.com>
2020-07-29 02:14:19 +0000

    master - a79a71d2b  chore: check in firebase function used for populating caretaker app (#20120)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-bbf56706e`;
+const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-a79a71d2b`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
