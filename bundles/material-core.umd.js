57a9d4cb99c0b9b0b4c714c5905f01fd6d1a8d23
Wagner Maciel <wagnermaciel@google.com>
2020-07-29 02:16:55 +0000

    master - 28f8edfd1 fix(form-field): fix form-field comment for google sync (#20109)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.0-next.0-sha-3ce03c448');
+    var VERSION = new i0.Version('10.2.0-next.0-sha-28f8edfd1');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-3ce03c448');
+    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-28f8edfd1');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
