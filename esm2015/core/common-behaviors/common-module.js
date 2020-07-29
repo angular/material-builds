ccfe566c26962d68fd7321439299bc12f3ffd020
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-07-29 05:40:25 +0000

    master - 5f5f5aa76 Cdk listbox control accessor (#20071)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-28f8edfd1');
+const VERSION = new Version('10.2.0-next.0-sha-5f5f5aa76');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
