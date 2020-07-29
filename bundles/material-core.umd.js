1879ec8bc44d867b0ffd5242c73874e70cb244fa
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-29 15:24:40 +0000

    10.1.x - 57600a5bb build: fix lint failure in MDC option (#20124)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.1.1-sha-3d0f82775');
+    var VERSION = new i0.Version('10.1.1-sha-57600a5bb');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.1.1-sha-3d0f82775');
+    var VERSION$1 = new i0.Version('10.1.1-sha-57600a5bb');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
