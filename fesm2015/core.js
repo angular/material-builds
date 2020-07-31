dc53fa6df998d9779fffeea606d88acbe1973d7f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-07-31 23:00:38 +0000

    master - 338c02ff3 build: add server-side rendering check for kml layer (#20147)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.0-next.0-sha-6470ef3b3');
+const VERSION = new Version('10.2.0-next.0-sha-338c02ff3');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.0-next.0-sha-6470ef3b3');
+const VERSION$1 = new Version('10.2.0-next.0-sha-338c02ff3');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
