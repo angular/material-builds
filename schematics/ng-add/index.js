f36c8e7697bc2e32145767595c1b0fa615f99fd2
Annie Wang <annieyw@google.com>
2020-07-31 23:36:16 +0000

    10.1.x - f80c7d523 prototype(progress-spinner): create prototype progress-spinner based on MDC Web (#20048)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.1.1-sha-fdcf67534`;
+const fallbackMaterialVersionRange = `~10.1.1-sha-f80c7d523`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
