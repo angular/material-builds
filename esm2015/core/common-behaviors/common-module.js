add3b3602a448f97ac890678287ee1767fe29ac7
Jeremy Elbourn <jelbourn@google.com>
2020-08-05 18:15:17 +0000

    10.1.x - 81d904ded build: swap patch & major options in release stage tool (#20167)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.2-sha-7b0bb2c40');
+const VERSION = new Version('10.1.2-sha-81d904ded');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
