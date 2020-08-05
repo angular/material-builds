cc0c3a68cf7e6c6d16b87ee73f49e580661d8b87
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-05 18:13:42 +0000

    master - 37a4eb6c1 docs: add readme for MDC-based experimental dialog (#20180)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-abce69b45`;
+const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-37a4eb6c1`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
