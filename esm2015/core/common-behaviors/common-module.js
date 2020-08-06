90f34d44fbbc4c55fd863c0328b4364b05dc602b
Wagner Maciel <wagnermaciel@google.com>
2020-08-06 17:30:56 +0000

    master - 25ce323fb test(mdc-form-field): add performance tests for mdc-form-field (#20135)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-e83dbb11c');
+const VERSION = new Version('10.2.0-next.0-sha-25ce323fb');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
