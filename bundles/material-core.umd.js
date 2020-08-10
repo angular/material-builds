253011e46be3d6244b7461cb52e994e114e03492
mmalerba <mmalerba@google.com>
2020-08-10 22:22:17 +0000

    10.1.x - df7f880d0 chore: bump version to 10.1.3 w/ changelog (#20261)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.1.2-sha-51b947d05');
+    var VERSION = new i0.Version('10.1.3-sha-df7f880d0');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.1.2-sha-51b947d05');
+    var VERSION$1 = new i0.Version('10.1.3-sha-df7f880d0');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.1.2-sha-51b947d05');
+const VERSION = new Version('10.1.3-sha-df7f880d0');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
