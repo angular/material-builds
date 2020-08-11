7b3ca1517855cfea101867d69f764b79f302b461
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-11 13:38:16 +0000

    master - 098be5f0e doc(stepper): add missing doc region (#20264)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-25ce323fb');
+const VERSION = new Version('10.2.0-next.0-sha-098be5f0e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
