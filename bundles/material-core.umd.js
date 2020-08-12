0eb7776ad0b72ca234c8c814c98ca498b0ee0650
crisbeto <crisbeto@abv.bg>
2020-08-12 13:59:45 +0000

    master - cb8de6195 test(cdk-experimental/menu): fix test failure

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.0-next.0-sha-3828b7e2e');
+    var VERSION = new i0.Version('10.2.0-next.0-sha-cb8de6195');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-3828b7e2e');
+    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-cb8de6195');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
