8600de3f1577f7efabfbe8e40176a1652c9a1372
Andy Chrzaszcz <andymchr@google.com>
2020-08-12 04:28:06 +0000

    master - beaf07254 fix(cdk-experimental/menu): ensure context menu is closed out with other menu elements on page (#20234)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-75042e47d');
+const VERSION = new Version('10.2.0-next.0-sha-beaf07254');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
