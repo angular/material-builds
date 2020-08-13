4d16b6741786219709fce2e620f5138c087e4182
Wagner Maciel <wagnermaciel@google.com>
2020-08-13 19:10:46 +0000

    master - df516c521 test(mdc-checkbox): add performance tests for mdc-checkbox (#20129)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-68b43740b');
+const VERSION = new Version('10.2.0-next.0-sha-df516c521');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
