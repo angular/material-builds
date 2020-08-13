07a218bd7e07bc54f2af32cbb324e10209fb1b7f
Rafael Santana <rafaelsantana3095@gmail.com>
2020-08-13 19:06:37 +0000

    master - b2d11a6f2 docs(stepper): correct file name in markdown (#20288)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-5f87552d1`;
+const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-b2d11a6f2`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
