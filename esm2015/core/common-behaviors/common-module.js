aebc20b652ba0e3b8f5008458b03f95c59e3ed74
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-08-14 19:15:52 +0000

    master - 23d3c216c feat(cdk-experimental/combobox): make CdkListbox compatible with CdkCombobox (#20291)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-809f1579e');
+const VERSION = new Version('10.2.0-next.0-sha-23d3c216c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
