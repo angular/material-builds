a445521d1ae9594e464dc4e8061203b90069bfb7
nielsr98 <54564567+nielsr98@users.noreply.github.com>
2020-08-18 00:55:32 +0000

    master - d7754e7f7 Combobox listbox refactor (#20339)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-23d3c216c');
+const VERSION = new Version('10.2.0-next.0-sha-d7754e7f7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
