39537ba76cf7b853fc6038fb5d76c0dbb4f1a644
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-18 01:07:02 +0000

    master - d04a12694 docs(form-field): implement error state getter in custom control example (#20275)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-69481861f`;
+const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-d04a12694`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
