955e19eb94b610020207410d06d91925839981be
crisbeto <crisbeto@abv.bg>
2020-08-18 01:00:14 +0000

    master - c223fa77b build: add material experimental testing packages to system config

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-d7754e7f7');
+const VERSION = new Version('10.2.0-next.0-sha-c223fa77b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
