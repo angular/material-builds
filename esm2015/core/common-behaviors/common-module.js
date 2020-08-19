5bf4c06a99e33d1892f0f3c40ba54d7079cbff6a
Alexey Zuev <alexzuza@yandex.ru>
2020-08-19 01:19:51 +0000

    master - 8c336c9cf fix(cdk/drag-drop): preserve checked state for grouped radio inputs (#20237)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-490a8939f');
+const VERSION = new Version('10.2.0-next.0-sha-8c336c9cf');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
