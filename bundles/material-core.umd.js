d2bb5818f9e1cba1a9bcad1efb7ce159a0839e65
Rafael Santana <rafaelsantana3095@gmail.com>
2020-08-19 01:21:03 +0000

    master - e3699dc3c fix(cdk/layout): support readonly arrays for public methods (#20252)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.0-next.0-sha-8c336c9cf');
+    var VERSION = new i0.Version('10.2.0-next.0-sha-e3699dc3c');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-8c336c9cf');
+    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-e3699dc3c');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-8c336c9cf');
+const VERSION = new Version('10.2.0-next.0-sha-e3699dc3c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
