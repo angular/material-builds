996f5a696b1e38c5ed13a27ea32396502964bbec
Andy Chrzaszcz <andymchr@google.com>
2020-08-20 18:47:23 +0000

    master - 06a9843ef feat(material-experimental/menubar): update the dev-app to include a fake mat-menu (#20365)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-55f788200');
+const VERSION = new Version('10.2.0-next.0-sha-06a9843ef');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
