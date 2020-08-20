767c67b0475336a828fae09f4e160466e7e1d7fd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-20 18:55:05 +0000

    master - e784f8f2f fix(material-experimental/mdc-menu/testing): add unimplemented methods (#20352)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-9a4adb7bb');
+const VERSION = new Version('10.2.0-next.0-sha-e784f8f2f');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-9a4adb7bb`;
+const fallbackMaterialVersionRange = `~10.2.0-next.0-sha-e784f8f2f`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
