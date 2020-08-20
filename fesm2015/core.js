b4753c8575505997509475f9c6c6953eab298840
Duncan <info@e-ips.com>
2020-08-20 18:52:13 +0000

    master - ac2d385de docs(material/form-field): updated documentation (#20355)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.0-next.0-sha-bbf75f3c7');
+const VERSION = new Version('10.2.0-next.0-sha-ac2d385de');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.0-next.0-sha-bbf75f3c7');
+const VERSION$1 = new Version('10.2.0-next.0-sha-ac2d385de');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
