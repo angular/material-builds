3d26ab61a276e5037ab2fc7984dc7e8e5b41ce93
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-21 15:10:26 +0000

    master - 64145fd60 fix(checkbox): not handling unknown color palette (#18467)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.0-next.0-sha-21f2df349');
+    var VERSION = new i0.Version('10.2.0-next.0-sha-64145fd60');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-21f2df349');
+    var VERSION$1 = new i0.Version('10.2.0-next.0-sha-64145fd60');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-next.0-sha-21f2df349');
+const VERSION = new Version('10.2.0-next.0-sha-64145fd60');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
