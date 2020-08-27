f8b282eb16df7d4cb56dbf1b9bf9d16c4a79a86e
Jeremy Elbourn <jelbourn@google.com>
2020-08-27 00:24:15 +0000

    master - 9094de847 chore: bump version to 10.2.0-rc.0 w/ changelog (#20430)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.0-next.0-sha-7a2a7327a');
+const VERSION = new Version('10.2.0-rc.0-sha-9094de847');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.0-next.0-sha-7a2a7327a');
+const VERSION$1 = new Version('10.2.0-rc.0-sha-9094de847');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
