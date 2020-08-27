759d1584d91ae5c0b9e3d979aa3facf0778b00ef
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-27 19:01:02 +0000

    master - de8fd2166 build: fix error from latest MDC canary version (#20439)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-rc.0-sha-9094de847');
+const VERSION = new Version('10.2.0-rc.0-sha-de8fd2166');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
