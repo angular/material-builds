6e42269df62cacd465aa0f25bae0d95146f8112e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:18:46 +0000

    master - f795b9d8c fix(drag-drop): expose some missing injection tokens (#20410)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.0.0-next.0-sha-1d0e3c76f');
+const VERSION = new Version('11.0.0-next.0-sha-f795b9d8c');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.0.0-next.0-sha-1d0e3c76f');
+const VERSION$1 = new Version('11.0.0-next.0-sha-f795b9d8c');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
