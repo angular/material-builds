9808627a9187e702119e934ef3ac06abceb065b0
Andy Chrzaszcz <andymchr@google.com>
2020-08-28 05:17:14 +0000

    master - 1d0e3c76f fix(cdk/overlay): ensure error isn't thrown when many overlay items are closed from handler (#20377)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.0.0-next.0-sha-1cc520c8c');
+    var VERSION = new i0.Version('11.0.0-next.0-sha-1d0e3c76f');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.0.0-next.0-sha-1cc520c8c');
+    var VERSION$1 = new i0.Version('11.0.0-next.0-sha-1d0e3c76f');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
