d53635145b84cc88dc1597b0149f40ccda3e37a4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:22:26 +0000

    master - 23b4c88ed fix(material-experimental/mdc-core): active option not visible in high contrast mode (#20396)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.0-next.0-sha-169ebc47a`;
+const fallbackMaterialVersionRange = `~11.0.0-next.0-sha-23b4c88ed`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
