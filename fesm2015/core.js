1092cab12b5b3674363cc87a27503edba33b4836
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:22:50 +0000

    master - 54218cd78 build: add support for shorthand names in API golden approval script (#20394)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.0.0-next.0-sha-23b4c88ed');
+const VERSION = new Version('11.0.0-next.0-sha-54218cd78');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.0.0-next.0-sha-23b4c88ed');
+const VERSION$1 = new Version('11.0.0-next.0-sha-54218cd78');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
