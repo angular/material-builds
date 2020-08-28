6c81d2b2fa746ae38885c20ed2d8cb1027c2d239
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-08-28 05:18:52 +0000

    master - 6d1a25964 fix(cdk-experimental/combobox): not cleaning up overlay on destroy (#20407)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-1d0e3c76f');
+const VERSION = new Version('11.0.0-next.0-sha-6d1a25964');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
