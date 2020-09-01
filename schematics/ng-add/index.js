904aad3e51e0ea517dbbc505ddb1c6f6275052e8
mmalerba <mmalerba@google.com>
2020-09-01 20:42:24 +0000

    master - e01fac457 fix(cdk/testing): fix change detection timing in testbed (#20465)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.0-next.0-sha-781594826`;
+const fallbackMaterialVersionRange = `~11.0.0-next.0-sha-e01fac457`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
