e871a967ba9901b32cb8b040b1b0bcff5017223b
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2020-09-02 21:41:30 +0000

    master - 58e0c4875 feat(cdk/table): fixed table layouts (#20258)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-971553d96');
+const VERSION = new Version('11.0.0-next.0-sha-58e0c4875');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
