96689d7d198f37339745822c7b201a0ab3da236c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-02 21:38:50 +0000

    master - 42fb35b26 build: add a script to flag missing unit tests in MDC packages (#20471)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-17c67e5d2');
+const VERSION = new Version('11.0.0-next.0-sha-42fb35b26');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
