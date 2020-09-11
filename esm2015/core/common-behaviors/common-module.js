a5dcdf8b28a533d3b0cc4f65ba07e7d81735c326
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-11 14:08:05 +0000

    master - 1f5b0a8c4 docs: fix missing space in mdc-dialog readme (#20542)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-8b1c7f294');
+const VERSION = new Version('11.0.0-next.0-sha-1f5b0a8c4');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
