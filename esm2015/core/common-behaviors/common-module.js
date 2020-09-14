fd0e0829da631b7a8738d88195e45601320e9249
Andrew Seguin <andrewjs@google.com>
2020-09-14 20:40:12 +0000

    10.2.x - ec14f8f3b chore: bump version to 10.2.1 w/ changelog

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.0-sha-2ca019474');
+const VERSION = new Version('10.2.1-sha-ec14f8f3b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
