95bcb7e3c60a54d50990d57fc57db63ae340327b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-16 23:58:11 +0000

    master - f8ab9ba8e refactor(drag-drop): change deprecated APIs for v11 (#20500)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-4b3178c60');
+const VERSION = new Version('11.0.0-next.0-sha-f8ab9ba8e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
