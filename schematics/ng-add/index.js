3b398c85e89f5a37c103a8b2bbf53a19952a3860
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-22 16:03:26 +0000

    10.2.x - fc7ba87e0 docs(material/paginator): add explicit type for better formatting in docs (#20608)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.2-sha-b8f624e4a`;
+const fallbackMaterialVersionRange = `~10.2.2-sha-fc7ba87e0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
