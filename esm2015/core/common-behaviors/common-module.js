c78e557d882dc9b0068c985e5ee1942ab6e4e629
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-22 16:10:12 +0000

    master - 2967ad0c2 docs(material/paginator): add explicit type for better formatting in docs (#20608)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-e8265ccaf');
+const VERSION = new Version('11.0.0-next.0-sha-2967ad0c2');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
