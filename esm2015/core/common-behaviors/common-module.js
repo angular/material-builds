349418b104f893be0de682589411b131b73b6525
Ron Lapushner <lapushnerron@gmail.com>
2020-09-29 17:05:50 +0000

    master - b0d0388c1 fix(youtube-player): YT.Player is not a constructor (#20616)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.0-sha-5c42f3cea');
+const VERSION = new Version('11.0.0-next.0-sha-b0d0388c1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
