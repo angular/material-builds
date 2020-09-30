1c87b8f27db2ace0571e3b3e456b44c6538e1033
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-09-30 21:12:00 +0000

    10.2.x - 2634c3318 test(material-experimental): add missing test coverage (#20678)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.3-sha-3393ec7d8');
+const VERSION = new Version('10.2.3-sha-2634c3318');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
