02ce4be76dd0cd40765f1315e4584a12080c2183
Joey Perrott <josephperrott.github@gmail.com>
2020-10-02 22:30:46 +0000

    10.2.x - ce49e2f2c build: configure dependabot updates (#20708)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.3-sha-2e00875d3');
+const VERSION = new Version('10.2.3-sha-ce49e2f2c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
