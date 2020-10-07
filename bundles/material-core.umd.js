73c535fb904207a79a172cd6d567a4acf1a9dbb2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-07 21:13:44 +0000

    master - 6201764bf test(material/datepicker): fix test failure (#20746)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.0.0-next.1-sha-54f36a3e6');
+    var VERSION = new i0.Version('11.0.0-next.1-sha-6201764bf');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.0.0-next.1-sha-54f36a3e6');
+    var VERSION$1 = new i0.Version('11.0.0-next.1-sha-6201764bf');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.1-sha-54f36a3e6');
+const VERSION = new Version('11.0.0-next.1-sha-6201764bf');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
