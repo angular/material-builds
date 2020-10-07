ca240834a51875833d4190da259199616e53296e
Mark Hansen <mark@markhansen.co.nz>
2020-10-07 19:25:55 +0000

    master - d9359c4f5 fix(material/tooltip): convert message to string (#20685)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.0-next.1-sha-b59e4a18c`;
+const fallbackMaterialVersionRange = `~11.0.0-next.1-sha-d9359c4f5`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
