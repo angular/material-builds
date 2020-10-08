b9187b04098dc5fe3b28d9af73cda157e44afb50
Joey Perrott <josephperrott.github@gmail.com>
2020-10-08 03:17:49 +0000

    master - c9b7a1ac0 Restore commit messages on failures (#20707)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.1-sha-b7cfe6737');
+const VERSION = new Version('11.0.0-next.1-sha-c9b7a1ac0');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
