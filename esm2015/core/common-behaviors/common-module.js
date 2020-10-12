ad9b7986250606d9b07358aeb47e8969710a08ce
Wagner Maciel <wagnermaciel@google.com>
2020-10-12 17:50:16 +0000

    master - d82e3cb86 build: update dev-infra-private version (#20770)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.1-sha-7d99c358a');
+const VERSION = new Version('11.0.0-next.1-sha-d82e3cb86');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.0.0-next.1-sha-7d99c358a');
+const VERSION = new Version('11.0.0-next.1-sha-d82e3cb86');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.0.0-next.1-sha-7d99c358a');
+const VERSION$1 = new Version('11.0.0-next.1-sha-d82e3cb86');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
