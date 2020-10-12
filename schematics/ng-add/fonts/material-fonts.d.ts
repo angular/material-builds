d64c2cdf28ff86c11178a98fe579c70950fcdc38
Kristiyan Kostadinov <crisbeto@abv.bg>
2020-10-12 19:03:16 +0000

    master - 5bac82832 refactor(cdk/schematics): switch to latest API version

--- schematics/ng-add/fonts/material-fonts.d.ts
+++ schematics/ng-add/fonts/material-fonts.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Tree } from '@angular-devkit/schematics';
+import { Rule } from '@angular-devkit/schematics';
 import { Schema } from '../schema';
 /** Adds the Material Design fonts to the index HTML file. */
-export declare function addFontsToIndex(options: Schema): (host: Tree) => Tree;
+export declare function addFontsToIndex(options: Schema): Rule;
--- schematics/ng-add/theming/theming.d.ts
+++ schematics/ng-add/theming/theming.d.ts
@@ -5,9 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { Rule } from '@angular-devkit/schematics';
 import { Schema } from '../schema';
 /** Add pre-built styles to the main project style file. */
 export declare function addThemeToAppStyles(options: Schema): Rule;
 /** Adds the global typography class to the body element. */
-export declare function addTypographyClass(options: Schema): (host: Tree) => Tree;
+export declare function addTypographyClass(options: Schema): Rule;
