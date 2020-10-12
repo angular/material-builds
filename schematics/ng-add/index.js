499bf689e3a3f37c168dd9fce47a1b99f489bce8
Wagner Maciel <wagnermaciel@google.com>
2020-10-12 17:50:15 +0000

    10.2.x - 69ac70ca2 build: update dev-infra-private version (#20770)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.4-sha-e3445d698`;
+const fallbackMaterialVersionRange = `~10.2.4-sha-69ac70ca2`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
