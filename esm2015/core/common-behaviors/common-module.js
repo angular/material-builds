6de45d820dc5c10b33d19707b2d7096f747e25c4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-14 23:50:45 +0000

    master - 59d3840ce build: fix errors against latest @angular/bazel version (#20779)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.1-sha-0909bc91a');
+const VERSION = new Version('11.0.0-next.1-sha-59d3840ce');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
