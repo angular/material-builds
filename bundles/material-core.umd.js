2895965032e1148ae5bc7c8a35042e18d1c5fd8d
Annie Wang <annieyw@google.com>
2020-10-15 01:41:01 +0000

    10.2.x - 8ad400344 release: bump version to 10.2.5 w/ changelog (#20797)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.4-sha-36ce94eb6');
+    var VERSION = new i0.Version('10.2.5-sha-8ad400344');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.4-sha-36ce94eb6');
+    var VERSION$1 = new i0.Version('10.2.5-sha-8ad400344');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.4-sha-36ce94eb6');
+const VERSION = new Version('10.2.5-sha-8ad400344');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
