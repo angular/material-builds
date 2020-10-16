59f7484d193e17146ddb81fafd4c31b99df8ad44
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-16 19:05:01 +0000

    master - f854196a8 fix(material-experimental/mdc-slide-toggle): align focus behavior with standard version (#20772)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.0.0-next.2-sha-5eb10359d');
+    var VERSION = new i0.Version('11.0.0-next.2-sha-f854196a8');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.0.0-next.2-sha-5eb10359d');
+    var VERSION$1 = new i0.Version('11.0.0-next.2-sha-f854196a8');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.2-sha-5eb10359d');
+const VERSION = new Version('11.0.0-next.2-sha-f854196a8');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
