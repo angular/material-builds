63fe917708a3cf5a5bdeb5ad003d54aac2c5ff81
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-16 19:03:38 +0000

    master - 5e9e41bfd build: fix error in size check tests (#20808)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.2-sha-e03a4d6fa');
+const VERSION = new Version('11.0.0-next.2-sha-5e9e41bfd');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
