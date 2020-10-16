3d9741923b34ae558bb4e5c0cee6044d85bf70e8
Annie Wang <annieyw@google.com>
2020-10-16 19:11:13 +0000

    master - 2d79ab793 docs(material/button): add harness example (#20722)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.2-sha-06294d1ca');
+const VERSION = new Version('11.0.0-next.2-sha-2d79ab793');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
