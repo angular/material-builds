6b8d33f19063bb4dbca8a783302124b9847d0913
Lukáš Matta <lukasmatta@users.noreply.github.com>
2020-10-19 19:26:37 +0000

    master - 298ed5c92 docs: fix a handful of typos (#20824)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.2-sha-ca13dc0d8');
+const VERSION = new Version('11.0.0-next.2-sha-298ed5c92');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
