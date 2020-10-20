529e9d2f6585fcfddf186010fb309615e222ac22
Aristeidis Bampakos <bampakoa@gmail.com>
2020-10-20 14:47:14 +0000

    master - d5eb389f3 docs: add types in injection token examples (#20818)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.0-next.2-sha-17de49c43`;
+const fallbackMaterialVersionRange = `~11.0.0-next.2-sha-d5eb389f3`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
