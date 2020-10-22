08440968a7fc661d6cabbadac10b4e64320b091a
Jeremy Elbourn <jelbourn@google.com>
2020-10-22 18:21:20 +0000

    master - 41479fc02 release: set master to 11.1.0-next.0 (#20860)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-next.2-sha-cc9758d49');
+const VERSION = new Version('11.1.0-next.0-sha-41479fc02');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
