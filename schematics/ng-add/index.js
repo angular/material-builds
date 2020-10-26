30efce2520775efa619df93d6780fafdd83880ee
Annie Wang <annieyw@google.com>
2020-10-26 14:19:47 +0000

    10.2.x - 97ab37ad0 docs(material/button-toggle): add button toggle harness example (#20837)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~10.2.5-sha-8c46f07af`;
+const fallbackMaterialVersionRange = `~10.2.5-sha-97ab37ad0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
