449111df20384056c14b940f06a15e103840f2f8
Annie Wang <annieyw@google.com>
2020-10-26 14:17:01 +0000

    11.0.x - b2a1ea5ec docs(material/button-toggle): add button toggle harness example (#20837)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-rc.0-sha-83a54ad4d');
+const VERSION = new Version('11.0.0-rc.0-sha-b2a1ea5ec');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
