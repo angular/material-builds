ec6476635c04a39a76a9c97e06e9d95cf5481159
Jeremy Elbourn <jelbourn@google.com>
2020-10-27 20:07:52 +0000

    10.2.x - 0ccaef987 release: bump version to 10.2.6 w/ changelog (#20899)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.5-sha-97ab37ad0');
+const VERSION = new Version('10.2.6-sha-0ccaef987');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.5-sha-97ab37ad0');
+const VERSION$1 = new Version('10.2.6-sha-0ccaef987');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
