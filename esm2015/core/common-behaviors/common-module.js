2baa6a712574988f204e4e2c6aff6d9e3e878aad
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-10-28 18:10:46 +0000

    10.2.x - 38dc5113c fix(google-maps): maintain subscriptions across event targets (#20897)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.6-sha-863845613');
+const VERSION = new Version('10.2.6-sha-38dc5113c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
