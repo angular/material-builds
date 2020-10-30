62a4e1d808e8b98f8c6d43034e1c96c0836b7163
mmalerba <mmalerba@google.com>
2020-10-30 03:58:20 +0000

    10.2.x - 308dd2470 fix(material-experimental/mdc-snack-bar): add missing exports (#20928)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.6-sha-3127b0107');
+const VERSION = new Version('10.2.6-sha-308dd2470');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('10.2.6-sha-3127b0107');
+const VERSION = new Version('10.2.6-sha-308dd2470');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('10.2.6-sha-3127b0107');
+const VERSION$1 = new Version('10.2.6-sha-308dd2470');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
