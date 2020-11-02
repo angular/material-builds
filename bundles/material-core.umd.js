be0c774d1f9566041c44293158c56bbbb6ae9177
mmalerba <mmalerba@google.com>
2020-11-02 14:23:17 +0000

    master - 3e9a26c92 fix(material-experimental/mdc-checkbox): fix broken dark theme (#20940)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.1.0-next.0-sha-07f6a4892');
+    var VERSION = new i0.Version('11.1.0-next.0-sha-3e9a26c92');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-07f6a4892');
+    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-3e9a26c92');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
