e861b7f49cfb8f55fb66673729473606587e504c
Wagner Maciel <wagnermaciel@google.com>
2020-11-02 23:11:59 +0000

    10.2.x - 7226f2184 release: bump version to 10.2.7 w/ changelog (#20955)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('10.2.6-sha-15e480285');
+    var VERSION = new i0.Version('10.2.7-sha-7226f2184');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('10.2.6-sha-15e480285');
+    var VERSION$1 = new i0.Version('10.2.7-sha-7226f2184');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.6-sha-15e480285');
+const VERSION = new Version('10.2.7-sha-7226f2184');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
