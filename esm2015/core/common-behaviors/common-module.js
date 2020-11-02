9f83f80f2a3f1b34b378ba49e00e8879a4bf7778
mmalerba <mmalerba@google.com>
2020-11-02 14:24:48 +0000

    10.2.x - 15e480285 fix(material-experimental/mdc-checkbox): fix broken dark theme (#20940)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('10.2.6-sha-0fa0b458a');
+const VERSION = new Version('10.2.6-sha-15e480285');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
