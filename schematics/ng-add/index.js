1901eb04c373f89443950b1bea93c6ecc6d19398
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-06 03:56:16 +0000

    master - c052ab39c refactor(material/core): remove underscores from symbols consumed in other files (#20906)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-a0d4ce302`;
+const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-c052ab39c`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
