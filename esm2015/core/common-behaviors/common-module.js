6a55a6a8d19bf92e3f8d9c2292f655c1253def27
Annie Wang <annieyw@google.com>
2020-11-10 22:51:56 +0000

    master - bab8d4f7a release: bump version to 11.0.0-rc.4 w/ changelog (#21010)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-rc.2-sha-bc43bf63f');
+const VERSION = new Version('11.0.0-rc.4-sha-bab8d4f7a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
