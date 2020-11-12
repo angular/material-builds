025b5f2d235577c0f31504e485044ebe52dcbfd1
mmalerba <mmalerba@google.com>
2020-11-12 02:18:41 +0000

    master - 478e5780e release: add 11.0.0 changelog and update packages.bzl (#21023)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.0.0-rc.4-sha-d6a357bfa');
+const VERSION = new Version('11.0.0-rc.4-sha-478e5780e');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.0.0-rc.4-sha-d6a357bfa');
+const VERSION$1 = new Version('11.0.0-rc.4-sha-478e5780e');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
