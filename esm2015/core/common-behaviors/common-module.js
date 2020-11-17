f529d7be4ded33c331f8626beddb200d09aa7b8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 17:21:34 +0000

    master - 17c357f69 fix(google-maps): info window not opening if no anchor is passed in (#21014)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-5e9cc5f88');
+const VERSION = new Version('11.1.0-next.0-sha-17c357f69');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
