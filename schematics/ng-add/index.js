97de8a1b0f58062b71bddafc8e7abebeb5ea9869
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 23:55:21 +0000

    11.0.x - 445b1a59c refactor(google-maps): simplify marker internal setup (#20975)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.0-sha-7ab8a8e0c`;
+const fallbackMaterialVersionRange = `~11.0.0-sha-445b1a59c`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
