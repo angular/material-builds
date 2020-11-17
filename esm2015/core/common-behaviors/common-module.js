151cb00784c7926ba549eb6606a94a45beaf070b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-17 23:54:29 +0000

    master - 0e881259d refactor(google-maps): simplify marker internal setup (#20975)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-de5a940b0');
+const VERSION = new Version('11.1.0-next.0-sha-0e881259d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
