71e3a56af18a2f7d6ce73da504635701d90ef760
Annie Wang <annieyw@google.com>
2020-11-19 20:38:16 +0000

    master - 5ab55fc52 docs(material/tooltip): add harness example for tooltip (#21083)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-dd8a92503`;
+const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-5ab55fc52`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
