338635c8d6bbe13972aa045231a3711e96aa89c1
Annie Wang <annieyw@google.com>
2020-11-19 13:20:12 +0000

    master - 2a9919a65 docs(material/tree): add harness example for tree (#21082)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.1.0-next.0-sha-d511cd043');
+    var VERSION = new i0.Version('11.1.0-next.0-sha-2a9919a65');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-d511cd043');
+    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-2a9919a65');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-d511cd043');
+const VERSION = new Version('11.1.0-next.0-sha-2a9919a65');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
