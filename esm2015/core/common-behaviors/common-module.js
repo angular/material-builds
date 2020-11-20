0949665e7102252de51144ced7e93f89719d602a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-11-20 19:50:11 +0000

    master - bb2bb9c30 refactor(material-experimental/mdc-core): remove underscores from imported mixins (#21060)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-43997571d');
+const VERSION = new Version('11.1.0-next.0-sha-bb2bb9c30');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
