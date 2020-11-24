e7b997832a0cf024234941c87a0b0bcc03d2cde2
Wagner Maciel <wagnermaciel@google.com>
2020-11-24 18:15:18 +0000

    master - c14d92ce7 release: update changelog (#21140)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-5bfe312b5');
+const VERSION = new Version('11.1.0-next.0-sha-c14d92ce7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
