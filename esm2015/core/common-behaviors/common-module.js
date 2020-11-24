b4bbc03f37a714a57ed90e10edecd097be8f7003
Wagner Maciel <wagnermaciel@google.com>
2020-11-24 01:11:18 +0000

    11.0.x - 00127c12c build: update yarn.lock (#21125)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.0-sha-5800ab99b');
+const VERSION = new Version('11.0.0-sha-00127c12c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
