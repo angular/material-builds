b86c19b345af047d2fedf72c334c0e9a98cfabae
Amit Gharat <amit.2006.it@gmail.com>
2020-12-01 22:15:17 +0000

    master - bc9d56c94 docs(material/checkbox): use the defined color in the template (#21163) (#21167)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.1.0-next.0-sha-2f43fd9d3');
+    var VERSION = new i0.Version('11.1.0-next.0-sha-bc9d56c94');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-2f43fd9d3');
+    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-bc9d56c94');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
