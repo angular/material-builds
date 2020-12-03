f9502ce33c6ae1b50692e10d52e6b81be280c02a
mmalerba <mmalerba@google.com>
2020-12-03 18:45:18 +0000

    master - 1a6f9f4f3 feat(material/core): add typography config normalization function (#21059)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.1.0-next.0-sha-8ee56a5b2');
+const VERSION = new Version('11.1.0-next.0-sha-1a6f9f4f3');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.1.0-next.0-sha-8ee56a5b2');
+const VERSION$1 = new Version('11.1.0-next.0-sha-1a6f9f4f3');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-8ee56a5b2`;
+const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-1a6f9f4f3`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
