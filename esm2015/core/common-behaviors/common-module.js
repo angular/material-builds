eddaf8425419ed258ea1272d19ed00ace6e75e92
mmalerba <mmalerba@google.com>
2020-12-12 00:07:11 +0000

    11.0.x - c6231e19b refactor(material/core): make typography normalization function private (#21286)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.2-sha-75e23cb8d');
+const VERSION = new Version('11.0.2-sha-c6231e19b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
