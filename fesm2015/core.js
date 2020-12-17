736bfde306c4ca932d6b5c80f2e08e0316cf2b86
Esteban Gehring <esteban.marin@bithost.ch>
2020-12-17 07:31:24 +0000

    master - 98f6fd99e docs(material/select): update accessibility information on roles (#21372)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.1.0-next.0-sha-d2aa75159');
+const VERSION = new Version('11.1.0-next.0-sha-98f6fd99e');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.1.0-next.0-sha-d2aa75159');
+const VERSION$1 = new Version('11.1.0-next.0-sha-98f6fd99e');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
