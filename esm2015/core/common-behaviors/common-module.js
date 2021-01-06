0ddd2fcb463b4c4babdfdb941da3b22dcb9ba29b
Annie Wang <annieyw@google.com>
2021-01-06 01:55:50 +0000

    11.0.x - ec43ff2c3 docs(material/list): add harness example for list (#21450)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.3-sha-b1d8fb3e6');
+const VERSION = new Version('11.0.3-sha-ec43ff2c3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
