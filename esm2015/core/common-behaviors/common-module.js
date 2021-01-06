1c829e104e7ef793dc247711689d41c0cd109580
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-06 17:19:25 +0000

    master - f7bd2242e fix(material/datepicker): a couple of accessibility issues in touch UI mode (#21228)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-e42d502f8');
+const VERSION = new Version('11.1.0-next.0-sha-f7bd2242e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
