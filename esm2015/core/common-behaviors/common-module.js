78733f3c2e37986900ed1f07af9711289de64b09
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-06 17:20:49 +0000

    master - eb318d938 fix(cdk/drag-drop): don't block scrolling if it happens before delay has elapsed (#21382)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-f7bd2242e');
+const VERSION = new Version('11.1.0-next.0-sha-eb318d938');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
