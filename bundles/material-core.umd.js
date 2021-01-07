1f4898717aedf5ffd54735fc6301ddb511f0f593
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-07 16:58:45 +0000

    master - 6c1001f5b build: update to gulp 4 (#21445)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.1.0-next.0-sha-fb833e75c');
+    var VERSION = new i0.Version('11.1.0-next.0-sha-6c1001f5b');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-fb833e75c');
+    var VERSION$1 = new i0.Version('11.1.0-next.0-sha-6c1001f5b');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-fb833e75c`;
+const fallbackMaterialVersionRange = `~11.1.0-next.0-sha-6c1001f5b`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
