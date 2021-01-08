82b225a6571fd9bbe1ac56376ef5625be4075f5b
Karl Seamon <bannanafiend@gmail.com>
2021-01-08 07:27:37 +0000

    master - deead4c69 feat(cdk-experimental/table-scroll-container): Create directive and demo (#21200)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-6c1001f5b');
+const VERSION = new Version('11.1.0-next.0-sha-deead4c69');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
