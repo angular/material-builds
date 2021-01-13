8bfb776635d3a45f73fd5c2c447f2ccb4cbe2604
Joey Perrott <josephperrott.github@gmail.com>
2021-01-13 18:18:46 +0000

    11.0.x - f0abd76c1 build: update to latest @angular/dev-infra-private and husky@5 (#21559)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.0.3-sha-6771ca470');
+const VERSION = new Version('11.0.3-sha-f0abd76c1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
