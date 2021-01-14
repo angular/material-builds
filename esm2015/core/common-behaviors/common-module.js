1ffbab5f70481e370680abad956a2f5fe0900f7d
mmalerba <mmalerba@google.com>
2021-01-14 20:09:35 +0000

    master - 270b751c5 release: update angular deps (#21580)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-next.0-sha-5fc425bc3');
+const VERSION = new Version('11.1.0-next.0-sha-270b751c5');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
