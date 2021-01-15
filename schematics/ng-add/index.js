cab83337b68e4f4ea438f87f03ac491f1b4cdf07
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-01-15 21:59:59 +0000

    11.1.x - 0611826e0 build: downgrade bazel dependencies (#21610)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.1.0-rc.0-sha-7343060fd`;
+const fallbackMaterialVersionRange = `~11.1.0-rc.0-sha-0611826e0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
