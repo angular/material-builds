ceeb64eeed3ef2110b3cb9170c4642bfcad003a7
Miles Malerba <mmalerba@google.com>
2021-01-15 22:04:01 +0000

    11.0.x - 02201c9ea Revert "build: bump @bazel/jasmine from 2.2.1 to 3.0.0 (#21424)"

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.0.4-sha-a13da618d`;
+const fallbackMaterialVersionRange = `~11.0.4-sha-02201c9ea`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
