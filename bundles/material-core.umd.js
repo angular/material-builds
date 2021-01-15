28bfd23b30ac219b304c620633370f92477e9267
Joey Perrott <josephperrott.github@gmail.com>
2021-01-15 23:08:41 +0000

    master - 0911820f9 build: configure the ng-dev caretaker check command (#21611)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.2.0-next.0-sha-4ecdcb53a');
+    var VERSION = new i0.Version('11.2.0-next.0-sha-0911820f9');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.2.0-next.0-sha-4ecdcb53a');
+    var VERSION$1 = new i0.Version('11.2.0-next.0-sha-0911820f9');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.0-next.0-sha-4ecdcb53a');
+const VERSION = new Version('11.2.0-next.0-sha-0911820f9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
