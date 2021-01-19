2ca19259467e5189410fdd698d80da189bbb63a7
Annie Wang <annieyw@google.com>
2021-01-19 17:24:55 +0000

    11.1.x - f76369ab7 fix(material/snack-bar): fix Firefox/JAWS not reading out snackbar message (#21552)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.1.0-rc.0-sha-fd547f4aa');
+const VERSION = new Version('11.1.0-rc.0-sha-f76369ab7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
