eb540e35bdb3b2ac7ac61f50d7db8b6666675124
Esteban Gehring <esteban.gehring@bithost.ch>
2021-01-30 16:09:58 +0000

    11.1.x - 9f0c19f64 docs(material-experimental/mdc-core): fix scss examples (#21740)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.1.1-sha-e089c3a09');
+const VERSION = new Version('11.1.1-sha-9f0c19f64');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.1.1-sha-e089c3a09');
+const VERSION$1 = new Version('11.1.1-sha-9f0c19f64');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
