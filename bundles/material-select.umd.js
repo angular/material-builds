a198f512cb7ad38dfee93939e9c8e25e196f57d7
Adam Kocsis <kocccs@gmail.com>
2021-02-01 04:25:35 +0000

    master - da1f7e7a5 fix(material/select): overlay offset calculation for disabled option centering (#21716)

--- bundles/material-select.umd.js
+++ bundles/material-select.umd.js
@@ -1381,6 +1381,9 @@
             if (this.multiple) {
                 offsetX = SELECT_MULTIPLE_PANEL_PADDING_X;
             }
+            else if (this.disableOptionCentering) {
+                offsetX = SELECT_PANEL_PADDING_X;
+            }
             else {
                 var selected = this._selectionModel.selected[0] || this.options.first;
                 offsetX = selected && selected.group ? SELECT_PANEL_INDENT_PADDING_X : SELECT_PANEL_PADDING_X;
