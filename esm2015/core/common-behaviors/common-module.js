7e93d35e8b2de4b06a1fd5b4e669157e07971b7a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-01 04:22:39 +0000

    master - 42361c0b3 fix(cdk/collections): recycle repeater strategy retaining references to destroyed views (#21744)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.0-next.0-sha-ccf2b60a2');
+const VERSION = new Version('11.2.0-next.0-sha-42361c0b3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
