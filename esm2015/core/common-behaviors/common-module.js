af31142a519eb7bdba86bcb16f2c83fbed3a3db4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-02 04:11:18 +0000

    master - 8f558eeb7 fix(material/schematics): fix typo in warning (#21754)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.0-next.0-sha-d7ec42c2c');
+const VERSION = new Version('11.2.0-next.0-sha-8f558eeb7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
