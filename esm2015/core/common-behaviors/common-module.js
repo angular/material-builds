e2ee4bd3e96a8dd1a00f02d8a243a77227fb7ea7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-02 20:20:30 +0000

    master - d3deb8b9c fix(material/expansion): add focus indication in high contrast mode (#21549)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.0-next.0-sha-435a741f0');
+const VERSION = new Version('11.2.0-next.0-sha-d3deb8b9c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
