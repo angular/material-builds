f8d0eb7f74e46cbd29ebb5260c36d7f7bb9beb03
Wagner Maciel <wagnermaciel@google.com>
2021-02-04 17:30:01 +0000

    11.1.x - 62c8ae560 release: bump version to 11.1.2 w/ changelog (#21804)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.1.1-sha-fdacb22f2');
+    var VERSION = new i0.Version('11.1.2-sha-62c8ae560');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.1.1-sha-fdacb22f2');
+    var VERSION$1 = new i0.Version('11.1.2-sha-62c8ae560');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
