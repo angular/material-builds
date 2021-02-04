4cc1806fadb76431a581509eee1edc1f0dd817c8
Wagner Maciel <wagnermaciel@google.com>
2021-02-04 00:49:25 +0000

    master - 23b21a8d4 release: update changelog w/ 11.2.0-rc.0 (#21801)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-d042b1a81');
+const VERSION = new Version('12.0.0-next.0-sha-23b21a8d4');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-d042b1a81`;
+const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-23b21a8d4`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
