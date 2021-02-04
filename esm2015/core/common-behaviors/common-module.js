8f4171aca5fe9b0ce75481542be69bc780cc9b37
Wagner Maciel <wagnermaciel@google.com>
2021-02-04 00:29:17 +0000

    master - d042b1a81 build: bump master branch version to 12.0.0-next.0 (#21800)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.0-next.0-sha-d3deb8b9c');
+const VERSION = new Version('12.0.0-next.0-sha-d042b1a81');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
