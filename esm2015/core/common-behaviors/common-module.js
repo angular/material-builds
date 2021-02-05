77e033781bd482df514a86416da5cc8cc59805c8
mbehrlich <mehrlich@google.com>
2021-02-05 10:07:18 +0000

    master - 9dbf49b66 feat(google-maps): Add MapDirectionsRenderer and MapDirectionsService (#21736)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-6fee2713c');
+const VERSION = new Version('12.0.0-next.0-sha-9dbf49b66');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
