38073539cac0ddaaef3dfb41dbc265efaad4896b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:20:02 +0000

    master - ab8f62f49 refactor(material-experimental/mdc-slide-toggle): de-duplicate test harness logic (#21747)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-da5e2eb39');
+const VERSION = new Version('12.0.0-next.0-sha-ab8f62f49');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
