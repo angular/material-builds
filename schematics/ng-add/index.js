fee4489a1243365481ac3d451a57534b11ec862a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:17:19 +0000

    master - d3cdb345e fix(material/menu): unable to move focus from inside opened event (#20968)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-060ab9eb5`;
+const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-d3cdb345e`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
