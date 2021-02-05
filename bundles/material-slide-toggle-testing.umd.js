d783082b58c89162ad6b15f825d1a8ae6b572847
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-05 10:09:23 +0000

    11.1.x - eeb77f06a refactor(material-experimental/mdc-slide-toggle): de-duplicate test harness logic (#21747)

--- bundles/material-slide-toggle-testing.umd.js
+++ bundles/material-slide-toggle-testing.umd.js
@@ -313,13 +313,201 @@
         return value;
     }
 
+    var _MatSlideToggleHarnessBase = /** @class */ (function (_super) {
+        __extends(_MatSlideToggleHarnessBase, _super);
+        function _MatSlideToggleHarnessBase() {
+            var _this = _super.apply(this, __spread(arguments)) || this;
+            _this._label = _this.locatorFor('label');
+            _this._input = _this.locatorFor('input');
+            return _this;
+        }
+        /** Whether the slide-toggle is checked. */
+        _MatSlideToggleHarnessBase.prototype.isChecked = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var checked, _a;
+                return __generator(this, function (_b) {
+                    switch (_b.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1:
+                            checked = (_b.sent()).getProperty('checked');
+                            _a = coercion.coerceBooleanProperty;
+                            return [4 /*yield*/, checked];
+                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
+                    }
+                });
+            });
+        };
+        /** Whether the slide-toggle is disabled. */
+        _MatSlideToggleHarnessBase.prototype.isDisabled = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var disabled, _a;
+                return __generator(this, function (_b) {
+                    switch (_b.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1:
+                            disabled = (_b.sent()).getAttribute('disabled');
+                            _a = coercion.coerceBooleanProperty;
+                            return [4 /*yield*/, disabled];
+                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
+                    }
+                });
+            });
+        };
+        /** Whether the slide-toggle is required. */
+        _MatSlideToggleHarnessBase.prototype.isRequired = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var required, _a;
+                return __generator(this, function (_b) {
+                    switch (_b.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1:
+                            required = (_b.sent()).getAttribute('required');
+                            _a = coercion.coerceBooleanProperty;
+                            return [4 /*yield*/, required];
+                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
+                    }
+                });
+            });
+        };
+        /** Whether the slide-toggle is valid. */
+        _MatSlideToggleHarnessBase.prototype.isValid = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var invalid;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this.host()];
+                        case 1:
+                            invalid = (_a.sent()).hasClass('ng-invalid');
+                            return [4 /*yield*/, invalid];
+                        case 2: return [2 /*return*/, !(_a.sent())];
+                    }
+                });
+            });
+        };
+        /** Gets the slide-toggle's name. */
+        _MatSlideToggleHarnessBase.prototype.getName = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('name')];
+                    }
+                });
+            });
+        };
+        /** Gets the slide-toggle's aria-label. */
+        _MatSlideToggleHarnessBase.prototype.getAriaLabel = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('aria-label')];
+                    }
+                });
+            });
+        };
+        /** Gets the slide-toggle's aria-labelledby. */
+        _MatSlideToggleHarnessBase.prototype.getAriaLabelledby = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('aria-labelledby')];
+                    }
+                });
+            });
+        };
+        /** Gets the slide-toggle's label text. */
+        _MatSlideToggleHarnessBase.prototype.getLabelText = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._label()];
+                        case 1: return [2 /*return*/, (_a.sent()).text()];
+                    }
+                });
+            });
+        };
+        /** Focuses the slide-toggle. */
+        _MatSlideToggleHarnessBase.prototype.focus = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).focus()];
+                    }
+                });
+            });
+        };
+        /** Blurs the slide-toggle. */
+        _MatSlideToggleHarnessBase.prototype.blur = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).blur()];
+                    }
+                });
+            });
+        };
+        /** Whether the slide-toggle is focused. */
+        _MatSlideToggleHarnessBase.prototype.isFocused = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this._input()];
+                        case 1: return [2 /*return*/, (_a.sent()).isFocused()];
+                    }
+                });
+            });
+        };
+        /**
+         * Puts the slide-toggle in a checked state by toggling it if it is currently unchecked, or doing
+         * nothing if it is already checked.
+         */
+        _MatSlideToggleHarnessBase.prototype.check = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this.isChecked()];
+                        case 1:
+                            if (!!(_a.sent())) return [3 /*break*/, 3];
+                            return [4 /*yield*/, this.toggle()];
+                        case 2:
+                            _a.sent();
+                            _a.label = 3;
+                        case 3: return [2 /*return*/];
+                    }
+                });
+            });
+        };
+        /**
+         * Puts the slide-toggle in an unchecked state by toggling it if it is currently checked, or doing
+         * nothing if it is already unchecked.
+         */
+        _MatSlideToggleHarnessBase.prototype.uncheck = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this.isChecked()];
+                        case 1:
+                            if (!_a.sent()) return [3 /*break*/, 3];
+                            return [4 /*yield*/, this.toggle()];
+                        case 2:
+                            _a.sent();
+                            _a.label = 3;
+                        case 3: return [2 /*return*/];
+                    }
+                });
+            });
+        };
+        return _MatSlideToggleHarnessBase;
+    }(testing.ComponentHarness));
     /** Harness for interacting with a standard mat-slide-toggle in tests. */
     var MatSlideToggleHarness = /** @class */ (function (_super) {
         __extends(MatSlideToggleHarness, _super);
         function MatSlideToggleHarness() {
             var _this = _super.apply(this, __spread(arguments)) || this;
-            _this._label = _this.locatorFor('label');
-            _this._input = _this.locatorFor('input');
             _this._inputContainer = _this.locatorFor('.mat-slide-toggle-bar');
             return _this;
         }
@@ -344,146 +532,6 @@
                 }
             }); }); });
         };
-        /** Whether the slide-toggle is checked. */
-        MatSlideToggleHarness.prototype.isChecked = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var checked, _a;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1:
-                            checked = (_b.sent()).getProperty('checked');
-                            _a = coercion.coerceBooleanProperty;
-                            return [4 /*yield*/, checked];
-                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
-                    }
-                });
-            });
-        };
-        /** Whether the slide-toggle is disabled. */
-        MatSlideToggleHarness.prototype.isDisabled = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var disabled, _a;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1:
-                            disabled = (_b.sent()).getAttribute('disabled');
-                            _a = coercion.coerceBooleanProperty;
-                            return [4 /*yield*/, disabled];
-                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
-                    }
-                });
-            });
-        };
-        /** Whether the slide-toggle is required. */
-        MatSlideToggleHarness.prototype.isRequired = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var required, _a;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1:
-                            required = (_b.sent()).getAttribute('required');
-                            _a = coercion.coerceBooleanProperty;
-                            return [4 /*yield*/, required];
-                        case 2: return [2 /*return*/, _a.apply(void 0, [_b.sent()])];
-                    }
-                });
-            });
-        };
-        /** Whether the slide-toggle is valid. */
-        MatSlideToggleHarness.prototype.isValid = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var invalid;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this.host()];
-                        case 1:
-                            invalid = (_a.sent()).hasClass('ng-invalid');
-                            return [4 /*yield*/, invalid];
-                        case 2: return [2 /*return*/, !(_a.sent())];
-                    }
-                });
-            });
-        };
-        /** Gets the slide-toggle's name. */
-        MatSlideToggleHarness.prototype.getName = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('name')];
-                    }
-                });
-            });
-        };
-        /** Gets the slide-toggle's aria-label. */
-        MatSlideToggleHarness.prototype.getAriaLabel = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('aria-label')];
-                    }
-                });
-            });
-        };
-        /** Gets the slide-toggle's aria-labelledby. */
-        MatSlideToggleHarness.prototype.getAriaLabelledby = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).getAttribute('aria-labelledby')];
-                    }
-                });
-            });
-        };
-        /** Gets the slide-toggle's label text. */
-        MatSlideToggleHarness.prototype.getLabelText = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._label()];
-                        case 1: return [2 /*return*/, (_a.sent()).text()];
-                    }
-                });
-            });
-        };
-        /** Focuses the slide-toggle. */
-        MatSlideToggleHarness.prototype.focus = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).focus()];
-                    }
-                });
-            });
-        };
-        /** Blurs the slide-toggle. */
-        MatSlideToggleHarness.prototype.blur = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).blur()];
-                    }
-                });
-            });
-        };
-        /** Whether the slide-toggle is focused. */
-        MatSlideToggleHarness.prototype.isFocused = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this._input()];
-                        case 1: return [2 /*return*/, (_a.sent()).isFocused()];
-                    }
-                });
-            });
-        };
         /** Toggle the checked state of the slide-toggle. */
         MatSlideToggleHarness.prototype.toggle = function () {
             return __awaiter(this, void 0, void 0, function () {
@@ -495,48 +543,8 @@
                 });
             });
         };
-        /**
-         * Puts the slide-toggle in a checked state by toggling it if it is currently unchecked, or doing
-         * nothing if it is already checked.
-         */
-        MatSlideToggleHarness.prototype.check = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this.isChecked()];
-                        case 1:
-                            if (!!(_a.sent())) return [3 /*break*/, 3];
-                            return [4 /*yield*/, this.toggle()];
-                        case 2:
-                            _a.sent();
-                            _a.label = 3;
-                        case 3: return [2 /*return*/];
-                    }
-                });
-            });
-        };
-        /**
-         * Puts the slide-toggle in an unchecked state by toggling it if it is currently checked, or doing
-         * nothing if it is already unchecked.
-         */
-        MatSlideToggleHarness.prototype.uncheck = function () {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, this.isChecked()];
-                        case 1:
-                            if (!_a.sent()) return [3 /*break*/, 3];
-                            return [4 /*yield*/, this.toggle()];
-                        case 2:
-                            _a.sent();
-                            _a.label = 3;
-                        case 3: return [2 /*return*/];
-                    }
-                });
-            });
-        };
         return MatSlideToggleHarness;
-    }(testing.ComponentHarness));
+    }(_MatSlideToggleHarnessBase));
     /** The selector for the host element of a `MatSlideToggle` instance. */
     MatSlideToggleHarness.hostSelector = '.mat-slide-toggle';
 
@@ -557,6 +565,7 @@
      */
 
     exports.MatSlideToggleHarness = MatSlideToggleHarness;
+    exports._MatSlideToggleHarnessBase = _MatSlideToggleHarnessBase;
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
--- slide-toggle/testing/slide-toggle-harness.d.ts
+++ slide-toggle/testing/slide-toggle-harness.d.ts
@@ -7,20 +7,11 @@
  */
 import { ComponentHarness, HarnessPredicate } from '@angular/cdk/testing';
 import { SlideToggleHarnessFilters } from './slide-toggle-harness-filters';
-/** Harness for interacting with a standard mat-slide-toggle in tests. */
-export declare class MatSlideToggleHarness extends ComponentHarness {
-    /** The selector for the host element of a `MatSlideToggle` instance. */
-    static hostSelector: string;
-    /**
-     * Gets a `HarnessPredicate` that can be used to search for a `MatSlideToggleHarness` that meets
-     * certain criteria.
-     * @param options Options for filtering which slide toggle instances are considered a match.
-     * @return a `HarnessPredicate` configured with the given options.
-     */
-    static with(options?: SlideToggleHarnessFilters): HarnessPredicate<MatSlideToggleHarness>;
+export declare abstract class _MatSlideToggleHarnessBase extends ComponentHarness {
     private _label;
-    private _input;
-    private _inputContainer;
+    protected _input: import("@angular/cdk/testing").AsyncFactoryFn<import("@angular/cdk/testing").TestElement>;
+    /** Toggle the checked state of the slide-toggle. */
+    abstract toggle(): Promise<void>;
     /** Whether the slide-toggle is checked. */
     isChecked(): Promise<boolean>;
     /** Whether the slide-toggle is disabled. */
@@ -43,8 +34,6 @@ export declare class MatSlideToggleHarness extends ComponentHarness {
     blur(): Promise<void>;
     /** Whether the slide-toggle is focused. */
     isFocused(): Promise<boolean>;
-    /** Toggle the checked state of the slide-toggle. */
-    toggle(): Promise<void>;
     /**
      * Puts the slide-toggle in a checked state by toggling it if it is currently unchecked, or doing
      * nothing if it is already checked.
@@ -55,4 +44,19 @@ export declare class MatSlideToggleHarness extends ComponentHarness {
      * nothing if it is already unchecked.
      */
     uncheck(): Promise<void>;
+}
+/** Harness for interacting with a standard mat-slide-toggle in tests. */
+export declare class MatSlideToggleHarness extends _MatSlideToggleHarnessBase {
+    /** The selector for the host element of a `MatSlideToggle` instance. */
+    static hostSelector: string;
+    /**
+     * Gets a `HarnessPredicate` that can be used to search for a `MatSlideToggleHarness` that meets
+     * certain criteria.
+     * @param options Options for filtering which slide toggle instances are considered a match.
+     * @return a `HarnessPredicate` configured with the given options.
+     */
+    static with(options?: SlideToggleHarnessFilters): HarnessPredicate<MatSlideToggleHarness>;
+    private _inputContainer;
+    /** Toggle the checked state of the slide-toggle. */
+    toggle(): Promise<void>;
 }
