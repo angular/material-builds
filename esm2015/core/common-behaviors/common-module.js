2fa4c47238baf39cfe290930aa7a21fdc613639d
Michael Prentice <splaktar@gmail.com>
2021-02-05 10:14:32 +0000

    master - e773f9078 docs: fix input text color to match theme in custom form guide (#21750)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-f7ace54fe');
+const VERSION = new Version('12.0.0-next.0-sha-e773f9078');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
