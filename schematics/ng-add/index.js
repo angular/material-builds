6b907d9368d899653a5b1f89c955a44ce269b00f
Annie Wang <annieyw@google.com>
2021-02-07 14:39:50 +0000

    11.2.x - 1c16d912d fix(material/menu): prevent mat-menu from affecting flex layout (#21246)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.0-rc.0-sha-fd75a47c2`;
+const fallbackMaterialVersionRange = `~11.2.0-rc.0-sha-1c16d912d`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
