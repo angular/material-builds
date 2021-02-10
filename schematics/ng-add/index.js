1739e3c89302d1ae5fa14c2154e498b6ee31ca7c
Annie Wang <annieyw@google.com>
2021-02-10 14:37:42 +0000

    11.2.x - 14b5c3f89 release: bump version to 11.2.0 w/ changelog (#21864)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.0-rc.0-sha-5fcb47a49`;
+const fallbackMaterialVersionRange = `~11.2.0-sha-14b5c3f89`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
