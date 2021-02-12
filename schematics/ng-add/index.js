7350fb45ca203a82fa2803a49b5115df6056b0ec
adhispace <53649964+adhispace@users.noreply.github.com>
2021-02-12 22:47:25 +0000

    11.2.x - 03386fc71 fix(material/theming): change to update comments in theming (#21824)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.0-sha-e60a02bd2`;
+const fallbackMaterialVersionRange = `~11.2.0-sha-03386fc71`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
