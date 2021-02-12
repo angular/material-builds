0f4b94ee97dd263d77dfbff8a36dab39a34353bc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-12 21:36:25 +0000

    master - 6f998527c fix(material/stepper): don't allow focus inside a hidden element (#21842)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-e7b604f2d');
+const VERSION = new Version('12.0.0-next.0-sha-6f998527c');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
