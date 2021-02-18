9bd2021e831e8c3973735fbf6a25d54a9ab9171a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-02-18 19:52:12 +0000

    master - 4b8427e2f fix(cdk/overlay): not setting correct opacity in high contrast mode on Firefox (#21896)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-ca37709ca`;
+const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-4b8427e2f`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
