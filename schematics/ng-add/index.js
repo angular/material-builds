ad104dd101904e63de377452b289c2d62da48542
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-02-18 05:03:00 +0000

    11.2.x - 3d7e46fb3 build: add .import sass files (#21869)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.1-sha-c3abc1415`;
+const fallbackMaterialVersionRange = `~11.2.1-sha-3d7e46fb3`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
