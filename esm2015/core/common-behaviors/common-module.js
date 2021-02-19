f47f3242f5044584e20292bdf7f77dcb858c5898
Annie Wang <annieyw@google.com>
2021-02-19 21:11:36 +0000

    11.2.x - 61deb7129 fix(material-experimental/mdc-button): use mdc mixins to style icons within buttons (#21623)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.1-sha-a30f656e2');
+const VERSION = new Version('11.2.1-sha-61deb7129');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
