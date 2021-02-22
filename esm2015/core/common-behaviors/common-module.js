5aa83dcff0e7738831ada30e5cd47dec6cda7c8e
Joey Perrott <josephperrott.github@gmail.com>
2021-02-22 21:33:43 +0000

    11.2.x - c0149eb9f build: update mdc_snapshot_test_cronjob to yarn install before bazel setup (#21980)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.1-sha-05e7e744a');
+const VERSION = new Version('11.2.1-sha-c0149eb9f');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('11.2.1-sha-05e7e744a');
+const VERSION = new Version('11.2.1-sha-c0149eb9f');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('11.2.1-sha-05e7e744a');
+const VERSION$1 = new Version('11.2.1-sha-c0149eb9f');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
