bf382764bd505ca45eb9e84b27c37c17e5fdc5e6
jeripeierSBB <jeremias.peier@sbb.ch>
2021-02-23 18:51:56 +0000

    master - dd4a17102 test(material/select): remove form-field dependency in deselection test (#21963)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-087330f25`;
+const fallbackMaterialVersionRange = `~12.0.0-next.0-sha-dd4a17102`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
