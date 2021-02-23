9523a2f74217c68b2c4f307d6e4f72ab37c1703d
Michael-James <2666285+MichaelJamesParsons@users.noreply.github.com>
2021-02-23 21:29:25 +0000

    master - c42ece36d feat(cdk/table): add element offsets to sticky styler state (#21886)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('12.0.0-next.0-sha-3a50c41c8');
+    var VERSION = new i0.Version('12.0.0-next.0-sha-c42ece36d');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('12.0.0-next.0-sha-3a50c41c8');
+    var VERSION$1 = new i0.Version('12.0.0-next.0-sha-c42ece36d');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
