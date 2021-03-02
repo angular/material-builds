de479ea33a70ad1dcbde60df439ebb2f8f894923
Wagner Maciel <wagnermaciel@google.com>
2021-03-02 18:33:47 +0000

    11.2.x - 2010bb534 fix(cdk/overlay): resolve regression when overlay is imported through shorthand path (#22043) (#22086)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.2-sha-9f7a2f058');
+const VERSION = new Version('11.2.2-sha-2010bb534');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
