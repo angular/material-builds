f7ba66db4f9c3c6395988919766ca77011ea5a90
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-02 15:09:12 +0000

    master - a41fb6e41 fix(cdk/accordion): allow for closeAll to be used when multiple mode is disabled (#22055)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.0-sha-8497bb166');
+const VERSION = new Version('12.0.0-next.0-sha-a41fb6e41');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
