eb11f3d7c16c04f0be1e3a9ef8e2350a2154e80c
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-02 19:35:03 +0000

    master - 7006b482c fix(material-experimental/mdc-checkbox): remove background state styles (#21748)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('12.0.0-next.0-sha-c791d6a6d');
+const VERSION = new Version('12.0.0-next.0-sha-7006b482c');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('12.0.0-next.0-sha-c791d6a6d');
+const VERSION$1 = new Version('12.0.0-next.0-sha-7006b482c');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
