db98ebca41fb61f04fafc1a2b5842bcdf0cf2438
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:47:19 +0000

    master - 40cce7691 fix(material-experimental/mdc-autocomplete): only animate along y axis (#21588)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.1-sha-373af254c');
+const VERSION = new Version('12.0.0-next.1-sha-40cce7691');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('12.0.0-next.1-sha-373af254c');
+const VERSION = new Version('12.0.0-next.1-sha-40cce7691');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('12.0.0-next.1-sha-373af254c');
+const VERSION$1 = new Version('12.0.0-next.1-sha-40cce7691');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
