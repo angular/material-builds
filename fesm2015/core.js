649a8415833b5febbd3d088a76f592f5099782f8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-05 03:49:50 +0000

    master - bd2c324d5 fix(material-experimental/mdc-progress-bar): run resize observer outside zone (#21564)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('12.0.0-next.1-sha-be508daa2');
+const VERSION = new Version('12.0.0-next.1-sha-bd2c324d5');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('12.0.0-next.1-sha-be508daa2');
+const VERSION$1 = new Version('12.0.0-next.1-sha-bd2c324d5');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
