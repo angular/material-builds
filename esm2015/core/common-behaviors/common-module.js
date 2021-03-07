b2b6f2d97a5a9c23f2a2377dd3c8658584bb5eee
Michael Prentice <splaktar@gmail.com>
2021-03-07 22:44:28 +0000

    master - 4aa48a15f docs(cdk/a11y): fix overview table of contents (#22136)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.1-sha-fef9179b6');
+const VERSION = new Version('12.0.0-next.1-sha-4aa48a15f');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
