7214678c641f2f5e04d8b8b74a77ee541aae60ca
ahong11 <ahong1@andrew.cmu.edu>
2021-03-09 14:43:16 +0000

    master - 0003d8fde fix(material-experimental/mdc-chips): remove icon click interaction (#22132)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.1-sha-17f66d693');
+const VERSION = new Version('12.0.0-next.1-sha-0003d8fde');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
