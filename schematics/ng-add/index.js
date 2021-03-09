6eb4bd45aa862c6cfe8b91a3b41e328bcd53be2e
AlexElin <alexey.elin@list.ru>
2021-03-09 20:57:32 +0000

    11.2.x - 045af6f3b docs: monospace DataSource (table docs) (#22114)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.3-sha-766c9ea0d`;
+const fallbackMaterialVersionRange = `~11.2.3-sha-045af6f3b`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
