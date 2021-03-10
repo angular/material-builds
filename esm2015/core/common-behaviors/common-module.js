56f0113026151a2f4284164863ff37f19ed415d8
Jeremy Elbourn <jelbourn@google.com>
2021-03-10 00:02:52 +0000

    11.2.x - 48e14315f docs(material/badge): specify badge targets must be block-level (#22169)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.3-sha-045af6f3b');
+const VERSION = new Version('11.2.3-sha-48e14315f');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
