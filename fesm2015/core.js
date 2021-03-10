1587f99ea4013b936f314dd9805378086e374469
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-10 22:32:57 +0000

    master - 94d585080 fix(material/progress-bar): adjust buffer colors (#22164)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -18,7 +18,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('12.0.0-next.2-sha-7a972fbe3');
+const VERSION = new Version('12.0.0-next.2-sha-94d585080');
 
 /**
  * @license
@@ -52,7 +52,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION$1 = new Version('12.0.0-next.2-sha-7a972fbe3');
+const VERSION$1 = new Version('12.0.0-next.2-sha-94d585080');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
