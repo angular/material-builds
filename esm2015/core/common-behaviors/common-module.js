2144645ba072d79e3e1cfd15ff8d9be3124e513f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-10 22:44:52 +0000

    master - 97f1cccd7 fix(material-experimental/mdc-progress-bar): improve buffer color (#22152)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.2-sha-fe9b81c7e');
+const VERSION = new Version('12.0.0-next.2-sha-97f1cccd7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
