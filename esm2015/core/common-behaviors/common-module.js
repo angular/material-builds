3d55cc747f96f6631af557a8f0e46b10d69f40d0
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-11 16:00:00 +0000

    master - 8a10451f9 build: add exception for missing MDC test (#22188)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.2-sha-97f1cccd7');
+const VERSION = new Version('12.0.0-next.2-sha-8a10451f9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
