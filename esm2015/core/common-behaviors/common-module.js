861f6a1c551b41b41eb0ff76002f20137026b05e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-15 18:06:50 +0000

    master - 9f560f4fd docs(material/button-toggle): add missing type on public API (#22224)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.2-sha-d66cb83f7');
+const VERSION = new Version('12.0.0-next.2-sha-9f560f4fd');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
