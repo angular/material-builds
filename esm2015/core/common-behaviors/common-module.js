1c4eceab690565d74daad279e6338b5d8629a543
Bruno Kilian <bruno222@users.noreply.github.com>
2021-03-16 15:32:09 +0000

    master - af26fe578 ci: fix the warning on lock-closed gh-actions: Warning: Unexpected input(s) 'lock-bot-key' (#21547)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.2-sha-84d9a3065');
+const VERSION = new Version('12.0.0-next.2-sha-af26fe578');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
