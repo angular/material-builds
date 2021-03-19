85666377d83750a34336f110f3f022f7a08ab67a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 20:16:29 +0000

    master - 1fac4918e fix(google-maps): instantiate geocoder lazily (#22159)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-4b11445a9');
+const VERSION = new Version('12.0.0-next.3-sha-1fac4918e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
