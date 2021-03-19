c719c69de880cf81ce754ff7a286cc5087187310
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 19:12:09 +0000

    master - 50b3207d1 fix(cdk/platform): avoid error during server-side rendering if document is stubbed out (#22260)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-ee499222d');
+const VERSION = new Version('12.0.0-next.3-sha-50b3207d1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
