5186e5e3d5e7bd7b96f944b9c27898741bc9b280
Annie Wang <annieyw@google.com>
2021-03-19 20:15:18 +0000

    master - 4b11445a9 fix(material/grid-list): remove mat-figure class (#22231)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-476a90b08');
+const VERSION = new Version('12.0.0-next.3-sha-4b11445a9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
