35b44d3a00401445525cc48d717d25f7160e8fe6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-19 16:18:08 +0000

    11.2.x - 5aac0c9df build: schematics unit tests not being run (#22293)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.5-sha-6a7dbeda5`;
+const fallbackMaterialVersionRange = `~11.2.5-sha-5aac0c9df`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
