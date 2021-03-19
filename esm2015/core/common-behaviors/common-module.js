c6cb317c778e9288ca795e7407758708757a7660
Michael Prentice <splaktar@gmail.com>
2021-03-19 19:09:38 +0000

    master - 7718947db fix(cdk/a11y): add ConfigurableFocusTrapConfig to the public API (#22138)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-c0286ea50');
+const VERSION = new Version('12.0.0-next.3-sha-7718947db');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
