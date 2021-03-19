03f87ec70e86427b1f59e66034bfcb8f6cc32298
mmalerba <mmalerba@google.com>
2021-03-19 19:04:07 +0000

    master - 16b7adb84 fix(material-experimental/mdc-select): fix floating label behavior (#22134)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-5b7b03ea7');
+const VERSION = new Version('12.0.0-next.3-sha-16b7adb84');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
