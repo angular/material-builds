2b206989da72b85e19e74c2c7182127de9ca796f
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-03-19 19:36:29 +0000

    master - 08bbd50a6 fix(material-experimental/mdc-button): remove unthemed icon color (#22281)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.3-sha-2da7135e4');
+const VERSION = new Version('12.0.0-next.3-sha-08bbd50a6');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
