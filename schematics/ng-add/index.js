3943c2323e3a0c9b5150f538291bdb87c22c392a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-27 20:31:05 +0000

    master - a2c4b7664 build: align MDC version requirement (#22326)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.4-sha-c67941602`;
+const fallbackMaterialVersionRange = `~12.0.0-next.4-sha-a2c4b7664`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
