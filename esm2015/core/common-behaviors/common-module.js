50daf27a70695ba4865de4a7deb4031f1cbb8a54
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-03-30 12:26:27 +0000

    master - 9fb92f044 refactor(cdk/overlay): clean up variable names (#22359)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.4-sha-0c83adc8f');
+const VERSION = new Version('12.0.0-next.4-sha-9fb92f044');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
