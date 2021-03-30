4c1901592b120dd42f7265754a687e66aee90e96
Andrew <aslapierre@gmail.com>
2021-03-30 17:39:05 +0000

    master - 4a06d4fd2 docs(material/theming): Fix typo in theming.md (#22342)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.4-sha-9fb92f044`;
+const fallbackMaterialVersionRange = `~12.0.0-next.4-sha-4a06d4fd2`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
