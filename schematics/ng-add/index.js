ab9ffb0debb29d58ef22863b483a2e258ec41dbd
Jeremy Elbourn <jelbourn@google.com>
2021-04-01 00:44:05 +0000

    11.2.x - 03947c75b build: don't require all reviewers to approve PRs (#22363)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.7-sha-ef408b8c9`;
+const fallbackMaterialVersionRange = `~11.2.7-sha-03947c75b`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
