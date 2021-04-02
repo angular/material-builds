58347edcc14d3539fa69d0bf51691f295c1a6f90
timonkrebs <timon.krebs@zuehlke.com>
2021-04-02 07:20:06 +0000

    master - 1d906b64a feat(cdk/drag-drop): added dropPoint to dropped event (#22347)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-a9cdb084d');
+    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-1d906b64a');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-next.5-sha-a9cdb084d');
+    var VERSION = new i0.Version('12.0.0-next.5-sha-1d906b64a');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
