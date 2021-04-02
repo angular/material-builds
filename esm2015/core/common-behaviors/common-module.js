1be67c39dcec4afc35e0b2e4b4a0c5ed07d94d32
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-02 07:21:20 +0000

    master - ea5e5e04d fix(material/datepicker): toggle icon not visible in high contrast mode on Chromium browsers (#22352)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.5-sha-1d906b64a');
+const VERSION = new Version('12.0.0-next.5-sha-ea5e5e04d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
