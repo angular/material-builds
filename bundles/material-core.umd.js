4eb31acbf127614c88bceed78dd13c19f12ec9d4
Rafael Santana <rafaelsantana3095@gmail.com>
2021-04-02 07:23:38 +0000

    master - 5384550ae build: enforce consistent readonly array type (#20290)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-92af5f117');
+    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-5384550ae');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-next.5-sha-92af5f117');
+    var VERSION = new i0.Version('12.0.0-next.5-sha-5384550ae');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
