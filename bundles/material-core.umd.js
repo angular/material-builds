1d01615f53ff33acf8875aace5a4692839bb88bc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-02 07:25:55 +0000

    master - f65d5b574 fix(material-experimental/mdc-form-field): set explicit text-align (#22353)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-10ecfa0ec');
+    var VERSION$1 = new i0.Version('12.0.0-next.5-sha-f65d5b574');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-next.5-sha-10ecfa0ec');
+    var VERSION = new i0.Version('12.0.0-next.5-sha-f65d5b574');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
