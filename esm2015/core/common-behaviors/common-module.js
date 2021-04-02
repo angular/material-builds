f3730340b14e38e82d97030bbee3176aeea45e02
mmalerba <mmalerba@google.com>
2021-04-02 07:11:54 +0000

    master - 2427abe09 fix(cdk/text-field): autosize text areas using the placeholder (#22314)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.5-sha-34dba1366');
+const VERSION = new Version('12.0.0-next.5-sha-2427abe09');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
