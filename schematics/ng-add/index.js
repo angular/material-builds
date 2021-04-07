77a71a595b281be4afe17694f37204387bd060e2
Annie Wang <annieyw@google.com>
2021-04-07 05:46:01 +0000

    master - 7b7c1cf35 build: fix broken import paths (#22415)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.5-sha-4ad2d9608`;
+const fallbackMaterialVersionRange = `~12.0.0-next.5-sha-7b7c1cf35`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
