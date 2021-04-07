cff7202ab892a95ad6db42116a3152312cd6de3e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-07 05:15:32 +0000

    master - 4ad2d9608 fix(material-experimental/mdc-list): align ripple timings with MDC (#22396)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('12.0.0-next.5-sha-c45cd69f3');
+const VERSION$1 = new Version('12.0.0-next.5-sha-4ad2d9608');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.5-sha-c45cd69f3');
+const VERSION = new Version('12.0.0-next.5-sha-4ad2d9608');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
