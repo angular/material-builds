a27755105d0021d0ccede0c2761c8d512f58250a
Annie Wang <annieyw@google.com>
2021-04-07 18:08:41 +0000

    11.2.x - 84bde9819 release: bump version to 11.2.8 w/ changelog (#22420)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.2.7-sha-f1cba07e9');
+    var VERSION = new i0.Version('11.2.8-sha-84bde9819');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.2.7-sha-f1cba07e9');
+    var VERSION$1 = new i0.Version('11.2.8-sha-84bde9819');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.7-sha-f1cba07e9');
+const VERSION = new Version('11.2.8-sha-84bde9819');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
