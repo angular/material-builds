e3f43a44ae390823da96c1f11857e05fd6f72f85
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-08 16:55:59 +0000

    master - 0f739dbc9 refactor(form-field): remove unnecessary bindings (#16941)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-next.6-sha-70564f3b8');
+    var VERSION$1 = new i0.Version('12.0.0-next.6-sha-0f739dbc9');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-next.6-sha-70564f3b8');
+    var VERSION = new i0.Version('12.0.0-next.6-sha-0f739dbc9');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.6-sha-70564f3b8');
+const VERSION = new Version('12.0.0-next.6-sha-0f739dbc9');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
