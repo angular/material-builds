a01bb814beb6fcba5b4face83b51693b4ca55a66
mmalerba <mmalerba@google.com>
2021-04-09 17:02:27 +0000

    master - 10cc9cfa5 fix(material-experimental/mdc-select): correct some differences between (#22108)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-next.6-sha-fa7ed4cae`;
+const fallbackMaterialVersionRange = `~12.0.0-next.6-sha-10cc9cfa5`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
