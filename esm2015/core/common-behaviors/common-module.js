1b4d6164338872da8c5f87b813c01728b15e4239
mmalerba <mmalerba@google.com>
2021-04-12 17:20:41 +0000

    master - 03be9245b refactor(material/theming): normalize config in all typography mixins for mdc components (#21935)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.6-sha-ff69e199b');
+const VERSION = new Version('12.0.0-next.6-sha-03be9245b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
