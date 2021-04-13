762a408595824801f59b01f2f3bb620a24455417
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-13 17:30:15 +0000

    master - 2f177b622 fix(cdk/testing): unable to assign value to number inputs using sendKeys (#22395)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-next.6-sha-770217726');
+const VERSION = new Version('12.0.0-next.6-sha-2f177b622');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
