cfa3dc394b082bab9cc96bb97c83018a82224fb5
mmalerba <mmalerba@google.com>
2021-04-14 18:11:33 +0000

    11.2.x - 1399e3dab release: bump version to 11.2.9 w/ changelog (#22480)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.8-sha-a9e5ad264');
+const VERSION = new Version('11.2.9-sha-1399e3dab');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
