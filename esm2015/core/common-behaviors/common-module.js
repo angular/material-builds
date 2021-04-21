578419002a9fceb875d8591dfddc8b537dac1feb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-21 11:14:37 +0000

    master - 04d2f7709 fix(material/schematics): replace removed variables with their values in sass api migration (#22505)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-45f856659');
+const VERSION = new Version('12.1.0-next.0-sha-04d2f7709');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
