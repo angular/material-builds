6495596a7fb219527ab5236470862efb6c1a6d48
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-27 13:04:18 +0000

    master - 5497be56d fix(cdk/stepper): show error state if explicitly enabled (#22540)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-acf1b867a`;
+const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-5497be56d`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
--- stepper/public-api.d.ts
+++ stepper/public-api.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export { StepperOrientation } from '@angular/cdk/stepper';
+export { StepperOrientation, StepState } from '@angular/cdk/stepper';
 export * from './stepper-module';
 export * from './step-label';
 export * from './stepper';
