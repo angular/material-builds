9f5f0fa1839856015083f6040c27467a588e365b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-04-27 13:24:46 +0000

    11.2.x - 6349434d1 fix(material/datepicker): support for date range input in MDC-based form field (#22565)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.2.10-sha-90877709d');
+    var VERSION = new i0.Version('11.2.10-sha-6349434d1');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.2.10-sha-90877709d');
+    var VERSION$1 = new i0.Version('11.2.10-sha-6349434d1');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.10-sha-90877709d');
+const VERSION = new Version('11.2.10-sha-6349434d1');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
