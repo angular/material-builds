2b46791236fa9ac90b49907ecbdc8b49cdafb225
Annie Wang <annieyw@google.com>
2021-04-27 12:58:32 +0000

    master - acf1b867a refactor(material-experimental/mdc-progress-bar): remove wrapper element for progress bar (#22441)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-f88023c21');
+const VERSION = new Version('12.1.0-next.0-sha-acf1b867a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
