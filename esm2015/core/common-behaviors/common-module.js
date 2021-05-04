4b47cb2c3e7371284ca2ca70e8cf3a955107fa5c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-04 20:12:48 +0000

    master - 7fe4925f3 build: remove unnecessary dependency in webdriver test (#22621)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-b111d4f02');
+const VERSION = new Version('12.1.0-next.0-sha-7fe4925f3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
