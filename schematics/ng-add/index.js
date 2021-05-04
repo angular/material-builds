ada30f8a38cde74c488be4ce31c7ff8e46ee314f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-04 18:24:05 +0000

    master - cabb9ce49 fix(material-experimental/mdc-form-field): error with latest canary version (#22623)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-0f9c1e6ef`;
+const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-cabb9ce49`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
