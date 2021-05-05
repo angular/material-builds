d5c32336e828794fc68675ba8eec914391888d1c
Annie Wang <annieyw@google.com>
2021-05-05 17:31:32 +0000

    11.2.x - 960093992 release: bump version to 11.2.12 w/ changelog (#22629)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.2.11-sha-b5ee92fbe');
+    var VERSION = new i0.Version('11.2.12-sha-960093992');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.2.11-sha-b5ee92fbe');
+    var VERSION$1 = new i0.Version('11.2.12-sha-960093992');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
