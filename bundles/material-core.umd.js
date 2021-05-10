f5898995fe192ad1709c6e177fc15608d130db03
Michael Prentice <splaktar@gmail.com>
2021-05-10 16:47:44 +0000

    12.0.x - 7636cd83e refactor(material/schematics): fix some typos and add JSDoc (#22610)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-rc.2-sha-2b99e4b60');
+    var VERSION$1 = new i0.Version('12.0.0-rc.2-sha-7636cd83e');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-rc.2-sha-2b99e4b60');
+    var VERSION = new i0.Version('12.0.0-rc.2-sha-7636cd83e');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
