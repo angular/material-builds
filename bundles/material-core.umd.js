1e9a0d0c75cb8bb05331dd68cba9879c0dc5aab2
mmalerba <mmalerba@google.com>
2021-05-11 19:44:27 +0000

    12.0.x - f8ea8767b release: bump version to 12.0.0-rc.3 w/ changelog (#22666)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.0.0-rc.2-sha-369cdd32a');
+    var VERSION$1 = new i0.Version('12.0.0-rc.3-sha-f8ea8767b');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.0.0-rc.2-sha-369cdd32a');
+    var VERSION = new i0.Version('12.0.0-rc.3-sha-f8ea8767b');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
