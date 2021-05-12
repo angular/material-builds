514de75e01d723f069049314c8539a8f165a9d28
Jeremy Elbourn <jelbourn@google.com>
2021-05-12 16:35:29 +0000

    master - 38baf2486 refactor(material/schematics): accept extra symbols for sass api migration (#22658)

--- schematics/ng-update/migrations/theming-api-v12/migration.d.ts
+++ schematics/ng-update/migrations/theming-api-v12/migration.d.ts
@@ -5,6 +5,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/** Addition mixin and function names that can be updated when invoking migration directly. */
+interface ExtraSymbols {
+    mixins?: Record<string, string>;
+    functions?: Record<string, string>;
+}
 /**
  * Migrates the content of a file to the new theming API. Note that this migration is using plain
  * string manipulation, rather than the AST from PostCSS and the schematics string manipulation
@@ -19,4 +24,5 @@
  * @param newMaterialImportPath New import to the Material theming API (e.g. `~@angular/material`).
  * @param newCdkImportPath New import to the CDK Sass APIs (e.g. `~@angular/cdk`).
  */
-export declare function migrateFileContent(content: string, oldMaterialPrefix: string, oldCdkPrefix: string, newMaterialImportPath: string, newCdkImportPath: string): string;
+export declare function migrateFileContent(content: string, oldMaterialPrefix: string, oldCdkPrefix: string, newMaterialImportPath: string, newCdkImportPath: string, extraMaterialSymbols?: ExtraSymbols): string;
+export {};
