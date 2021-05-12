af6879c4692b113cf289a6e8ea75b5acd2dec350
mmalerba <mmalerba@google.com>
2021-05-12 19:37:13 +0000

    12.0.x - 043a742df release: update angular version in packages.bzl (#22680)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-rc.3-sha-ef0ba8b9b');
+const VERSION = new Version('12.0.0-rc.3-sha-043a742df');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
