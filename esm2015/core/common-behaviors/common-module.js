953cfb1d980e7d0e92c25e2a1c6e76c6bd2afd84
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-05-13 18:25:39 +0000

    12.0.x - e89422add fix(material-experimental/mdc-progress-spinner): set line-height (#22668)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.0-sha-06c8e7903');
+const VERSION = new Version('12.0.0-sha-e89422add');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
