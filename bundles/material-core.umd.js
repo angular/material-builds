692101b1606b649b6e1e4dbf3f98d730da383a34
Michael Prentice <splaktar@gmail.com>
2021-05-13 16:56:50 +0000

    11.2.x - b5224ea4a fix(material-experimental/mdc-core): update the required MDC version (#22608)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -12,7 +12,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION = new i0.Version('11.2.12-sha-9df8ced45');
+    var VERSION = new i0.Version('11.2.12-sha-b5224ea4a');
 
     /**
      * @license
@@ -52,7 +52,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION$1 = new i0.Version('11.2.12-sha-9df8ced45');
+    var VERSION$1 = new i0.Version('11.2.12-sha-b5224ea4a');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.12-sha-9df8ced45`;
+const fallbackMaterialVersionRange = `~11.2.12-sha-b5224ea4a`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
