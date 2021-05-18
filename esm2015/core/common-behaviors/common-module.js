a78b0e3bb5d3ce34c856b2126d03fd61d93b5d03
Kristiyan Kostadinov <crisbeto@abv.bg>
2021-05-18 19:43:46 +0000

    master - 3027f8c22 fixup! fix(material/core): reduce the number of module resolutions required for the legacy theming bundle

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-4cf77164b');
+const VERSION = new Version('12.1.0-next.0-sha-3027f8c22');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
