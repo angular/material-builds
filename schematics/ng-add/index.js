fce22878aa4a62011bd6eacb6b2a0a3e0c04c2c0
Nikhil Walvekar <walvekar@gmail.com>
2021-05-19 14:38:34 +0000

    12.0.x - fe4df3f2c docs(tree): A11Y improvement for Nested Tree example. (#22583)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.0-sha-90ecea89a`;
+const fallbackMaterialVersionRange = `~12.0.0-sha-fe4df3f2c`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
