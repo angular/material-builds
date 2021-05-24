3d25d80661e3b5cde46cdbe2d0e77b6c00f43d6d
Dominik <plondrein@gmail.com>
2021-05-24 14:03:24 +0000

    12.0.x - 54f53ed29 docs: Fix typo in theming example (#22765)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.1-sha-c8abcd9ec');
+const VERSION = new Version('12.0.1-sha-54f53ed29');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
