fff00713dfd772ae3fc7816a48c0044aa0f27871
mmalerba <mmalerba@google.com>
2021-05-26 21:43:37 +0000

    12.0.x - e92133eb4 docs(material/form-field): update documentation about the form-field appearances (#22817)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.2-sha-f9990fe6f');
+const VERSION = new Version('12.0.2-sha-e92133eb4');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
