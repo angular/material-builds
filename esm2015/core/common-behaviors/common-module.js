6d05d32e9439557770e593e40876d9cf2abc96c6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-26 02:51:30 +0000

    12.0.x - 8f0904f1a docs: add example for using ripples within table (#22797)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.1-sha-d1743b97b');
+const VERSION = new Version('12.0.1-sha-8f0904f1a');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
