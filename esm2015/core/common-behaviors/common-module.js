d0625d8df062ca904ab4b70418dc226185999dbb
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-05-26 18:53:42 +0000

    master - 0b04570a7 docs(cdk/coercion): expand overview (#22791)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-8d95c2fa2');
+const VERSION = new Version('12.1.0-next.0-sha-0b04570a7');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
