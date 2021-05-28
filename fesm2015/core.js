cf3214c1453158d09e8299277b0b73613deed706
Billy Lando <billy_lando@yahoo.com>
2021-05-28 06:06:25 +0000

    12.0.x - 3eeb92e6c docs: add missing parenthese to harness guide (#22833)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('12.0.2-sha-ed8418cdc');
+const VERSION$1 = new Version('12.0.2-sha-3eeb92e6c');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.2-sha-ed8418cdc');
+const VERSION = new Version('12.0.2-sha-3eeb92e6c');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
