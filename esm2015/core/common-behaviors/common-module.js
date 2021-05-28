9da020a96f96a6e5dfa8ec38278f7c02599ed16f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-05-28 03:30:28 +0000

    master - 3462c22e8 docs: clarify coordinate parameters for `MatRipple.launch` method. (#22806)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-4f75c2740');
+const VERSION = new Version('12.1.0-next.0-sha-3462c22e8');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
