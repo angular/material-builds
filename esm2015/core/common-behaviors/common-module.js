7b3d6ae2ad43be6b79ee3aeba92e23de84af47bf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-01 19:53:37 +0000

    master - bf2277625 docs(material/autocomplete): add descriptions for properties showing up in API docs (#22851)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-0c33b2e55');
+const VERSION = new Version('12.1.0-next.0-sha-bf2277625');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
