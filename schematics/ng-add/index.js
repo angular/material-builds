76904829d9db53c919552feb768a8f7265522171
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-01 19:50:09 +0000

    master - 21b81d60c docs: explicitly set form field appearance (#22837)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-ade090104`;
+const fallbackMaterialVersionRange = `~12.1.0-next.0-sha-21b81d60c`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
