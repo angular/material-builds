b1ec2f24ad3e39cebb9b0ff5c215bdc80709166a
AlexElin <alexey.elin@list.ru>
2021-06-11 20:40:03 +0000

    master - 9b9fd7549 docs(material/chips): remove unused field in the autocomplete example (#22958)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.1.0-next.0-sha-9027d835a');
+    var VERSION$1 = new i0.Version('12.1.0-next.0-sha-9b9fd7549');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.1.0-next.0-sha-9027d835a');
+    var VERSION = new i0.Version('12.1.0-next.0-sha-9b9fd7549');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
