93a7fd2751078788d26c656a85e02a61fc3d4415
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-11 03:09:38 +0000

    master - 87ab033fe fix(material-experimental/mdc-slide-toggle): increase contrast in high contrast mode (#22945)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('12.1.0-next.0-sha-10725496d');
+const VERSION$1 = new Version('12.1.0-next.0-sha-87ab033fe');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-10725496d');
+const VERSION = new Version('12.1.0-next.0-sha-87ab033fe');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
