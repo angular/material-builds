83f2debea5a788f9540321e9f33af24cd7b22a63
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-15 15:05:40 +0000

    12.0.x - 90ec90450 build: fix firebase deployment of dev-app (#22961)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.0.4-sha-0012409b7`;
+const fallbackMaterialVersionRange = `~12.0.4-sha-90ec90450`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
