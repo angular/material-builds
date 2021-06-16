3c7a2f40eefbdabe68f9f35f757a573b42a73133
Nitin Malave <51904027+nitinmalave@users.noreply.github.com>
2021-06-16 01:54:50 +0000

    master - efabc019e test(multiple): refactor unit tests of several components (#22976)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-5391cad08');
+const VERSION = new Version('12.1.0-next.0-sha-efabc019e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
