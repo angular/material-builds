b30ffa7fced1dc56965e6246adf0ad3ff0ecf7f8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-16 01:47:13 +0000

    master - 437aba6e7 fix(material-experimental/mdc-checkbox): add larger touch target (#22892)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('12.1.0-next.0-sha-94289397c');
+const VERSION$1 = new Version('12.1.0-next.0-sha-437aba6e7');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-next.0-sha-94289397c');
+const VERSION = new Version('12.1.0-next.0-sha-437aba6e7');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
