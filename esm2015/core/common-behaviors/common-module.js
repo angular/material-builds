0969f86418716c549fb5711e83918bea15e48b26
Nitin Malave <51904027+nitinmalave@users.noreply.github.com>
2021-06-16 01:54:32 +0000

    12.0.x - 90f33167b test(material/bottom-sheet): refactor variable type to const in bottom-sheet component unit tests (#22895)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.0.4-sha-90ec90450');
+const VERSION = new Version('12.0.4-sha-90f33167b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
