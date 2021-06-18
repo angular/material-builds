820dbc10be083901dc659222a4437f03cded03e5
mmalerba <mmalerba@google.com>
2021-06-18 17:12:27 +0000

    master - 2c57c58c3 fix(material-experimental/mdc-snack-bar): increase the specificity of styles targeting the action buttons (#22959)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-2cd745ab3');
+const VERSION = new Version('12.2.0-next.0-sha-2c57c58c3');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
