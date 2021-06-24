c50c5d36122441c6a83d8e5f860c66d70e8d5c26
mmalerba <mmalerba@google.com>
2021-06-24 22:13:45 +0000

    12.1.x - e8a9a99bb release: bump angular version (#23066)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.0-rc.0-sha-6d5fec760');
+const VERSION = new Version('12.1.0-rc.0-sha-e8a9a99bb');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
