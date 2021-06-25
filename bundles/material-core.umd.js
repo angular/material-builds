1beb17a8e4247de464ec88231e61b65440334a63
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-06-25 21:33:44 +0000

    master - d6b6b5ac9 build: enable public API verification on the MDC slider (#23053)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.2.0-next.0-sha-8a28f1559');
+    var VERSION$1 = new i0.Version('12.2.0-next.0-sha-d6b6b5ac9');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.2.0-next.0-sha-8a28f1559');
+    var VERSION = new i0.Version('12.2.0-next.0-sha-d6b6b5ac9');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
