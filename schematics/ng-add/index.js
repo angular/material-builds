6707a27aad6cf53e7836f2a071e6bef821b97069
mmalerba <mmalerba@google.com>
2021-06-28 18:40:43 +0000

    master - 8b2ec8bc3 fix(material-experimental/mdc-dialog): use same container selector as non-mdc version (#23074)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.2.0-next.0-sha-fa3525178`;
+const fallbackMaterialVersionRange = `~12.2.0-next.0-sha-8b2ec8bc3`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
