1e3dbbfe34e4867717d2dba70ce3163686f9de9d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-01 02:13:04 +0000

    master - f361b6c0d fix(material-experimental/mdc-slider): don't disable ripples for noop animations (#23084)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-238fd193a');
+const VERSION = new Version('12.2.0-next.0-sha-f361b6c0d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
