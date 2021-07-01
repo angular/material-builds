59fcb2faf98c7a92496e7b17748a12b7f516526c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-01 02:15:19 +0000

    master - a1e8e409d fix(material-experimental/mdc-menu): align ripple timings with MDC (#23015)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-ca02022ad');
+const VERSION = new Version('12.2.0-next.0-sha-a1e8e409d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
