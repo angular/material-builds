5cc0672b0216cd916bbb469b9936c2f0e99db99c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-02 18:43:28 +0000

    master - cd9f5ebfd fix(google-maps): throw an error if clustering library hasn't been loaded (#23064)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-653f412a9');
+const VERSION = new Version('12.2.0-next.0-sha-cd9f5ebfd');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
