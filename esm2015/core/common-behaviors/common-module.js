aef94aa41f14a56bb8f1b073ea8b9a8a0bb9122c
Alex Morris <alexemorris@icloud.com>
2021-07-02 18:33:39 +0000

    master - 36fbae1c2 fix(material-experimental/mdc-button): pass query param to extended mixin  (#23103)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-a1e8e409d');
+const VERSION = new Version('12.2.0-next.0-sha-36fbae1c2');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
