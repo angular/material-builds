bafdad878d7e2d1d5d375fe37c82f1fb94f4b081
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-09 13:11:20 +0000

    master - fec20ad49 perf(cdk/scrolling): don't re-measure viewport on resize (#23124)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-7fae1a966');
+const VERSION = new Version('12.2.0-next.0-sha-fec20ad49');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
