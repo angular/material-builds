4a932ee88b555e09b8cffe25a8d04118d7367187
Annie Wang <annieyw@google.com>
2021-07-12 13:34:50 +0000

    master - 71d75f41e fix(cdk/table): set default role of table to 'table' (#22491)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-c76a09ed8');
+const VERSION = new Version('12.2.0-next.0-sha-71d75f41e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
