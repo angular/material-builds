9b55a390d97cd7a20dc524cf95a63b3d68c95a45
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-15 21:38:43 +0000

    12.1.x - 90dc73c4f build: update to rules_nodejs v4.0.0-beta.0 and use new API golden tool [patch port] (#23180)

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -1,7 +1,8 @@
-(function (factory) {
-	typeof define === 'function' && define.amd ? define('@angular/material', factory) :
-	factory();
-}((function () { 'use strict';
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define('@angular/material', ['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory((global.ng = global.ng || {}, global.ng.material = {})));
+}(this, (function (exports) { 'use strict';
 
 	/**
 	 * @license
@@ -13,6 +14,13 @@
 	// primary entry-point which is empty as of version 9. All components should
 	// be imported through their individual entry-points. This file is needed to
 	// satisfy the "ng_package" bazel rule which also requires a primary entry-point.
+	// Workaround for: https://github.com/microsoft/rushstack/issues/2806.
+	// This is a private export that can be removed at any time.
+	var ɵɵtsModuleIndicatorApiExtractorWorkaround = true;
+
+	exports.ɵɵtsModuleIndicatorApiExtractorWorkaround = ɵɵtsModuleIndicatorApiExtractorWorkaround;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
 //# sourceMappingURL=material.umd.js.map
