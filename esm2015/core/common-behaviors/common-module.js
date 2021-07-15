286b803b12fd954d62bacc61c795b48e6645c96a
Zach Arend <zarend@users.noreply.github.com>
2021-07-15 17:04:27 +0000

    master - 2acd218bb build: add vim swamp files to .gitignore (#23173)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-0aa223f85');
+const VERSION = new Version('12.2.0-next.0-sha-2acd218bb');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
