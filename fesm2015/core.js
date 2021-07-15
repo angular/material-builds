22f151f9938f0f04b6a989c80e0e9803104acabd
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-07-15 18:51:42 +0000

    master - 9c2ea191c fix(material-experimental/mdc-table): match legacy examples (#23159)

--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('12.2.0-next.0-sha-2acd218bb');
+const VERSION$1 = new Version('12.2.0-next.0-sha-9c2ea191c');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-2acd218bb');
+const VERSION = new Version('12.2.0-next.0-sha-9c2ea191c');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
