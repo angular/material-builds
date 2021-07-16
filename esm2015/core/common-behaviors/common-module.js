7b4478d1a61931f716543ab9697eaa1b6b560433
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-16 19:27:53 +0000

    12.1.x - c26264ff4 fix(material/radio): hidden circle visible on some zoom levels (#23154)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.2-sha-eba631a7d');
+const VERSION = new Version('12.1.2-sha-c26264ff4');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
