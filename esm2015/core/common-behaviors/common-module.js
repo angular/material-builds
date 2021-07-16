569dea9f17169f13aa9b24ab3f2497045074afcf
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-16 15:01:17 +0000

    12.1.x - eba631a7d build: enable automatic test module teardown (#23176)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.2-sha-90dc73c4f');
+const VERSION = new Version('12.1.2-sha-eba631a7d');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
