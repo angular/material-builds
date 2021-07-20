e023efb69712fd015c8ed0c7808d4746bb34fc35
Wagner Maciel <wagnermaciel@google.com>
2021-07-20 22:56:48 +0000

    master - a81834a9e fix(material-experimental/mdc-menu): increase specificity of menu panel styles (#23178)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-3231d1d5b');
+const VERSION = new Version('12.2.0-next.0-sha-a81834a9e');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
