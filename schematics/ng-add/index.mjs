114e0576019d36bf4eeeeb3bd26e582944b61537
Wagner Maciel <wagnermaciel@google.com>
2021-07-21 21:13:42 +0000

    12.1.x - e5d685a59 release: bump version to 12.1.3 w/ changelog (#23213)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.1.2-sha-f7a4a1c03`;
+const fallbackMaterialVersionRange = `~12.1.3-sha-e5d685a59`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
