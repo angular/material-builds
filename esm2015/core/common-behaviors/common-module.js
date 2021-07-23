4019b360aed4fc6c4306430af68f15526a93c53e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-23 20:13:48 +0000

    master - 8a1b6f297 build: fix being unable to run e2e tests locally on windows (#23219)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-c7baa54a9');
+const VERSION = new Version('12.2.0-next.0-sha-8a1b6f297');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
