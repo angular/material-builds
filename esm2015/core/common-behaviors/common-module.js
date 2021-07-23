67345fc9e8b06c14802f86f11dcf414a9afb34c6
Zach Arend <zarend@users.noreply.github.com>
2021-07-23 20:09:09 +0000

    master - 85a7e4546 fix(material-experimental/mdc-paginator): target page size label with `aria-labelledby` (#23172)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-30e92057f');
+const VERSION = new Version('12.2.0-next.0-sha-85a7e4546');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
