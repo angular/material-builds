506004014e0a71bfd95145689a740a940130132e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-23 20:08:14 +0000

    12.1.x - 98a8b7c0b build: fix being unable to run e2e tests locally on windows (#23219)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.1.3-sha-e5d685a59');
+    var VERSION$1 = new i0.Version('12.1.3-sha-98a8b7c0b');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.1.3-sha-e5d685a59');
+    var VERSION = new i0.Version('12.1.3-sha-98a8b7c0b');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.1.3-sha-e5d685a59');
+const VERSION = new Version('12.1.3-sha-98a8b7c0b');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
