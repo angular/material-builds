43abe68dd6909a1b78e2ae974212e54be294bb45
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-23 20:11:28 +0000

    master - 26366e759 fix(cdk/testing): protractor element not extracting hidden text (#21540)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-cfd8e78df');
+const VERSION = new Version('12.2.0-next.0-sha-26366e759');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
