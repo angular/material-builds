2c09705da2d1b97c6b25ca71e0b2d4431f2f8e28
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-23 20:13:44 +0000

    12.1.x - 2cc11e6d7 test(material/menu): switch all tests to fakeAsync (#23203)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~12.1.3-sha-98a8b7c0b`;
+const fallbackMaterialVersionRange = `~12.1.3-sha-2cc11e6d7`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
