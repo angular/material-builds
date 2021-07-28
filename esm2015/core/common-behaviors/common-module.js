cbb8f123fa26683ee09da2dcf99b92fa0909f8d3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-07-28 08:01:12 +0000

    master - 80d3e28bf test(material/expansion): fix failing unit test on iOS14 safari

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-next.0-sha-0e3bd88dd');
+const VERSION = new Version('12.2.0-next.0-sha-80d3e28bf');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
