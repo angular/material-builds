3786643943560bb74be7c55062eba314e9a5c487
Andrew Seguin <andrewseguin@users.noreply.github.com>
2021-07-29 15:38:24 +0000

    master - b17ed9dde fix(material-experimental/mdc-table): flex tables should stretch cells (#23259)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('13.0.0-next.0-sha-53d2d4bd0');
+    var VERSION$1 = new i0.Version('13.0.0-next.0-sha-b17ed9dde');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('13.0.0-next.0-sha-53d2d4bd0');
+    var VERSION = new i0.Version('13.0.0-next.0-sha-b17ed9dde');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
--- fesm2015/core.js
+++ fesm2015/core.js
@@ -19,7 +19,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('13.0.0-next.0-sha-53d2d4bd0');
+const VERSION$1 = new Version('13.0.0-next.0-sha-b17ed9dde');
 
 /**
  * @license
@@ -53,7 +53,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('13.0.0-next.0-sha-53d2d4bd0');
+const VERSION = new Version('13.0.0-next.0-sha-b17ed9dde');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
