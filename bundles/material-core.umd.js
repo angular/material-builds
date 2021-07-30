7279dc0cd580f1439e99cc042333e61211ed4507
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-07-30 22:11:20 +0000

    12.2.x - 28b589acc fix(material/menu): not interrupting keyboard events to other overlays (#22928)

--- bundles/material-core.umd.js
+++ bundles/material-core.umd.js
@@ -34,7 +34,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     /** Current version of Angular Material. */
-    var VERSION$1 = new i0.Version('12.2.0-rc.0-sha-47a9c532d');
+    var VERSION$1 = new i0.Version('12.2.0-rc.0-sha-28b589acc');
 
     /**
      * @license
@@ -74,7 +74,7 @@
     // i.e. avoid core to depend on the @angular/material primary entry-point
     // Can be removed once the Material primary entry-point no longer
     // re-exports all secondary entry-points
-    var VERSION = new i0.Version('12.2.0-rc.0-sha-47a9c532d');
+    var VERSION = new i0.Version('12.2.0-rc.0-sha-28b589acc');
     /** @docs-private */
     function MATERIAL_SANITY_CHECKS_FACTORY() {
         return true;
