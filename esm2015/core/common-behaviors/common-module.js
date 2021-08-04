e515aae8a58c7c0f3d0176086809fade310ee4f9
Zach Arend <zarend@users.noreply.github.com>
2021-08-04 18:30:06 +0000

    master - 50655e2bb Revert "fix(material/menu): not interrupting keyboard events to other overlays (#22928)" (#23306)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('13.0.0-next.0-sha-df7c46b82');
+const VERSION = new Version('13.0.0-next.0-sha-50655e2bb');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
