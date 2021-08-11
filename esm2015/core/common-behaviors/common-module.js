e4c81cd4ed9afde5b35713846b893ed0f05f5e2a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-08-11 15:35:07 +0000

    12.2.x - 8a4ee2582 test: fix syntax error for slide-toggle mdc test

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.0-sha-7a97590ae');
+const VERSION = new Version('12.2.0');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
