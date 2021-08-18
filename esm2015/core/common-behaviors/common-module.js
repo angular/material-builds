0ebeb25dcffd5237abba4a3b08ef646a703eba42
Wagner Maciel <wagnermaciel@google.com>
2021-08-18 19:30:30 +0000

    12.2.x - 7aca47faf release: cut the v12.2.2 release (#23398)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.1');
+const VERSION = new Version('12.2.2');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
