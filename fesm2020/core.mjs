3c79abfb7daa408a95a8456488c05499703602a8
Zach Arend <zarend@users.noreply.github.com>
2021-10-08 19:20:30 +0000

    master - 467e43bed Update next branch to reflect new release-train "v13.1.0-next.0". (#23720)

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -22,7 +22,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION$1 = new Version('13.0.0-next.8');
+const VERSION$1 = new Version('13.1.0-next.0');
 
 /**
  * @license
@@ -56,7 +56,7 @@ AnimationDurations.EXITING = '195ms';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('13.0.0-next.8');
+const VERSION = new Version('13.1.0-next.0');
 /** @docs-private */
 function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
