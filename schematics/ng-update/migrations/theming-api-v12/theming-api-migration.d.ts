6728a9269d8639a951c94951240933bf72552d6b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-10-21 17:57:21 +0000

    master - 44ba4244c refactor(material/schematics): do not print hammer migration message when updating to v13 (#23752)

--- schematics/ng-update/migrations/theming-api-v12/theming-api-migration.d.ts
+++ schematics/ng-update/migrations/theming-api-v12/theming-api-migration.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { SchematicContext } from '@angular-devkit/schematics';
-import { DevkitMigration, ResolvedResource } from '@angular/cdk/schematics';
+import { DevkitMigration, ResolvedResource, TargetVersion } from '@angular/cdk/schematics';
 /** Migration that switches all Sass files using Material theming APIs to `@use`. */
 export declare class ThemingApiMigration extends DevkitMigration<null> {
     /** Number of files that have been migrated. */
@@ -14,5 +14,5 @@ export declare class ThemingApiMigration extends DevkitMigration<null> {
     enabled: boolean;
     visitStylesheet(stylesheet: ResolvedResource): void;
     /** Logs out the number of migrated files at the end of the migration. */
-    static globalPostMigration(_tree: unknown, context: SchematicContext): void;
+    static globalPostMigration(_tree: unknown, _targetVersion: TargetVersion, context: SchematicContext): void;
 }
