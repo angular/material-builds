826e8f6b774b517d6a5a3aeed5d3cd5d62827090
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-10-24 22:39:05 +0000

    13.0.x - 6fa3c1ddd refactor(multiple): clean up explicit teardown flags (#23814)

--- fesm2015/bottom-sheet.mjs
+++ fesm2015/bottom-sheet.mjs
@@ -588,6 +588,14 @@ function _applyConfigDefaults(defaults, config) {
  */
 
 /**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
  * Generated bundle index. Do not edit.
  */
 
--- fesm2020/button-toggle.mjs
+++ fesm2020/button-toggle.mjs
@@ -510,6 +510,14 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.0-next.15",
  */
 
 /**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
  * Generated bundle index. Do not edit.
  */
 
