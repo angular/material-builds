d9e880a5a0c090d00e4b5ef70ec8271c66415f6f
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2021-11-10 20:06:15 +0000

    12.2.x - e5d210d88 release: cut the v12.2.13 release

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -15,7 +15,7 @@ import { _isTestEnvironment } from '@angular/cdk/platform';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('12.2.12');
+const VERSION = new Version('12.2.13');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
