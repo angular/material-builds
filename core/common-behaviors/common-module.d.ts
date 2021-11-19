4bad2e5366a414cfbe865d363125a26b3e5cac02
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2021-11-19 04:29:49 +0000

    master - 738e514ec perf(material/core): tree shake sanity checks (#23969)

--- core/common-behaviors/common-module.d.ts
+++ core/common-behaviors/common-module.d.ts
@@ -31,19 +31,13 @@ export interface GranularSanityChecks {
  * This module should be imported to each top-level component module (e.g., MatTabsModule).
  */
 export declare class MatCommonModule {
+    private _sanityChecks;
+    private _document;
     /** Whether we've done the global sanity checks (e.g. a theme is loaded, there is a doctype). */
     private _hasDoneGlobalChecks;
-    /** Configured sanity checks. */
-    private _sanityChecks;
-    /** Used to reference correct document/window */
-    protected _document: Document;
-    constructor(highContrastModeDetector: HighContrastModeDetector, sanityChecks: any, document: any);
+    constructor(highContrastModeDetector: HighContrastModeDetector, _sanityChecks: SanityChecks, _document: Document);
     /** Gets whether a specific sanity check is enabled. */
     private _checkIsEnabled;
-    private _checkDoctypeIsDefined;
-    private _checkThemeIsPresent;
-    /** Checks whether the material version matches the cdk version */
-    private _checkCdkVersionMatch;
     static ɵfac: i0.ɵɵFactoryDeclaration<MatCommonModule, [null, { optional: true; }, null]>;
     static ɵmod: i0.ɵɵNgModuleDeclaration<MatCommonModule, never, [typeof i1.BidiModule], [typeof i1.BidiModule]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<MatCommonModule>;
