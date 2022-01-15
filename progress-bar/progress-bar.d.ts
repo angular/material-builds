263a3f6cbb5f42258665f04f1cca6c9d851626d4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-01-15 21:55:10 +0000

    13.1.x - 33d98e297 chore: improve type safety in breakpoint observer (#14356)

--- progress-bar/progress-bar.d.ts
+++ progress-bar/progress-bar.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { NumberInput } from '@angular/cdk/coercion';
-import { AfterViewInit, ElementRef, EventEmitter, InjectionToken, NgZone, OnDestroy } from '@angular/core';
+import { AfterViewInit, ElementRef, EventEmitter, InjectionToken, NgZone, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CanColor, ThemePalette } from '@angular/material/core';
 import * as i0 from "@angular/core";
 /** Last animation end data. */
@@ -50,12 +50,22 @@ export declare const MAT_PROGRESS_BAR_DEFAULT_OPTIONS: InjectionToken<MatProgres
 export declare class MatProgressBar extends _MatProgressBarBase implements CanColor, AfterViewInit, OnDestroy {
     private _ngZone;
     _animationMode?: string | undefined;
+    /**
+     * @deprecated `_changeDetectorRef` parameter to be made required.
+     * @breaking-change 11.0.0
+     */
+    private _changeDetectorRef?;
     constructor(elementRef: ElementRef, _ngZone: NgZone, _animationMode?: string | undefined, 
     /**
      * @deprecated `location` parameter to be made required.
      * @breaking-change 8.0.0
      */
-    location?: MatProgressBarLocation, defaults?: MatProgressBarDefaultOptions);
+    location?: MatProgressBarLocation, defaults?: MatProgressBarDefaultOptions, 
+    /**
+     * @deprecated `_changeDetectorRef` parameter to be made required.
+     * @breaking-change 11.0.0
+     */
+    _changeDetectorRef?: ChangeDetectorRef | undefined);
     /** Flag that indicates whether NoopAnimations mode is set to true. */
     _isNoopAnimation: boolean;
     /** Value of the progress bar. Defaults to zero. Mirrored to aria-valuenow. */
@@ -100,7 +110,7 @@ export declare class MatProgressBar extends _MatProgressBarBase implements CanCo
     } | null;
     ngAfterViewInit(): void;
     ngOnDestroy(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<MatProgressBar, [null, null, { optional: true; }, { optional: true; }, { optional: true; }]>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<MatProgressBar, [null, null, { optional: true; }, { optional: true; }, { optional: true; }, null]>;
     static ɵcmp: i0.ɵɵComponentDeclaration<MatProgressBar, "mat-progress-bar", ["matProgressBar"], { "color": "color"; "value": "value"; "bufferValue": "bufferValue"; "mode": "mode"; }, { "animationEnd": "animationEnd"; }, never, never>;
 }
 export {};
