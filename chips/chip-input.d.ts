d529bb978dc830ba370779603d6a04d6a096a6e8
Miles Malerba <mmalerba@google.com>
2022-02-04 17:39:50 +0000

    master - d67dad6b1 fix(material/chips): Make `MatChipInputEvent.chipInput` required. (#23692)

--- chips/chip-input.d.ts
+++ chips/chip-input.d.ts
@@ -21,11 +21,8 @@ export interface MatChipInputEvent {
     input: HTMLInputElement;
     /** The value of the input. */
     value: string;
-    /**
-     * Reference to the chip input that emitted the event.
-     * @breaking-change 13.0.0 This property will be made required.
-     */
-    chipInput?: MatChipInput;
+    /** Reference to the chip input that emitted the event. */
+    chipInput: MatChipInput;
 }
 /**
  * Directive that adds chip-specific behaviors to an input element inside `<mat-form-field>`.
