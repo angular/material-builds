a54c6dbfa9270da0ff3c113210ede245f6549844
Artur Androsovych <arthurandrosovich@gmail.com>
2022-02-24 19:54:15 +0000

    master - 3d2aefb3b perf(cdk/overlay): add event listeners for overlay dispatchers outside of zone (#24408)

--- progress-spinner/progress-spinner-module.d.ts
+++ progress-spinner/progress-spinner-module.d.ts
@@ -4,6 +4,6 @@ import * as i2 from "@angular/material/core";
 import * as i3 from "@angular/common";
 export declare class MatProgressSpinnerModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<MatProgressSpinnerModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<MatProgressSpinnerModule, [typeof i1.MatProgressSpinner, typeof i1.MatSpinner], [typeof i2.MatCommonModule, typeof i3.CommonModule], [typeof i1.MatProgressSpinner, typeof i1.MatSpinner, typeof i2.MatCommonModule]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<MatProgressSpinnerModule, [typeof i1.MatProgressSpinner], [typeof i2.MatCommonModule, typeof i3.CommonModule], [typeof i1.MatProgressSpinner, typeof i2.MatCommonModule]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<MatProgressSpinnerModule>;
 }
