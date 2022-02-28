19039a5f5c9cc856244ede834f76449272fa85fb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-02-28 18:27:21 +0000

    master - 0f8d5296c build: prepare framework peer dependency and migration collection for v14 (#24492)

--- schematics/ng-update/index.d.ts
+++ schematics/ng-update/index.d.ts
@@ -22,3 +22,5 @@ export declare function updateToV11(): Rule;
 export declare function updateToV12(): Rule;
 /** Entry point for the migration schematics with target of Angular Material v13 */
 export declare function updateToV13(): Rule;
+/** Entry point for the migration schematics with target of Angular Material v14 */
+export declare function updateToV14(): Rule;
