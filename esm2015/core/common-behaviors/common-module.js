61e188d5e779fa308ea7fbb26ba71259b29a09ff
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-04-21 16:14:20 +0000

    11.2.x - 5327b2478 build: [11.x] preparation for primary branch rename in the Angular repos (#24790)

--- esm2015/core/common-behaviors/common-module.js
+++ esm2015/core/common-behaviors/common-module.js
@@ -14,7 +14,7 @@ import { DOCUMENT } from '@angular/common';
 // i.e. avoid core to depend on the @angular/material primary entry-point
 // Can be removed once the Material primary entry-point no longer
 // re-exports all secondary entry-points
-const VERSION = new Version('11.2.13-sha-3a4e7d92b');
+const VERSION = new Version('11.2.13-sha-5327b2478');
 /** @docs-private */
 export function MATERIAL_SANITY_CHECKS_FACTORY() {
     return true;
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~11.2.13-sha-3a4e7d92b`;
+const fallbackMaterialVersionRange = `~11.2.13-sha-5327b2478`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
