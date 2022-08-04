6d2ee7e56af26bc2113f14ac925d9024cf0d5938
Egor Volvachev <34657605+volvachev@users.noreply.github.com>
2022-08-04 17:52:09 +0000

    main - 3041cdab6 fix(material/tooltip): avoid problem when triggers hide animation for not visible tooltip (#24652)

--- fesm2020/tooltip.mjs
+++ fesm2020/tooltip.mjs
@@ -792,8 +792,18 @@ class _TooltipComponentBase {
         const tooltip = this._tooltip.nativeElement;
         const showClass = this._showAnimation;
         const hideClass = this._hideAnimation;
-        tooltip.classList.remove(isVisible ? hideClass : showClass);
-        tooltip.classList.add(isVisible ? showClass : hideClass);
+        if (isVisible) {
+            tooltip.classList.remove(hideClass);
+            tooltip.classList.add(showClass);
+        }
+        if (!isVisible) {
+            // It's avoids the problem when `mat-tooltip-hide` triggers the animation of
+            // a tooltip that has not been shown yet.
+            if (tooltip.classList.contains(showClass)) {
+                tooltip.classList.add(hideClass);
+            }
+            tooltip.classList.remove(showClass);
+        }
         this._isVisible = isVisible;
         // It's common for internal apps to disable animations using `* { animation: none !important }`
         // which can break the opening sequence. Try to detect such cases and work around them.
