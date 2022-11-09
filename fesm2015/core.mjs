f12bff35146e669a75b32e3db47c735d41b73e34
Miles Malerba <mmalerba@google.com>
2022-11-09 23:10:19 +0000

    14.2.x - 441742713 release: cut the v14.2.7 release

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -22,7 +22,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('14.2.6');
+const VERSION = new Version('14.2.7');
 
 /**
  * @license
