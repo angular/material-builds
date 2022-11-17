589bd520332ab9ccf6bb6024821988145b6f6abc
Wagner Maciel <wagnermaciel@google.com>
2022-11-17 18:08:04 +0000

    15.0.x - 8aa4a49d7 fix(material/slider): form control w/ initial disabled state (#25972)

--- fesm2015/slider.mjs
+++ fesm2015/slider.mjs
@@ -1201,7 +1201,11 @@ class MatSliderThumb {
     /** @docs-private */
     initProps() {
         this._updateWidthInactive();
-        this.disabled = this._slider.disabled;
+        // If this or the parent slider is disabled, just make everything disabled.
+        if (this.disabled !== this._slider.disabled) {
+            // The MatSlider setter for disabled will relay this and disable both inputs.
+            this._slider.disabled = true;
+        }
         this.step = this._slider.step;
         this.min = this._slider.min;
         this.max = this._slider.max;
