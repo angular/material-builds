8f5715f0292c4fb463b74a48bc99bdeeef919f52
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2022-11-29 15:26:01 +0000

    15.0.x - f61a21382 fix(material/select): changed after checked error if option label changes (#23315)

--- fesm2015/core.mjs
+++ fesm2015/core.mjs
@@ -1669,8 +1669,10 @@ class _MatOptionBase {
         if (this._selected) {
             const viewValue = this.viewValue;
             if (viewValue !== this._mostRecentViewValue) {
+                if (this._mostRecentViewValue) {
+                    this._stateChanges.next();
+                }
                 this._mostRecentViewValue = viewValue;
-                this._stateChanges.next();
             }
         }
     }
