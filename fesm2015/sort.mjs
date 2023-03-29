f0def8d893bd3ee855fac72f2f9dfaa54abaf6f5
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-03-29 11:06:08 +0000

    main - 56c5ff740 refactor(material/sort): Remove unnecessary ngSkipHydration attribute (#26852)

--- fesm2015/sort.mjs
+++ fesm2015/sort.mjs
@@ -147,7 +147,7 @@ class MatSort extends _MatSortBase {
     }
 }
 MatSort.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.2", ngImport: i0, type: MatSort, deps: [{ token: MAT_SORT_DEFAULT_OPTIONS, optional: true }], target: i0.ɵɵFactoryTarget.Directive });
-MatSort.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "14.0.0", version: "16.0.0-next.2", type: MatSort, selector: "[matSort]", inputs: { disabled: ["matSortDisabled", "disabled"], active: ["matSortActive", "active"], start: ["matSortStart", "start"], direction: ["matSortDirection", "direction"], disableClear: ["matSortDisableClear", "disableClear"] }, outputs: { sortChange: "matSortChange" }, host: { attributes: { "ngSkipHydration": "true" }, classAttribute: "mat-sort" }, exportAs: ["matSort"], usesInheritance: true, usesOnChanges: true, ngImport: i0 });
+MatSort.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "14.0.0", version: "16.0.0-next.2", type: MatSort, selector: "[matSort]", inputs: { disabled: ["matSortDisabled", "disabled"], active: ["matSortActive", "active"], start: ["matSortStart", "start"], direction: ["matSortDirection", "direction"], disableClear: ["matSortDisableClear", "disableClear"] }, outputs: { sortChange: "matSortChange" }, host: { classAttribute: "mat-sort" }, exportAs: ["matSort"], usesInheritance: true, usesOnChanges: true, ngImport: i0 });
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.2", ngImport: i0, type: MatSort, decorators: [{
             type: Directive,
             args: [{
@@ -155,7 +155,6 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.2", 
                     exportAs: 'matSort',
                     host: {
                         'class': 'mat-sort',
-                        'ngSkipHydration': 'true',
                     },
                     inputs: ['disabled: matSortDisabled'],
                 }]
