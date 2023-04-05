2ba183dceeb7c7cfa0a8f1465bc406cbe1d29bcd
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2023-04-05 21:52:36 +0000

    15.2.x - a95a90401 release: cut the v15.2.6 release

--- fesm2020/core.mjs
+++ fesm2020/core.mjs
@@ -22,7 +22,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of Angular Material. */
-const VERSION = new Version('15.2.5');
+const VERSION = new Version('15.2.6');
 
 /**
  * @license
