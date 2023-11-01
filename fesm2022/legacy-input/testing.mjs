f4c3b586480877d758b78c722f46ca9b8372e849
Miles Malerba <mmalerba@google.com>
2023-11-01 18:23:48 +0000

    main - e638bab11 docs: release notes for the v16.2.11 release

--- fesm2022/legacy-input/testing.mjs
+++
@@ -1,2 +0,0 @@
-export { MatInputHarness as MatLegacyInputHarness, MatNativeOptionHarness as MatLegacyNativeOptionHarness, MatNativeSelectHarness as MatLegacyNativeSelectHarness } from '@angular/material/input/testing';
-//# sourceMappingURL=testing.mjs.map
