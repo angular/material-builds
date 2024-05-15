a6beba3ba959789ba71a92505106fecc9d40fc17
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-05-15 11:49:20 +0000

    18.0.x - 1327b2d release: cut the v18.0.0-rc.2 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -18,7 +18,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~18.0.0-rc.1`;
+const fallbackMaterialVersionRange = `~18.0.0-rc.2`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
