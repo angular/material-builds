d8ed71d1e083255ceb43f4e3aa8f5d7237a10f9a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-06-25 14:43:55 +0000

    main - e74065a fix(material/datepicker): avoid losing focus when re-rendering the current view (#29287)

--- fesm2022/datepicker.mjs
+++ fesm2022/datepicker.mjs
@@ -2177,10 +2177,13 @@ class MatCalendar {
             !this._dateAdapter.sameDate(changes['maxDate'].previousValue, changes['maxDate'].currentValue)
             ? changes['maxDate']
             : undefined;
-        const change = minDateChange || maxDateChange || changes['dateFilter'];
-        if (change && !change.firstChange) {
+        const changeRequiringRerender = minDateChange || maxDateChange || changes['dateFilter'];
+        if (changeRequiringRerender && !changeRequiringRerender.firstChange) {
             const view = this._getCurrentViewComponent();
             if (view) {
+                // Schedule focus to be moved to the active date since re-rendering
+                // can blur the active cell. See #29265.
+                this._moveFocusOnNextTick = true;
                 // We need to `detectChanges` manually here, because the `minDate`, `maxDate` etc. are
                 // passed down to the view via data bindings which won't be up-to-date when we call `_init`.
                 this._changeDetectorRef.detectChanges();
