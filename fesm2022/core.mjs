59fb4ea977abdce31609498f5fbfa6b271e0c2c8
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-07-17 13:05:27 +0000

    main - 9fc9c0d release: cut the v18.2.0-next.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -13,7 +13,7 @@ import { startWith } from 'rxjs/operators';
 import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('18.2.0-next.0');
+const VERSION = new Version('18.2.0-next.1');
 
 /** @docs-private */
 class AnimationCurves {
