ea368e533528a61ca35b687ef48433b0bada2097
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-07-31 20:19:36 +0000

    18.1.x - 0e1bf7c release: cut the v18.1.3 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -13,7 +13,7 @@ import { startWith } from 'rxjs/operators';
 import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('18.1.2');
+const VERSION = new Version('18.1.3');
 
 /** @docs-private */
 class AnimationCurves {
