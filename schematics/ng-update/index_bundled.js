47523fb9a4ff488d79cfbe0a3351d6a339e5ddc5
Naji <54370141+naaajii@users.noreply.github.com>
2024-09-19 11:02:21 +0000

    main - 5782834 docs: replace angular.io links with angular.dev (#29734)

--- schematics/ng-update/index_bundled.js
+++ schematics/ng-update/index_bundled.js
@@ -787,6 +787,6 @@ function onMigrationComplete(context, targetVersion, hasFailures) {
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 //# sourceMappingURL=index_bundled.js.map
