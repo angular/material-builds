f483264f7ad4298f63a4add1cd7c710a1402f059
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-10-23 15:51:08 +0000

    main - 0064b42 fix(material/tabs): remove IE animation workaround (#29899)

--- fesm2022/tabs.mjs
+++ fesm2022/tabs.mjs
@@ -10,7 +10,7 @@ import { hasModifierKey, SPACE, ENTER } from '@angular/cdk/keycodes';
 import { SharedResizeObserver } from '@angular/cdk/observers/private';
 import { normalizePassiveListenerOptions, Platform } from '@angular/cdk/platform';
 import { ViewportRuler, CdkScrollable } from '@angular/cdk/scrolling';
-import { takeUntil, debounceTime, startWith, switchMap, skip, filter, distinctUntilChanged } from 'rxjs/operators';
+import { takeUntil, debounceTime, startWith, switchMap, skip, filter } from 'rxjs/operators';
 import { CdkObserveContent } from '@angular/cdk/observers';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
@@ -1044,13 +1044,7 @@ class MatTabBody {
                 changeDetectorRef.markForCheck();
             });
         }
-        // Ensure that we get unique animation events, because the `.done` callback can get
-        // invoked twice in some browsers. See https://github.com/angular/angular/issues/24084.
-        this._translateTabComplete
-            .pipe(distinctUntilChanged((x, y) => {
-            return x.fromState === y.fromState && x.toState === y.toState;
-        }))
-            .subscribe(event => {
+        this._translateTabComplete.subscribe(event => {
             // If the transition to the center is complete, emit an event.
             if (this._isCenterPosition(event.toState) && this._isCenterPosition(this._position)) {
                 this._onCentered.emit();
