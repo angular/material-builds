def2e7aa3ea28dcd02172d49f7e98101d7d60753
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2024-10-28 13:34:46 +0000

    19.0.x - d28e99c fix(material/sidenav): remove IE animation workaround (#29929)

--- fesm2022/sidenav.mjs
+++ fesm2022/sidenav.mjs
@@ -9,7 +9,7 @@ import { ESCAPE, hasModifierKey } from '@angular/cdk/keycodes';
 import { Platform } from '@angular/cdk/platform';
 import { DOCUMENT } from '@angular/common';
 import { Subject, fromEvent, merge } from 'rxjs';
-import { filter, map, mapTo, takeUntil, distinctUntilChanged, take, startWith, debounceTime } from 'rxjs/operators';
+import { filter, map, mapTo, takeUntil, take, startWith, debounceTime } from 'rxjs/operators';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 /**
@@ -248,13 +248,7 @@ class MatDrawer {
                 event.preventDefault();
             }));
         });
-        // We need a Subject with distinctUntilChanged, because the `done` event
-        // fires twice on some browsers. See https://github.com/angular/angular/issues/24084
-        this._animationEnd
-            .pipe(distinctUntilChanged((x, y) => {
-            return x.fromState === y.fromState && x.toState === y.toState;
-        }))
-            .subscribe((event) => {
+        this._animationEnd.subscribe((event) => {
             const { fromState, toState } = event;
             if ((toState.indexOf('open') === 0 && fromState === 'void') ||
                 (toState === 'void' && fromState.indexOf('open') === 0)) {
