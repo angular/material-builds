2e6ce904f88e839ded8fe116a78f246ca56af627
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-11-19 16:37:40 +0000

    19.0.x - 2f3cea1 release: cut the v19.0.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -11,7 +11,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
 import { DOCUMENT } from '@angular/common';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('19.0.0-rc.3');
+const VERSION = new Version('19.0.0');
 
 /** @docs-private */
 class AnimationCurves {
