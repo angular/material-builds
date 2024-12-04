a9b037f69a1a08114262c48d05aa32488da3b52e
Andrew Seguin <andrewjs@google.com>
2024-12-04 19:54:38 +0000

    main - 5a51819 release: cut the v19.1.0-next.1 release

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~19.1.0-next.0`;
+const fallbackMaterialVersionRange = `~19.1.0-next.1`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
