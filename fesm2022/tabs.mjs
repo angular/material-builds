46f4f9b2c3340d36330885f09148cdd4cc10cefd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-01-13 15:18:19 +0000

    main - 59a7985 fix(material/tabs): tab nav bar animation not working when navigating forwards (#30313)

--- fesm2022/tabs.mjs
+++ fesm2022/tabs.mjs
@@ -1737,9 +1737,9 @@ class MatTabNav extends MatPaginatedTabHeader {
         this._inkBar = new MatInkBar(this._items);
         // We need this to run before the `changes` subscription in parent to ensure that the
         // selectedIndex is up-to-date by the time the super class starts looking for it.
-        this._items.changes.pipe(startWith(null), takeUntil(this._destroyed)).subscribe(() => {
-            this.updateActiveLink();
-        });
+        this._items.changes
+            .pipe(startWith(null), takeUntil(this._destroyed))
+            .subscribe(() => this.updateActiveLink());
         super.ngAfterContentInit();
     }
     ngAfterViewInit() {
@@ -1764,9 +1764,7 @@ class MatTabNav extends MatPaginatedTabHeader {
                 return;
             }
         }
-        // The ink bar should hide itself if no items are active.
         this.selectedIndex = -1;
-        this._inkBar.hide();
     }
     _getRole() {
         return this.tabPanel ? 'tablist' : this._elementRef.nativeElement.getAttribute('role');
