eebddc8b9d1eead3ccb5fa9e7b1c38a3aba0ff43
Karl Seamon <bannanafiend@gmail.com>
2025-02-07 15:44:26 +0000

    19.1.x - 9b323df perf(material/tooltip): Defer injection of injectables not needed until tooltip is shown. (#30440)

--- tooltip/index.d.ts
+++ tooltip/index.d.ts
@@ -70,10 +70,7 @@ export declare const MAT_TOOLTIP_SCROLL_STRATEGY_FACTORY_PROVIDER: {
  * https://material.io/design/components/tooltips.html
  */
 export declare class MatTooltip implements OnDestroy, AfterViewInit {
-    private _overlay;
     private _elementRef;
-    private _scrollDispatcher;
-    private _viewContainerRef;
     private _ngZone;
     private _platform;
     private _ariaDescriber;
@@ -88,7 +85,6 @@ export declare class MatTooltip implements OnDestroy, AfterViewInit {
     private _positionAtOrigin;
     private _disabled;
     private _tooltipClass;
-    private _scrollStrategy;
     private _viewInitialized;
     private _pointerExitEventsInitialized;
     private readonly _tooltipComponent;
@@ -145,8 +141,6 @@ export declare class MatTooltip implements OnDestroy, AfterViewInit {
     });
     /** Manually-bound passive event listeners. */
     private readonly _passiveListeners;
-    /** Reference to the current document. */
-    private _document;
     /** Timer started at the last `touchstart` event. */
     private _touchstartTimeout;
     /** Emits when the component is destroyed. */
