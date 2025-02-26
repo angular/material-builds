a5b509aa46b11367c8cda7edd7b357a711aa8180
Wagner Maciel <wagnermaciel@google.com>
2025-02-26 20:00:08 +0000

    19.2.x - c254880 release: cut the v19.2.1 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -11,7 +11,7 @@ import { ENTER, SPACE, hasModifierKey } from '@angular/cdk/keycodes';
 import { DOCUMENT } from '@angular/common';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('19.2.0');
+const VERSION = new Version('19.2.1');
 
 /**
  * @deprecated No longer used, will be removed.
