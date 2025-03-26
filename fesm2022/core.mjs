73ea8645ac012da294ec1646691513c155c6d277
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-26 11:51:45 +0000

    19.2.x - 30456bc release: cut the v19.2.7 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -26,7 +26,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('19.2.6');
+const VERSION = new Version('19.2.7');
 
 /**
  * @deprecated No longer used, will be removed.
