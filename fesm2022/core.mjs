09be906b54415b056f648dbd77cd51abbb8a4b7f
Andrew Seguin <andrewjs@google.com>
2025-04-16 17:29:03 +0000

    19.2.x - 652dd33 release: cut the v19.2.10 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -26,7 +26,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('19.2.9');
+const VERSION = new Version('19.2.10');
 
 /**
  * @deprecated No longer used, will be removed.
