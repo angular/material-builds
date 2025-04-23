ba6eb97692dea22077fdaebd0433906952e40c7a
Joey Perrott <josephperrott@gmail.com>
2025-04-23 18:20:37 +0000

    19.2.x - 54cc2cf [PATCH] Migrate to use rules_js based ng_package (#30940)

---
+++ npm_package/schematics/ng-add/theming/create-custom-theme.js
@@ -0,0 +1,30 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createCustomTheme = createCustomTheme;
+/** Create custom theme for the given application configuration. */
+function createCustomTheme(name = 'app') {
+    return `
+// Custom Theming for Angular Material
+// For more information: https://material.angular.io/guide/theming
+@use '@angular/material' as mat;
+
+html {
+  @include mat.theme((
+    color: (
+      theme-type: light,
+      primary: mat.$azure-palette,
+      tertiary: mat.$blue-palette,
+    ),
+    typography: Roboto,
+    density: 0,
+  ));
+}`;
+}
+//# sourceMappingURL=create-custom-theme.js.map
