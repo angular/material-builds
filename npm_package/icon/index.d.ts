c5573d02fb02af1d8034a428496b93f45ed238d4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-05-01 08:24:48 +0000

    main - 280a233 build: release script producing duplicate results (#30999)

--- npm_package/icon/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { b as MAT_ICON_DEFAULT_OPTIONS, c as MAT_ICON_LOCATION, e as MAT_ICON_LOCATION_FACTORY, f as MatIcon, a as MatIconDefaultOptions, d as MatIconLocation, M as MatIconModule } from '../icon-module.d-sA1hmRKS.js';
-export { f as ICON_REGISTRY_PROVIDER, e as ICON_REGISTRY_PROVIDER_FACTORY, I as IconOptions, d as IconResolver, M as MatIconRegistry, S as SafeResourceUrlWithIconOptions, c as getMatIconFailedToSanitizeLiteralError, b as getMatIconFailedToSanitizeUrlError, g as getMatIconNameNotFoundError, a as getMatIconNoHttpProviderError } from '../icon-registry.d-BVwP8t9_.js';
-import '@angular/core';
-import '../common-module.d-C8xzHJDr.js';
-import '@angular/cdk/bidi';
-import '../palette.d-BSSFKjO6.js';
-import '@angular/common/http';
-import '@angular/platform-browser';
-import 'rxjs';
