2dd9822939d5618a06d228f253ed45510655b051
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-05-28 15:41:48 +0000

    20.0.x - 7150ac7 build: update to Angular v20 final (#31241) (#31242)

--- fesm2022/dialog/testing.mjs
+++ fesm2022/dialog/testing.mjs
@@ -2,7 +2,7 @@ import { ContentContainerComponentHarness, HarnessPredicate, TestKey } from '@an
 import { __decorate, __metadata } from 'tslib';
 import { inject, NgZone, Component, ChangeDetectionStrategy, ViewEncapsulation, NgModule } from '@angular/core';
 import { MATERIAL_ANIMATIONS } from '@angular/material/core';
-import { h as MatDialog, l as MatDialogModule } from '../module-CsrHD_Xt.mjs';
+import { h as MatDialog, l as MatDialogModule } from '../module-Ce6F7TNm.mjs';
 import '@angular/cdk/dialog';
 import '@angular/cdk/overlay';
 import '@angular/cdk/portal';
@@ -14,7 +14,7 @@ import 'rxjs/operators';
 import '@angular/cdk/keycodes';
 import '@angular/cdk/a11y';
 import '@angular/cdk/scrolling';
-import '../common-module-C69n6I4V.mjs';
+import '../common-module-cKSwHniA.mjs';
 import '@angular/cdk/bidi';
 
 /** Selectors for different sections of the mat-dialog that can contain user content. */
---
+++ fesm2022/index-BFRo2fUq.mjs
@@ -0,0 +1,20 @@
+import * as i0 from '@angular/core';
+import { NgModule } from '@angular/core';
+import { M as MatCommonModule } from './common-module-cKSwHniA.mjs';
+import { M as MatRipple } from './ripple-BYgV4oZC.mjs';
+
+class MatRippleModule {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.0.0", ngImport: i0, type: MatRippleModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.0.0", ngImport: i0, type: MatRippleModule, imports: [MatCommonModule, MatRipple], exports: [MatRipple, MatCommonModule] });
+    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.0.0", ngImport: i0, type: MatRippleModule, imports: [MatCommonModule, MatCommonModule] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.0.0", ngImport: i0, type: MatRippleModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    imports: [MatCommonModule, MatRipple],
+                    exports: [MatRipple, MatCommonModule],
+                }]
+        }] });
+
+export { MatRippleModule as M };
+//# sourceMappingURL=index-BFRo2fUq.mjs.map
