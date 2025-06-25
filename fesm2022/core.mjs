c9cfa639a26d5828c2f4d8488d8a5b26ec7ecbd7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-06-25 16:24:52 +0000

    main - ea7148c build: links to headers not working (#31436)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -27,7 +27,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('20.1.0-next.1');
+const VERSION = new Version('20.1.0-next.2');
 
 /**
  * Matches strings that have the form of a valid RFC 3339 string
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~20.1.0-next.1`;
+const fallbackMaterialVersionRange = `~20.1.0-next.2`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
