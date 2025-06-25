cb4cc0a208effe02d076e14a9d607f447748124c
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-25 16:15:43 +0000

    20.0.x - 7b87677 release: cut the v20.0.4 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -27,7 +27,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('20.0.3');
+const VERSION = new Version('20.0.4');
 
 /**
  * Matches strings that have the form of a valid RFC 3339 string
--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~20.0.3`;
+const fallbackMaterialVersionRange = `~20.0.4`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
