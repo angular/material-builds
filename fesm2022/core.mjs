3e753a38600cafc1edd50d0d4d91d2af97f1b4cb
Andrew Seguin <andrewjs@google.com>
2025-07-09 17:49:32 +0000

    20.1.x - 24f86fd release: cut the v20.1.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -27,7 +27,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('20.1.0-rc.0');
+const VERSION = new Version('20.1.0');
 
 /**
  * Matches strings that have the form of a valid RFC 3339 string
