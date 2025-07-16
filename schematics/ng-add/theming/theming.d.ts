d26317b6366d6494ded9e92d89a0f64324beaba0
Andrew Seguin <andrewseguin@users.noreply.github.com>
2025-07-16 17:03:14 +0000

    20.1.x - d833f7c fix(material/schematics): always add a custom theme with ng add (#31522)

--- schematics/ng-add/theming/theming.d.ts
+++ schematics/ng-add/theming/theming.d.ts
@@ -9,5 +9,3 @@ import { Rule } from '@angular-devkit/schematics';
 import { Schema } from '../schema';
 /** Add pre-built styles to the main project style file. */
 export declare function addThemeToAppStyles(options: Schema): Rule;
-/** Adds the global typography class to the body element. */
-export declare function addTypographyClass(options: Schema): Rule;
