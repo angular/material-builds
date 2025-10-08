00c7f0b19ad6a08a5933958427dbc2dbf03d27a3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-08 15:21:36 +0000

    main - 8b4b850 release: cut the v21.0.0-next.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -25,7 +25,7 @@ import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
 /** Current version of Angular Material. */
-const VERSION = new Version('21.0.0-next.7');
+const VERSION = new Version('21.0.0-next.8');
 
 /**
  * Matches strings that have the form of a valid RFC 3339 string
