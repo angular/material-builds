a712432991029fa6af32917aa991556609825549
Angular Robot <angular-robot@google.com>
2025-10-28 22:46:16 +0000

    main - 6a8d9f5 build: update bazel dependencies (#32137)

--- fesm2022/_option-module-chunk.mjs
+++ fesm2022/_option-module-chunk.mjs
@@ -6,17 +6,43 @@ import { MatPseudoCheckboxModule } from './_pseudo-checkbox-module-chunk.mjs';
 import { MatOption, MatOptgroup } from './_option-chunk.mjs';
 
 class MatOptionModule {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.2.0-next.2", ngImport: i0, type: MatOptionModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.2.0-next.2", ngImport: i0, type: MatOptionModule, imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, MatOptgroup], exports: [MatOption, MatOptgroup, BidiModule] });
-    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.2.0-next.2", ngImport: i0, type: MatOptionModule, imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, BidiModule] });
+  static ɵfac = i0.ɵɵngDeclareFactory({
+    minVersion: "12.0.0",
+    version: "20.2.0-next.2",
+    ngImport: i0,
+    type: MatOptionModule,
+    deps: [],
+    target: i0.ɵɵFactoryTarget.NgModule
+  });
+  static ɵmod = i0.ɵɵngDeclareNgModule({
+    minVersion: "14.0.0",
+    version: "20.2.0-next.2",
+    ngImport: i0,
+    type: MatOptionModule,
+    imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, MatOptgroup],
+    exports: [MatOption, MatOptgroup, BidiModule]
+  });
+  static ɵinj = i0.ɵɵngDeclareInjector({
+    minVersion: "12.0.0",
+    version: "20.2.0-next.2",
+    ngImport: i0,
+    type: MatOptionModule,
+    imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, BidiModule]
+  });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", ngImport: i0, type: MatOptionModule, decorators: [{
-            type: NgModule,
-            args: [{
-                    imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, MatOptgroup],
-                    exports: [MatOption, MatOptgroup, BidiModule],
-                }]
-        }] });
+i0.ɵɵngDeclareClassMetadata({
+  minVersion: "12.0.0",
+  version: "20.2.0-next.2",
+  ngImport: i0,
+  type: MatOptionModule,
+  decorators: [{
+    type: NgModule,
+    args: [{
+      imports: [MatRippleModule, MatPseudoCheckboxModule, MatOption, MatOptgroup],
+      exports: [MatOption, MatOptgroup, BidiModule]
+    }]
+  }]
+});
 
 export { MatOptionModule };
 //# sourceMappingURL=_option-module-chunk.mjs.map
--- fesm2022/input-testing.mjs
+++ fesm2022/input-testing.mjs
@@ -4,109 +4,69 @@ import { MatFormFieldControlHarnessBase } from './form-field-testing-control.mjs
 import '@angular/material/form-field/testing/control';
 import '@angular/cdk/coercion';
 
-/** Harness for interacting with a native `option` in tests. */
 class MatNativeOptionHarness extends ComponentHarness {
-    /** Selector used to locate option instances. */
-    static hostSelector = 'select[matNativeControl] option';
-    /**
-     * Gets a `HarnessPredicate` that can be used to search for a `MatNativeOptionHarness` that meets
-     * certain criteria.
-     * @param options Options for filtering which option instances are considered a match.
-     * @return a `HarnessPredicate` configured with the given options.
-     */
-    static with(options = {}) {
-        return new HarnessPredicate(MatNativeOptionHarness, options)
-            .addOption('text', options.text, async (harness, title) => HarnessPredicate.stringMatches(await harness.getText(), title))
-            .addOption('index', options.index, async (harness, index) => (await harness.getIndex()) === index)
-            .addOption('isSelected', options.isSelected, async (harness, isSelected) => (await harness.isSelected()) === isSelected);
-    }
-    /** Gets the option's label text. */
-    async getText() {
-        return (await this.host()).getProperty('label');
-    }
-    /** Index of the option within the native `select` element. */
-    async getIndex() {
-        return (await this.host()).getProperty('index');
-    }
-    /** Gets whether the option is disabled. */
-    async isDisabled() {
-        return (await this.host()).getProperty('disabled');
-    }
-    /** Gets whether the option is selected. */
-    async isSelected() {
-        return (await this.host()).getProperty('selected');
-    }
+  static hostSelector = 'select[matNativeControl] option';
+  static with(options = {}) {
+    return new HarnessPredicate(MatNativeOptionHarness, options).addOption('text', options.text, async (harness, title) => HarnessPredicate.stringMatches(await harness.getText(), title)).addOption('index', options.index, async (harness, index) => (await harness.getIndex()) === index).addOption('isSelected', options.isSelected, async (harness, isSelected) => (await harness.isSelected()) === isSelected);
+  }
+  async getText() {
+    return (await this.host()).getProperty('label');
+  }
+  async getIndex() {
+    return (await this.host()).getProperty('index');
+  }
+  async isDisabled() {
+    return (await this.host()).getProperty('disabled');
+  }
+  async isSelected() {
+    return (await this.host()).getProperty('selected');
+  }
 }
 
-/** Harness for interacting with a native `select` in tests. */
 class MatNativeSelectHarness extends MatFormFieldControlHarnessBase {
-    static hostSelector = 'select[matNativeControl]';
-    /**
-     * Gets a `HarnessPredicate` that can be used to search for a `MatNativeSelectHarness` that meets
-     * certain criteria.
-     * @param options Options for filtering which select instances are considered a match.
-     * @return a `HarnessPredicate` configured with the given options.
-     */
-    static with(options = {}) {
-        return new HarnessPredicate(MatNativeSelectHarness, options).addOption('label', options.label, (harness, label) => {
-            return HarnessPredicate.stringMatches(harness.getLabel(), label);
-        });
+  static hostSelector = 'select[matNativeControl]';
+  static with(options = {}) {
+    return new HarnessPredicate(MatNativeSelectHarness, options).addOption('label', options.label, (harness, label) => {
+      return HarnessPredicate.stringMatches(harness.getLabel(), label);
+    });
+  }
+  async isDisabled() {
+    return (await this.host()).getProperty('disabled');
+  }
+  async isRequired() {
+    return (await this.host()).getProperty('required');
+  }
+  async isMultiple() {
+    return (await this.host()).getProperty('multiple');
+  }
+  async getName() {
+    return await (await this.host()).getProperty('name');
+  }
+  async getId() {
+    return await (await this.host()).getProperty('id');
+  }
+  async focus() {
+    return (await this.host()).focus();
+  }
+  async blur() {
+    return (await this.host()).blur();
+  }
+  async isFocused() {
+    return (await this.host()).isFocused();
+  }
+  async getOptions(filter = {}) {
+    return this.locatorForAll(MatNativeOptionHarness.with(filter))();
+  }
+  async selectOptions(filter = {}) {
+    const [isMultiple, options] = await parallel(() => {
+      return [this.isMultiple(), this.getOptions(filter)];
+    });
+    if (options.length === 0) {
+      throw Error('Select does not have options matching the specified filter');
     }
-    /** Gets a boolean promise indicating if the select is disabled. */
-    async isDisabled() {
-        return (await this.host()).getProperty('disabled');
-    }
-    /** Gets a boolean promise indicating if the select is required. */
-    async isRequired() {
-        return (await this.host()).getProperty('required');
-    }
-    /** Gets a boolean promise indicating if the select is in multi-selection mode. */
-    async isMultiple() {
-        return (await this.host()).getProperty('multiple');
-    }
-    /** Gets the name of the select. */
-    async getName() {
-        // The "name" property of the native select is never undefined.
-        return await (await this.host()).getProperty('name');
-    }
-    /** Gets the id of the select. */
-    async getId() {
-        // We're guaranteed to have an id, because the `matNativeControl` always assigns one.
-        return await (await this.host()).getProperty('id');
-    }
-    /** Focuses the select and returns a void promise that indicates when the action is complete. */
-    async focus() {
-        return (await this.host()).focus();
-    }
-    /** Blurs the select and returns a void promise that indicates when the action is complete. */
-    async blur() {
-        return (await this.host()).blur();
-    }
-    /** Whether the select is focused. */
-    async isFocused() {
-        return (await this.host()).isFocused();
-    }
-    /** Gets the options inside the select panel. */
-    async getOptions(filter = {}) {
-        return this.locatorForAll(MatNativeOptionHarness.with(filter))();
-    }
-    /**
-     * Selects the options that match the passed-in filter. If the select is in multi-selection
-     * mode all options will be clicked, otherwise the harness will pick the first matching option.
-     */
-    async selectOptions(filter = {}) {
-        const [isMultiple, options] = await parallel(() => {
-            return [this.isMultiple(), this.getOptions(filter)];
-        });
-        if (options.length === 0) {
-            throw Error('Select does not have options matching the specified filter');
-        }
-        const [host, optionIndexes] = await parallel(() => [
-            this.host(),
-            parallel(() => options.slice(0, isMultiple ? undefined : 1).map(option => option.getIndex())),
-        ]);
-        await host.selectOptions(...optionIndexes);
-    }
+    const [host, optionIndexes] = await parallel(() => [this.host(), parallel(() => options.slice(0, isMultiple ? undefined : 1).map(option => option.getIndex()))]);
+    await host.selectOptions(...optionIndexes);
+  }
 }
 
 export { MatNativeOptionHarness, MatNativeSelectHarness };
