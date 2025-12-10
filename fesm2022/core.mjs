27bd059d2bb0dab32551b9dd0280c06e34472a8d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-10 12:57:00 +0000

    21.0.x - 79603d6 refactor(aria/tree): split directives and resolve circular dependencies

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/private';
 import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
-const VERSION = new Version('21.0.2+sha-a0489f2');
+const VERSION = new Version('21.0.2+sha-79603d6');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
