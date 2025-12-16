ad3bbb50d169379b06df92652804f8b614bf5fe6
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2025-12-16 21:51:21 +0000

    main - 204788b fix(material/slide-toggle): Fix a11y issues by hiding label when it has no content. (#32480)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~21.1.0-next.2+sha-a947f68`;
+const fallbackMaterialVersionRange = `~21.1.0-next.2+sha-204788b`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
