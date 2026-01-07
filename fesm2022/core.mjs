d69c37d84105d98d58efe6e0fb70980a2962b56e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-07 17:21:57 +0000

    21.0.x - 00c8d29 build: bump year (#32608)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/private';
 import '@angular/cdk/platform';
 import '@angular/cdk/coercion';
 
-const VERSION = new Version('21.0.5+sha-ca4a7a4');
+const VERSION = new Version('21.0.5+sha-00c8d29');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
