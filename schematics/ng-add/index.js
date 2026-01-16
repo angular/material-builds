4adacaf2bb85f0103c1183d1e61183be18f97a2a
Angular Robot <angular-robot@google.com>
2026-01-16 10:37:57 +0000

    main - 71cdb84 build: update cross-repo angular dependencies (#32656)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~21.2.0-next.0+sha-1cef533`;
+const fallbackMaterialVersionRange = `~21.2.0-next.0+sha-71cdb84`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
