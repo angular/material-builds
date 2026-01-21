c458f9e87f0f43465c7ccb22483b5b98714a2f40
Andrew Seguin <andrewjs@google.com>
2026-01-21 19:40:51 +0000

    21.1.x - 0da3836 release: cut the v21.1.1 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~21.1.0+sha-d24f099`;
+const fallbackMaterialVersionRange = `~21.1.1+sha-0da3836`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
