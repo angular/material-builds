e5e4cd9a3a99ef8e2624662ecb8b0b78cb7e6bdc
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-02-10 22:28:15 +0000

    21.1.x - 2a26959 fix(multiple): change aria keyboard manager to only handle repeated events in correct places (#32728)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~21.1.3+sha-d175b01`;
+const fallbackMaterialVersionRange = `~21.1.3+sha-2a26959`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
