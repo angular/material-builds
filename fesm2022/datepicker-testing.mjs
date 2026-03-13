82dced68b89864e6d9545f6e496e7189b52b2341
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-13 05:03:46 +0000

    main - 46b67d1 fix(material/datepicker): add harness for datepicker actions (#32925)

--- fesm2022/datepicker-testing.mjs
+++ fesm2022/datepicker-testing.mjs
@@ -1,6 +1,6 @@
 import { DatepickerTriggerHarnessBase } from './_date-range-input-harness-chunk.mjs';
 export { CalendarView, MatCalendarCellHarness, MatCalendarHarness, MatDateRangeInputHarness, MatDatepickerInputHarness, MatEndDateHarness, MatStartDateHarness } from './_date-range-input-harness-chunk.mjs';
-import { HarnessPredicate } from '@angular/cdk/testing';
+import { HarnessPredicate, ContentContainerComponentHarness } from '@angular/cdk/testing';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import '@angular/material/form-field/testing/control';
 
@@ -22,5 +22,27 @@ class MatDatepickerToggleHarness extends DatepickerTriggerHarnessBase {
   }
 }
 
-export { MatDatepickerToggleHarness };
+class MatDatepickerActionsHarness extends ContentContainerComponentHarness {
+  static hostSelector = '.mat-datepicker-actions';
+  _applyLocator = this.locatorForOptional('[matDatepickerApply], [matDateRangePickerApply]');
+  _cancelLocator = this.locatorForOptional('[matDatepickerCancel], [matDateRangePickerCancel]');
+  static with(options = {}) {
+    return new HarnessPredicate(MatDatepickerActionsHarness, options);
+  }
+  apply() {
+    return this._clickAction('apply', this._applyLocator);
+  }
+  cancel() {
+    return this._clickAction('cancel', this._cancelLocator);
+  }
+  async _clickAction(name, locator) {
+    const button = await locator();
+    if (!button) {
+      throw new Error(`MatDatepickerActions does not have ${name} button`);
+    }
+    await button.click();
+  }
+}
+
+export { MatDatepickerActionsHarness, MatDatepickerToggleHarness };
 //# sourceMappingURL=datepicker-testing.mjs.map
--- types/datepicker-testing.d.ts
+++ types/datepicker-testing.d.ts
@@ -1,6 +1,6 @@
-import { DatepickerTriggerHarnessBase, DatepickerToggleHarnessFilters } from './_date-range-input-harness-chunk.js';
+import { DatepickerTriggerHarnessBase, DatepickerToggleHarnessFilters, DatepickerActionsHarnessFilters } from './_date-range-input-harness-chunk.js';
 export { CalendarCellHarnessFilters, CalendarHarnessFilters, CalendarView, DateRangeInputHarnessFilters, DatepickerInputHarnessFilters, MatCalendarCellHarness, MatCalendarHarness, MatDateRangeInputHarness, MatDatepickerInputHarness, MatEndDateHarness, MatStartDateHarness } from './_date-range-input-harness-chunk.js';
-import { HarnessPredicate } from '@angular/cdk/testing';
+import { HarnessPredicate, ContentContainerComponentHarness } from '@angular/cdk/testing';
 import '@angular/material/form-field/testing/control';
 
 /** Harness for interacting with a standard Material datepicker toggle in tests. */
@@ -22,4 +22,23 @@ declare class MatDatepickerToggleHarness extends DatepickerTriggerHarnessBase {
     protected _openCalendar(): Promise<void>;
 }
 
-export { DatepickerToggleHarnessFilters, MatDatepickerToggleHarness };
+/** Harness for interacting with a standard Material datepicker actions in tests. */
+declare class MatDatepickerActionsHarness extends ContentContainerComponentHarness<string> {
+    static hostSelector: string;
+    private _applyLocator;
+    private _cancelLocator;
+    /**
+     * Gets a `HarnessPredicate` that can be used to search for a `MatDatepickerActionsHarness` that
+     * meets certain criteria.
+     * @param options Options for filtering which datepicker actions instances are considered a match.
+     * @return a `HarnessPredicate` configured with the given options.
+     */
+    static with(options?: DatepickerActionsHarnessFilters): HarnessPredicate<MatDatepickerActionsHarness>;
+    /** Applies the current selection. */
+    apply(): Promise<void>;
+    /** Cancels the current selection. */
+    cancel(): Promise<void>;
+    private _clickAction;
+}
+
+export { DatepickerActionsHarnessFilters, DatepickerToggleHarnessFilters, MatDatepickerActionsHarness, MatDatepickerToggleHarness };
