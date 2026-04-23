3f420b96be818c480a35edcdecbda54f5811fd7a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-04-23 09:31:31 +0000

    main - e7533d4 build: align icons in docs (#33127)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.0-next.6+sha-39b80fe');
+const VERSION = new Version('22.0.0-next.6+sha-e7533d4');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
