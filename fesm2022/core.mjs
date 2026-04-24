5d4535c1762e4930880a2011517259e366af7bdc
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-04-24 17:19:59 +0000

    main - e4abb46 refactor(multiple): Clean up afterRenderEffect usage across angular aria (#33054)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.0-next.6+sha-dd12062');
+const VERSION = new Version('22.0.0-next.6+sha-e4abb46');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
