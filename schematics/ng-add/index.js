81e95283282982b28b64727f06e1c910a7aae985
swseverance <swseverance@gmail.com>
2026-04-28 12:29:39 +0000

    21.2.x - 8734084 docs(material/tooltip): update tooltip-delay example layout styles (#33098)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~21.2.8+sha-a6c0242`;
+const fallbackMaterialVersionRange = `~21.2.8+sha-8734084`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
