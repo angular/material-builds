d89b52890aa3fd72aff9a0d6be57bde639490c20
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-29 12:36:26 +0000

    main - 8e5be4c docs: release notes for the v21.2.9 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.0-next.6+sha-e1481e4');
+const VERSION = new Version('22.0.0-next.6+sha-8e5be4c');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
