9abc7208d02a3b2e9eaa8f996959439e200b240a
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-04-29 18:24:32 +0000

    main - 8c3f9b0 Autocomplete - Uses simple-combobox example (#33155)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~22.0.0-next.7+sha-75718e4`;
+const fallbackMaterialVersionRange = `~22.0.0-next.7+sha-8c3f9b0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
