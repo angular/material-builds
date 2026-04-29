76fff4d79d1644b7fdaf2d9830c32d8f73d17a98
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-04-29 12:35:35 +0000

    21.2.x - d17f95d release: cut the v21.2.9 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('21.2.8+sha-8734084');
+const VERSION = new Version('21.2.9+sha-d17f95d');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
