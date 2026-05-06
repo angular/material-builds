7f60a86bed65b5a6b99f322138311ea9412bed85
Andrey Dolgachev <adolgachev@users.noreply.github.com>
2026-05-06 20:07:01 +0000

    main - 2b668f2 docs: release notes for the v22.0.0-next.8 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.0-next.7+sha-46763fb');
+const VERSION = new Version('22.1.0-next.0+sha-2b668f2');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
