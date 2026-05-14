fa5bf10c20482062acedff32f4193b122839f3af
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-05-14 00:25:23 +0000

    22.0.x - d9f78d1 release: cut the v22.0.0-rc.0 release

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.0-next.8+sha-ed96096');
+const VERSION = new Version('22.0.0-rc.0+sha-d9f78d1');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
