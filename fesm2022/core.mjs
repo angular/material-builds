11cdc83ce543eb81836c9670da70a1e908901557
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-21 11:24:58 +0000

    main - 11e2eb3 fix(multiple): prevent form submissions in aria directives (#33297)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.1.0-next.0+sha-5d73083');
+const VERSION = new Version('22.1.0-next.0+sha-11e2eb3');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
