73d5bf1320eebeb59aae21812ad9d609150ae6f1
Angular Robot <angular-robot@google.com>
2026-05-28 23:45:38 +0000

    22.0.x - 9750685 build: update bazel dependencies (#33256)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~22.0.0-rc.2+sha-5ba22b6`;
+const fallbackMaterialVersionRange = `~22.0.0-rc.2+sha-9750685`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
