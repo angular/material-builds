996b5a50fcd4055bc952cd079d81ac0582588174
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-07 06:13:36 +0000

    main - 330b496 fix(cdk/a11y): avoid prototype conflicts in id generator (#33356)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.1.0-next.0+sha-5a8f7df');
+const VERSION = new Version('22.1.0-next.0+sha-330b496');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
