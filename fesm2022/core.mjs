a36201418c352c9d9826608eaadb1e63b9120c8c
Cheng-Hsuan Tsai <webcat@google.com>
2026-06-26 19:12:57 +0000

    22.0.x - e87216c fix(aria/combobox): zonejs compatibility (#33453)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.0.2+sha-9e552a7');
+const VERSION = new Version('22.0.2+sha-e87216c');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
