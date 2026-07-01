9776ad648814fcffe38cdbbb26d5708907c1fa1a
Angular Robot <angular-robot@google.com>
2026-07-01 06:19:08 +0000

    22.0.x - 66a5923 build: update dependency node to v22.23.1 (#33438)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~22.0.2+sha-1c9bd0d`;
+const fallbackMaterialVersionRange = `~22.0.2+sha-66a5923`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
