4624c246b7e1095c7886001cab09a7d2f6c2e3d5
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-07-29 09:47:51 +0000

    main - af2b626 docs: convert CDK overlay to signals (#33591)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~22.2.0-next.0+sha-7f8a729`;
+const fallbackMaterialVersionRange = `~22.2.0-next.0+sha-af2b626`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
