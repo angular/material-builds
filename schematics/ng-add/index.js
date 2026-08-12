e4d3cba72f0226a8ebb1e3bbc5ca9059252c6b0d
tjshiu <35056071+tjshiu@users.noreply.github.com>
2026-08-12 21:15:56 +0000

    22.1.x - aa598a0 release: cut the v22.1.2 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -19,7 +19,7 @@ const package_config_1 = require("./package-config");
  * Note that the fallback version range does not use caret, but tilde because that is
  * the default for Angular framework dependencies in CLI projects.
  */
-const fallbackMaterialVersionRange = `~22.1.1+sha-b9b11cb`;
+const fallbackMaterialVersionRange = `~22.1.2+sha-aa598a0`;
 /**
  * Schematic factory entry-point for the `ng-add` schematic. The ng-add schematic will be
  * automatically executed if developers run `ng add @angular/material`.
