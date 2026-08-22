1f35a21aff194992d664f3a0d6a0ca39f2f65255
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-08-22 08:17:35 +0000

    22.1.x - 6be9b4c test(material/sort): switch tests away from fakeAsync (#33716)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.1.3+sha-98842bf');
+const VERSION = new Version('22.1.3+sha-6be9b4c');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
