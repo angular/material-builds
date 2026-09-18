6bfe27ed4c39d9116790e86f83f70a87e7a62f0b
Michael Small <85510853+msmallest@users.noreply.github.com>
2026-09-18 13:59:41 +0000

    22.1.x - d8a86db docs: mention forms support in outstanding controls (#33823)

--- fesm2022/core.mjs
+++ fesm2022/core.mjs
@@ -24,7 +24,7 @@ import '@angular/cdk/coercion';
 import '@angular/cdk/bidi';
 import 'rxjs/operators';
 
-const VERSION = new Version('22.1.7+sha-e950f29');
+const VERSION = new Version('22.1.7+sha-d8a86db');
 
 const ISO_8601_REGEX = /^\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:(?:\+|-)\d{2}:\d{2}))?)?$/;
 const TIME_REGEX = /^(\d?\d)[:.](\d?\d)(?:[:.](\d?\d))?\s*(AM|PM)?$/i;
